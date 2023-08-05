71889d98c76123834b45e81d56e839c23853c9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:07 +0000

    Add package dohome-api/0.2.11/dohome_api-0.2.11.tar.gz

---
+++ packages/dohome-api/dohome_api-0.2.11.tar.gz/dohome_api-0.2.11/dohome_api/transport/__init__.py
@@ -0,0 +1,5 @@
+"""DoHome transport module"""
+
+from .broadcast import DoHomeBroadcastTransport
+from .direct import DoHomeDirectTransport
+from .interface import DoHomeApiTransport
---
+++ packages/dohome-api/dohome_api-0.2.11.tar.gz/dohome_api-0.2.11/dohome_api/transport/constants.py
@@ -0,0 +1,7 @@
+"""DoHome transport contantants"""
+from typing import Final
+
+# Connection
+API_PORT: Final[int] = 6091
+API_ENCODING: Final[str] = "utf-8"
+BUFFER_SIZE: Final[int] = 1024
