dffcdc447249235dd92a8bc2fca0b6481a2817a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:00 +0000

    Add package happyaccidentsapi/0.0.1/happyaccidentsapi-0.0.1-py3-none-any.whl

---
+++ packages/happyaccidentsapi/happyaccidentsapi-0.0.1-py3-none-any.whl/happyaccidentsapi/__init__.py
@@ -0,0 +1,14 @@
+"""
+HappyAccidents API Wrapper
+
+:license: Apache License 2.0, see LICENSE for more details.
+"""
+
+__title__ = "happyaccidentsapi"
+__author__ = "hoopengo"
+__license__ = "Apache License 2.0"
+__version__ = "0.0.0"
+
+from ._happyaccidentsapi import *
+from .enums import *
+from .models import *
