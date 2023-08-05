b308dccb2a75dd254ce3deb84efe6a71b69e71e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:44 +0000

    Add package odoo12-addon-sm-crm-donation/12.0.0.0.1/odoo12_addon_sm_crm_donation-12.0.0.0.1-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-sm-crm-donation/odoo12_addon_sm_crm_donation-12.0.0.0.1-py2.py3-none-any.whl/odoo/addons/sm_crm_donation/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "sm_crm_donations",
+
+    'summary': """""",
+
+    'description': """""",
+
+    'author': "Som Mobilitat",
+    'website': "https://www.sommobilitat.coop",
+
+    'category': 'vertical-cooperative',
+    'version': '12.0.0.0.1',
+
+    'depends': ['base', 'crm', 'crm_metadata'],
+
+    'data': [
+        'data/utm_source_data.xml',
+    ],
+
+    'demo': [
+
+    ],
+    'application': True,
+}
