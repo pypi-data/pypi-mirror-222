a3cdb578a4080d5c89f4439bfd8fbbd1588b116a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:38 +0000

    Add package llmlink/0.0.1/llmlink-0.0.1.tar.gz

---
+++ packages/llmlink/llmlink-0.0.1.tar.gz/llmlink-0.0.1/llmlink/model/__init__.py
@@ -0,0 +1,6 @@
+"""
+Chainlink.model is a subpackage of the Chainlink package which focuses on building
+the models to be used in Chainlink applications
+"""
+from .Model import Model
+from .BaseModel import BaseModel
