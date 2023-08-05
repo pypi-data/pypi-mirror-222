8ed0220601b27345973916d36dbc45b11d87b75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:25 +0000

    Add package zen-engine/0.9.0/zen_engine-0.9.0.tar.gz

---
+++ packages/zen-engine/zen_engine-0.9.0.tar.gz/zen_engine-0.9.0/local_dependencies/zen-engine/src/error.rs
@@ -0,0 +1,74 @@
+use crate::handler::node::NodeError;
+use crate::loader::LoaderError;
+use serde::ser::SerializeMap;
+use serde::{Serialize, Serializer};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum EvaluationError {
+    #[error("Loader error")]
+    LoaderError(Box<LoaderError>),
+
+    #[error("Node error")]
+    NodeError(Box<NodeError>),
+
+    #[error("Depth limit exceeded")]
+    DepthLimitExceeded,
+}
+
+impl Serialize for EvaluationError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(None)?;
+        match self {
+            EvaluationError::DepthLimitExceeded => {
+                map.serialize_entry("type", "DepthLimitExceeded")?;
+            }
+            EvaluationError::NodeError(err) => {
+                map.serialize_entry("type", "NodeError")?;
+                map.serialize_entry("nodeId", &err.node_id)?;
+                map.serialize_entry("source", &err.source.to_string())?;
+            }
+            EvaluationError::LoaderError(err) => {
+                map.serialize_entry("type", "LoaderError")?;
+                match err.as_ref() {
+                    LoaderError::Internal { key, source } => {
+                        map.serialize_entry("key", key)?;
+                        map.serialize_entry("source", &source.to_string())?;
+                    }
+                    LoaderError::NotFound(key) => {
+                        map.serialize_entry("key", key)?;
+                    }
+                }
+            }
+        }
+
+        map.end()
+    }
+}
+
+impl From<LoaderError> for Box<EvaluationError> {
+    fn from(error: LoaderError) -> Self {
+        Box::new(EvaluationError::LoaderError(error.into()))
+    }
+}
+
+impl From<Box<LoaderError>> for Box<EvaluationError> {
+    fn from(error: Box<LoaderError>) -> Self {
+        Box::new(EvaluationError::LoaderError(error))
+    }
+}
+
+impl From<NodeError> for Box<EvaluationError> {
+    fn from(error: NodeError) -> Self {
+        Box::new(EvaluationError::NodeError(error.into()))
+    }
+}
+
+impl From<Box<NodeError>> for Box<EvaluationError> {
+    fn from(error: Box<NodeError>) -> Self {
+        Box::new(EvaluationError::NodeError(error))
+    }
+}
