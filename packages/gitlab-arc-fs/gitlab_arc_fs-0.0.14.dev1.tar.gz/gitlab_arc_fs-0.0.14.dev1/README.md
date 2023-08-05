0b1c692d115cc8c78716fc0bc826c5dbd60d6ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:58 +0000

    Add package gitlab-arc-fs/0.0.14.dev1/gitlab_arc_fs-0.0.14.dev1.tar.gz

---
+++ packages/gitlab-arc-fs/gitlab_arc_fs-0.0.14.dev1.tar.gz/gitlab_arc_fs-0.0.14.dev1/README.md
@@ -0,0 +1,8 @@
+# GitlabFS
+
+GitlabFS is a **prototypical** implementation of a [PyFilesystem](https://github.com/PyFilesystem/pyfilesystem2) for [DataPLANT ARCs](https://github.com/nfdi4plants/ARC-specification/blob/main/ARC%20specification.md)
+
+# Installation
+`pip install .`
+
+## How to use
---
+++ packages/gitlab-arc-fs/gitlab_arc_fs-0.0.14.dev1.tar.gz/gitlab_arc_fs-0.0.14.dev1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name='gitlab_arc_fs',  # Name in PyPi
+    author="Julian Weidhase",
+    author_email="julian.weidhase@rz.uni-freiburg.de",
+    description="An experimental and readonly gitlab filesystem extension "
+                "for PyFilesystem2.!",
+    install_requires=[
+        "fs>=2.4.16",
+        "urllib3>=1.26.13",
+        "requests>=2.28.1",
+        "aiohttp>=3.8.3"
+    ],
+    entry_points={
+        'fs.opener': [
+            'arcfs = gitlab_arc_fs.opener:GitlabFSOpener',
+        ]
+    },
+    license="MY LICENSE",
+    packages=['gitlab_arc_fs'],
+    package_dir={'gitlab_arc_fs': 'src/gitlab_arc_fs'},
+    version="0.0.14.dev1",
+    url="https://git.bwcloud.uni-freiburg.de/julian.weidhase/GitlabFS",
+    python_requires='>=3.8'
+)
