205caa6a68704e327ab69ef16bcf6282c26e9ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:01 +0000

    Add package oobleck/0.1.0/oobleck-0.1.0.tar.gz

---
+++ packages/oobleck/oobleck-0.1.0.tar.gz/oobleck-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import Extension, setup
+
+__version__ = "0.1.0"
+
+setup(
+    name="oobleck",
+    version=__version__,
+    author="Insu Jang",
+    author_email="insujang@umich.edu",
+    description="Resilient Distributed Training Framework",
+    long_description="",
+    zip_safe=False,
+    python_requires=">=3.8",
+)
