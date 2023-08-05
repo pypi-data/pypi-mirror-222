2ef87eb8b52df2f70e8bd4bea78efd8788c6c1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:41 +0000

    Add package PDFAgent/0.2.2/PDFAgent-0.2.2.tar.gz

---
+++ packages/PDFAgent/PDFAgent-0.2.2.tar.gz/PDFAgent-0.2.2/PDFAgent/agent.py
@@ -0,0 +1,74 @@
+"""Main Agent for reading PDFs
+"""
+from blitzchain import Client
+
+class PDFAgent:
+    def __init__(
+            self, 
+            name: str,
+            api_key: str, 
+            base_url: str=None
+        ):
+        self.api_key = api_key
+        self.base_url = base_url if base_url else "https://app.twilix.io/api/v1"
+        self.client: Client = Client(api_key)
+        self.name = name
+        self.collection = self.client.Collection(name)
+
+    def read_pdf(self, url: str):
+        """Inserting a PDF via URL.
+        """
+        result = self.collection.insert_pdf(url)
+        print("Inserting PDF... Please wait 30-60 seconds for this to finish indexing.")
+        return result
+
+    def view(
+        self,
+        limit: int = 5,
+        offset: int = 0,
+        fields: list = None,
+        where=None,
+        sort: list = None,
+    ):
+        return self.collection.list_objects(limit=limit, offset=offset, where=where, sort=sort)
+    
+    def ask(self, question: str, conversation_id: str=None):
+        """Ask your PDF a question
+        """
+        return self.collection.generative_qa(
+            prompt=question,
+            prompt_fields=None,
+            conversation_id=conversation_id
+        )
+
+    def template(
+        self, 
+        prompt: str, 
+        conversation_id: str=None,
+        limit: int=5, 
+        minimum_score: float=0.01,
+        include_moderation: bool=False, 
+    ):
+        """Use a template to analyze how your PDF
+        """
+        return self.collection.template(
+            prompt=prompt,
+            prompt_fields=["autoGen_content"],
+            fields=["autoGen_content"],
+            conversation_id=conversation_id,
+            limit=limit,
+            include_rerank=True,
+            minimum_score=minimum_score,
+            include_moderation=include_moderation
+        )
+
+    def copilot(self, prompt: str, conversation_id: str=None):
+        """The endpoint for advanced analysis
+        """
+        return self.collection.copilot(
+            prompt=prompt,
+            prompt_fields=["autoGen_content"],
+            conversation_id=conversation_id,
+            include_rerank=True,
+            minimum_rerank_score=0.05
+        )
---
+++ packages/PDFAgent/PDFAgent-0.2.2.tar.gz/PDFAgent-0.2.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PDFAgent',
+    version='0.2.2',
+    url='https://github.com/mr-gpt/PDFAgent',
+    author='Twilix Team',
+    author_email='jacky@twilix.io',
+    description='Production Ready PDF Agent',
+    packages=find_packages(),    
+    install_requires=['blitzchain>=0.8.2'],
+)
