aad78c8bff52d54c2d20ad25f1a1e864910ffcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package profcomff-parse-lib/2023.7.28/profcomff_parse_lib-2023.7.28-py3-none-any.whl

---
+++ packages/profcomff-parse-lib/profcomff_parse_lib-2023.7.28-py3-none-any.whl/profcomff_parse_lib/database/__init__.py
@@ -0,0 +1,10 @@
+from .add_lessons import add_lessons
+from .completion import completion
+from .id_instead_name import to_id
+from .delete_lessons import delete_lessons
+from .groups_to_array import all_to_array
+
+__all__ = ["completion",
+           "to_id", "add_lessons", "delete_lessons", "all_to_array"]
+
+
