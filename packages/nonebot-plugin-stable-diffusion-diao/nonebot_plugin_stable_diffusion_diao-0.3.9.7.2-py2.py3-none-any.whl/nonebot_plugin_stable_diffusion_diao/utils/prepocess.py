a05b43793c7cc3db46259cafce064f1442df645f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:56 +0000

    Add package nonebot-plugin-stable-diffusion-diao/0.3.9.7.2/nonebot_plugin_stable_diffusion_diao-0.3.9.7.2-py2.py3-none-any.whl

---
+++ packages/nonebot-plugin-stable-diffusion-diao/nonebot_plugin_stable_diffusion_diao-0.3.9.7.2-py2.py3-none-any.whl/nonebot_plugin_stable_diffusion_diao/utils/prepocess.py
@@ -0,0 +1,26 @@
+import re
+from ..extension.translation import translate
+
+
+async def prepocess_tags(tags: list[str], translation=True):
+    tags: str = "".join([i+", " for i in tags if isinstance(i,str)])
+    # 去除CQ码
+    tags = re.sub("\[CQ[^\s]*?]", "", tags)
+    # 检测中文
+    taglist = tags.split(",")
+    if not translation:
+        return ', '.join(taglist)
+    tagzh = ""
+    tags_ = ""
+    for i in taglist:
+        if re.search('[\u4e00-\u9fa5]', tags):
+            tagzh += f"{i},"
+        else:
+            tags_ += f"{i},"
+    if tagzh:
+        tags_en = await translate(tagzh, "en")
+        if tags_en == tagzh:
+            return ""
+        else:
+            tags_ += tags_en
+    return tags_
