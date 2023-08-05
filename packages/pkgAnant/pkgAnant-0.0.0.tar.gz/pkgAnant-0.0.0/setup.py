99534ed7bbbd52466bb9da6d13dea04218c2618f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:02 +0000

    Add package pkgAnant/0.0.0/pkgAnant-0.0.0.tar.gz

---
+++ packages/pkgAnant/pkgAnant-0.0.0.tar.gz/pkgAnant-0.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+VERSION = ''
+
+setuptools.setup(
+    name="pkgAnant",                     # This is the name of the package
+    version=VERSION,                        # The initial release version
+    author="Anant Chaudhary",                     # Full name of the author
+    description="password generator package",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["quicksample"],             # Name of the python package
+    install_requires=[]                     # Install other dependencies if any
+)
