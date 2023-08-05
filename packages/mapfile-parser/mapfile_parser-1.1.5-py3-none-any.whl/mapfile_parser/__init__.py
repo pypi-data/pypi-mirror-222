bd12376825a1f5b02e5bbcdec7690cd74e3e9263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:18 +0000

    Add package mapfile-parser/1.1.5/mapfile_parser-1.1.5-py3-none-any.whl

---
+++ packages/mapfile-parser/mapfile_parser-1.1.5-py3-none-any.whl/mapfile_parser/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+__version_info__ = (1, 1, 5)
+__version__ = ".".join(map(str, __version_info__))
+__author__ = "Decompollaborate"
+
+from . import utils
+
+from .mapfile import MapFile
+from .mapfile import Symbol, File, FoundSymbolInfo
+
+from .progress_stats import ProgressStats
+
+from . import frontends
