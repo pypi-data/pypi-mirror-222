b87dc64976cc0fea5b71a8e850caa135ee80d6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:36 +0000

    Add package aiofauna/0.1.49/aiofauna-0.1.49.tar.gz

---
+++ packages/aiofauna/aiofauna-0.1.49.tar.gz/aiofauna-0.1.49/aiofauna/__main__.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+import click
+
+from .logging import setup_logging
+
+logger = setup_logging(__name__)
+
+
+@click.group()
+def main():
+    pass
+
+
+def print_tree(dirname, pref=""):
+    for item in os.listdir(dirname):
+        if item.startswith("."):
+            continue
+        print(pref + item)
+        if os.path.isdir(os.path.join(dirname, item)):
+            logger.info(
+                os.path.join(dirname, item), " " * len(pref) + "|" + ("_" * 5) + " "
+            )
+
+
+@main.command()
+def tree():
+    if len(sys.argv) == 1:
+        dirname = "."
+    else:
+        dirname = sys.argv[1]
+    logger.info(dirname)
