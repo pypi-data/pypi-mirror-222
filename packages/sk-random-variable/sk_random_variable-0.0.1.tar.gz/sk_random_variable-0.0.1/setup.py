84edb1654614d88cd433804b4563d5cc5de2b628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:07 +0000

    Add package sk-random-variable/0.0.1/sk_random_variable-0.0.1.tar.gz

---
+++ packages/sk-random-variable/sk_random_variable-0.0.1.tar.gz/sk_random_variable-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sk_random_variable',
+    version='0.0.1',
+    description='A simple calculator program',
+    author='gkibria',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify the content type as Markdown
+    author_email='gkibria121@gmail.com',
+    packages=find_packages(),
+    install_requires=['regex'],
+    python_requires='>=3.6',
+    keywords='python, calculator, python calculator, gk calculator, advanced calculator',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
