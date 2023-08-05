696fb8a622227ff2358d4c5a3aa60a4981e4cd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package navis/1.5.0/navis-1.5.0.tar.gz

---
+++ packages/navis/navis-1.5.0.tar.gz/navis-1.5.0/navis/io/__init__.py
@@ -0,0 +1,30 @@
+#    This script is part of navis (http://www.github.com/navis-org/navis).
+#    Copyright (C) 2018 Philipp Schlegel
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+from .json_io import write_json, read_json
+from .swc_io import read_swc, write_swc
+from .nrrd_io import read_nrrd, write_nrrd
+from .precomputed_io import write_precomputed, read_precomputed
+from .hdf_io import read_h5, write_h5, inspect_h5
+from .rda_io import read_rda
+from .nmx_io import read_nmx
+from .mesh_io import read_mesh, write_mesh
+from .tiff_io import read_tiff
+
+__all__ = ['write_json', 'read_json',
+           'read_swc', 'write_swc',
+           'read_nrrd', 'write_nrrd',
+           'read_h5', 'write_h5', 'inspect_h5',
+           'write_precomputed', 'read_precomputed',
+           'read_tiff',
+           'read_rda', 'read_nmx', 'read_mesh', 'write_mesh']
