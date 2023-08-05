df1387c1f80d43c321e4fa11eaef938b278fc5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:45 +0000

    Add package axdd-person-client/1.1.18/axdd-person-client-1.1.18.tar.gz

---
+++ packages/axdd-person-client/axdd-person-client-1.1.18.tar.gz/axdd-person-client-1.1.18/uw_person_client/clients/__init__.py
@@ -0,0 +1,35 @@
+# Copyright 2023 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+class AbstractUWPersonClient():
+
+    def get_person_by_uwnetid(self, uwnetid):
+        raise NotImplementedError()
+
+    def get_person_by_uwregid(self, uwregid):
+        raise NotImplementedError()
+
+    def get_person_by_student_number(self, student_number):
+        raise NotImplementedError()
+
+    def get_persons(self, page=None, page_size=None):
+        raise NotImplementedError()
+
+    def get_registered_students(self):
+        raise NotImplementedError()
+
+    def get_active_students(self, page=None, page_size=None):
+        raise NotImplementedError()
+
+    def get_active_employees(self, page=None, page_size=None):
+        raise NotImplementedError()
+
+    def get_advisers(self, advising_program=None):
+        raise NotImplementedError()
+
+    def get_persons_by_adviser_netid(self, uwnetid):
+        raise NotImplementedError()
+
+    def get_persons_by_adviser_regid(self, uwregid):
+        raise NotImplementedError()
