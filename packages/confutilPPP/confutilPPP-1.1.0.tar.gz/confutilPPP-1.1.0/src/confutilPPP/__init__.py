c9606a18f99d69edf6554e1412308331101cd34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:09 +0000

    Add package confutilPPP/1.1.0/confutilPPP-1.1.0.tar.gz

---
+++ packages/confutilPPP/confutilPPP-1.1.0.tar.gz/confutilPPP-1.1.0/src/confutilPPP/__init__.py
@@ -0,0 +1,8 @@
+from .__confutil__ import confutil, LOG_IS_COLOR
+
+__all__ = ['__version__', 'check_config', 'LOG_IS_COLOR']
+__version__ = '1.1.0'
+
+
+def check_config(_object=None, _filename='config'):
+    return confutil.check_config(_object, _filename)
