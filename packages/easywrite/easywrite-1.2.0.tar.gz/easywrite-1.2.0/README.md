2a2af8cc35ce3a85c5d28495804d875bee846f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package easywrite/1.2.0/easywrite-1.2.0.tar.gz

---
+++ packages/easywrite/easywrite-1.2.0.tar.gz/easywrite-1.2.0/README.md
@@ -0,0 +1,8 @@
+# EasyWrite
+
+EasyWrite is a Python module that provides a convenient way to output text with different colors in the terminal.
+
+## Installation
+
+You can install EasyWrite using pip:
+
---
+++ packages/easywrite/easywrite-1.2.0.tar.gz/easywrite-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='easywrite',
+    version='1.2.0',
+    author='skxllhead',
+    description='A Python module for easy text output with colors',
+    long_description='Put a more detailed description of your module here',
+    long_description_content_type='text/markdown',
+    url='https://github.com/skxllhead/easywrite',
+    py_modules=['easywrite'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
