ca4e2debd984f817cc381ae2bc7d7634714d7c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:10 +0000

    Add package code-data-science/1.1.3/code_data_science-1.1.3.tar.gz

---
+++ packages/code-data-science/code_data_science-1.1.3.tar.gz/code_data_science-1.1.3/code_data_science/data_table.py
@@ -0,0 +1,11 @@
+import os
+from typing import Union
+
+import pandas as pd
+from pandas import DataFrame
+
+FilePath = Union[str, "PathLike[str]"]
+
+
+def read_csv(sample: FilePath) -> DataFrame:
+    return pd.read_csv(os.environ.get('NB_DATA_TABLE', sample), on_bad_lines='skip', skip_blank_lines=True)
