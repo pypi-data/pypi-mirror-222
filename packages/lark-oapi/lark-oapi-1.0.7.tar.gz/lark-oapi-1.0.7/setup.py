ea03f2bbc6917554e799509fec3a67a89bd6c50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package lark-oapi/1.0.7/lark-oapi-1.0.7.tar.gz

---
+++ packages/lark-oapi/lark-oapi-1.0.7.tar.gz/lark-oapi-1.0.7/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+from lark_oapi.core import VERSION
+
+setup(
+    name='lark-oapi',
+    version=VERSION,
+    description='Lark OpenAPI SDK for Python',
+    author='Wenbo Mao',
+    author_email='maowenbo@bytedance.com',
+    url='https://github.com/larksuite/oapi-sdk-python',
+    packages=find_packages(),
+    install_requires=['requests', 'requests_toolbelt', 'pycryptodome'],
+    python_requires=">=3.8",
+    keywords=['Lark', 'OpenAPI'],
+    include_package_data=True,
+    project_urls={
+        "Source": "https://github.com/larksuite/oapi-sdk-python",
+    },
+)
