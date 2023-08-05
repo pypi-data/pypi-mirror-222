977906146e7f6b1918568d6315f6913c8ac1eb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package easywrite/1.0.0/easywrite-1.0.0.tar.gz

---
+++ packages/easywrite/easywrite-1.0.0.tar.gz/easywrite-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='easywrite',
+    version='1.0.0',
+    author='Your Name',
+    description='A Python module for easy text output with colors',
+    long_description='Put a more detailed description of your module here',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/easywrite',
+    py_modules=['easywrite'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
