2d9526b6ec1b002dfed4eddc92d2ef2dedc47ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:51 +0000

    Add package orchestrate-api/0.3.5/orchestrate_api-0.3.5.tar.gz

---
+++ packages/orchestrate-api/orchestrate_api-0.3.5.tar.gz/orchestrate_api-0.3.5/orchestrate/convert.py
@@ -0,0 +1,13 @@
+from orchestrate._internal.fhir import Bundle
+
+ConvertHl7ToFhirR4Response = Bundle
+
+ConvertCdaToFhirR4Response = Bundle
+
+ConvertCdaToPdfResponse = bytes
+
+ConvertFhirR4ToCdaResponse = str
+
+ConvertFhirR4ToOmopResponse = bytes
+
+ConvertX12ToFhirR4Response = Bundle
