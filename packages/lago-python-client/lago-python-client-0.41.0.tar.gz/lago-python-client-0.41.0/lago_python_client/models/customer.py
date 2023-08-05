654a0606aec72721476aa6f25e29ba23ab4f018e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:11 +0000

    Add package lago-python-client/0.41.0/lago-python-client-0.41.0.tar.gz

---
+++ packages/lago-python-client/lago-python-client-0.41.0.tar.gz/lago-python-client-0.41.0/lago_python_client/models/customer.py
@@ -0,0 +1,75 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from ..base_model import BaseResponseModel
+
+from .tax import Taxes
+
+
+class CustomerBillingConfiguration(BaseModel):
+    invoice_grace_period: Optional[int]
+    payment_provider: Optional[str]
+    provider_customer_id: Optional[str]
+    sync: Optional[bool]
+    sync_with_provider: Optional[bool]
+    document_locale: Optional[str]
+
+
+class Metadata(BaseModel):
+    id: Optional[str]
+    key: Optional[str]
+    value: Optional[str]
+    display_in_invoice: Optional[bool]
+
+
+class MetadataList(BaseModel):
+    __root__: List[Metadata]
+
+
+class Customer(BaseModel):
+    external_id: str
+    address_line1: Optional[str]
+    address_line2: Optional[str]
+    city: Optional[str]
+    country: Optional[str]
+    currency: Optional[str]
+    email: Optional[str]
+    legal_name: Optional[str]
+    legal_number: Optional[str]
+    tax_identification_number: Optional[str]
+    logo_url: Optional[str]
+    name: str
+    phone: Optional[str]
+    state: Optional[str]
+    timezone: Optional[str]
+    url: Optional[str]
+    zipcode: Optional[str]
+    metadata: Optional[MetadataList]
+    billing_configuration: Optional[CustomerBillingConfiguration]
+    taxes: Optional[Taxes]
+
+
+class CustomerResponse(BaseResponseModel):
+    lago_id: str
+    external_id: str
+    address_line1: Optional[str]
+    address_line2: Optional[str]
+    city: Optional[str]
+    country: Optional[str]
+    currency: Optional[str]
+    email: Optional[str]
+    created_at: str
+    legal_name: Optional[str]
+    legal_number: Optional[str]
+    tax_identification_number: Optional[str]
+    logo_url: Optional[str]
+    name: str
+    phone: Optional[str]
+    state: Optional[str]
+    timezone: Optional[str]
+    applicable_timezone: str
+    url: Optional[str]
+    zipcode: Optional[str]
+    metadata: Optional[MetadataList]
+    billing_configuration: Optional[CustomerBillingConfiguration]
