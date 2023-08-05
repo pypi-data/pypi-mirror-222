18699200607cbf96d986aeda8cae8aabb72004f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:09 +0000

    Add package philander/0.1.1a0/philander-0.1.1a0-py3-none-any.whl

---
+++ packages/philander/philander-0.1.1a0-py3-none-any.whl/philander/fastgait/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""A package to provide modules related to the FastGait project.
+
+FastGait was a research project at IHP from 2019 to 2022 targeting a
+foot-warn mobile system to assist patients suffering from Parkinson's
+disease. In particular, the system is to detect and overcome freeze-of-
+gait (FOG) situations.
+
+For more information on the FastGait project see:
+`IHP project website
+<https://www.ihp-microelectronics.com/de/forschung/drahtlose-systeme-und-anwendungen/projekte/fast-gait>`_. 
+"""
