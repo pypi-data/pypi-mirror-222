52467ee1cf55be4f7819a403cda30a206b80cc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:43 +0000

    Add package thinkPass/2.0.0.1/thinkPass-2.0.0.1.tar.gz

---
+++ packages/thinkPass/thinkPass-2.0.0.1.tar.gz/thinkPass-2.0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="thinkPass",
+    version="2.0.0.1",
+    author="Anant Chaudhary",
+    description="password generator package",
+    long_description="A package for generating passwords.",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    package_data={'thinkPass': ['data/*.csv']},  # Correct the package_data key
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # Add any dependencies here
+    ]
+)
+
+
