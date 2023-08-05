4982f14cd829e48253621f1b750d6a2f86816943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:55 +0000

    Add package voysen/0.1.0/voysen-0.1.0.tar.gz

---
+++ packages/voysen/voysen-0.1.0.tar.gz/voysen-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='voysen',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Baudet Léo-Paul',
+    description='Bibliothèque automatisant partiellement les associations de Voysen',
+    url='https://github.com/leopaulbis/Voysen_',
+)
