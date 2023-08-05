ad9c09c963e47ade03017be1f0698ca793c2e645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package minet/1.0.0a46/minet-1.0.0a46.tar.gz

---
+++ packages/minet/minet-1.0.0a46.tar.gz/minet-1.0.0a46/minet/crawl/types.py
@@ -0,0 +1,347 @@
+from typing import TypeVar, Union, Generic, Optional, List
+
+import json
+from nanoid import generate
+from ebbe import format_repr
+from functools import partial
+from ural import get_domain_name
+
+from minet.web import Response
+from minet.serialization import serialize_error_as_slug
+
+CrawlJobDataType = TypeVar("CrawlJobDataType")
+CrawlResultDataType = TypeVar("CrawlResultDataType")
+
+
+class CrawlTarget(Generic[CrawlJobDataType]):
+    """
+    This class represent a crawl target, i.e. at least a URL, and should be
+    used by end-users when they need to specify what they want to crawl more
+    precisely (i.e. if they need to switch spiders, have a custom depth or
+    rely on some necessary data that will be passed along).
+    """
+
+    __slots__ = ("url", "depth", "spider", "data")
+
+    url: str
+    depth: Optional[int]
+    spider: Optional[str]
+    data: Optional[CrawlJobDataType]
+
+    def __init__(
+        self,
+        url: str,
+        depth: Optional[int] = None,
+        spider: Optional[str] = None,
+        data: Optional[CrawlJobDataType] = None,
+    ) -> None:
+        if not isinstance(url, str):
+            raise TypeError("url should be a string")
+
+        self.url = url
+
+        if depth is not None and not isinstance(depth, int):
+            raise TypeError("depth should be an int")
+
+        self.depth = depth
+
+        if spider is not None and not isinstance(spider, str):
+            raise TypeError("spider should be a string")
+
+        self.spider = spider
+        self.data = data
+
+    def __eq__(self, other):
+        return (
+            self.url == other.url
+            and self.depth == other.depth
+            and self.spider == other.spider
+            and self.data == other.data
+        )
+
+    def __repr__(self):
+        return format_repr(self, conditionals=("spider", "data", "depth"))
+
+
+UrlOrCrawlTarget = Union[str, CrawlTarget[CrawlJobDataType]]
+
+
+generate_crawl_job_id = partial(
+    generate,
+    alphabet="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    size=12,
+)
+
+
+class CrawlJob(Generic[CrawlJobDataType]):
+    """
+    This class represents an actual job that will be queued by the crawler. It
+    should not be created by users themselves as they are inferred from
+    a CrawlTarget instead.
+
+    This class should be pickle-safe as it will be serialized by persistent
+    queues.
+    """
+
+    __slots__ = (
+        "id",
+        "url",
+        "depth",
+        "spider",
+        "data",
+        "parent",
+        "__has_cached_domain",
+        "__domain",
+    )
+
+    id: str
+    url: str
+    depth: int
+    spider: Optional[str]
+    data: Optional[CrawlJobDataType]
+    parent: Optional[str]
+
+    # TODO: we should add headers, cookies and such here in the future
+
+    __has_cached_domain: bool
+    __domain: Optional[str]
+
+    @classmethod
+    def fieldnames(cls):
+        return [slot for slot in cls.__slots__ if not slot.startswith("_")]
+
+    def __init__(
+        self,
+        url: str,
+        depth: Optional[int] = None,
+        spider: Optional[str] = None,
+        data: Optional[CrawlJobDataType] = None,
+        parent: Optional[str] = None,
+    ):
+        self.id = generate_crawl_job_id()
+        self.url = url
+        self.depth = depth if depth is not None else 0
+        self.spider = spider
+        self.data = data
+        self.parent = parent
+
+        self.__has_cached_domain = False
+        self.__domain = None
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+    def __tabulate(self, serialize_data: bool = False):
+        return (
+            self.id,
+            self.url,
+            self.depth,
+            self.spider,
+            self.data
+            if not serialize_data
+            else json.dumps(self.data, ensure_ascii=False),
+            self.parent,
+        )
+
+    def __getstate__(self):
+        return self.__tabulate()
+
+    def __csv_row__(self):
+        return self.__tabulate(serialize_data=True)
+
+    def __setstate__(self, state):
+        (
+            self.id,
+            self.url,
+            self.depth,
+            self.spider,
+            self.data,
+            self.parent,
+        ) = state
+
+        self.__has_cached_domain = False
+        self.__domain = None
+
+    @property
+    def domain(self) -> Optional[str]:
+        if self.__has_cached_domain:
+            return self.__domain
+
+        if self.url is not None:
+            try:
+                self.__domain = get_domain_name(self.url)
+            except Exception:
+                pass
+
+        self.__has_cached_domain = True
+
+        return self.__domain
+
+    def __repr__(self):
+        return format_repr(self, conditionals=("data", "spider", "parent"))
+
+
+class CrawlResult(Generic[CrawlJobDataType, CrawlResultDataType]):
+    __slots__ = ("job", "data", "error", "response", "degree")
+
+    FIELDNAMES = [
+        "id",
+        "parent",
+        "spider",
+        "depth",
+        "url",
+        "resolved_url",
+        "error",
+        "status",
+        "mimetype",
+        "degree",
+        "body_size",
+    ]
+
+    job: CrawlJob[CrawlJobDataType]
+    data: Optional[CrawlResultDataType]
+    error: Optional[Exception]
+    response: Optional[Response]
+    degree: int
+
+    @classmethod
+    def fieldnames(cls, singular: bool = False) -> List[str]:
+        return (
+            cls.FIELDNAMES
+            if not singular
+            else [f for f in cls.FIELDNAMES if f != "spider"]
+        )
+
+    def __init__(self, job: CrawlJob[CrawlJobDataType]):
+        self.job = job
+        self.data = None
+        self.error = None
+        self.response = None
+        self.degree = 0
+
+    @property
+    def url(self) -> str:
+        return self.job.url
+
+    @property
+    def depth(self) -> int:
+        return self.job.depth
+
+    @property
+    def spider(self) -> Optional[str]:
+        return self.job.spider
+
+    @property
+    def error_code(self) -> Optional[str]:
+        return serialize_error_as_slug(self.error) if self.error else None
+
+    def __csv_row__(self):
+        job = self.job
+
+        row = [job.id, job.parent]
+
+        if job.spider is not None:
+            row.append(job.spider)
+
+        row.extend(
+            [
+                job.depth,
+                job.url,
+                self.response.end_url if self.response else None,
+                self.error_code,
+                self.response.status if self.response else None,
+                self.response.mimetype if self.response else None,
+                self.degree,
+                len(self.response) if self.response else None,
+            ]
+        )
+
+        return row
+
+    def as_csv_row(self):
+        return self.__csv_row__()
+
+    def __repr__(self) -> str:
+        return format_repr(
+            self,
+            attributes=(
+                (
+                    "url",
+                    "depth",
+                    "spider",
+                    ("error", self.error_code),
+                    ("status", self.response.status if self.response else None),
+                    ("degree", self.degree if self.error is None else None),
+                    (
+                        "dtype",
+                        type(self.data).__name__ if self.data is not None else None,
+                    ),
+                    ("size", self.response.human_size if self.response else None),
+                    ("mimetype", self.response.mimetype if self.response else None),
+                )
+            ),
+            conditionals=(
+                "spider",
+                "error",
+                "status",
+                "degree",
+                "dtype",
+                "size",
+                "mimetype",
+            ),
+        )
+
+
+class ErroredCrawlResult(CrawlResult[CrawlJobDataType, None]):
+    job: CrawlJob[CrawlJobDataType]
+    data: None
+    error: Exception
+    response: Optional[Response]
+    degree: int
+
+    def __init__(
+        self,
+        job: CrawlJob[CrawlJobDataType],
+        error: Exception,
+        response: Optional[Response] = None,
+    ):
+        self.job = job
+        self.data = None
+        self.error = error
+        self.response = response
+        self.degree = 0
+
+    @property
+    def error_code(self) -> str:
+        return serialize_error_as_slug(self.error)
+
+
+class SuccessfulCrawlResult(CrawlResult[CrawlJobDataType, CrawlResultDataType]):
+    job: CrawlJob[CrawlJobDataType]
+    data: CrawlResultDataType
+    error: None
+    response: Response
+    degree: int
+
+    def __init__(
+        self,
+        job: CrawlJob[CrawlJobDataType],
+        response: Response,
+        data: CrawlResultDataType,
+        degree: int,
+    ):
+        self.job = job
+        self.data = data
+        self.error = None
+        self.response = response
+        self.degree = degree
+
+    @property
+    def error_code(self) -> None:
+        return None
+
+
+AnyCrawlResult = Union[
+    ErroredCrawlResult[CrawlJobDataType],
+    SuccessfulCrawlResult[CrawlJobDataType, CrawlResultDataType],
+]
---
+++ packages/minet/minet-1.0.0a46.tar.gz/minet-1.0.0a46/minet/crawl/url_cache.py
@@ -0,0 +1,235 @@
+from typing import (
+    TypeVar,
+    Generic,
+    Set,
+    Iterable,
+    Iterator,
+    List,
+    Optional,
+    Callable,
+    overload,
+)
+
+import sqlite3
+from os import makedirs
+from os.path import join
+from threading import Lock
+from contextlib import contextmanager
+from ebbe import distinct
+from ural import canonicalize_url, normalize_url
+
+from minet.crawl.types import CrawlJob, CrawlJobDataType
+
+T = TypeVar("T")
+I = TypeVar("I")
+
+
+class AtomicSet(Generic[T]):
+    """
+    A simple set wrapper ensuring all operations are
+    atomic and thus threadsafe.
+    """
+
+    __items: Set[T]
+    __lock: Lock
+
+    def __init__(self):
+        self.__items = set()
+        self.__lock = Lock()
+
+    def add(self, item: T) -> bool:
+        with self.__lock:
+            len_before = len(self.__items)
+
+            self.__items.add(item)
+
+            return len(self.__items) > len_before
+
+    def add_many(self, items: Iterable[T]) -> int:
+        with self.__lock:
+            len_before = len(self.__items)
+
+            for item in items:
+                self.__items.add(item)
+
+            return len(self.__items) - len_before
+
+    @overload
+    def add_many_and_keep_new(
+        self, items: Iterable[I], key: Callable[[I], T] = ...
+    ) -> List[I]:
+        ...
+
+    @overload
+    def add_many_and_keep_new(self, items: Iterable[T], key: None = ...) -> List[T]:
+        ...
+
+    def add_many_and_keep_new(self, items, key=None):
+        with self.__lock:
+            new = []
+
+            for item in items:
+                k = item if key is None else key(item)
+
+                len_before = len(self.__items)
+                self.__items.add(k)
+
+                if len_before < len(self.__items):
+                    new.append(item)
+
+            return new
+
+    def __len__(self) -> int:
+        with self.__lock:
+            return len(self.__items)
+
+    def __contains__(self, item: T) -> bool:
+        with self.__lock:
+            return item in self.__items
+
+    def __iter__(self) -> Iterator[T]:
+        with self.__lock:
+            yield from self.__items
+
+
+class SQLiteStringSet:
+    def __init__(self, path: str, db_name: str = "set.db"):
+        makedirs(path, exist_ok=True)
+
+        self.path = path
+
+        self.__connection = sqlite3.connect(
+            join(self.path, db_name), check_same_thread=False
+        )
+        self.__lock = Lock()
+
+        # Setup
+        self.__connection.execute("PRAGMA journal_mode=wal;")
+        self.__connection.execute(
+            'CREATE TABLE IF NOT EXISTS "set" ("key" TEXT PRIMARY KEY);'
+        )
+        self.__connection.commit()
+
+    @contextmanager
+    def __transaction(self):
+        try:
+            with self.__lock, self.__connection:
+                cursor = self.__connection.cursor()
+                yield cursor
+        finally:
+            cursor.close()
+
+    def vacuum(self) -> None:
+        with self.__transaction() as cursor:
+            cursor.execute("VACUUM;")
+
+    def add(self, item: str) -> bool:
+        with self.__transaction() as cursor:
+            try:
+                cursor.execute('INSERT INTO "set" ("key") VALUES (?);', (item,))
+            except sqlite3.IntegrityError:
+                return False
+
+            return True
+
+    def add_many(self, items: Iterable[str]) -> int:
+        with self.__transaction() as cursor:
+            rows = [(item,) for item in items]
+            cursor.executemany('INSERT OR IGNORE INTO "set" ("key") VALUES (?);', rows)
+            return cursor.rowcount
+
+    @overload
+    def add_many_and_keep_new(
+        self, items: Iterable[I], key: Callable[[I], str] = ...
+    ) -> List[I]:
+        ...
+
+    @overload
+    def add_many_and_keep_new(self, items: Iterable[str], key: None = ...) -> List[str]:
+        ...
+
+    def add_many_and_keep_new(self, items, key=None):
+        with self.__transaction() as cursor:
+            new = []
+
+            for item in items:
+                try:
+                    k = item if key is None else key(item)
+                    cursor.execute('INSERT INTO "set" ("key") VALUES (?);', (k,))
+                    new.append(item)
+                except sqlite3.IntegrityError:
+                    pass
+
+            return new
+
+    # NOTE: I keep this for the ugly trick.
+    # def contains_many(self, items: Iterable[str]) -> List[str]:
+    #     with self.__transaction() as cursor:
+    #         cursor.execute(
+    #             'SELECT "key" FROM "set" WHERE "key" in (SELECT "value" FROM JSON_EACH(?));',
+    #             (json.dumps(items),),
+    #         )
+
+    def __contains__(self, item: str) -> bool:
+        with self.__transaction() as cursor:
+            cursor.execute('SELECT 1 FROM "set" WHERE "key" = ?;', (item,))
+            return cursor.fetchone() is not None
+
+    def __len__(self) -> int:
+        with self.__transaction() as cursor:
+            cursor.execute('SELECT COUNT(*) FROM "set";')
+            return cursor.fetchone()[0]
+
+    def __iter__(self) -> Iterator[str]:
+        with self.__transaction() as cursor:
+            cursor.execute('SELECT "key" FROM "set";')
+
+            while True:
+                rows = cursor.fetchmany(128)
+
+                if not rows:
+                    return
+
+                for row in rows:
+                    yield row[0]
+
+    def __del__(self) -> None:
+        self.__connection.close()
+
+
+class URLCache:
+    def __init__(self, path: Optional[str] = None, normalized: bool = False):
+        self.path = path
+        self.persistent = path is not None
+        self.preprocessing = normalize_url if normalized else canonicalize_url
+        self.normalized = normalized
+
+        self.__cache = (
+            SQLiteStringSet(path, "urls.db") if path is not None else AtomicSet()
+        )
+
+    def add(self, url: str) -> bool:
+        url = self.preprocessing(url)
+        return self.__cache.add(url)
+
+    def register(
+        self, jobs: Iterable[CrawlJob[CrawlJobDataType]]
+    ) -> List[CrawlJob[CrawlJobDataType]]:
+        def url_key(job: CrawlJob[CrawlJobDataType]):
+            return self.preprocessing(job.url)
+
+        # We deduplicate beforehand
+        jobs = distinct(jobs, key=url_key)
+        new = self.__cache.add_many_and_keep_new(jobs, key=url_key)
+
+        return new
+
+    def __len__(self) -> int:
+        return len(self.__cache)
+
+    def __iter__(self) -> Iterator[str]:
+        return self.__cache.__iter__()
+
+    def __contains__(self, url: str) -> bool:
+        url = self.preprocessing(url)
+        return url in self.__cache
