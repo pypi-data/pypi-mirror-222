abb3e5d2768c81a8743270aac7be7ae651ec5d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:11 +0000

    Add package lago-python-client/0.41.0/lago_python_client-0.41.0-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-0.41.0-py3-none-any.whl/lago_python_client/models/plan.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from .charge import Charges, ChargesResponse
+from ..base_model import BaseResponseModel
+
+
+class Plan(BaseModel):
+    name: Optional[str]
+    code: Optional[str]
+    interval: Optional[str]
+    description: Optional[str]
+    amount_cents: Optional[int]
+    amount_currency: Optional[str]
+    trial_period: Optional[float]
+    pay_in_advance: Optional[bool]
+    bill_charges_monthly: Optional[bool]
+    charges: Optional[Charges]
+
+
+class PlanResponse(BaseResponseModel):
+    lago_id: str
+    name: str
+    created_at: str
+    code: str
+    interval: Optional[str]
+    description: Optional[str]
+    amount_cents: Optional[int]
+    amount_currency: Optional[str]
+    trial_period: Optional[float]
+    pay_in_advance: Optional[bool]
+    bill_charges_monthly: Optional[bool]
+    charges: Optional[ChargesResponse]
+    active_subscriptions_count: int
+    draft_invoices_count: int
