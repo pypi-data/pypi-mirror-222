933ccd7091cdcab578d9eb40c3f1471726b256e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package arcesetc/0.3/arcesetc-0.3.tar.gz

---
+++ packages/arcesetc/arcesetc-0.3.tar.gz/arcesetc-0.3/docs/_build/html/plot_directive/arcesetc/gettingstarted-1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import astropy.units as u
+
+from arcesetc import plot_order_counts, plot_order_sn
+
+sptype = 'G4V'
+wavelength = 6562 * u.Angstrom
+exp_time = 30 * u.min
+V = 10
+
+fig, ax, exp_time = plot_order_counts(sptype, wavelength, V, exp_time=exp_time)
+plt.show()
