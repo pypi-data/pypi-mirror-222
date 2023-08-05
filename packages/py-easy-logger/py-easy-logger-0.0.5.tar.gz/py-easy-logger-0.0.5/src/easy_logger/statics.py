9025f72fe43aaf43149d31d5dbaeec806a59ae29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:04 +0000

    Add package py-easy-logger/0.0.5/py-easy-logger-0.0.5.tar.gz

---
+++ packages/py-easy-logger/py-easy-logger-0.0.5.tar.gz/py-easy-logger-0.0.5/src/easy_logger/statics.py
@@ -0,0 +1,10 @@
+"""Static."""
+
+ENCODING: str = "utf-8"
+STREAM_COLORS: dict[str, str] = {
+    'DEBUG': 'cyan',
+    'INFO': 'green',
+    'WARNING': 'yellow',
+    'ERROR': 'bold_red',
+    'CRITICAL': 'purple'
+}
