caf472370b54a0f9527e0d7f4566f40134b1f752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package statspack/0.1.2/statspack-0.1.2-py2.py3-none-any.whl

---
+++ packages/statspack/statspack-0.1.2-py2.py3-none-any.whl/statspack/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# statspack/__init__.py
+
+# Import sub-modules or functions
+from .binning import binning
+from .find_confidence_interval import find_confidence_interval
+from .density_contour import density_contour
+from .contour_pdf import contour_pdf
+
+__version__ = '0.1.2'
+__author__ = 'Fabio R. Herpich'
+
+# Define what names are imported with 'from statspack import *'
+__all__ = ['binning', 'find_confidence_interval',
+           'density_contour', 'contour_pdf']
+
+# Optional initializations or configurations
+# For example, you can set up logging here if needed
+import logging
+
+logging.basicConfig(level=logging.INFO)
