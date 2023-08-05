f577512805d140e62c13a59486f5c9aa91f8981f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:57 +0000

    Add package rfidtools/1.1.1/rfidtools-1.1.1-py3-none-any.whl

---
+++ packages/rfidtools/rfidtools-1.1.1-py3-none-any.whl/rfidtools/__init__.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import yaml
+
+
+global DB_SERVER
+global DB_TABLE
+global DB_USER
+global DB_PASS
+
+global SSH_SERVER
+global SSH_USER
+global SSH_PASS
+global SSH_LOGS_PATH
+global SSH_ARCHIVES_PATH
+
+config_path = Path(__file__).parent.joinpath('config.yaml')
+with open(config_path) as config_file:
+    config = yaml.load(config_file, Loader=yaml.Loader)
+
+    DB_SERVER = config['db']['server']
+    DB_TABLE = config['db']['table']
+    DB_USER = config['db']['user']
+    DB_PASS = config['db']['pass']
+
+    SSH_SERVER = config['ssh']['server']
+    SSH_USER = config['ssh']['user']
+    SSH_PASS = config['ssh']['pass']
+    SSH_LOGS_PATH = config['ssh']['logs-path']
+    SSH_ARCHIVES_PATH = config['ssh']['archives-path']
---
+++ packages/rfidtools/rfidtools-1.1.1-py3-none-any.whl/rfidtools/__main__.py
@@ -0,0 +1,38 @@
+import os
+import platform
+import argparse
+import PyInstaller.__main__ as build
+
+from rfidtools.core import gui_loop
+
+
+parser = argparse.ArgumentParser(
+    prog='rfidtools',
+    description='Tool for RFID tag production at CIOT.',
+    epilog='Use "python -m rfidtools" to run from the command line.')
+
+parser.add_argument('-b', '--build-exe',
+                    required=False,
+                    action='store_true',
+                    help='Build the program into an exe to be placed on the desktop')
+
+BUILD = vars(parser.parse_args())['build_exe']
+
+
+if not BUILD:
+    gui_loop()
+
+else:
+    if platform.system() == 'Windows':
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        build.run([
+            f'{dir_path}\\__main__.py',
+            '-F',
+            f'--distpath {desktop}',
+            '-n RFID_Tools',
+            '--windowed',
+            '-i RFID_Icon.ico'])
+    else:
+        print('Building to .exe is only supported on Windows.')
+        raise SystemError
