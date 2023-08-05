b176ede1b7838e206c5721f382b8d2665bdfc168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:01 +0000

    Add package desc-opt/0.9.2/desc_opt-0.9.2-py3-none-any.whl

---
+++ packages/desc-opt/desc_opt-0.9.2-py3-none-any.whl/desc/geometry/__init__.py
@@ -0,0 +1,5 @@
+"""Classes for representing geometric objects like curves and surfaces."""
+
+from .core import Curve, Surface
+from .curve import FourierPlanarCurve, FourierRZCurve, FourierXYZCurve
+from .surface import FourierRZToroidalSurface, ZernikeRZToroidalSection
