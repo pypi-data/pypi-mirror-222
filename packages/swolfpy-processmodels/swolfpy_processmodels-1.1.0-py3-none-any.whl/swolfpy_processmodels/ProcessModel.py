e437c8afccfe65259131883c0e76cf5564bd1e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:44 +0000

    Add package swolfpy-processmodels/1.1.0/swolfpy_processmodels-1.1.0-py3-none-any.whl

---
+++ packages/swolfpy-processmodels/swolfpy_processmodels-1.1.0-py3-none-any.whl/swolfpy_processmodels/ProcessModel.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+
+from swolfpy_inputdata import CommonData
+
+
+class ProcessModel(ABC):
+    def __init__(self, process_name, CommonDataObjct):
+        if CommonDataObjct:
+            self.CommonData = CommonDataObjct
+        else:
+            self.CommonData = CommonData()
+        self.process_name = process_name
+
+        # Read Material properties
+        self.Material_Properties = self.CommonData.Material_Properties
+
+        self.Index = self.CommonData.Index
+
+    @property
+    @abstractmethod
+    def Process_Type(self):
+        pass
+
+    @abstractmethod
+    def calc(self):
+        pass
+
+    @abstractmethod
+    def setup_MC(self, seed=None):
+        pass
+
+    @abstractmethod
+    def MC_calc(self):
+        pass
+
+    @abstractmethod
+    def report(self):
+        pass
