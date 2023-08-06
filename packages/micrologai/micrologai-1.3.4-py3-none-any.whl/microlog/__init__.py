3954029d0c7c3815227a7281c69dbcbb31dffb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:44 +0000

    Add package micrologai/1.3.4/micrologai-1.3.4-py3-none-any.whl

---
+++ packages/micrologai/micrologai-1.3.4-py3-none-any.whl/microlog/__init__.py
@@ -0,0 +1,15 @@
+#
+# Microlog. Copyright (c) 2023 laffra, dcharbon. All rights reserved.
+#
+
+__version__ = "1.3.4"
+
+from .api import info, warn, debug, error
+from .api import start, stop
+from .api import heap
+from .api import enabled
+
+try:
+    from . import server
+except:
+    pass # the server is not included in the UI
