c615a9880647370e812e01ce2b9f7062eb6bf012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:14 +0000

    Add package fyers-sdk/1.0.0/fyers_sdk-1.0.0.tar.gz

---
+++ packages/fyers-sdk/fyers_sdk-1.0.0.tar.gz/fyers_sdk-1.0.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='fyers_sdk',  
+     version='1.0.0',
+     author="Fyers-Tech",
+     author_email="support@fyers.in",
+     description="Fyers trading APIs.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/FyersDev/fyers-api-py",
+     packages=setuptools.find_packages(),
+     install_requires=[
+                'requests==2.31.0',
+                'asyncio==3.4.3',
+                'aiohttp==3.8.4',
+                'aws_lambda_powertools==1.25.5',
+                'websocket-client==1.6.1'
+          ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
