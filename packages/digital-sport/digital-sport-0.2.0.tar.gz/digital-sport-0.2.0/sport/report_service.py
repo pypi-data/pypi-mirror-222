26873c47798f7d7b36c472a12f10f39e110d65ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:56 +0000

    Add package digital-sport/0.2.0/digital-sport-0.2.0.tar.gz

---
+++ packages/digital-sport/digital-sport-0.2.0.tar.gz/digital-sport-0.2.0/sport/report_service.py
@@ -0,0 +1,33 @@
+"""
+Copyright (c) 2022 Plugin Andrey (9keepa@gmail.com)
+Licensed under the MIT License
+"""
+import csv
+import json
+from typing import List, Any
+
+class ReportService:
+
+    def __init__(self, objects_list: List[Any]):
+        self.objects_list = objects_list
+
+    def csv_dump(self, path, encoding="utf8"):
+        with open(path, mode='w', encoding=encoding) as csv_file:
+            writer = csv.writer(csv_file)
+            headers = None
+            for row in self.objects_list:
+                if isinstance(row, dict):
+                    if headers is None:
+                        writer.writerow(row.keys())
+                        headers = True
+                    writer.writerow(row.values())
+                else:
+                    writer.writerow(row)
+
+        return self
+
+    def json_dump(self, path, encoding="utf8"):
+        with open(path, "w", encoding=encoding) as f:
+            json.dump(self.objects_list, f, indent=4, sort_keys=True)
+
+        return self
---
+++ packages/digital-sport/digital-sport-0.2.0.tar.gz/digital-sport-0.2.0/sport/search_center.py
@@ -0,0 +1,120 @@
+"""
+Copyright (c) 2022 Plugin Andrey (9keepa@gmail.com)
+Licensed under the MIT License
+"""
+from typing import Callable, List
+from functools import partial
+from .interface import Ih2h, IStatistics
+
+
+def pprint(string, match_id:str):
+    if match_id:
+        string = f"{match_id}: {string}"
+    print(string)
+
+
+class H2hSearch:
+    """Класс который ищет последние матчи по условию
+    fixtures.search_last_result("w w w:home", team="home")
+    """
+
+    def __init__(self, objects_list, p_cls):
+        self.objects_list = objects_list
+        self.parent_cls = p_cls
+
+    def live_search_last_result(self, pattern, team, function=pprint):
+        fixtures = [x for x in self.objects_list]
+
+        for fixture in fixtures:
+            h2h_list = self.parent_cls.h2h(fixture['match_id'])
+            h2h_list.search_last_result(pattern, team, partial(function, match_id=fixture['match_id']))
+
+
+    def get_result_h2h(self, team_name,
+                   pattern, h2h_list: List[Ih2h], function):
+        template = "Found! Team [{team}] [pattern:{pattern}] [{place} games]"
+        place = ""
+        h2h_list = [x for x in h2h_list if x.match_type == team_name]
+        if ":" in pattern:
+            place = pattern.split(":")[-1]
+            h2h_list = [x for x in h2h_list if x.place == place]
+        result = self._h2h_search_last_result(pattern, h2h_list)
+        if result:
+            if function:
+                function(template.format(
+                    team=team_name.replace("Last matches:", ""),
+                    pattern=pattern.replace(":away", "").replace(":home", ""),
+                    place=place
+                ))
+
+
+    def h2h_search_last_result(self, pattern, team="", function:Callable=None):
+        h2h_list: List[Ih2h] = [Ih2h(**x) for x in self.objects_list if x['place']]
+        _team = {}
+        for x in h2h_list:
+            _team[x.match_type] = None
+        team_names = [x for x in _team.keys()]
+
+        if team == "home":
+            self.get_result_h2h(team_names[0], pattern, h2h_list, function)
+        elif team == "away":
+            self.get_result_h2h(team_names[1], pattern, h2h_list, function)
+        else:
+            self.get_result_h2h(team_names[0], pattern, h2h_list, function)
+            self.get_result_h2h(team_names[1], pattern, h2h_list, function)
+
+
+    def _h2h_search_last_result(self, pattern: str, h2h_list:List[Ih2h]):
+        pattern_list = pattern.split(":")[0].split()
+        quantity = len(pattern_list)
+        if quantity > len(h2h_list):
+            # Матчей меньше чем паттернов
+            return
+        i = 0
+        for e, pat in enumerate(pattern_list):
+
+            if h2h_list[e].win_draw_lose(pattern_list[e]):
+                i += 1
+            else:
+                return False
+
+            if i == quantity:
+                return True
+
+
+class SearchStatistics:
+    """Класс который ищет нужную статистику по различным условиям
+    .search_statistics("Ball Possession", period="Match", expression=">70", function=pprint)
+    """
+
+    def __init__(self, statistics: List[IStatistics], name_statistics,
+                 period=None, expression=None, function=None):
+        self.statistics = statistics
+        self.name_statistics = name_statistics
+        self.f_period = period
+        self.f_expression = expression
+        self.function = function
+        self.filters = {k:v for k,v in self.__dict__.items() if "f_" in k and v}
+        self.quantity = len(self.filters)
+        self.accum = 0
+
+    def get_update(self, statistics: IStatistics):
+        self.function(statistics)
+
+    def run(self):
+        for statistics in self.statistics:
+            if statistics.get_name(self.name_statistics):
+                for key, value in self.filters.items():
+                    result = getattr(statistics, key.replace("f_", "get_"))(value)
+                    if result:
+                        self.accum+=1
+                    else:
+                        break
+
+                if self.accum == self.quantity:
+                    self.get_update(statistics)
+                    self.accum = 0
+
+
+    def __call__(self, *args, **kwargs):
+        self.run()
