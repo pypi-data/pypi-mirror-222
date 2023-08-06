b2142a76158bb38c71f62390127f619aaeb4b830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:57 +0000

    Add package dist-alx-test/0.1/dist_alx_test-0.1.tar.gz

---
+++ packages/dist-alx-test/dist_alx_test-0.1.tar.gz/dist_alx_test-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_alx_test',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_alx_test'],
+      author_email='jayantreddybodkurwar@gmail.com',
+      zip_safe=False)
