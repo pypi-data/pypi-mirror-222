d1474732138daad949f964d003c3ce776431373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:15 +0000

    Add package upbge-stubs/0.3.1.27.dev5719807072/upbge-stubs-0.3.1.27.dev5719807072.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev5719807072.tar.gz/upbge-stubs-0.3.1.27.dev5719807072/region_type_items/__init__.pyi
@@ -0,0 +1,53 @@
+"""
+
+
+Region Type Items
+^^^^^^^^^^^^^^^^^
+
+:WINDOW:          
+  Window.
+
+:HEADER:          
+  Header.
+
+:CHANNELS:        
+  Channels.
+
+:TEMPORARY:       
+  Temporary.
+
+:UI:              
+  Sidebar.
+
+:TOOLS:           
+  Tools.
+
+:TOOL_PROPS:      
+  Tool Properties.
+
+:PREVIEW:         
+  Preview.
+
+:HUD:             
+  Floating Region.
+
+:NAVIGATION_BAR:  
+  Navigation Bar.
+
+:EXECUTE:         
+  Execute Buttons.
+
+:FOOTER:          
+  Footer.
+
+:TOOL_HEADER:     
+  Tool Header.
+
+:XR:              
+  XR.
+
+.. _rna-enum-region-type-items:
+
+"""
+
+import typing
