aa7595eb29e8c2502a3d22eff90bb332c6df76a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:28 +0000

    Add package thirdweb-sdk/3.1.1/thirdweb_sdk-3.1.1-py3-none-any.whl

---
+++ packages/thirdweb-sdk/thirdweb_sdk-3.1.1-py3-none-any.whl/thirdweb/types/settings/__init__.py
@@ -0,0 +1,12 @@
+from .metadata import (
+    NFTCollectionContractMetadata,
+    EditionContractMetadata,
+    TokenContractMetadata,
+    MarketplaceContractMetadata,
+    NFTDropContractMetadata,
+    EditionDropContractMetadata,
+    MultiwrapContractMetadata,
+    ContractMetadataSchema,
+    ContractPlatformFeeSchema,
+    ContractRoyaltySchema,
+)
