e1d222b7203bf217f724ac54cc4685a30aad3dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:35 +0000

    Add package templaterepo/0.0.0/templaterepo-0.0.0-py3-none-any.whl

---
+++ packages/templaterepo/templaterepo-0.0.0-py3-none-any.whl/templaterepo/__init__.py
@@ -0,0 +1,22 @@
+"""
+ProjectName
+
+A short description of the project.
+
+A longer description of the project.
+
+Copyright disclaimer:
+
+A copyright disclaimer.
+"""
+
+__version_details__ = dict(
+    version="0.0.0",
+    version_tuple=(0, 0, 0),
+    author_date="2023.08.01",
+    build_date="2023.08.01",
+    branch="None",
+    distance="0",
+    head_commit_hash="f174ecfcf1eede114bb18597548ff7d94a6a468d",
+)
+__version__ = __version_details__["version"]
