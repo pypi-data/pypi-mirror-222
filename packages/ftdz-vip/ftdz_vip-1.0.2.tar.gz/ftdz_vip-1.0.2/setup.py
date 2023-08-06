fb170b35b9174d1f7fd871e76c5c541b59e8ba7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:47 +0000

    Add package ftdz-vip/1.0.2/ftdz_vip-1.0.2.tar.gz

---
+++ packages/ftdz-vip/ftdz_vip-1.0.2.tar.gz/ftdz_vip-1.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name='ftdz_vip',
+    version='1.0.2',
+    author='bob',
+    author_email='56141537@qq.com',
+    description='',
+    long_description_content_type="""text/markdown""",
+    url='https://www.github.com/',
+    packages=['ftdz_vip'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
