49881d5ea38d81a754ea79bab1f7fcbbb41578be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:35 +0000

    Add package meile-gui/0.13.3.2/meile-gui-0.13.3.2.tar.gz

---
+++ packages/meile-gui/meile-gui-0.13.3.2.tar.gz/meile-gui-0.13.3.2/src/typedef/win.py
@@ -0,0 +1,18 @@
+ICANHAZURL = "https://icanhazip.com"
+ICANHAZDNS = "icanhazip.com"
+
+class WindowNames():
+    WALLET_RESTORE = "walletrestore"
+    MAIN_WINDOW    = "main"
+    PRELOAD        = "preload"
+    NODES          = "nodes"
+    WALLET         = "wallet"
+    HELP           = "helpscreen"
+    SUBSCRIPTIONS  = "subscriptions"
+    
+    
+class CoinsList():
+    SATOSHI = 1000000
+    ibc_mu_coins = ["dvpn", "scrt", "osmo", "atom", "dec"]
+    ibc_coins  = { "dvpn" : "udvpn", "scrt" : "uscrt", "osmo": "uosmo", "atom" : "uatom", "dec" : "udec"}
+    coins = ["atom", "dec", "dvpn", "osmo", "scrt"]
