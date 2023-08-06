a8dfb335c38b80490d3959e55062f0598a7bc28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package auto-code-generation-execution/1.0.3/auto-code-generation-execution-1.0.3.tar.gz

---
+++ packages/auto-code-generation-execution/auto-code-generation-execution-1.0.3.tar.gz/auto-code-generation-execution-1.0.3/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name="auto-code-generation-execution",
+    version="1.0.3",
+    packages=find_packages(),
+    py_modules=['auto_code_generation_execution'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'auto_code_generation_execution = auto_code_generation_execution:main',
+        ],
+    },
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',)
