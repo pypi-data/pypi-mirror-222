33ddac4d5b6a760506c12b1611980b6a2994a9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:39 +0000

    Add package sentiment-analysis-tagit/0.15/sentiment-analysis-tagit-0.15.tar.gz

---
+++ packages/sentiment-analysis-tagit/sentiment-analysis-tagit-0.15.tar.gz/sentiment-analysis-tagit-0.15/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+  
+setup(
+    name='sentiment-analysis-tagit',
+    version='0.15',
+    description='A sentiment analysis tool for Tagit. Solely for internal use.',
+    author='Adithya Narayanan',
+    author_email='adithya.narayanan01@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'app_store_scraper',
+        'fpdf',
+        'datetime',
+        'llama_index',
+        'openai'
+    ],
+)
