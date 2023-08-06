bea8ebc901408ed8e6933447d6d8d9f5205d7816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:08 +0000

    Add package parsuricata/0.4.0/parsuricata-0.4.0-py3-none-any.whl

---
+++ packages/parsuricata/parsuricata-0.4.0-py3-none-any.whl/parsuricata/_parser.py
@@ -0,0 +1,98 @@
+from lark import Lark
+
+from .transformer import RuleTransformer
+
+grammar = r'''
+    %ignore " "
+    COMMENT: "#" /[^\r\n]/*
+    %ignore COMMENT
+
+    _NEWLINE: /[\r\n]+/
+    _ESCAPED_NEWLINE: /(\\(\r\n|\r|\n))+/
+
+    start: rules
+    rules: (_NEWLINE* rule)* _NEWLINE*
+
+    rule: action protocol target port direction target port "(" body ")"
+
+    !action: "pass"
+           | "drop"
+           | "reject"
+           | "alert"
+
+    !protocol: /[^\s]+/
+
+    ?target: any
+           | target_spec
+
+    !any: "any"
+
+    ?target_spec: variable
+                | ip
+                | cidr
+                | "[" target_spec ("," target_spec)* "]"    -> target_grouping
+                | "!" target_spec                           -> negated
+
+    variable: /\$[a-z_]+/i
+
+    ip: ip_v4
+      | ip_v6
+
+    ?ip_v4: /\b(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\b/
+    ?ip_v6: /\b(fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|2[0-4][0-9]|1{0,1}[0-9]{0,1}[0-9])\.{3,3})(25[0-5]|2[0-4][0-9]|1{0,1}[0-9]{0,1}[0-9])|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|2[0-4][0-9]|1{0,1}[0-9]{0,1}[0-9])\.{3,3})(25[0-5]|2[0-4][0-9]|1{0,1}[0-9]{0,1}[0-9])|:((:[0-9a-fA-F]{1,4}){1,7}|:))\b/
+
+    cidr: cidr_v4
+        | cidr_v6
+
+    ?cidr_v4: /\b(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))\b/
+    ?cidr_v6: /\b(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(\/((1(1[0-9]|2[0-8]))|([0-9][0-9])|([0-9])))\b/
+
+    ?port: any
+         | port_spec
+
+    ?port_spec: variable
+              | integer
+              | port_range
+              | "[" port_spec ("," port_spec)* "]"    -> port_grouping
+              | "!" port_spec                         -> negated
+
+    ?port_grouping_spec: port_spec
+                       | port_range
+
+    !port_range: integer ":" integer
+               | ":" integer
+               | integer ":"
+
+    integer: /\d+/
+
+    !direction: "->"
+              | "<>"
+
+    body: _ESCAPED_NEWLINE* (option _ESCAPED_NEWLINE*)+
+
+    option: KEYWORD _SEMICOLON
+          | KEYWORD _COLON settings _SEMICOLON
+
+    // Higher priorities, as these terminals are used to demarcate the option name
+    // and settings, in a context-free manner.
+    _COLON . 2: ":"
+    _SEMICOLON . 3: ";"
+
+    KEYWORD: /[a-z0-9_.\-]+/i
+
+    settings: "!" settings_expr   -> negated_settings
+            | settings_expr ("," settings_expr)*
+
+    ?settings_expr: STRING              -> string
+                  | (LITERAL | KEYWORD) -> literal
+
+    STRING: /"[^\r\n]+?"(?=\s*[;])/
+    LITERAL: /(?!\s+)([^,;\\"]|(?!\\)\\[,;\\"])+(?=;|,)/
+'''
+
+parser = Lark(
+    start='rules',
+    parser='lalr',
+    grammar=grammar,
+    transformer=RuleTransformer(),
+)
---
+++ packages/parsuricata/parsuricata-0.4.0-py3-none-any.whl/parsuricata/transformer.py
@@ -0,0 +1,106 @@
+from ipaddress import ip_address, ip_interface
+from typing import Any, Union
+
+from lark import InlineTransformer, Token, Tree
+
+from .rules import (
+    Grouping,
+    Literal,
+    Negated,
+    NegatedSetting,
+    Option,
+    PortRange,
+    Rule,
+    RulesList,
+    Setting,
+    Settings,
+    String,
+    Variable,
+)
+
+
+class RuleTransformer(InlineTransformer):
+    str = str
+
+    def rules(self, *rules):
+        return RulesList(rules)
+
+    def rule(self,
+             action,
+             protocol,
+             src,
+             src_port,
+             direction,
+             dst,
+             dst_port,
+             body
+             ) -> Rule:
+        return Rule(
+            action,
+            protocol,
+            src,
+            src_port,
+            direction,
+            dst,
+            dst_port,
+            body.children,
+        )
+
+    action = str
+    protocol = str
+    any = str
+    direction = str
+
+    def target_grouping(self, *components):
+        return Grouping(components)
+
+    def port_grouping(self, *components):
+        return Grouping(components)
+
+    def port_range(self, *components):
+        if len(components) == 3:
+            start, colon, end = components
+        elif isinstance(components[0], int):
+            (start, colon), end = components, None
+        else:
+            start, (colon, end) = None, components
+        return PortRange(start, end)
+
+    def option(self, keyword: Token, settings: Union[Token, Tree] = None) -> Option:
+        if settings is not None:
+            value = settings
+        else:
+            value = None
+
+        return Option(str(keyword), value)
+
+    def settings(self, *values: Any):
+        if len(values) == 1:
+            return Setting(values[0])
+
+        return Settings(Setting(value) for value in values)
+
+    def negated_settings(self, value: Any):
+        return NegatedSetting(value)
+
+    def variable(self, variable: Token):
+        identifier = str(variable)[1:]
+        return Variable(identifier)
+
+    def ip(self, tok: Token):
+        return ip_address(str(tok))
+
+    def cidr(self, tok: Token):
+        return ip_interface(str(tok))
+
+    def string(self, tok: Token):
+        return String(tok.value[1:-1])
+
+    def literal(self, tok: Token):
+        return Literal(tok.value)
+
+    def integer(self, tok):
+        return int(tok.value)
+
+    def negated(self, value: Any):
+        return Negated(value)
