1d56867664d858ec1c9bfa34a04f38a096b53720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:02 +0000

    Add package mess-client-project45/0.0.1/mess_client_project45-0.0.1.tar.gz

---
+++ packages/mess-client-project45/mess_client_project45-0.0.1.tar.gz/mess_client_project45-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_client_project45",
+      version="0.0.1",
+      description="mess_client_proj45",
+      author="dilbazi",
+      author_email="dilbazi.mardaliyeva@bk.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy==1.4.49', 'pycryptodome', 'pycryptodomex']
+      )
