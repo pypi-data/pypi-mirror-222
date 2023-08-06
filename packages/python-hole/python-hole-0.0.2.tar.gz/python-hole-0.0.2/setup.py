0d4a76567b8d54171827032f4961a938534032fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:46 +0000

    Add package python-hole/0.0.2/python-hole-0.0.2.tar.gz

---
+++ packages/python-hole/python-hole-0.0.2.tar.gz/python-hole-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-hole',
+    version='0.0.2',
+    packages=['pyhole', 'pyhole.antlr', 'pyhole.visualizer'],
+    package_dir={'': 'src'},
+    url='https://github.com/JulienLie/python-hole',
+    license='MIT',
+    author='Julien Lienard',
+    author_email='',
+    description=''
+)
