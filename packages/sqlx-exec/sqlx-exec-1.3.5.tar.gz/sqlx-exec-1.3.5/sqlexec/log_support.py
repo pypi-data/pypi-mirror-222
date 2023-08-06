a5d914f97740f746c530ceb1953fb8f20369278d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:31 +0000

    Add package sqlx-exec/1.3.5/sqlx-exec-1.3.5.tar.gz

---
+++ packages/sqlx-exec/sqlx-exec-1.3.5.tar.gz/sqlx-exec-1.3.5/sqlexec/log_support.py
@@ -0,0 +1,21 @@
+from logging import basicConfig, INFO, getLogger
+
+logger = getLogger(__name__)
+basicConfig(level=INFO, format='[%(levelname)s]: %(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+def insert_log(function: str, table: str, **kwargs):
+    logger.debug("Exec func 'sqlexec.%s' \n\t Table: '%s', kwargs: %s" % (function, table, kwargs))
+
+
+def save_log(function: str, select_key: str, table: str, **kwargs):
+    logger.debug("Exec func 'sqlexec.%s', 'select_key': %s \n\t Table: '%s', kwargs: %s" % (function, select_key, table, kwargs))
+
+
+def get_log(function: str, sql: str, *args):
+    logger.debug("Exec func 'sqlexec.%s' \n\t sql: %s \n\t args: %s" % (function, sql, args))
+
+
+def db_ctx_log(action, connection):
+    logger.debug('%s connection <%s>...' % (action, hex(id(connection))))
+
