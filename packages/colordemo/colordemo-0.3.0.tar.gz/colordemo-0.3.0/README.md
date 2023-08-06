7ee4b18a1bb4d837202ca944987ffc1ee57abf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package colordemo/0.3.0/colordemo-0.3.0.tar.gz

---
+++ packages/colordemo/colordemo-0.3.0.tar.gz/colordemo-0.3.0/README.md
@@ -0,0 +1,191 @@
+# `colordemo`
+
+`colordemo` is a Python package implementing RGB queries on xterm-like
+terminals.  It includes a demo script for presenting terminal color
+schemes, complete with RGB hex codes.
+
+<div align="center">
+    <img src="https://raw.githubusercontent.com/dranjan/python-colordemo/master/examples/colordemo-example.png">
+</div>
+
+## Quick start
+
+There are many ways to run the demo script, and we will briefly describe
+them here. Note that with any of the methods below, the `colordemo`
+tool can accept command-line options. To see which options are available,
+use `--help` with any of the methods, e.g.,
+
+    pipx run colordemo --help
+
+### Using `pipx`
+
+With only an active internet connection and the `pipx` Python utility,
+the script can be run directly without installation:
+
+    pipx run colordemo
+
+Alternatively, it can also be installed explicitly and run that way:
+
+    pipx install colordemo
+    colordemo
+
+### Using `pip`
+
+The package can also be installed and run as a normal Python package
+using `pip` (preferably in a `virtualenv` environment):
+
+    pip install colordemo
+    colordemo
+
+    # Equivalent
+    python -m colordemo
+
+### From the source tree
+
+Finally, the script can be run easily from the source tree directly.
+This package has no dependencies besides Python, so no actions are
+needed besides cloning the repository.
+
+    git clone 'git@github.com:dranjan/python-colordemo.git'
+    cd python-colordemo
+    python -m colordemo
+
+## Overview of functionality
+
+`colordemo` allows you to programmatically determine the RGB values of
+some terminals' ANSI colors (or more colors, if the terminal has them).
+The functions must be run from the terminal whose colors you want to
+determine, and with caveats if within a screen or tmux session (see
+below for more on this).  Not all terminal types are supported (see the
+next section).
+
+## Terminal support
+
+The fundamental requirement for a terminal to be supported by `colordemo`
+is for it to support the xterm-like OSC ("Operating System Command") control
+sequences, listed under "Operating System Commands" here:
+
+https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Operating-System-Commands
+
+(Sometimes this support can be ascertained from the documentation of
+the terminal, but often you just need to try it.)
+
+We call these terminals "xterm-like". If a terminal emulator doesn't
+support those sequences, then it won't be supported here.
+There may be other ways to obtain RGB values for these terminals, such
+as parsing configuration files or perhaps parsing the output of
+`xrdb --query`, but we have no plans to implement any of these.
+The only way to add support for a currently unsupported terminal is to
+patch the terminal with support for the OSC sequences.
+
+There are too many terminals for us to test all of them, so the lists
+below are not exhaustive. If a terminal isn't mentioned in one of the
+following subsections, attempt at your own risk!
+
+### Fully supported terminals
+
+- xterm
+- urxvt
+- VTE-based terminals, including:
+  - vte
+  - Terminal (XFCE)
+  - gnome-terminal
+  - terminator
+  - tilda
+  - (etc.)
+- kitty
+- alacritty
+- wezterm
+- ttyd
+- (etc.)
+
+### Unsupported terminals
+
+- Konsole-based terminals, which are buggy:
+  - Konsole
+  - yakuake
+  - (etc.)
+- terminology
+- st (unpatched)
+- Linux basic TTY (text mode without X)
+- (etc.)
+
+Some terminals (like terminology) don't seem to allow their colors to be
+queried dynamically, so all RGB queries will fail, but the failure can
+be detected. The demo script will therefore be able to output a color
+table, but without RGB values.
+
+Other terminals (like Konsole) seem to support the query codes but are
+extremely buggy, returning incorrect values and even segfaulting
+sometimes.
+
+In other cases (like the basic TTY), `colordemo` will garble
+the TTY and make it unreadable. (Try `tput reset<ENTER>` to restore it
+to something usable.)
+
+## Note regarding screen and tmux
+
+It generally doesn't make sense to query a terminal from inside a screen
+or tmux session, since a single screen or tmux session can be attached
+to multiple terminals.  However, in the special case of being attached
+to a single terminal, it is possible because tmux and screen provide
+(different) methods to pass control sequences through to the attached
+terminal.  Of course, it makes no sense to try this if there are
+multiple terminals attached, and you should expect crazy results if you
+do.  Thus, forwarding queries through screen or tmux is currently an
+opt-in feature (see the `screen_forward` optional argument in
+`TerminalQueryContext.__init__` or the
+`--screen-forward`/`--tmux-forward` command-line options).
+This will fail inside a nested screen or tmux session.
+
+(Not using the optional screen/tmux control-passthrough support, it's
+interesting to observe that screen and tmux emulate a 256-color terminal
+independently of the terminal(s) to which they are attached, which is
+very apparent if you run the script with 256-color output on a screen
+session attached to a terminal with 8- or 16-color terminfo (or with
+TERM set to such).)
+
+## Python API
+
+For greater flexbility, the functionality of this package can also be
+accessed through its Python API. For example, this could be useful to
+create new color scheme demo scripts.
+
+The primary interface is the context manager,
+`colordemo.TerminalQueryContext`. The color queries must be performed
+inside the context manager's context.
+
+```Python Console
+import colordemo
+
+with colordemo.TerminalQueryContext() as tq:
+    # Simplest method: get everything at once.
+    # This provides a list of RGBAColor instances.
+    colors = tq.get_all_indexed_colors()
+
+    # Alternatively, you can query individual colors.
+    n = tq.get_num_colors()
+    colors = [tq.get_indexed_color(k) for k in range(n)]
+
+    # The foreground and background colors need to
+    # be queried separately:
+    fg = tq.get_fg()
+    bg = tq.get_bg()
+
+# Color values are represented as instances of `RGBAColor`, which is
+# a specialization of `namedtuple`.
+(r, g, b, a) = fg
+
+# Equivalent:
+r, g, b, a = fg.r, fg.g, fg.b, fg.a
+
+# Color components are floating-point numbers in the range [0, 1].
+# To convert these to two-digit hex codes:
+r_hex = '%02x' % (int(r * 0xffff) // 256)  # etc.
+```
+
+## Credits
+
+- dranjan: main implementation
+- oblique: improved tmux support
+- Xyne: much useful discussion and review
---
+++ packages/colordemo/colordemo-0.3.0.tar.gz/colordemo-0.3.0/colordemo/terminal_query.py
@@ -0,0 +1,443 @@
+#   Copyright 2012-2023 Darsh Ranjan, oblique
+#
+#   This file is part of termcolors.
+#
+#   termcolors is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   termcolors is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with termcolors.  If not, see
+#   <http://www.gnu.org/licenses/>.
+
+import os
+import re
+import select
+import termios
+
+from .colors import RGBAColor
+
+#######################################################################
+# Query-related error conditions
+
+
+class TerminalQueryError(Exception):
+    """
+    Base class for the other exceptions.
+    """
+
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+
+class TerminalSetupError(TerminalQueryError):
+    """
+    We couldn't set up the terminal properly.
+    """
+
+    def __init__(self, fd):
+        TerminalQueryError.__init__(
+            self,
+            ("Couldn't set up terminal on file " +
+             ("descriptor %d" % fd)))
+
+
+class NoResponseError(TerminalQueryError):
+    """
+    The terminal didn't respond, or we were too impatient.
+    """
+
+    def __init__(self, q):
+        TerminalQueryError.__init__(
+            self,
+            "Timeout on query " + repr(q))
+
+
+class TerminalUninitializedError(TerminalQueryError):
+    """
+    Someone tried to do something without setting up the terminal
+    properly (by calling TerminalQueryContext.__enter__).
+    """
+
+    def __init__(self, fd):
+        TerminalQueryError.__init__(
+            self,
+            (("Terminal on file descriptor %d " % fd) +
+             "not set up"))
+
+
+########################################################################
+
+class TerminalQueryContext(object):
+    """
+    Context manager for terminal RGB queries.
+    """
+
+    # Operating system command
+    osc = "\033]"
+
+    # String terminator
+    #  ("\033\\" is another option, but "\007" seems to be understood by
+    #  more terminals.  Terminology, for example, doesn't seem to like
+    #  "\033\\".)
+    st = "\007"
+
+    # Control sequence introducer
+    csi = "\033["
+
+    # ANSI SGR0
+    reset = csi + 'm'
+
+    def __init__(self, fd=0, screen_forward=False):
+        """
+        Arguments:
+            fd: open file descriptor referring to the terminal we care
+                about. The default (0) is almost always correct.
+            screen_forward: whether to attempt to forward queries
+                through a screen or tmux session if we are in one.
+        """
+        self.tc_save = None
+        self.fd = fd
+
+        self.num_errors = 0
+
+        self.screen_forward = screen_forward
+
+    def __enter__(self):
+        """
+        Set up the terminal for queries.
+        """
+        self.tc_save = termios.tcgetattr(self.fd)
+
+        tc = termios.tcgetattr(self.fd)
+
+        # Don't echo the terminal's responses
+        tc[3] &= ~termios.ECHO
+
+        # Noncanonical mode (i.e., disable buffering on the terminal
+        # level)
+        tc[3] &= ~termios.ICANON
+
+        # Make input non-blocking
+        tc[6][termios.VMIN] = 0
+        tc[6][termios.VTIME] = 0
+
+        termios.tcsetattr(self.fd, termios.TCSANOW, tc)
+
+        # Check if it succeeded
+        if termios.tcgetattr(self.fd) != tc:
+            termios.tcsetattr(self.fd, termios.TCSANOW, self.tc_save)
+            raise TerminalSetupError(self.fd)
+
+        self.P = select.poll()
+        self.P.register(self.fd, select.POLLIN)
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Reset the terminal to its original state.
+        """
+        self.flush_input()
+
+        if self.tc_save is not None:
+            termios.tcsetattr(self.fd, termios.TCSANOW, self.tc_save)
+
+        del self.P
+
+    def get_num_colors(self, timeout=-1):
+        """
+        Attempt to determine the number of colors we are able to query
+        from the terminal. A larger timeout is safer but will cause this
+        function to take proportionally more time.
+
+        Arguments:
+            timeout: millisecond timeout, same interpretation as in
+                self.guarded_query.
+
+        Return: integer number of colors that are queryable.
+
+        Errors:
+            TerminalUninitializedError: if this instance's context has
+                not been entered.
+        """
+        # We won't count failed queries in this function, since we're
+        # guaranteed to fail a few.
+        num_errors = self.num_errors
+
+        if not self.get_indexed_color(0, timeout):
+            return 0
+
+        a = 0
+        b = 1
+        while self.get_indexed_color(b, timeout):
+            a = b
+            b += b
+
+        while b - a > 1:
+            c = (a + b) >> 1
+            if self.get_indexed_color(c, timeout):
+                a = c
+            else:
+                b = c
+
+        self.num_errors = num_errors
+        return b
+
+    def get_all_indexed_colors(self, limit=-1, timeout=-1):
+        """
+        Query as many indexed RGB values as possible up to `limit`
+        and return them all in a list.
+
+        Arguments:
+            limit: maximum number of colors to query, if nonnegative.
+                Negative values disable the limit.
+            timeout: millisecond timeout, same interpretation as in
+                self.guarded_query.
+
+        Return: list of RGBAColor instances.
+
+        Errors:
+            TerminalUninitializedError: if this instance's context has
+                not been entered.
+        """
+        colors = []
+
+        k = 0
+        while limit < 0 or k < limit:
+            c = self.get_indexed_color(k, timeout)
+            if c:
+                colors.append(c)
+                k += 1
+            else:
+                if limit < 0:
+                    self.num_errors -= 1
+                break
+
+        return colors
+
+    # Wrappers for xterm & urxvt operating system controls.
+    #
+    # These codes are all common to xterm and urxvt. Their responses
+    # aren't always in the same format (xterm generally being more
+    # consistent), but the regular expression used to parse the
+    # responses is general enough to work for both.
+    #
+    # Note: none of these functions is remotely thread-safe.
+
+    def get_fg(self, timeout=-1):
+        """
+        Get the terminal's foreground (text) color.
+
+        Arguments:
+            timeout: millisecond timeout, same interpretation as in
+                self.guarded_query.
+
+        Return: RGBAColor instance.
+
+        Errors:
+            TerminalUninitializedError: if this instance's context has
+                not been entered.
+        """
+        return self.rgb_query([10], timeout)
+
+    def get_bg(self, timeout=-1):
+        """
+        Get the terminal's background color.
+
+        Arguments:
+            timeout: millisecond timeout, same interpretation as in
+                self.guarded_query.
+
+        Return: RGBAColor instance.
+
+        Errors:
+            TerminalUninitializedError: if this instance's context has
+                not been entered.
+        """
+        return self.rgb_query([11], timeout)
+
+    def get_indexed_color(self, a, timeout=-1):
+        """
+        Get color number `a`.
+
+        Arguments:
+            a: color index to query.
+            timeout: millisecond timeout, same interpretation as in
+                self.guarded_query.
+
+        Return: RGBAColor instance.
+
+        Errors:
+            TerminalUninitializedError: if this instance's context has
+                not been entered.
+        """
+        return self.rgb_query([4, a], timeout)
+
+    def flush_input(self):
+        """
+        Discard any input that can be read at this moment.
+        """
+        while self.P.poll(0):
+            os.read(self.fd, 4096)
+
+    # Patterns matching unsigned decimal and hexadecimal integer
+    # literals
+    ndec = "[0-9]+"
+    nhex = "[0-9a-fA-F]+"
+
+    # The "guard" query and its response pattern.
+    q_guard = csi + "6n"
+
+    str_guard = "(.*)\033\\[{ndec};{ndec}R".format(**vars())
+    re_guard = re.compile(str_guard)
+
+    # This is what we expect the terminal's response to a query for a
+    # color to look like.  If we didn't care about urxvt, we could get
+    # away with a simpler implementation here, since xterm and vte seem
+    # to give pretty consistent and systematic responses.
+    str_rgb = ("\033\\]({ndec};)+rgba?:" +
+               "({nhex})/({nhex})/({nhex})(/({nhex}))?"
+              ).format(**vars())
+    re_rgb = re.compile(str_rgb)
+
+    def rgb_query(self, q, timeout=-1):
+        r"""
+        Query a color-valued terminal parameter.
+
+        See
+            http://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+
+        ("Operating System Controls") to see the various queries
+        supported by xterm.  Urxvt supports some, but not all, of them,
+        and has a number of its own (see man -s7 urxvt).
+
+        self.__enter__ must be called prior to calling this function, or
+        TerminalUninitializedError will be raised.
+
+        Arguments:
+            q: The query code as a sequence of nonnegative integers,
+                i.e., [q0, q1, ...] if the escape sequence in
+                pseudo-Python is
+
+                    "\033]{q0};{q1};...;?\007"
+
+            timeout: millisecond timeout, same interpretation as in
+                self.guarded_query.
+
+        Return: the color value as an RGBAColor instance. If the
+            terminal provides an unparseable (or no) response, then None
+            will be returned.
+
+        Errors:
+            TerminalUninitializedError: if this instance's context has
+                not been entered.
+        """
+        query = (self.osc +
+                 ';'.join([str(k) for k in q]) + ';?' +
+                 self.st)
+
+        try:
+            response = self.guarded_query(query, timeout)
+        except NoResponseError:
+            return None
+
+        m = self.re_rgb.match(response)
+
+        if not m:
+            self.num_errors += 1
+            return None
+
+        # (possibly overkill, since all terminals that reply seem to
+        # give 4-digit RGB components, in which case `nd' is 4 and `u'
+        # is 0xffff)
+        nd = len(m.group(4))
+        u = (1 << (nd << 2)) - 1
+
+        alpha = float(int(m.group(6), 16))/u if m.group(6) else 1.0
+        (r, g, b) = (int(m.group(i), 16)/u for i in [2, 3, 4])
+
+        return RGBAColor(r, g, b, alpha)
+
+    # If a terminal sees an escape sequence it doesn't like, it will
+    # simply ignore it. Also, it's hard to predict how long a terminal
+    # will take to respond to a query it does like. However, some
+    # escape sequences, like "\033[6n", will produce a predictable
+    # response on *most* (but not all) terminals, and this fact can be
+    # used to test for the absence of a response to a particular query
+    # on such terminals.
+
+    def guarded_query(self, q, timeout=-1, flush=True):
+        """
+        Send the terminal query string `q` and return the terminal's
+        response.
+
+        Arguments:
+            q: the query string to send to the terminal.
+            timeout: how many milliseconds to wait for a response, a
+                negative number meaning "infinite".
+            flush: whether to discard waiting input before sending the
+                query.  It usually makes sense to do this, but note that
+                the terminal may still send seemingly unsolicited data
+                (possibly in response to an earlier query) after the
+                input is flushed, so flushing the input does not
+                provide any guarantees.
+
+        Return: The terminal's response to the query as a string.
+
+        Errors:
+            NoResponseError: if the query times out.
+            TerminalUninitializedError: if this instance's context has
+                not been entered.
+        """
+        if not hasattr(self, "P"):
+            raise TerminalUninitializedError(self.fd)
+
+        query = q + self.q_guard
+
+        if self.screen_forward:
+            if os.getenv("TMUX"):
+                # We seem to be in a tmux session.  tmux can be used
+                # like a proxy to send queries to the actual terminal.
+                # The format of proxy query is:
+                #
+                #     \033Ptmux;{original query}\0\033\\
+                #
+                # Also, all the \033 of the original query must be
+                # escaped with \033.
+                query = ("\033Ptmux;" +
+                         query.replace("\033", "\033\033") +
+                         "\0\033\\")
+
+            elif os.getenv("STY"):
+                # We seem to be in a screen session.  Like tmux, screen
+                # can also send queries through to the terminal.  Unlike
+                # tmux, \033 does not need to be (cannot be???) escaped.
+                #
+                # XXX Is there a better way to "escape" string
+                # terminators?
+                query = ("\033P" +
+                         query.replace("\033\\", "\033\033\\\033P\\") +
+                         "\033\\")
+
+        if flush:
+            self.flush_input()
+
+        os.write(self.fd, query.encode())
+
+        response = ""
+
+        while self.P.poll(timeout):
+            response += os.read(self.fd, 4096).decode()
+            m = self.re_guard.match(response)
+            if m:
+                return m.group(1)
+        else:
+            self.num_errors += 1
+            raise NoResponseError(query)
