a5920d4d0ff22c70fee0100a6dc5a78a5708a333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:45 +0000

    Add package uidom/0.3a4/uidom-0.3a4-py3-none-any.whl

---
+++ packages/uidom/uidom-0.3a4-py3-none-any.whl/uidom/dom/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2022 uidom
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+
+from .htmlelement import *  # isort: skip
+from .htmldocument import *  # isort: skip
+
+from .icons import *  # isort: skip
+from .jinja import *  # isort: skip
+
+from .src import *  # isort: skip
+
+from .src.html_string import *  # isort: skip
+from .uniqueid import *  # isort: skip
+
+# https://github.com/microsoft/vscode/issues/35350#issuecomment-1093627529
+# here is the need to include isort skip.
---
+++ packages/uidom/uidom-0.3a4-py3-none-any.whl/uidom/dom/src/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 uidom
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+
+from .component import *
+from .csstags import *
+from .ext import *
+from .htmltags import *
+from .jinjatags import *
+from .svgtags import *
+
+from .xmltags import *  # isort: skip
+from .utils import *  # isort: skip
+from .dom_tag import *  # isort: skip
