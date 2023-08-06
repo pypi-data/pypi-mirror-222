77b6cff4eb729d679875ea8ae18089956c8561d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:34 +0000

    Add package python-config-server-client/2.0.0/python_config_server_client-2.0.0.tar.gz

---
+++ packages/python-config-server-client/python_config_server_client-2.0.0.tar.gz/python_config_server_client-2.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+
+    name = 'python_config_server_client', 
+
+    version = '2.0.0',
+    
+
+    description = 'A config_server_client package for python & play framework',
+ 
+    author = 'Jayant',
+    author_email = 'jayant.bodkurwar@turtlemint.com',
+    install_requires = [
+        'requests',
+    ],
+
+)
