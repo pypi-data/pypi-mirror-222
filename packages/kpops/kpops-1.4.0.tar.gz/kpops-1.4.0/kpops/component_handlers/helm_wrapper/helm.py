30cc31378c9c9f0f38aba4718a6102fe6873c85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:24 +0000

    Add package kpops/1.4.0/kpops-1.4.0.tar.gz

---
+++ packages/kpops/kpops-1.4.0.tar.gz/kpops-1.4.0/kpops/component_handlers/helm_wrapper/helm.py
@@ -0,0 +1,310 @@
+from __future__ import annotations
+
+import logging
+import re
+import subprocess
+import tempfile
+from collections.abc import Iterator
+from typing import Iterable
+
+import yaml
+
+from kpops.component_handlers.helm_wrapper.exception import ReleaseNotFoundException
+from kpops.component_handlers.helm_wrapper.model import (
+    HelmChart,
+    HelmConfig,
+    HelmTemplate,
+    HelmTemplateFlags,
+    HelmUpgradeInstallFlags,
+    RepoAuthFlags,
+    Version,
+)
+
+log = logging.getLogger("Helm")
+
+
+class Helm:
+    def __init__(self, helm_config: HelmConfig) -> None:
+        self._context = helm_config.context
+        self._debug = helm_config.debug
+        self._version = self.get_version()
+        if self._version.major != 3:
+            raise RuntimeError(
+                f"The supported Helm version is 3.x.x. The current Helm version is {self._version.major}.{self._version.minor}.{self._version.patch}"
+            )
+
+    def add_repo(
+        self,
+        repository_name: str,
+        repository_url: str,
+        repo_auth_flags: RepoAuthFlags = RepoAuthFlags(),
+    ) -> None:
+        command = [
+            "helm",
+            "repo",
+            "add",
+            repository_name,
+            repository_url,
+        ]
+
+        if repo_auth_flags.username and repo_auth_flags.password:
+            command.extend(
+                [
+                    "--username",
+                    repo_auth_flags.username,
+                    "--password",
+                    repo_auth_flags.password,
+                ]
+            )
+
+        command = Helm.__extend_tls_config(command, repo_auth_flags)
+
+        try:
+            self.__execute(command)
+        except Exception as e:
+            if (
+                len(e.args) == 1
+                and re.match(
+                    "Error: repository name (.*) already exists, please specify a different name",
+                    e.args[0],
+                )
+                is not None
+            ):
+                log.error(f"Could not add repository {repository_name}. {e}")
+            else:
+                raise e
+
+        if self._version.minor > 7:
+            self.__execute(["helm", "repo", "update", repository_name])
+        else:
+            self.__execute(["helm", "repo", "update"])
+
+    def upgrade_install(
+        self,
+        release_name: str,
+        chart: str,
+        dry_run: bool,
+        namespace: str,
+        values: dict,
+        flags: HelmUpgradeInstallFlags = HelmUpgradeInstallFlags(),
+    ) -> str:
+        """Prepares and executes the `helm upgrade --install` command"""
+        with tempfile.NamedTemporaryFile("w") as values_file:
+            yaml.safe_dump(values, values_file)
+
+            command = ["helm"]
+            command.extend(
+                [
+                    "upgrade",
+                    release_name,
+                    chart,
+                    "--install",
+                    f"--timeout={flags.timeout}",
+                    "--namespace",
+                    namespace,
+                    "--values",
+                    values_file.name,
+                ]
+            )
+            command = Helm.__extend_tls_config(command, flags.repo_auth_flags)
+
+            command = Helm.__enrich_upgrade_install_command(command, dry_run, flags)
+            return self.__execute(command)
+
+    def uninstall(
+        self,
+        namespace: str,
+        release_name: str,
+        dry_run: bool,
+    ) -> str | None:
+        """Prepares and executes the helm uninstall command"""
+        command = [
+            "helm",
+            "uninstall",
+            release_name,
+            "--namespace",
+            namespace,
+        ]
+        if dry_run:
+            command.append("--dry-run")
+        try:
+            return self.__execute(command)
+        except ReleaseNotFoundException:
+            log.warning(
+                f"Release with name {release_name} not found. Could not uninstall app."
+            )
+
+    def template(
+        self,
+        release_name: str,
+        chart: str,
+        namespace: str,
+        values: dict,
+        flags: HelmTemplateFlags = HelmTemplateFlags(),
+    ) -> str:
+        """From HELM: Render chart templates locally and display the output.
+
+        Any values that would normally be looked up or retrieved in-cluster will
+        be faked locally. Additionally, none of the server-side testing of chart
+        validity (e.g. whether an API is supported) is done.
+
+        :param str release_name: the release name for which the command is ran
+        :type release_name: str
+        :param chart: Helm chart to be templated
+        :type chart: str
+        :param namespace: The Kubernetes namespace the command should execute in
+        :type namespace: str
+        :param values: `values.yaml` to be used
+        :type values: dict
+        :param flags: the flags to be set for `helm template`, defaults to HelmTemplateFlags()
+        :type flags: HelmTemplateFlags, optional
+        :return: the output of `helm template`
+        :rtype: str
+        """
+        with tempfile.NamedTemporaryFile("w") as values_file:
+            yaml.safe_dump(values, values_file)
+            command = [
+                "helm",
+                "template",
+                release_name,
+                chart,
+                "--namespace",
+                namespace,
+                "--values",
+                values_file.name,
+            ]
+            command = Helm.__enrich_template_command(command, flags)
+            return self.__execute(command)
+
+    def get_manifest(self, release_name: str, namespace: str) -> Iterable[HelmTemplate]:
+        command = [
+            "helm",
+            "get",
+            "manifest",
+            release_name,
+            "--namespace",
+            namespace,
+        ]
+
+        try:
+            stdout = self.__execute(command=command)
+            return Helm.load_manifest(stdout)
+        except ReleaseNotFoundException:
+            return ()
+
+    def get_version(self) -> Version:
+        command = ["helm", "version", "--short"]
+        short_version = self.__execute(command)
+        version_match = re.search(r"^v(\d+(?:\.\d+){0,2})", short_version)
+        if version_match is None:
+            raise RuntimeError(
+                f"Could not parse the Helm version.\n\nHelm output:\n{short_version}"
+            )
+        version = map(int, version_match.group(1).split("."))
+        return Version(*version)
+
+    @staticmethod
+    def load_manifest(yaml_contents: str) -> Iterator[HelmTemplate]:
+        is_beginning: bool = False
+        template_name = None
+        current_yaml_doc: list[str] = []
+        for line in HelmChart(yaml_contents):
+            if line.startswith("---"):
+                is_beginning = True
+                if template_name and current_yaml_doc:
+                    yield HelmTemplate.load(template_name, "\n".join(current_yaml_doc))
+                    template_name = None
+                    current_yaml_doc.clear()
+            elif is_beginning:
+                template_name = HelmTemplate.parse_source(line)
+                is_beginning = False
+            else:
+                current_yaml_doc.append(line)
+
+    @staticmethod
+    def __enrich_template_command(
+        command: list[str],
+        helm_command_config: HelmTemplateFlags,
+    ) -> list[str]:
+        """
+        Enrich `self.template()` with the flags to be used for `helm template`
+
+        :param list[str] command: command that contains a call to
+            `helm template` with specified release name, chart and values
+        :param helm_command_config: flags to be set
+        :type helm_command_config: HelmTemplateFlags
+        :return: the enriched with flags `helm template`
+        :rtype: list[str]
+        """
+        if helm_command_config.api_version:
+            command.extend(["--api-versions", helm_command_config.api_version])
+        if helm_command_config.ca_file:
+            command.extend(["--ca-file", helm_command_config.ca_file])
+        if helm_command_config.cert_file:
+            command.extend(["--cert-file", helm_command_config.cert_file])
+        if helm_command_config.version:
+            command.extend(["--version", helm_command_config.version])
+        return command
+
+    @staticmethod
+    def __extend_tls_config(
+        command: list[str], repo_auth_flags: RepoAuthFlags
+    ) -> list[str]:
+        if repo_auth_flags.ca_file:
+            command.extend(["--ca-file", str(repo_auth_flags.ca_file)])
+        if repo_auth_flags.insecure_skip_tls_verify:
+            command.append("--insecure-skip-tls-verify")
+        return command
+
+    @staticmethod
+    def __enrich_upgrade_install_command(
+        command: list[str],
+        dry_run: bool,
+        helm_command_config: HelmUpgradeInstallFlags,
+    ) -> list[str]:
+        if helm_command_config.create_namespace:
+            command.append("--create-namespace")
+        if dry_run:
+            command.append("--dry-run")
+        if helm_command_config.force:
+            command.append("--force")
+        if helm_command_config.wait:
+            command.append("--wait")
+        if helm_command_config.wait_for_jobs:
+            command.append("--wait-for-jobs")
+        if helm_command_config.version:
+            command.extend(["--version", helm_command_config.version])
+        return command
+
+    def __execute(self, command: list[str]) -> str:
+        command = self.__set_global_flags(command)
+        log.debug(f"Executing {' '.join(command)}")
+        process = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        Helm.parse_helm_command_stderr_output(process.stderr)
+        log.debug(process.stdout)
+        return process.stdout
+
+    def __set_global_flags(self, command: list[str]) -> list[str]:
+        if self._context:
+            log.debug(f"Changing the Kubernetes context to {self._context}")
+            command.extend(["--kube-context", self._context])
+        if self._debug:
+            log.debug("Enabling verbose mode.")
+            command.append("--debug")
+        return command
+
+    @staticmethod
+    def parse_helm_command_stderr_output(stderr: str) -> None:
+        for line in stderr.splitlines():
+            lower = line.lower()
+            if "release: not found" in lower:
+                raise ReleaseNotFoundException()
+            elif "error" in lower:
+                raise RuntimeError(stderr)
+            elif "warning" in lower:
+                log.warning(line)
---
+++ packages/kpops/kpops-1.4.0.tar.gz/kpops-1.4.0/kpops/component_handlers/helm_wrapper/model.py
@@ -0,0 +1,181 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Iterator
+
+import yaml
+from pydantic import BaseModel, Field
+
+from kpops.component_handlers.helm_wrapper.exception import ParseError
+from kpops.utils.docstring import describe_attr
+from kpops.utils.pydantic import CamelCaseConfig, DescConfig
+
+
+class HelmDiffConfig(BaseModel):
+    ignore: set[str] = Field(
+        default_factory=set,
+        description="Set of keys that should not be checked.",
+        example="- name\n- imageTag",
+    )
+
+
+class RepoAuthFlags(BaseModel):
+    """Authorisation-related flags for `helm repo`
+
+    :param username: Username, defaults to None
+    :type username: str, optional
+    :param password: Password, defaults to None
+    :type password: str, optional
+    :param ca_file: Certificate file, defaults to None
+    :type ca_file: Path, optional
+    :param insecure_skip_tls_verify: If true, Kubernetes API server's certificate will not be checked for validity
+        , defaults to False
+    :type insecure_skip_tls_verify: bool, optional
+    """
+
+    username: str | None = Field(
+        default=None, description=describe_attr("username", __doc__)
+    )
+    password: str | None = Field(
+        default=None, description=describe_attr("password", __doc__)
+    )
+    ca_file: Path | None = Field(
+        default=None, description=describe_attr("ca_file", __doc__)
+    )
+    insecure_skip_tls_verify: bool = Field(
+        default=False, description=describe_attr("insecure_skip_tls_verify", __doc__)
+    )
+
+    class Config(CamelCaseConfig, DescConfig):
+        pass
+
+
+class HelmRepoConfig(BaseModel):
+    """Helm repository configuration
+
+    :param repository_name: Name of the Helm repository
+    :type repository_name: str
+    :param url: URL to the Helm repository
+    :type url: str
+    :param repo_auth_flags: Authorisation-related flags
+    :type repo_auth_flags: RepoAuthFlags
+    """
+
+    repository_name: str = Field(
+        default=..., description=describe_attr("repository_name", __doc__)
+    )
+    url: str = Field(default=..., description=describe_attr("url", __doc__))
+    repo_auth_flags: RepoAuthFlags = Field(
+        default=RepoAuthFlags(), description=describe_attr("repo_auth_flags", __doc__)
+    )
+
+    class Config(CamelCaseConfig, DescConfig):
+        pass
+
+
+class HelmConfig(BaseModel):
+    context: str | None = Field(
+        default=None,
+        description="Set the name of the kubeconfig context. (--kube-context)",
+        example="dev-storage",
+    )
+    debug: bool = Field(
+        default=False,
+        description="Run Helm in Debug mode.",
+    )
+
+
+@dataclass
+class HelmUpgradeInstallFlags:
+    create_namespace: bool = False
+    force: bool = False
+    repo_auth_flags: RepoAuthFlags = field(default_factory=RepoAuthFlags)
+    timeout: str = "5m0s"
+    version: str | None = None
+    wait: bool = True
+    wait_for_jobs: bool = False
+
+
+@dataclass
+class HelmTemplateFlags:
+    api_version: str | None = None
+    ca_file: str | None = None
+    cert_file: str | None = None
+    version: str | None = None
+
+
+HELM_SOURCE_PREFIX = "# Source: "
+
+
+@dataclass
+class HelmTemplate:
+    filepath: str
+    template: dict
+
+    @staticmethod
+    def parse_source(source: str) -> str:
+        """Parse source path from comment at the beginning of the YAML doc.
+
+        :Example:
+
+        # Source: chart/templates/serviceaccount.yaml
+        """
+        if not source.startswith(HELM_SOURCE_PREFIX):
+            raise ParseError("Not a valid Helm template source")
+        return source.removeprefix(HELM_SOURCE_PREFIX).strip()
+
+    @classmethod
+    def load(cls, filepath: str, content: str):
+        template = yaml.load(content, yaml.Loader)
+        return cls(filepath, template)
+
+
+# Indicates the beginning of `NOTES:` section in the output of `helm install` or
+# `helm upgrade`
+HELM_NOTES = "\n\nNOTES:\n"
+HELM_MANIFEST = "MANIFEST:\n"
+
+
+@dataclass(frozen=True)
+class HelmChart:
+    content: str
+
+    def __iter__(self) -> Iterator[str]:
+        yield from self.manifest.splitlines()
+        yield "---"  # add final divider to make parsing easier
+
+    @property
+    def manifest(self) -> str:
+        """
+        Reads the manifest section of Helm stdout. `helm upgrade --install` output message contains three sections
+        in the following order:
+
+        - HOOKS
+        - MANIFEST
+        - NOTES (optional)
+
+        The content of the manifest is used to create the diff. If a NOTES.txt exists in the Helm chart, the NOTES
+        section will be included in the output.
+
+        It is important to note that the `helm get manifest` command only returns the manifests without the MANIFEST
+        header in the stdout. Instead, the output starts with `---`.
+
+        :return: The content of the manifest section
+        """
+        manifest_start = (
+            self.content.index(HELM_MANIFEST) + len(HELM_MANIFEST)
+            if HELM_MANIFEST in self.content
+            else self.content.index("---")
+        )
+
+        manifest_end = (
+            self.content.index(HELM_NOTES) if HELM_NOTES in self.content else -1
+        )
+
+        return self.content[manifest_start:manifest_end]
+
+
+@dataclass(frozen=True)
+class Version:
+    major: int
+    minor: int = 0
+    patch: int = 0
