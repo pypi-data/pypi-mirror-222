c00877f57bb0e0f022425eb90dbe883edfb86d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:22 +0000

    Add package budgetinvoice/0.0.3/budgetinvoice-0.0.3.tar.gz

---
+++ packages/budgetinvoice/budgetinvoice-0.0.3.tar.gz/budgetinvoice-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='budgetinvoice',
+    version='0.0.3',
+    url='https://github.com/dveldhoen/budgetinvoice-pip',
+    author='Danny Veldhoen',
+    author_email='info@budgetinvoice.com',
+    description='',
+    packages=find_packages(),  
+    install_requires=[],  
+)
