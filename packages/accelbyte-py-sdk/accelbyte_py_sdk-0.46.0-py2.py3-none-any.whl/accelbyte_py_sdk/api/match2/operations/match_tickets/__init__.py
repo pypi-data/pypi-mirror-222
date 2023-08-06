5103e67bf10ac253a969ced954d0cd1ee1497e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:55 +0000

    Add package accelbyte-py-sdk/0.46.0/accelbyte_py_sdk-0.46.0-py2.py3-none-any.whl

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.46.0-py2.py3-none-any.whl/accelbyte_py_sdk/api/match2/operations/match_tickets/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Match Service V2."""
+
+__version__ = "2.7.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_match_ticket import CreateMatchTicket
+from .delete_match_ticket import DeleteMatchTicket
+from .match_ticket_details import MatchTicketDetails
---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.46.0-py2.py3-none-any.whl/accelbyte_py_sdk/api/platform/models/debit_request.py
@@ -0,0 +1,150 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+# AccelByte Gaming Services Platform Service (4.31.1)
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+from __future__ import annotations
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from ....core import Model
+
+
+class DebitRequest(Model):
+    """A DTO for wallet's debit (DebitRequest)
+
+    Properties:
+        amount: (amount) REQUIRED int
+
+        reason: (reason) OPTIONAL str
+    """
+
+    # region fields
+
+    amount: int  # REQUIRED
+    reason: str  # OPTIONAL
+
+    # endregion fields
+
+    # region with_x methods
+
+    def with_amount(self, value: int) -> DebitRequest:
+        self.amount = value
+        return self
+
+    def with_reason(self, value: str) -> DebitRequest:
+        self.reason = value
+        return self
+
+    # endregion with_x methods
+
+    # region to methods
+
+    def to_dict(self, include_empty: bool = False) -> dict:
+        result: dict = {}
+        if hasattr(self, "amount"):
+            result["amount"] = int(self.amount)
+        elif include_empty:
+            result["amount"] = 0
+        if hasattr(self, "reason"):
+            result["reason"] = str(self.reason)
+        elif include_empty:
+            result["reason"] = ""
+        return result
+
+    # endregion to methods
+
+    # region static methods
+
+    @classmethod
+    def create(
+        cls, amount: int, reason: Optional[str] = None, **kwargs
+    ) -> DebitRequest:
+        instance = cls()
+        instance.amount = amount
+        if reason is not None:
+            instance.reason = reason
+        return instance
+
+    @classmethod
+    def create_from_dict(cls, dict_: dict, include_empty: bool = False) -> DebitRequest:
+        instance = cls()
+        if not dict_:
+            return instance
+        if "amount" in dict_ and dict_["amount"] is not None:
+            instance.amount = int(dict_["amount"])
+        elif include_empty:
+            instance.amount = 0
+        if "reason" in dict_ and dict_["reason"] is not None:
+            instance.reason = str(dict_["reason"])
+        elif include_empty:
+            instance.reason = ""
+        return instance
+
+    @classmethod
+    def create_many_from_dict(
+        cls, dict_: dict, include_empty: bool = False
+    ) -> Dict[str, DebitRequest]:
+        return (
+            {k: cls.create_from_dict(v, include_empty=include_empty) for k, v in dict_}
+            if dict_
+            else {}
+        )
+
+    @classmethod
+    def create_many_from_list(
+        cls, list_: list, include_empty: bool = False
+    ) -> List[DebitRequest]:
+        return (
+            [cls.create_from_dict(i, include_empty=include_empty) for i in list_]
+            if list_
+            else []
+        )
+
+    @classmethod
+    def create_from_any(
+        cls, any_: any, include_empty: bool = False, many: bool = False
+    ) -> Union[DebitRequest, List[DebitRequest], Dict[Any, DebitRequest]]:
+        if many:
+            if isinstance(any_, dict):
+                return cls.create_many_from_dict(any_, include_empty=include_empty)
+            elif isinstance(any_, list):
+                return cls.create_many_from_list(any_, include_empty=include_empty)
+            else:
+                raise ValueError()
+        else:
+            return cls.create_from_dict(any_, include_empty=include_empty)
+
+    @staticmethod
+    def get_field_info() -> Dict[str, str]:
+        return {
+            "amount": "amount",
+            "reason": "reason",
+        }
+
+    @staticmethod
+    def get_required_map() -> Dict[str, bool]:
+        return {
+            "amount": True,
+            "reason": False,
+        }
+
+    # endregion static methods
