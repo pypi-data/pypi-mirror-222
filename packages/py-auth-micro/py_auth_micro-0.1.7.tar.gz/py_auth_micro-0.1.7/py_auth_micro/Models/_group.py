a41dd7dd431a928d3a14b5a9080b6d08c7383f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:37 +0000

    Add package py-auth-micro/0.1.7/py_auth_micro-0.1.7.tar.gz

---
+++ packages/py-auth-micro/py_auth_micro-0.1.7.tar.gz/py_auth_micro-0.1.7/py_auth_micro/Models/_group.py
@@ -0,0 +1,20 @@
+from tortoise import fields
+from tortoise.models import Model
+
+from py_auth_micro.Models._user import User
+
+
+class Group(Model):
+    """Database Model Representing a Group.
+
+    Groups are used for Permission.
+
+    Attributes:
+        name (str): Name of the Usergroup
+        users (list[User]): List of Users in this group
+    """
+    name: str = fields.CharField(unique=True, max_length=50, pk=True)
+    users: fields.ManyToManyRelation[User]
+
+    def __str__(self):
+        return self.name
