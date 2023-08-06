3a0e7035a8780cfc050d0f81c0f42db98ac45d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:14 +0000

    Add package hecdbetz/2.0/hecdbetz-2.0.tar.gz

---
+++ packages/hecdbetz/hecdbetz-2.0.tar.gz/hecdbetz-2.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import  setup
+import setuptools
+packages = ['hecdbetz']# 唯一的包名，自己取名
+setup(name='hecdbetz',
+	version='2.0',
+	author='zmw',
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
