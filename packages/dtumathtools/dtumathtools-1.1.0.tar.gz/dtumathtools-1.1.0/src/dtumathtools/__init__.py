c342a56105a0b0d40697b7878099f18ffd5af276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:16 +0000

    Add package dtumathtools/1.1.0/dtumathtools-1.1.0.tar.gz

---
+++ packages/dtumathtools/dtumathtools-1.1.0.tar.gz/dtumathtools-1.1.0/src/dtumathtools/__init__.py
@@ -0,0 +1,4 @@
+from . import dtuplot
+from . import dtutools
+
+__all__ = ["dtuplot", "dtutools"]
---
+++ packages/dtumathtools/dtumathtools-1.1.0.tar.gz/dtumathtools-1.1.0/src/dtumathtools/dtuplot/asdfasdf.py
@@ -0,0 +1,7 @@
+#%%
+from sympy import *
+from dtumathtools import *
+
+# galt med range...
+dtuplot.quiver(Matrix([1, 2, 3]), Matrix([0,10,100]), backend=dtuplot.MAB)
+# %%
