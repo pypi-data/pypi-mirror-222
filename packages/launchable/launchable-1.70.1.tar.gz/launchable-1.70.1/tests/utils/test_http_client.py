157d908d0a48addbb676e88f2cad40aae21062f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:27 +0000

    Add package launchable/1.70.1/launchable-1.70.1.tar.gz

---
+++ packages/launchable/launchable-1.70.1.tar.gz/launchable-1.70.1/tests/utils/test_http_client.py
@@ -0,0 +1,45 @@
+import os
+import platform
+from unittest import TestCase, mock
+
+from launchable.utils.http_client import LaunchableClient
+from launchable.version import __version__
+
+
+class LaunchableClientTest(TestCase):
+    @mock.patch.dict(
+        os.environ,
+        {"LAUNCHABLE_ORGANIZATION": "launchableinc", "LAUNCHABLE_WORKSPACE": "test"},
+        clear=True,
+    )
+    def test_header(self):
+        cli = LaunchableClient("/test")
+        self.assertEqual(cli._headers(True), {
+            'Content-Encoding': 'gzip',
+            'Content-Type': 'application/json',
+            "User-Agent": "Launchable/{} (Python {}, {})".format(
+                __version__,
+                platform.python_version(),
+                platform.platform(),
+            ),
+        })
+
+        self.assertEqual(cli._headers(False), {
+            'Content-Type': 'application/json',
+            "User-Agent": "Launchable/{} (Python {}, {})".format(
+                __version__,
+                platform.python_version(),
+                platform.platform(),
+            ),
+        })
+
+        cli = LaunchableClient("/test", test_runner="dummy")
+        self.assertEqual(cli._headers(False), {
+            'Content-Type': 'application/json',
+            "User-Agent": "Launchable/{} (Python {}, {}) TestRunner/{}".format(
+                __version__,
+                platform.python_version(),
+                platform.platform(),
+                "dummy",
+            ),
+        })
