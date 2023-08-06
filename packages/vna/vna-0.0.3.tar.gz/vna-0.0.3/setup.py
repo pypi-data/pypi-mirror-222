bb4c6535f00ae521902c99b6104f55fd8278edd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:26 +0000

    Add package vna/0.0.3/vna-0.0.3.tar.gz

---
+++ packages/vna/vna-0.0.3.tar.gz/vna-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import distutils.sysconfig
+
+setup(
+    name='vna',
+    version='0.0.3',
+    description='Instrument control library for the PicoVNA 106 and PicoVNA 108. Please note that additional libraries are required, not installed by this package. See the Python Programming Manual for details.',
+    url='http://picotech.com',
+    author='AAI Robotics Ltd',
+    author_email='help@aairobotics.com',
+    license='MIT',
+    packages=["vna"],
+    include_package_data=True,
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+)
