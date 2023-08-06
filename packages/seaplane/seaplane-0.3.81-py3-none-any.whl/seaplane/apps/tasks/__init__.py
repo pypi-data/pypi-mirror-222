0d607fbe2d8a04a2814c619a15c7b2348c5656ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:56 +0000

    Add package seaplane/0.3.81/seaplane-0.3.81-py3-none-any.whl

---
+++ packages/seaplane/seaplane-0.3.81-py3-none-any.whl/seaplane/apps/tasks/__init__.py
@@ -0,0 +1,7 @@
+from .substation import SubstationTask
+from .openai import OpenAI
+from .vectordb import Store
+from .replicate import Replicate
+from .sql import Sql
+
+__all__ = ("SubstationTask", "OpenAI", "Store", "Replicate", "Sql")
