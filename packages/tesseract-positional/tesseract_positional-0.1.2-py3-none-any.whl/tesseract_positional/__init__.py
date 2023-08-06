9d32b948b13b1ba71ea9d8bef476e986ac532de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:57 +0000

    Add package tesseract-positional/0.1.2/tesseract_positional-0.1.2-py3-none-any.whl

---
+++ packages/tesseract-positional/tesseract_positional-0.1.2-py3-none-any.whl/tesseract_positional/__init__.py
@@ -0,0 +1,4 @@
+from tesseract_positional.positional_ocr import positional_ocr
+import tesseract_positional.cmd_line
+import pkg_resources
+__version__ = pkg_resources.get_distribution('tesseract_positional').version
