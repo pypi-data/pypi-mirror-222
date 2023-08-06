2a8efa9ec794b4967c0feec7e3dda2dec1913ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:09 +0000

    Add package ctfcli/0.0.13/ctfcli-0.0.13.tar.gz

---
+++ packages/ctfcli/ctfcli-0.0.13.tar.gz/ctfcli-0.0.13/ctfcli/utils/git.py
@@ -0,0 +1,37 @@
+import subprocess
+
+
+def get_git_repo_head_branch(repo):
+    """
+    A helper method to get the reference of the HEAD branch of a git remote repo.
+    https://stackoverflow.com/a/41925348
+    """
+    out = subprocess.check_output(
+        ["git", "ls-remote", "--symref", repo, "HEAD"]
+    ).decode()
+    head_branch = out.split()[1]
+    if head_branch.startswith("refs/heads/"):
+        head_branch = head_branch[11:]
+    return head_branch
+
+
+def check_if_dir_is_inside_git_repo(dir=None):
+    """
+    Checks whether a given directory is inside of a git repo.
+    """
+    try:
+        out = (
+            subprocess.check_output(
+                ["git", "rev-parse", "--is-inside-work-tree"],
+                cwd=dir,
+                stderr=subprocess.DEVNULL,
+            )
+            .decode()
+            .strip()
+        )
+        print(out)
+        if out == "true":
+            return True
+        return False
+    except subprocess.CalledProcessError:
+        return False
---
+++ packages/ctfcli/ctfcli-0.0.13.tar.gz/ctfcli-0.0.13/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+with open("ctfcli/__init__.py") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+
+def read(fname):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), fname), "r") as fp:
+            return fp.read().strip()
+    except IOError:
+        return ""
+
+
+setup(
+    name="ctfcli",
+    version=version,
+    author="Kevin Chung",
+    author_email="kchung@ctfd.io",
+    license="Apache 2.0",
+    description="Tool for creating and running Capture The Flag competitions",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    keywords=["ctf"],
+    classifiers=[],
+    zip_safe=False,
+    install_requires=[
+        "cookiecutter==2.1.1",
+        "click==7.0",
+        "fire==0.2.1",
+        "pyyaml>=6.0.1",
+        "Pygments==2.7.4",
+        "requests==2.28.1",
+        "colorama==0.4.6",
+        "appdirs==1.4.3",
+        "python-frontmatter==1.0.0",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["ctf = ctfcli.__main__:main"]},
+)
