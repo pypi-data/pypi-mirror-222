6c8bbd4104cbb510745b7ee1a2327eebb3f3cbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:37 +0000

    Add package invoice-parser/0.0.1/invoice_parser-0.0.1.tar.gz

---
+++ packages/invoice-parser/invoice_parser-0.0.1.tar.gz/invoice_parser-0.0.1/invoice_parser/imports.py
@@ -0,0 +1,10 @@
+from dreamai.core import *
+from dreamai.vision import *
+from dreamai.imports import *
+
+from langchain_ray.utils import *
+from langchain_ray.chains import *
+from langchain_ray.imports import *
+
+import pdfplumber
+from pypdf import PdfReader
