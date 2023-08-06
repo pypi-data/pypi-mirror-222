2ec176e560fab17ba75cb4a26d508274c1e99ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:32 +0000

    Add package htagweb/0.5.0/htagweb-0.5.0-py3-none-any.whl

---
+++ packages/htagweb/htagweb-0.5.0-py3-none-any.whl/htagweb/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2023 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htagweb
+# #############################################################################
+
+from .webbase import WebServer
+from .webbase import WebServerWS
+
+from .htagserver import HtagServer  # a completly different beast.
+
+__all__= ["WebServer","WebServerWS","HtagServer"]
+
+__version__ = "0.5.0" # auto updated
