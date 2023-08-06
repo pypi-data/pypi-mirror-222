4d4a3c3ae424ffa935b21bd84464c7b6b07f401d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:29 +0000

    Add package amatak-shop/1.0.4/amatak_shop-1.0.4-py3-none-any.whl

---
+++ packages/amatak-shop/amatak_shop-1.0.4-py3-none-any.whl/amatak_shop/models/refund.py
@@ -0,0 +1,28 @@
+"""
+AutoStore( Automotive Online Shop).
+We launch this business Store live at www.autocare7.com
+Copyright© Amatak Holdings Pty Ltd licensed under the MIT Agreement.
+If you interesting to be part of this project pleaese contact:
+Rony MAN <amatak.io@outlook.com>
+for business <www.amatak.io>
+OpenSource <www.amatak.org>
+"""
+from django.db.models.signals import post_save
+from django.conf import settings
+from django.db import models
+from django.db.models import Sum
+from django.shortcuts import reverse
+from django_countries.fields import CountryField
+from amatak_shop.models.order import Order
+
+
+
+class Refund(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    reason = models.TextField()
+    accepted = models.BooleanField(default=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.pk}"
+
---
+++ packages/amatak-shop/amatak_shop-1.0.4-py3-none-any.whl/eCommerce/settings/production.py
@@ -0,0 +1,34 @@
+"""
+AutoStore( Automotive Online Shop).
+We launch this business Store live at www.autocare7.com
+Copyright© Amatak Holdings Pty Ltd licensed under the MIT Agreement.
+If you interesting to be part of this project pleaese contact:
+Rony MAN <amatak.io@outlook.com>
+for business <www.amatak.io>
+OpenSource <www.amatak.org>
+"""
+from .base import *
+
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = ['ip-address', 'www.shop.autocare7.com']
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': ''
+    }
+}
+
+STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
