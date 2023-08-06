9e6c7034c61fb6a43be0981fa7a89ce50e1d84b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:25 +0000

    Add package llm-blocks/0.2.4/llm-blocks-0.2.4.tar.gz

---
+++ packages/llm-blocks/llm-blocks-0.2.4.tar.gz/llm-blocks-0.2.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="llm-blocks",
+    version="0.2.4",
+    author="Jamie Voynow",
+    author_email="voynow99@gmail.com",
+    description="Simple interface for creating and managing LLM chains",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/voynow/llm-blocks",
+    packages=find_packages(),
+    install_requires=[
+        'langchain',
+        'python-dotenv',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
