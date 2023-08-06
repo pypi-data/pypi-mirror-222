e9d132435a6e326a5d6a0ec879a501d4c04d9f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:50 +0000

    Add package neolibrary/0.1.3b0/neolibrary-0.1.3b0-py3-none-any.whl

---
+++ packages/neolibrary/neolibrary-0.1.3b0-py3-none-any.whl/neolibrary/utils.py
@@ -0,0 +1,31 @@
+from typing import Callable
+from functools import wraps
+from time import time
+from neolibrary.monitoring.logger import NeoLogger
+
+logger = NeoLogger(__name__).get_logger()
+
+
+
+def timer(orig_func: Callable):
+    """This is custom timer decorator.
+    Parameters
+    ----------
+    orig_func : object
+        The `orig_func` is the python function which is decorated.
+    Returns
+    -------
+    type
+        elapsed runtime for the function.
+
+    """
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = orig_func(*args, **kwargs)
+        t2 = time() - t1
+        logger.info('Runtime for {}: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
