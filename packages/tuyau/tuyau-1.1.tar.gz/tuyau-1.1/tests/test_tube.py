ae90e2592ade7b806b61634c82e8170ea8bdfa50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:10 +0000

    Add package tuyau/1.1/tuyau-1.1.tar.gz

---
+++ packages/tuyau/tuyau-1.1.tar.gz/tuyau-1.1/tests/test_tube.py
@@ -0,0 +1,31 @@
+"""Simple test."""
+
+
+from tuyau import Tube
+
+
+def add_one(number: int) -> int:
+    """Simple steps, add one to number."""
+    return number + 1
+
+
+def test_simple_send() -> None:
+    """Test sending values through the Tuyau. with .send."""
+    _ret: list[int] = []
+
+    tuyau = Tube(*[add_one] * 100, lambda v: _ret.append(v))
+    for i in range(1, 4):
+        tuyau.send(i)
+
+    assert _ret == [101, 102, 103]  # noqa: S101
+
+
+def test_simple_call() -> None:
+    """Test calling the Tuyau directly with values. call directly."""
+    _ret: list[int] = []
+
+    tuyau = Tube(*[add_one] * 100, lambda v: _ret.append(v))
+    for i in range(1, 4):
+        tuyau(i)
+
+    assert _ret == [101, 102, 103]  # noqa: S101
