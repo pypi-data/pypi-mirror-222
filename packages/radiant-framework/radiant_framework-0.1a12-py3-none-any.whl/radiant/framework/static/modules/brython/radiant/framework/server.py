a92fba615ce8da01aaadc65bf1326e7251250305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:04 +0000

    Add package radiant-framework/0.1a12/radiant_framework-0.1a12-py3-none-any.whl

---
+++ packages/radiant-framework/radiant_framework-0.1a12-py3-none-any.whl/radiant/framework/static/modules/brython/radiant/framework/server.py
@@ -0,0 +1,53 @@
+import os
+
+from browser import html, document, timer
+from .utils import LocalInterpreter
+import inspect
+from string import ascii_lowercase
+import random
+import logging
+import json
+
+RadiantServer = None
+
+
+# # ----------------------------------------------------------------------
+# def delay(t):
+    # """"""
+    # def wrap(fn):
+        # def inset(*args, **kwargs):
+            # print(f'DELAYING: {t}')
+            # return timer.set_timeout(lambda: fn(*args, **kwargs), t)
+        # return inset
+    # return wrap
+
+
+########################################################################
+class RadiantAPI:
+    """"""
+
+    # ----------------------------------------------------------------------
+    def __init__(self, class_, python=None, **kwargs):
+        """"""
+        for module, class_, endpoint in python:
+            if module and module != 'None':
+                setattr(self, class_, LocalInterpreter(endpoint=endpoint))
+
+        self.body = document.select_one('body')
+        self.head = document.select_one('head')
+
+    # ----------------------------------------------------------------------
+    def add_css_file(self, file):
+        """"""
+        document.select('head')[0] <= html.LINK(
+            href=os.path.join('root', file), type='text/css', rel='stylesheet')
+
+    # # ----------------------------------------------------------------------
+    # def on_load(self, callback, evt='DOMContentLoaded'):
+        # """"""
+        # logging.warning('#' * 30)
+        # logging.warning('#' * 30)
+        # document.addEventListener('load', callback)
+        # logging.warning('#' * 30)
+        # logging.warning('#' * 30)
+
