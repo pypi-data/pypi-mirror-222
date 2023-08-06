7d5889526871a9c71c4078f99accfe1086eb20b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:33 +0000

    Add package testjbpackage/1.0.0/testjbpackage-1.0.0.tar.gz

---
+++ packages/testjbpackage/testjbpackage-1.0.0.tar.gz/testjbpackage-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+
+    name = 'testjbpackage', 
+
+    version = '1.0.0',
+    
+
+    description = 'A config_server_client package for python framework',
+ 
+    author = 'Jayant',
+    author_email = 'jayantreddybodkurwar@gmail.com',
+    install_requires = [
+        'requests',
+    ],
+
+)
