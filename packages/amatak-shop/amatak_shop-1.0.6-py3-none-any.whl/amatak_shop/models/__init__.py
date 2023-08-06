ab6378725601bd697292cc15269fa27e3164e51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:35 +0000

    Add package amatak-shop/1.0.6/amatak_shop-1.0.6-py3-none-any.whl

---
+++ packages/amatak-shop/amatak_shop-1.0.6-py3-none-any.whl/amatak_shop/models/__init__.py
@@ -0,0 +1,16 @@
+"""
+Amatak Online Shop
+Copyright© Amatak Holdings Pty Ltd licensed under the MIT Agreement.
+If you interesting to be part of this project pleaese contact:
+Rony MAN <amatak.io@outlook.com>
+for business <www.amatak.io>
+OpenSource <www.amatak.org>
+"""
+from amatak_shop.models.address import *
+from amatak_shop.models.category import *
+from amatak_shop.models.coupon import *
+from amatak_shop.models.item import *
+from amatak_shop.models.order import *
+from amatak_shop.models.payment import *
+from amatak_shop.models.refund import *
+from amatak_shop.models.user_profile import *
---
+++ packages/amatak-shop/amatak_shop-1.0.6-py3-none-any.whl/amatak_shop/views/logout.py
@@ -0,0 +1,20 @@
+"""
+Amatak Online Shop
+Copyright© Amatak Holdings Pty Ltd licensed under the MIT Agreement.
+If you interesting to be part of this project pleaese contact:
+Rony MAN <amatak.io@outlook.com>
+for business <www.amatak.io>
+OpenSource <www.amatak.org>
+"""
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+
+
+@login_required
+def LogoutView(request):
+    return render(request, "amatak_shop/account/logout.html",)
