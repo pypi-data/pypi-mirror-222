ab77cc4c8299a418c2129f55a0d091ff1b705956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:56 +0000

    Add package SMAGrader/0.1.11/SMAGrader-0.1.11.tar.gz

---
+++ packages/SMAGrader/SMAGrader-0.1.11.tar.gz/SMAGrader-0.1.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="SMAGrader",
+    packages=find_packages(include=["SMAGrader"]),
+    version="0.1.11",
+    description="Library for social media analysis course autograder",
+    author="arisharma",
+    license="MIT",
+    install_requires=["numpy", "pandas", "nltk", "praw"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest==4.4.1"],
+)
