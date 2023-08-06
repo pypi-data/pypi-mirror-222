7ccdf050ef95ef2cb754722d896dcf2c286d63f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:34 +0000

    Add package tnnt-discordbot-cogs/0.5.0/tnnt_discordbot_cogs-0.5.0-py3-none-any.whl

---
+++ packages/tnnt-discordbot-cogs/tnnt_discordbot_cogs-0.5.0-py3-none-any.whl/tnnt_discordbot_cogs/__init__.py
@@ -0,0 +1,11 @@
+"""
+Application init
+"""
+
+# Standard Library
+from importlib import metadata
+
+__version__ = metadata.version(distribution_name="tnnt-discordbot-cogs")
+__title__ = "TN-NT Discordbot Cogs"
+
+del metadata
