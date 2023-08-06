5c9cbada7241dd0918039526f5fa244638f4df9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:18 +0000

    Add package many-abis/0.1.8/many_abis-0.1.8.tar.gz

---
+++ packages/many-abis/many_abis-0.1.8.tar.gz/many_abis-0.1.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='many_abis',
+    # packages=['many_abis'],
+    packages=find_packages(where="many_abis"),
+    package_dir={'many_abis': 'many_abis'},
+    package_data={'': ["assets/*"]},
+    version='0.1.8',
+    license='MIT',
+    description='A simple way to get different DEXs abis for block chains.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Yong',
+    author_email='ackness8@gmail.com',
+    url='https://github.com/ackness/many_abis',
+    keywords=['abi', 'dex', 'block chain', 'bsc', 'eth', 'matic', 'heco',
+              'kcc', 'pancakeswap', 'mdex', 'quickswap', 'uniswap', 'koffeeswap'],
+
+    install_requires=[
+        "eth_utils",
+        "addict"
+    ],
+    python_requires='>=3.8',
+
+)
