912cea6848613821df6c7320746345023b031827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:09 +0000

    Add package gitlab-sdk/0.2.4/gitlab_sdk-0.2.4.tar.gz

---
+++ packages/gitlab-sdk/gitlab_sdk-0.2.4.tar.gz/gitlab_sdk-0.2.4/setup.py
@@ -0,0 +1,17 @@
+"""
+gitlab_sdk setup module.
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name="gitlab_sdk",
+    version="0.2.4",
+    description="Client side Python SDK for GitLab Application services",
+    author="GitLab",
+    url="https://gitlab.com/gitlab-org/analytics-section/product-analytics/gl-application-sdk-python",
+    author_email="gitlab_rubygems@gitlab.com",
+    packages=find_packages(),
+    install_requires=[
+        "snowplow_tracker>=1.0.0",
+    ],
+)
