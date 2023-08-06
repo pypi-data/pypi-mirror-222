bcb52995dec6891dfcd939c3a1f5e786d2b1730c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:30 +0000

    Add package knowledge-graph/0.0.30/knowledge-graph-0.0.30.tar.gz

---
+++ packages/knowledge-graph/knowledge-graph-0.0.30.tar.gz/knowledge-graph-0.0.30/setup.py
@@ -0,0 +1,38 @@
+import subprocess
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    _LONG_DESCRIPTION = fh.read()
+
+knowledge_graph_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+if "-" in knowledge_graph_version:
+    v, i, s = knowledge_graph_version.split("-")
+    knowledge_graph_version = v + "+" + i + ".git." + s
+
+
+setuptools.setup(
+    name="knowledge-graph",
+    version=knowledge_graph_version,
+    python_requires=">=3.11",
+    description="A library to store data in a knowledge graph",
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Hyro AI",
+    author_email="contact@hyro.ai",
+    url="https://github.com/hyroai/knowledge-graph/",
+    keywords=["tag1", "tag2"],
+    classifiers=[],
+    packages=setuptools.find_namespace_packages(),
+    install_requires=[
+        "dataclasses_json==0.5.7",
+        "immutables",
+        "pytest",
+        "phonenumbers",
+    ],
+)
