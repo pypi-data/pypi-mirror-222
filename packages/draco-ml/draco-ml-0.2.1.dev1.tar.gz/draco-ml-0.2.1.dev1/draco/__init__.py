b501a27e8e5675f3bb9bb676af340e0c17f9b53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:32 +0000

    Add package draco-ml/0.2.1.dev1/draco-ml-0.2.1.dev1.tar.gz

---
+++ packages/draco-ml/draco-ml-0.2.1.dev1.tar.gz/draco-ml-0.2.1.dev1/draco/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Draco."""
+
+__author__ = """MIT Data To AI Lab"""
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.2.1.dev1'
+
+import os
+
+from draco.pipeline import DracoPipeline, get_pipelines
+
+_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+MLBLOCKS_PRIMITIVES = os.path.join(_BASE_PATH, 'primitives')
+MLBLOCKS_PIPELINES = tuple(
+    dirname
+    for dirname, _, _ in os.walk(os.path.join(_BASE_PATH, 'pipelines'))
+)
+
+__all__ = (
+    'DracoPipeline',
+    'get_pipelines',
+)
