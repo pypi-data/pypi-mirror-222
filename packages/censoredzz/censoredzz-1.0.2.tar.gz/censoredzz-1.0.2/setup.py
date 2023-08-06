9f1495a4256129c399070d00ecf85371dd68239d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:59 +0000

    Add package censoredzz/1.0.2/censoredzz-1.0.2.tar.gz

---
+++ packages/censoredzz/censoredzz-1.0.2.tar.gz/censoredzz-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='censoredzz',
+    version='1.0.2',
+    description='Python package for censoring profane words in Nepali-(Roman) text',
+    author='Kuber Budhathoki',
+    author_email='koobear99@gmail.com',
+    url='https://github.com/12-Twelvve/pypi_censored_text',
+    packages=['censoredzz'],
+    package_data={
+        'censoredzz': ['models/rf_model.pkl', 'models/vocabulary.pkl'],
+    },
+    install_requires=[
+        'regex',
+        'sklearn',
+        'util'
+    ],
+) 
