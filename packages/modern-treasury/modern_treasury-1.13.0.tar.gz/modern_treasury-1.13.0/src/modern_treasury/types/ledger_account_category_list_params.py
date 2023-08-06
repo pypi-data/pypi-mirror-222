825aea91474bed4c7061bbc4669b260b366f0f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:15 +0000

    Add package modern-treasury/1.13.0/modern_treasury-1.13.0.tar.gz

---
+++ packages/modern-treasury/modern_treasury-1.13.0.tar.gz/modern_treasury-1.13.0/src/modern_treasury/types/ledger_account_category_list_params.py
@@ -0,0 +1,34 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import Dict, Optional
+from typing_extensions import TypedDict
+
+__all__ = ["LedgerAccountCategoryListParams"]
+
+
+class LedgerAccountCategoryListParams(TypedDict, total=False):
+    after_cursor: Optional[str]
+
+    ledger_account_id: str
+    """
+    Query categories which contain a ledger account directly or through child
+    categories.
+    """
+
+    ledger_id: str
+
+    metadata: Dict[str, str]
+    """
+    For example, if you want to query for records with metadata key `Type` and value
+    `Loan`, the query would be `metadata%5BType%5D=Loan`. This encodes the query
+    parameters.
+    """
+
+    name: str
+
+    parent_ledger_account_category_id: str
+    """Query categories that are nested underneath a parent category"""
+
+    per_page: int
