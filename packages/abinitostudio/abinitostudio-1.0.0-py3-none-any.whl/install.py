67be12247620cd13e1cb612086d57d5f39abc8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:05 +0000

    Add package abinitostudio/1.0.0/abinitostudio-1.0.0-py3-none-any.whl

---
+++ packages/abinitostudio/abinitostudio-1.0.0-py3-none-any.whl/install.py
@@ -0,0 +1,13 @@
+import os
+
+
+def gettxt():
+    txt = open("requirements.txt", "r").readlines()
+    return txt
+
+
+libs = gettxt()
+for lib in libs:
+    lib = lib.strip('\n')
+    os.system("pip install " + str(lib) + r" -i https://pypi.tuna.tsinghua.edu.cn/simple")
+    # print("Successfully built {} ".format(lib))
