5cc77d6b94d65bb1f7b7fc14ab5f5b4efc4b40e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:45 +0000

    Add package lsstream/1.1.4/lsstream-1.1.4-py3-none-any.whl

---
+++ packages/lsstream/lsstream-1.1.4-py3-none-any.whl/lsstream/test.py
@@ -0,0 +1,17 @@
+import webbrowser
+import os
+from .defaults import TEST_PAGE
+
+# Tests the .txt contents on Browser
+def test_html(contents):
+    with open('temp.html', 'w') as f:
+        for content in contents:
+            f.write(content + '\n')
+        
+    webbrowser.open('file://' + os.path.realpath('temp.html'))
+
+
+# Opens a "Default Test page" on canvas so users can test it 
+def open_canvas_test_page():
+    webbrowser.open(TEST_PAGE)
+
