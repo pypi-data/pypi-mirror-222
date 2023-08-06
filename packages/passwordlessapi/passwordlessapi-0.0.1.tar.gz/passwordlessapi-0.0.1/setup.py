1282eefd7c9bbf7944d33b6a20de3487e79808aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:07 +0000

    Add package passwordlessapi/0.0.1/passwordlessapi-0.0.1.tar.gz

---
+++ packages/passwordlessapi/passwordlessapi-0.0.1.tar.gz/passwordlessapi-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='passwordlessapi',
+    version='0.0.1',
+    description='A placeholder for my future project.',
+    author='Matthew Fiallos',
+    author_email='matthew.fiallos@randstadusa.com',
+    url='https://github.com/yourusername/my_project',
+    packages=['passwordlessapi'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
