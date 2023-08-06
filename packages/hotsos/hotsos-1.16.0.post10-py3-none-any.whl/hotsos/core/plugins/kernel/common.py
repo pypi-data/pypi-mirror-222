4e1e0f7165aeaec112540d7117b2963685b13f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:36 +0000

    Add package hotsos/1.16.0.post10/hotsos-1.16.0.post10-py3-none-any.whl

---
+++ packages/hotsos/hotsos-1.16.0.post10-py3-none-any.whl/hotsos/core/plugins/kernel/common.py
@@ -0,0 +1,58 @@
+import os
+import re
+from functools import cached_property
+
+from hotsos.core import host_helpers, plugintools
+from hotsos.core.config import HotSOSConfig
+from hotsos.core.plugins.kernel.config import KernelConfig
+
+
+class KernelBase(object):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._kernel_version = ""
+        self._boot_parameters = []
+
+    @cached_property
+    def version(self):
+        """Returns string kernel version."""
+        uname = host_helpers.CLIHelper().uname()
+        if uname:
+            ret = re.compile(r"^Linux\s+\S+\s+(\S+)\s+.+").match(uname)
+            if ret:
+                self._kernel_version = ret[1]
+
+        return self._kernel_version
+
+    @cached_property
+    def isolcpus_enabled(self):
+        return KernelConfig().get('isolcpus') is not None
+
+    @cached_property
+    def boot_parameters(self):
+        """Returns list of boot parameters."""
+        parameters = []
+        path = os.path.join(HotSOSConfig.data_root, "proc/cmdline")
+        if os.path.exists(path):
+            cmdline = open(path).read().strip()
+            for entry in cmdline.split():
+                if entry.startswith("BOOT_IMAGE"):
+                    continue
+
+                if entry.startswith("root="):
+                    continue
+
+                parameters.append(entry)
+
+            self._boot_parameters = parameters
+
+        return self._boot_parameters
+
+
+class KernelChecksBase(KernelBase, plugintools.PluginPartBase):
+
+    @property
+    def plugin_runnable(self):
+        # Always run
+        return True
