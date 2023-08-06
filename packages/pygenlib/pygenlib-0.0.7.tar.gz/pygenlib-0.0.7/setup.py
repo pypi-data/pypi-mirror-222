81c1a884b478f559126125edf7002359b3c2fdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:12 +0000

    Add package pygenlib/0.0.7/pygenlib-0.0.7.tar.gz

---
+++ packages/pygenlib/pygenlib-0.0.7.tar.gz/pygenlib-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# Read the long description from README.md
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+version = "0.0.7"
+
+setup(
+    name='pygenlib',
+    version=version,
+    description='A Python library for generating other Python libraries',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Harsh Avinash',
+    author_email='harsh.avinash.official@gmail.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pygenlib = pygenlib.create_library_template:main'
+        ]
+    },
+)
