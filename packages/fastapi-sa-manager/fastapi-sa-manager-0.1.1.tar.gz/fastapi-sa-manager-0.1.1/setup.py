df6c9778be23b57590b951cc929eab6109ab086b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:46 +0000

    Add package fastapi-sa-manager/0.1.1/fastapi-sa-manager-0.1.1.tar.gz

---
+++ packages/fastapi-sa-manager/fastapi-sa-manager-0.1.1.tar.gz/fastapi-sa-manager-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fastapi-sa-manager",
+    version="0.1.1",
+    description="FastAPI-sa-manager is an SQLAlchemy ORM extension for FastAPI helping you to build CRUD APIs fast and easy.",
+    author="esanzy87",
+    author_email="esanzy87@gmail.com",
+    install_requires=["fastapi", "sqlalchemy"],
+    packages=find_packages(exclude=[]),
+    keywords=["fastapi", "sqlalchemy"],
+    python_requires=">=3.10",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
