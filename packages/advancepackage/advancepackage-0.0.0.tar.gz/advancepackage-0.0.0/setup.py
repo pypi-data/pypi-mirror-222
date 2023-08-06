e6638ad7ea6f028a1ef556053450cf0815d2caed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:34 +0000

    Add package advancepackage/0.0.0/advancepackage-0.0.0.tar.gz

---
+++ packages/advancepackage/advancepackage-0.0.0.tar.gz/advancepackage-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name="advancepackage",
+    author="Aastha Giri",
+    description="This is description",
+    long_description="This is long Description",
+    author_email="giriaastha54321@gmail.com",
+)
