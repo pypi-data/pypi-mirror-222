799c8e457897e9b9c209c45cc21ad6331b614716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:31 +0000

    Add package rapidfuzz/3.2.0/rapidfuzz-3.2.0.tar.gz

---
+++ packages/rapidfuzz/rapidfuzz-3.2.0.tar.gz/rapidfuzz-3.2.0/tests/distance/test_LCSseq.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from tests.distance.common import LCSseq
+
+
+def test_basic():
+    assert LCSseq.distance("", "") == 0
+    assert LCSseq.distance("test", "test") == 0
+    assert LCSseq.distance("aaaa", "bbbb") == 4
