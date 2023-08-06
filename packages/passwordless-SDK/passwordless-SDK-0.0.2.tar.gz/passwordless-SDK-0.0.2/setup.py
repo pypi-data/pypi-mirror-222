f46d071bb43d469cafd663378ffbc46910971f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:15 +0000

    Add package passwordless-SDK/0.0.2/passwordless-SDK-0.0.2.tar.gz

---
+++ packages/passwordless-SDK/passwordless-SDK-0.0.2.tar.gz/passwordless-SDK-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='passwordless-SDK',
+    version='0.0.2',
+    description='A placeholder for my future project.',
+    author='Matthew Fiallos',
+    author_email='matthew.fiallos@randstadusa.com',
+    url='https://github.com/yourusername/my_project',
+    packages=['passwordless-SDK'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
