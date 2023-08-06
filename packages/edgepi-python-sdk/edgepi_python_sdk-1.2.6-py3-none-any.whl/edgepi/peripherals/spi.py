004cbdcd9ee67f378edde6457d93ea56c7a3b582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:50 +0000

    Add package edgepi-python-sdk/1.2.6/edgepi_python_sdk-1.2.6-py3-none-any.whl

---
+++ packages/edgepi-python-sdk/edgepi_python_sdk-1.2.6-py3-none-any.whl/edgepi/peripherals/spi.py
@@ -0,0 +1,50 @@
+"""
+Module for SPI devices
+
+Classes:
+    SpiDevice
+"""
+
+
+import logging
+
+from periphery import SPI
+
+
+_logger = logging.getLogger(__name__)
+
+# TODO: This class needs to be changed as the SPI library changes
+class SpiDevice:
+    """Class representing an I2C device"""
+
+    _devPath = "/dev/spidev"
+
+    def __init__(
+        self,
+        bus_num: int = None,
+        dev_id: int = None,
+        mode: int = 1,
+        max_speed: int = 1000000,
+        bit_order: str = "msb",
+        bits_per_word: int = 8,
+        extra_flags: int = 0,
+    ):
+        self.devpath = f"/dev/spidev{bus_num}.{dev_id}"
+        _logger.debug(f"Initialized SPI device with path '{self.devpath}'")
+        self.spi = SPI(
+            self.devpath,
+            mode,
+            max_speed,
+            bit_order,
+            bits_per_word,
+            extra_flags,
+        )
+
+    def transfer(self, data: list) -> list:
+        """Conduct an SPI data transfer"""
+        out = self.spi.transfer(data)
+        return out
+
+    def close(self):
+        """Close SPI connection"""
+        self.spi.close()
