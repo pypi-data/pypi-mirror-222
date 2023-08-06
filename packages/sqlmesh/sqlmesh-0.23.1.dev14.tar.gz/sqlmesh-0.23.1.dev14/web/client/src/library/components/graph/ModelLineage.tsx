8db1ef5d69849f6e884f5d41a77bb585a335121a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:12 +0000

    Add package sqlmesh/0.23.1.dev14/sqlmesh-0.23.1.dev14.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.23.1.dev14.tar.gz/sqlmesh-0.23.1.dev14/web/client/src/library/components/graph/ModelLineage.tsx
@@ -0,0 +1,60 @@
+import { useApiModelLineage } from '@api/index'
+import { debounceAsync } from '@utils/index'
+import { useCallback, useEffect } from 'react'
+import { ModelColumnLineage } from './Graph'
+import { type ModelSQLMeshModel } from '@models/sqlmesh-model'
+import { useLineageFlow } from './context'
+import { mergeLineageWithModels } from './help'
+import { ReactFlowProvider } from 'reactflow'
+
+export default function ModelLineage({
+  model,
+  fingerprint,
+  highlightedNodes,
+  className,
+}: {
+  model: ModelSQLMeshModel
+  fingerprint: string | ID
+  highlightedNodes?: Record<string, string[]>
+  className?: string
+}): JSX.Element {
+  const { setActiveEdges, setConnections, setLineage, models, handleError } =
+    useLineageFlow()
+
+  const { refetch: getModelLineage } = useApiModelLineage(model.name)
+
+  const debouncedGetModelLineage = useCallback(
+    debounceAsync(getModelLineage, 500),
+    [model.name, models],
+  )
+
+  useEffect(() => {
+    setActiveEdges(new Map())
+    setConnections(new Map())
+
+    void debouncedGetModelLineage()
+      .then(({ data }) => {
+        setLineage(() =>
+          data == null ? undefined : mergeLineageWithModels({}, data),
+        )
+      })
+      .catch(error => {
+        handleError?.(error)
+      })
+  }, [fingerprint])
+
+  useEffect(() => {
+    setActiveEdges(new Map())
+    setConnections(new Map())
+  }, [model.name])
+
+  return (
+    <ReactFlowProvider>
+      <ModelColumnLineage
+        model={model}
+        highlightedNodes={highlightedNodes}
+        className={className}
+      />
+    </ReactFlowProvider>
+  )
+}
---
+++ packages/sqlmesh/sqlmesh-0.23.1.dev14.tar.gz/sqlmesh-0.23.1.dev14/web/client/src/library/pages/ide/context.tsx
@@ -0,0 +1,97 @@
+import { type ApiExceptionPayload } from '@api/client'
+import { createContext, type ReactNode, useState, useContext } from 'react'
+
+export const EnumErrorKey = {
+  Internal: 'internal',
+  General: 'general',
+  RunPlan: 'run-plan',
+  Environments: 'environments',
+  Models: 'models',
+  ApplyPlan: 'apply-plan',
+  Fetchdf: 'fetchdf',
+  EvaluateModel: 'evaluate-model',
+  RenderModel: 'render-model',
+  ColumnLineage: 'column-lineage',
+  Meta: 'meta',
+  FileExplorer: 'file-explorer',
+} as const
+
+export type ErrorKey = (typeof EnumErrorKey)[keyof typeof EnumErrorKey]
+
+export interface ErrorIDE extends ApiExceptionPayload {
+  key: ErrorKey
+}
+
+interface IDE {
+  isPlanOpen: boolean
+  setIsPlanOpen: (isPlanOpen: boolean) => void
+  errors: Map<ErrorKey, ErrorIDE>
+  addError: (key: ErrorKey, error: ErrorIDE) => void
+  removeError: (key: ErrorKey) => void
+}
+
+export const IDEContext = createContext<IDE>({
+  isPlanOpen: false,
+  errors: new Map(),
+  setIsPlanOpen: () => {},
+  addError: () => {},
+  removeError: () => {},
+})
+
+export default function IDEProvider({
+  children,
+}: {
+  children: ReactNode
+}): JSX.Element {
+  const [isPlanOpen, setIsPlanOpen] = useState(false)
+  const [errors, setErrors] = useState<Map<ErrorKey, ErrorIDE>>(new Map())
+
+  function addError(key: ErrorKey, error: ErrorIDE): void {
+    setErrors(errors => {
+      if (errors.has(EnumErrorKey.General)) {
+        errors.delete(EnumErrorKey.General)
+      }
+
+      return new Map(
+        errors.set(key, {
+          key,
+          status: error.status,
+          timestamp: error.timestamp ?? Date.now(),
+          message: error.message,
+          description: error.description,
+          type: error.type,
+          origin: error.origin,
+          trigger: error.trigger,
+          stack: error.stack,
+          traceback: error.traceback,
+        }),
+      )
+    })
+  }
+
+  function removeError(key: ErrorKey): void {
+    setErrors(errors => {
+      errors.delete(key)
+
+      return new Map(errors)
+    })
+  }
+
+  return (
+    <IDEContext.Provider
+      value={{
+        isPlanOpen,
+        setIsPlanOpen,
+        errors,
+        addError,
+        removeError,
+      }}
+    >
+      {children}
+    </IDEContext.Provider>
+  )
+}
+
+export function useIDE(): IDE {
+  return useContext(IDEContext)
+}
