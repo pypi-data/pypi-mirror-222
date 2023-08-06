4e91efb143a22f875afbff9c49f652486efd8ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:18 +0000

    Add package pyGuardPoint/0.9.8/pyGuardPoint-0.9.8-py3-none-any.whl

---
+++ packages/pyGuardPoint/pyGuardPoint-0.9.8-py3-none-any.whl/pyGuardPoint/_guardpoint_personaldetails.py
@@ -0,0 +1,34 @@
+import validators
+from .guardpoint_error import GuardPointError
+
+from .guardpoint_dataclasses import CardholderPersonalDetail
+
+
+class PersonalDetailsAPI:
+
+    def update_personal_details(self, cardholder_uid: str, personal_details: CardholderPersonalDetail):
+        if not validators.uuid(cardholder_uid):
+            raise ValueError(f'Malformed Cardholder UID {cardholder_uid}')
+
+        url = "/odata/API_CardholderPersonalDetails"
+        url_query_params = f"({cardholder_uid})"
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            # 'IgnoreNonEditable': ''
+        }
+
+        ch = personal_details.dict(editable_only=True, changed_only=True)
+
+        code, json_body = self.gp_json_query("PATCH", headers=headers, url=(url + url_query_params), json_body=ch)
+
+        if code != 204:  # HTTP NO_CONTENT
+            if 'error' in json_body:
+                raise GuardPointError(json_body['error'])
+            elif 'message' in json_body:
+                raise GuardPointError(json_body['message'])
+            else:
+                raise GuardPointError(str(code))
+
+        return True
