3345dbe5df6422a1bfa4ffef9682cf263d90efdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package roboto-sdk/0.1.15/roboto_sdk-0.1.15.tar.gz

---
+++ packages/roboto-sdk/roboto_sdk-0.1.15.tar.gz/roboto_sdk-0.1.15/src/roboto_sdk/domain/datasets/__init__.py
@@ -0,0 +1,30 @@
+#  Copyright (c) 2023 Roboto Technologies, Inc.
+from .dataset import Dataset
+from .delegate import (
+    AccessMode,
+    Credentials,
+    DatasetDelegate,
+)
+from .http_delegate import DatasetHttpDelegate
+from .http_resources import (
+    CreateDatasetRequest,
+    UpdateDatasetRequest,
+)
+from .record import (
+    Administrator,
+    DatasetRecord,
+    StorageLocation,
+)
+
+__all__ = (
+    "Administrator",
+    "AccessMode",
+    "CreateDatasetRequest",
+    "Credentials",
+    "Dataset",
+    "DatasetDelegate",
+    "DatasetHttpDelegate",
+    "DatasetRecord",
+    "StorageLocation",
+    "UpdateDatasetRequest",
+)
