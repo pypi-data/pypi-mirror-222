cd4a3b936f5dfb1dee1ad047ed497400137a4341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:19 +0000

    Add package opencv-python-rolling/4.8.0.20230729/opencv_python_rolling-4.8.0.20230729-cp37-abi3-macosx_10_16_x86_64.whl

---
+++ packages/opencv-python-rolling/opencv_python_rolling-4.8.0.20230729-cp37-abi3-macosx_10_16_x86_64.whl/cv2/gapi/onnx/ep/__init__.pyi
@@ -0,0 +1,31 @@
+__all__: list[str] = []
+
+import typing
+
+
+# Classes
+class OpenVINO:
+    # Functions
+    @typing.overload
+    def __init__(self) -> None: ...
+    @typing.overload
+    def __init__(self, dev_type: str) -> None: ...
+
+    def cfgCacheDir(self, dir: str) -> OpenVINO: ...
+
+    def cfgNumThreads(self, nthreads: int) -> OpenVINO: ...
+
+    def cfgEnableOpenCLThrottling(self) -> OpenVINO: ...
+
+    def cfgEnableDynamicShapes(self) -> OpenVINO: ...
+
+
+class DirectML:
+    # Functions
+    @typing.overload
+    def __init__(self) -> None: ...
+    @typing.overload
+    def __init__(self, device_id: int) -> None: ...
+
+
+
