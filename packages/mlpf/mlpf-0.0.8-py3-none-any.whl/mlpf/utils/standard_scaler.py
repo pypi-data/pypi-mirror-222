5c5f7b05b5ec4352c9946f8d4b2476b0d7dc5b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:21 +0000

    Add package mlpf/0.0.8/mlpf-0.0.8-py3-none-any.whl

---
+++ packages/mlpf/mlpf-0.0.8-py3-none-any.whl/mlpf/utils/standard_scaler.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn, Tensor
+
+
+class StandardScaler(nn.Module):
+    """
+    Standard scaler transform as a torch module.
+    """
+
+    def __init__(self, X):
+        super(StandardScaler, self).__init__()
+
+        self.mean = torch.mean(X, dim=0)
+        self.std = torch.std(X, dim=0)
+        self.std[self.std < 1e-9] = 1.0
+
+    def forward(self, x) -> Tensor:
+        return (x - self.mean) / self.std
+
+    def _apply(self, fn):
+        super(StandardScaler, self)._apply(fn)
+        self.mean = fn(self.mean)
+        self.std = fn(self.std)
+
+        return self
+
+    def inverse(self, z) -> Tensor:
+        return z * self.std + self.mean
