e106daeaf9929ce7adf297828768510ccf71e221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:43 +0000

    Add package freva/2307.0.3/freva-2307.0.3.tar.gz

---
+++ packages/freva/freva-2307.0.3.tar.gz/freva-2307.0.3/src/freva/_plugin.py
@@ -0,0 +1,342 @@
+"""Module to get information on and run Freva plgins.
+
+To make use of any of the methods a Freva plugin has to be set up. Either
+by you, the Freva admins or your collegues. If you want to create a plugin
+pleas refer to the :class:`evaluation_system.api.plugin` section.
+"""
+from __future__ import annotations
+import logging
+from pathlib import Path
+import json
+import textwrap
+from typing import Any, Union, List, Optional, NamedTuple, Tuple
+import time
+
+import appdirs
+import lazy_import
+from evaluation_system.misc import logger
+
+django = lazy_import.lazy_module("django")
+pm = lazy_import.lazy_module("evaluation_system.api.plugin_manager")
+user = lazy_import.lazy_module("evaluation_system.model.user")
+config = lazy_import.lazy_module("evaluation_system.misc.config")
+utils = lazy_import.lazy_module("evaluation_system.misc.utils")
+PluginNotFoundError = lazy_import.lazy_callable(
+    "evaluation_system.misc.exceptions.PluginNotFoundError"
+)
+
+
+CACHE_FILE = Path(appdirs.user_cache_dir()) / "freva" / "plugins.json"
+
+PluginInfo = NamedTuple(
+    "PluginInfo",
+    [
+        ("name", str),
+        ("description", str),
+        ("parameters", List[Tuple[str, str, Any]]),
+    ],
+)
+
+__all__ = ["run_plugin", "list_plugins", "plugin_doc"]
+
+
+def _write_plugin_cache() -> dict[str, dict[str, Any]]:
+    """Write all plugins to a file."""
+    out: dict[str, dict[str, Any]] = {}
+    for plugin in pm.get_plugins().keys():
+        pm_instance = pm.get_plugin_instance(plugin)
+        out[plugin] = {}
+        out[plugin]["description"] = pm_instance.__short_description__
+        out[plugin]["parameters"] = []
+        for key, param in pm_instance.__parameters__._params.items():
+            if param.mandatory:
+                desc = f"{param.help} (mandatory)"
+            else:
+                desc = f"{param.help} (default: {param.default})"
+            out[plugin]["parameters"].append((key, desc, param.default))
+
+    CACHE_FILE.parent.mkdir(exist_ok=True, parents=True)
+    with CACHE_FILE.open("w") as f_obj:
+        json.dump(out, f_obj)
+    return out
+
+
+def read_plugin_cache(max_mtime: int = 180) -> list[PluginInfo]:
+    """Cache plugin list."""
+    CACHE_FILE.parent.mkdir(exist_ok=True, parents=True)
+    CACHE_FILE.touch(exist_ok=True)
+    plugin_cache = []
+    if time.time() - CACHE_FILE.stat().st_mtime > max_mtime:
+        cache = _write_plugin_cache()
+    else:
+        try:
+            with CACHE_FILE.open("r") as f_obj:
+                cache = json.load(f_obj)
+        except:
+            cache = _write_plugin_cache()
+    if not cache:
+        cache = _write_plugin_cache()
+    for plugin, plugin_data in cache.items():
+        plugin_cache.append(PluginInfo(name=plugin, **plugin_data))
+    return plugin_cache
+
+
+def plugin_doc(tool_name: Optional[str]) -> str:
+    """Display the documentation of a given plugin.
+
+    Parameters
+    ----------
+    tool_name:
+        The name of the tool that should be documented
+
+    Returns
+    -------
+    str:
+        plugin help string.
+
+    Raises
+    ------
+    PluginNotFoundError:
+        if the plugin name does not exist.
+
+    Example
+    -------
+
+    .. execute_code::
+
+        import freva
+        print(freva.plugin_doc("animator"))
+
+
+    """
+    tool_name = (tool_name or "").lower()
+    _write_plugin_cache()
+    _check_if_plugin_exists(tool_name)
+    return pm.get_plugin_instance(tool_name).get_help()
+
+
+def list_plugins() -> list[str]:
+    """Get the plugins that are available on the system.
+
+    Returns
+    --------
+    list[str]:
+            List of available Freva plugins
+
+    Example
+    -------
+
+    .. execute_code::
+
+        import freva
+        print(freva.list_plugins())
+    """
+    _write_plugin_cache()
+    return list([k.lower() for k in pm.get_plugins().keys()])
+
+
+def get_tools_list() -> str:
+    """Get a list of plugins with their short description.
+
+    Returns
+    -------
+    str:
+        String representation of all available plugins.
+
+    :meta private:
+    """
+    _write_plugin_cache()
+    env = utils.get_console_size()
+    # we just have to show the list and stop processing
+    name_width = 0
+    plugins = pm.get_plugins()
+    for key in plugins:
+        name_width = max(name_width, len(key))
+    offset = name_width + 2
+    result = []
+    for key, plugin in sorted(plugins.items()):
+        lines = textwrap.wrap("%s" % plugin.description, env["columns"] - offset)
+        if not lines:
+            lines = ["No description."]
+        if len(lines) > 1:
+            # multi-line
+            result.append(f"{plugin.name}: {lines[0]}\n{' '*offset}\n{' '*offset}")
+        else:
+            result.append(f"{plugin.name}: {lines[0]}")
+    return "\n".join(result)
+
+
+def _check_if_plugin_exists(tool_name: Optional[str]) -> None:
+    """Check if a given plugin name is part of the plugin stack."""
+    if tool_name in pm.get_plugins():
+        return
+    if not tool_name:
+        error = "Available tools are:\n"
+        tool_list = "\n".join(list_plugins())
+    else:
+        tool_list = "\n".join(utils.find_similar_words(tool_name, list_plugins()))
+        error = f"{tool_name} plugin not found, did you mean:\n"
+    raise PluginNotFoundError(f"\n{error}{tool_list}")
+
+
+def _return_value(value: int, result: Any, return_result: bool = True) -> Any:
+    if return_result:
+        return value, result
+    return value, ""
+
+
+def run_plugin(
+    tool_name: str,
+    *,
+    save: bool = False,
+    save_config: Optional[Union[str, Path]] = None,
+    show_config: bool = False,
+    dry_run: bool = False,
+    scheduled_id: Optional[int] = None,
+    repo_version: bool = False,
+    unique_output: bool = False,
+    batchmode: bool = False,
+    caption: str = "",
+    tag: Optional[str] = None,
+    return_result: bool = False,
+    **options: dict[str, Union[str, float, int]],
+) -> tuple[int, Any]:
+    """Apply an available data analysis plugin.
+
+    Parameters
+    ----------
+    tool_name:
+        The name of the plugin that is to be applied.
+    caption:
+        Set a caption for the results.
+    save:
+        Save the plugin configuration to default destination.
+    save_config:
+        Save the plugin configuration.
+    show_config:
+        Show the resulting configuration (implies dry-run).
+    scheduled_id:
+        Run a scheduled job from database
+    dry_run:
+        Perform no computation. Useful for development.
+    batchmode:
+        Create a Batch job and submit it to the scheduling system.
+    unique_output:
+        Append a Freva run id to the output/cache folder(s).
+    return_result:
+        Return the plugin result, this can be useful for pipelining.
+    repo_version:
+        show the version number from the repository.
+    tag:
+       Use git commit hash to specify a specific versrion of this tool.
+
+    Returns
+    -------
+    tuple:
+        Return code, and the return value of the plugin
+
+
+    Example
+    -------
+
+    Run a plugin in the foreground.
+
+    .. execute_code::
+
+        import freva
+        freva.run_plugin("animator", variable="pr", project="obs*")
+
+    Run a plugin in the background
+
+    .. execute_code::
+
+        import freva
+        freva.run_plugin("animator",
+                         variable="pr",
+                         project="observations",
+                         batchmode=True)
+
+    """
+    tool_name = tool_name.lower()
+    _check_if_plugin_exists(tool_name)
+    if save_config:
+        save_config = str(Path(save_config).expanduser().absolute())
+    if repo_version:
+        (repos, version) = pm.get_plugin_version(tool_name)
+        return _return_value(
+            0, "Repository and version of " f":{tool_name}\n{repos}\n{version}"
+        )
+    options_str, tool_dict = [], {}
+    for k, v in options.items():
+        options_str.append(f"{k}={v}")
+    if scheduled_id is None:
+        tool_dict = pm.parse_arguments(tool_name, options_str)
+    if logger.level == logging.DEBUG:
+        tool_dict["debug"] = True
+    extra_scheduler_options = tool_dict.pop("extra_scheduler_options", "")
+    if caption:
+        caption = caption.strip()
+    if save_config or save:
+        save_in = pm.write_setup(tool_name, tool_dict, config_file=save_config)
+        logger.info("Configuration file saved in %s", save_in)
+    elif show_config:
+        return _return_value(
+            0,
+            pm.get_plugin_instance(tool_name).get_current_config(config_dict=tool_dict),
+        )
+    if scheduled_id and not dry_run:
+        logger.info(
+            "Running %s as scheduled in history with ID %i",
+            tool_name,
+            scheduled_id,
+        )
+        out = pm.run_tool(
+            tool_name, scheduled_id=scheduled_id, unique_output=unique_output
+        )
+        return _return_value(0, out, return_result)
+    extra_options: list[str] = [
+        opt.strip() for opt in extra_scheduler_options.split(",") if opt.strip()
+    ]
+    # now run the tool
+    (error, warning) = pm.get_error_warning(tool_name)
+    if warning:
+        logger.warning(warning)
+    if error:
+        logger.error(error)
+    logger.debug("Running %s with configuration: %s", tool_name, tool_dict)
+    if not dry_run and not error:
+        # we check if the user is external and activate batchmode
+        django_user = django.contrib.auth.models.User.objects.get(
+            username=user.User().getName()
+        )
+        if django_user.groups.filter(
+            name=config.get("external_group", "noexternalgroupset")
+        ).exists():
+            batchmode = True
+        if batchmode:
+            [scheduled_id, job_file] = pm.schedule_tool(
+                tool_name,
+                config_dict=tool_dict,
+                user=user.User(),
+                caption=caption,
+                extra_options=extra_options,
+                unique_output=unique_output,
+            )
+            print(f"Scheduled job with history id: {scheduled_id}")
+            print("You can view the job's status with the command squeue")
+            print("Your job's progress will be shown with the command")
+            print(f"tail -f {job_file}")
+            return 0, ""
+        results = pm.run_tool(
+            tool_name,
+            config_dict=tool_dict,
+            caption=caption,
+            unique_output=unique_output,
+        )
+        # repeat the warning at the end of the run
+        # for readability don't show the warning in debug mode
+        if warning:
+            logger.warning(warning)
+    logger.debug("Arguments: %s", options)
+    logger.debug("Current configuration:\n%s", json.dumps(tool_dict, indent=4))
+    return _return_value(0, results, return_result)
---
+++ packages/freva/freva-2307.0.3.tar.gz/freva-2307.0.3/src/freva/cli/plugin.py
@@ -0,0 +1,180 @@
+from __future__ import annotations
+import argparse
+from pathlib import Path
+import sys
+from typing import Any, Optional
+import lazy_import
+
+
+from evaluation_system import __version__
+from evaluation_system.misc import logger
+from .utils import BaseParser, BaseCompleter
+
+freva = lazy_import.lazy_module("freva")
+PluginNotFoundError = lazy_import.lazy_class(
+    "evaluation_system.misc.exceptions.PluginNotFoundError"
+)
+ParameterNotFoundError = lazy_import.lazy_class(
+    "evaluation_system.misc.exceptions.ParameterNotFoundError"
+)
+ValidationError = lazy_import.lazy_class(
+    "evaluation_system.misc.exceptions.ValidationError"
+)
+hide_exception = lazy_import.lazy_function(
+    "evaluation_system.misc.exceptions.hide_exception"
+)
+
+
+class Cli(BaseParser):
+    """Class that constructs the Plugin Argument Parser."""
+
+    desc = "Apply data analysis plugin."
+
+    def __init__(
+        self,
+        parser: Optional[argparse.ArgumentParser] = None,
+    ):
+        """Construct the plugin sub arg. parser."""
+        super().__init__(parser, "freva-plugin")
+        self.parser.add_argument(
+            "tool-name",
+            nargs="?",
+            metavar="plugin_name",
+            help="Plugin name",
+            default=None,
+        )
+        self.parser.add_argument(
+            "--repo-version",
+            default=False,
+            action="store_true",
+            help="Show the version number from the repository",
+        )
+        self.parser.add_argument(
+            "--caption",
+            default="",
+            help="Set a caption for the results",
+        )
+        self.parser.add_argument(
+            "--save",
+            default=False,
+            action="store_true",
+            help="Save the plugin configuration to default destination.",
+        )
+        self.parser.add_argument(
+            "--save-config",
+            type=Path,
+            default=None,
+            help="Save the plugin configuration.",
+        )
+        self.parser.add_argument(
+            "--show-config",
+            help="Show the resulting configuration (implies dry-run).",
+            action="store_true",
+            default=False,
+        )
+        self.parser.add_argument(
+            "--scheduled-id",
+            default=None,
+            type=int,
+            help=argparse.SUPPRESS,
+        )
+        self.parser.add_argument(
+            "--dry-run",
+            default=False,
+            action="store_true",
+            help="Perform no computation. Useful for development.",
+        )
+        self.parser.add_argument(
+            "--batchmode",
+            help="Create a Batch job and submit it to the scheduling system.",
+            default=False,
+            action="store_true",
+        )
+        self.parser.add_argument(
+            "--unique-output",
+            "--unique_output",
+            choices=["true", "false", "True", "False"],
+            help="Append a Freva run id to the output/cache folder(s).",
+            default="true",
+        )
+        self.parser.add_argument(
+            "--debug",
+            "-v",
+            "-d",
+            "--verbose",
+            help="Use verbose output.",
+            action="store_true",
+            default=False,
+        )
+        self.parser.add_argument(
+            "--list-tools",
+            "--list",
+            "-l",
+            default=False,
+            action="store_true",
+            help="Only list the available tools.",
+        )
+        self.parser.add_argument(
+            "--doc",
+            "--plugin-doc",
+            default=False,
+            action="store_true",
+            help="Display plugin documentation",
+        )
+        self.parser.set_defaults(apply_func=self.run_cmd)
+
+    @staticmethod
+    def run_cmd(
+        args: argparse.Namespace,
+        other_args: Optional[list[str]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """Call the plugin command and print the results."""
+        tool_name = kwargs.pop("tool-name")
+        if kwargs.pop("list_tools") or not tool_name:
+            print(freva.get_tools_list())
+            return
+        options: dict[str, Any] = BaseCompleter.arg_to_dict(other_args or [])
+        for key, val in options.items():
+            if len(val) == 1:
+                options[key] = val[0]
+        if kwargs["unique_output"].lower() == "true":
+            kwargs["unique_output"] = True
+        else:
+            kwargs["unique_output"] = False
+        tool_args = {**kwargs, **options}
+        try:
+            if tool_args.pop("doc"):
+                print(freva.plugin_doc(tool_name))
+                return
+            value, out = freva.run_plugin(tool_name or "", **tool_args)
+        except (
+            PluginNotFoundError,
+            ValidationError,
+            ParameterNotFoundError,
+        ) as e:
+            if args.debug:
+                raise e
+            with hide_exception():
+                raise e
+        if value != 0:
+            logger.warning("Tool failed to run")
+        if out:
+            print(out)
+
+
+def main(argv: Optional[list[str]] = None) -> None:
+    """Wrapper for entry point script."""
+    cli = Cli()
+    cli.parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version="%(prog)s {version}".format(version=__version__),
+    )
+    args = cli.parse_args(argv or sys.argv[1:])
+    try:
+        cli.run_cmd(args, **cli.kwargs)
+    except KeyboardInterrupt:  # pragma: no cover
+        print("KeyboardInterrupt, exiting", file=sys.stderr, flush=True)
+        sys.exit(130)
