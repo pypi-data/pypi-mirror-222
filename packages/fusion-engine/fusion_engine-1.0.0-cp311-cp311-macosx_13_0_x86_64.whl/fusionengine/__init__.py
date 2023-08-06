0ca24e1159eee23661a6b5459e5ff58ccc39ef16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:11 +0000

    Add package fusion-engine/1.0.0/fusion_engine-1.0.0-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/fusion-engine/fusion_engine-1.0.0-cp311-cp311-macosx_13_0_x86_64.whl/fusionengine/__init__.py
@@ -0,0 +1,26 @@
+__author__ = "Dimkauzh"
+__version__ = "1.0.0"
+
+from fusionengine.files.imports import *
+import fusionengine.files.systems as sysconfig
+
+
+class Main:
+    def __init__(self):
+        """A class that contains all the functions for the engine."""
+        pg.init()
+
+        self.window = window.Window()
+        self.color = color.Colors()
+        self.colortools = color.ColorTools()
+        self.event = event.Event()
+        self.keys = event.Keys()
+        self.draw = draw.Draw()
+        self.image = image.Image()
+        # self.body = body
+        self.system = sysconfig.System()
+        self.shape = shape.Shapes()
+        # self.ui = ui.UI()
+        self.fonts = fonts.Fonts()
+        self.debug = debug.DebugFiles()
+        self.vector = vector.Vectors()
