8dbfa48bd72fcdda8f07c0506aa26638b410def9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package streamlit-bls-connection/0.2/streamlit_bls_connection-0.2.tar.gz

---
+++ packages/streamlit-bls-connection/streamlit_bls_connection-0.2.tar.gz/streamlit_bls_connection-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='streamlit_bls_connection',
+    version='0.2',
+    description='A package to fetch Bureau of Labor Statistics data using Streamlit',
+    author='Tony Hollaar',
+    author_email='thollaar@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit',
+        'requests',
+        'pandas',
+    ],
+)
