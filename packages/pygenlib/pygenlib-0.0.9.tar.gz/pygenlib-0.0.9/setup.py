35d8bbfe22da1ea08dd83e23b7e59bfef6d60c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package pygenlib/0.0.9/pygenlib-0.0.9.tar.gz

---
+++ packages/pygenlib/pygenlib-0.0.9.tar.gz/pygenlib-0.0.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# Read the long description from README.md
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+version = "0.0.9"
+
+setup(
+    name='pygenlib',
+    version=version,
+    description='A Python library for generating other Python libraries',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
+    ],
+    packages=find_packages(),
+    package_data={
+        'pygenlib': ['templates/*'],
+    },
+    entry_points={
+        'console_scripts': [
+            'pygenlib = pygenlib.create_library_template:main'
+        ]
+    },
+)
