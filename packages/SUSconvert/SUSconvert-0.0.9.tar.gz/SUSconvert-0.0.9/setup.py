f5d34dafc5db6f24c8d95d01e5f46daf6d1bb9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:29 +0000

    Add package SUSconvert/0.0.9/SUSconvert-0.0.9.tar.gz

---
+++ packages/SUSconvert/SUSconvert-0.0.9.tar.gz/SUSconvert-0.0.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SUSconvert',
+    version='0.0.9',
+    author='Francisco Ibarra',
+    description='Increible librería para convertir tu texto en SUS..',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        
+    ],
+)
