903692f567ee753dde2a45b1c7c4e7a1aaef3691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:57 +0000

    Add package duffy/3.3.9/duffy-3.3.9.tar.gz

---
+++ packages/duffy/duffy-3.3.9.tar.gz/duffy-3.3.9/duffy/api_models/session.py
@@ -0,0 +1,66 @@
+from abc import ABC
+from datetime import datetime
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import BaseModel, conint
+
+try:
+    from ..database.types import NodeState
+except ImportError:  # pragma: no cover
+    NodeState = str
+from ..misc import APITimeDelta
+from .common import APIResult, CreatableMixin, RetirableMixin
+from .node import NodeBase
+from .tenant import TenantModel
+
+# nodes spec model
+
+
+class NodesSpec(BaseModel):
+    quantity: conint(ge=1)
+    pool: str
+
+
+# nodes in sessions model
+
+
+class SessionNodeModel(NodeBase):
+    id: int
+    state: Optional[NodeState]
+
+
+# session model
+
+
+class SessionBase(BaseModel, ABC):
+    class Config:
+        orm_mode = True
+
+
+class SessionCreateModel(SessionBase):
+    tenant_id: Optional[int]
+    nodes_specs: List[NodesSpec]
+
+
+class SessionUpdateModel(SessionBase):
+    active: Optional[bool]
+    expires_at: Optional[Union[datetime, APITimeDelta]]
+
+
+class SessionModel(SessionBase, CreatableMixin, RetirableMixin):
+    id: int
+    expires_at: Optional[datetime]
+    tenant: TenantModel
+    data: Dict[str, Any]
+    nodes: List[SessionNodeModel]
+
+
+# API results
+
+
+class SessionResult(APIResult):
+    session: SessionModel
+
+
+class SessionResultCollection(APIResult):
+    sessions: List[SessionModel]
