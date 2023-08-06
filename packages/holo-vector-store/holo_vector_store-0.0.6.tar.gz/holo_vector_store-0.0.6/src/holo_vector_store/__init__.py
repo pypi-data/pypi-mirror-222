78dc158cdadf0600f70c2435ca9cd84f958186d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:01 +0000

    Add package holo-vector-store/0.0.6/holo_vector_store-0.0.6.tar.gz

---
+++ packages/holo-vector-store/holo_vector_store-0.0.6.tar.gz/holo_vector_store-0.0.6/src/holo_vector_store/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2023-present Changgeng Zhao <zhaochanggeng.zcg@alibaba-inc.com>
+#
+# SPDX-License-Identifier: MIT
+
+from holo_vector_store.holo_vector_store import HoloVectorStore
