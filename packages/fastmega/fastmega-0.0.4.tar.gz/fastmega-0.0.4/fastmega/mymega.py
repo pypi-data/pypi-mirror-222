12ff593fece97489ecbbcc68a133287a0d859d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:06 +0000

    Add package fastmega/0.0.4/fastmega-0.0.4.tar.gz

---
+++ packages/fastmega/fastmega-0.0.4.tar.gz/fastmega-0.0.4/fastmega/mymega.py
@@ -0,0 +1,45 @@
+from mega import Mega
+
+"""
+https://www.geeksforgeeks.org/how-to-use-mega-nz-api-with-python/
+https://github.com/odwyersoftware/mega.py/blob/master/src/mega/mega.py
+"""
+
+class MyMega:
+    def __init__(self, username: str, password: str) -> None:
+        self.username=username
+        self.password=password
+        self.mega = Mega()
+        self.mega = self.mega.login(
+            self.username,
+            self.password
+        )
+        self.root = self.mega.find("workspace")[0]
+
+    def mkdir(self, dirname: str, dest: str) -> str:
+        """
+        Make a directory
+        args:
+            - dirname: directory's name
+            - dest: destination path on cloud
+        """
+        self.mega.create_folder(
+            dirname,
+            dest=dest
+        )
+        newdir_path = self.mega.find(dirname)[0]
+        return newdir_path
+
+    def upload_file(self, src: str, dest: str):
+        """
+        Upload file from your local to the cloud
+        args:
+            - src: filename path local path
+            - dest: directory path on the cloud 
+        """
+        resp = self.mega.upload(
+            src,
+            dest
+        )
+        file_path = self.mega.get_upload_link(resp)
+        return file_path
