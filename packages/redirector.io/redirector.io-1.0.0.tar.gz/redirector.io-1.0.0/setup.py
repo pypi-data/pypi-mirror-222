973f9e0350df5cd8101c3049b73d3d4859e4e9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:31 +0000

    Add package redirector.io/1.0.0/redirector.io-1.0.0.tar.gz

---
+++ packages/redirector.io/redirector.io-1.0.0.tar.gz/redirector.io-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='redirector.io',
+    version='1.0.0',
+    author='Tomer',
+    author_email='tomerw@illustria.io',
+    description='A simple HTTP redirect server',
+    url='https://github.com/yourusername/redirector',
+    project_urls={
+        'Documentation': 'https://drive.google.com/uc?export=download&id=1yoJ703Sjj5bzxXj8QXwDckWtqC1xDFPT'
+    },
+    packages=find_packages())
+
