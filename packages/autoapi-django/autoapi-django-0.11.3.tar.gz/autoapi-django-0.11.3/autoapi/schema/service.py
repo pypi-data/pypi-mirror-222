60d48c27df3073b85a881336878baf1320eb7fbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:36 +0000

    Add package autoapi-django/0.11.3/autoapi-django-0.11.3.tar.gz

---
+++ packages/autoapi-django/autoapi-django-0.11.3.tar.gz/autoapi-django-0.11.3/autoapi/schema/service.py
@@ -0,0 +1,30 @@
+import inspect
+from types import FunctionType
+
+from autoapi.schema.interfaces import IServiceBuilder
+from autoapi.schema.data import Type, ServiceSchema
+from autoapi.schema.utils import resolve_service_name, get_docstring_description_lines
+
+
+class ServiceBuilder(IServiceBuilder):
+
+    def build(self, service_cls: type) -> tuple[ServiceSchema, dict[str, Type]]:
+        members = inspect.getmembers(service_cls)
+        methods = []
+        models = {}
+        description = ''
+        if service_cls.__doc__:
+            description, params, return_description, raises = get_docstring_description_lines(service_cls.__doc__)
+        for member_name, member in members:
+            if isinstance(member, FunctionType):
+                method, models_ = self.method_builder.build(member_name, member)
+                methods.append(method)
+                models.update(models_)
+        service_schema = ServiceSchema(
+            name=resolve_service_name(service_cls.__name__),
+            cls=service_cls,
+            methods=methods,
+            description=description,
+        )
+        return service_schema, models
+
