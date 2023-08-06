4be4a6b2f62672bb7fa9e5ed8ad6c795967eceba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:53 +0000

    Add package agent-protocol/0.1.2/agent_protocol-0.1.2.tar.gz

---
+++ packages/agent-protocol/agent_protocol-0.1.2.tar.gz/agent_protocol-0.1.2/README.md
@@ -0,0 +1,101 @@
+# Agent Communication Protocol - Python SDK
+
+This SDK implements the Agent Communication Protocol in Python and allows you to easily wrap your agent in a webserver compatible with the protocol - you only need to define an agent task handler.
+
+## Installation
+
+```sh
+pip install agent-protocol
+```
+
+Then add the following code to your agent:
+
+```python
+from agent_protocol import (
+    Agent,
+    StepResult,
+    StepHandler,
+)
+
+async def task_handler(task_input) -> StepHandler:
+    # TODO: Initialize code for the agent task.
+    async def step_handler(step_input) -> StepResult:
+        # TODO: Execute code for the agent step.
+        # The step could for example be a single iteration of a ReAct loop.
+
+        # TODO: Return the result of the step.
+        return StepResult(
+            output=...,
+        )
+
+    return step_handler
+
+if __name__ == "__main__":
+    # Add the task handler and start the server
+    Agent.handle_task(task_handler).start()
+```
+
+## Usage
+
+To start the server run the file where you added the code above:
+
+```sh
+python file/where/you/added/code.py
+```
+
+and then you can call the API using the following terminal commands:
+
+To **create a task** run:
+
+```sh
+curl --request POST \
+  --url http://localhost:8000/agent/tasks \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"input": "task-input-to-your-agent"
+}'
+```
+
+You will get a response like this:
+
+```json
+{
+  "input": "task-input-to-your-agent",
+  "task_id": "e6d768bb-4c50-4007-9853-aeffb46c77be",
+  "artifacts": []
+}
+```
+
+Then to **execute one step of the task** copy the `task_id` you got from the previous request and run:
+
+```sh
+curl --request POST \
+  --url http://localhost:8000/agent/tasks/<task-id>/steps
+```
+
+To get response like this:
+
+```json
+{
+  "output": "output-from-the-agent",
+  "artifacts": [],
+  "is_last": false,
+  "input": null,
+  "task_id": "e6d768bb-4c50-4007-9853-aeffb46c77be",
+  "step_id": "8ff8ba39-2c3e-4246-8086-fbd2a897240b"
+}
+```
+
+## Test compliance
+Part of the package is also a test suite that can be used to test compliance with the protocol. To run the tests, run the following command:
+
+```sh
+agent-protocol test --url <url>
+```
+
+In the background it uses pytest, you can pass any pytest arguments to the command above.
+
+## Examples
+
+- [Smol Developer integration](./examples/smol_developer.py)
+- [Beebot integration](https://github.com/AutoPackAI/beebot/pull/3)
---
+++ packages/agent-protocol/agent_protocol-0.1.2.tar.gz/agent_protocol-0.1.2/agent_protocol/agent.py
@@ -0,0 +1,158 @@
+import asyncio
+
+import uuid
+
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+from typing import Awaitable, Callable, List, Optional, Tuple, Any
+
+
+from .server import app
+from .models import (
+    Task,
+    TaskRequestBody,
+    Step,
+    StepInput,
+    StepRequestBody,
+    TaskInput,
+    StepResult,
+    Artifact,
+)
+
+
+StepHandler = Callable[[StepInput | None], Awaitable[StepResult]]
+TaskHandler = Callable[[TaskInput | None], Awaitable[StepHandler]]
+
+tasks: List[Tuple[Task, StepHandler]] = []
+steps: List[Step] = []
+
+task_handler: Optional[TaskHandler]
+
+
+class StepResultWithDefaults(StepResult):
+    output: Any
+    artifacts: List[Artifact] = []
+    is_last: bool = False
+
+
+@app.post("/agent/tasks", response_model=Task, tags=["agent", "tasks"])
+async def create_agent_task(body: TaskRequestBody | None = None) -> Task:
+    """
+    Creates a task for the agent.
+    """
+    if not task_handler:
+        raise Exception("Task handler not defined")
+
+    step_handler = await task_handler(body.input if body else None)
+    task = Task(
+        task_id=str(uuid.uuid4()),
+        input=body.input if body else None,
+        artifacts=[],
+    )
+    tasks.append((task, step_handler))
+    return task
+
+
+@app.get("/agent/tasks", response_model=List[str], tags=["agent", "tasks"])
+async def list_agent_tasks_ids() -> List[str]:
+    """
+    List all tasks that have been created for the agent.
+    """
+    return [t[0].task_id for t in tasks]
+
+
+@app.get("/agent/tasks/{task_id}", response_model=Task, tags=["agent", "tasks"])
+async def get_agent_task(task_id: str) -> Task:
+    """
+    Get details about a specified agent task.
+    """
+    task = next(filter(lambda t: t[0].task_id == task_id, tasks), None)
+    if not task:
+        raise Exception(f"Task with id {task_id} not found")
+    return task[0]
+
+
+@app.get(
+    "/agent/tasks/{task_id}/steps",
+    response_model=List[str],
+    tags=["agent", "tasks", "steps"],
+)
+async def list_agent_task_steps(task_id: str) -> List[str]:
+    """
+    List all steps for the specified task.
+    """
+    return [t.step_id for t in steps if t.task_id == task_id]
+
+
+@app.post(
+    "/agent/tasks/{task_id}/steps",
+    response_model=Step,
+    tags=["agent", "tasks", "steps"],
+)
+async def execute_agent_task_step(
+    task_id: str,
+    body: StepRequestBody | None = None,
+) -> Step:
+    """
+    Execute a step in the specified agent task.
+    """
+    task = next(filter(lambda t: t[0].task_id == task_id, tasks), None)
+    if not task:
+        raise Exception(f"Task with id {task_id} not found")
+
+    handler = task[1]
+    result = await handler(body.input if body else None)
+
+    step = Step(
+        task_id=task_id,
+        step_id=str(uuid.uuid4()),
+        input=body.input if body else None,
+        output=result.output,
+        artifacts=result.artifacts,
+        is_last=result.is_last,
+    )
+
+    if step.artifacts:
+        if task[0].artifacts is None:
+            task[0].artifacts = step.artifacts
+        else:
+            task[0].artifacts.extend(step.artifacts)
+
+    steps.append(step)
+    return step
+
+
+@app.get(
+    "/agent/tasks/{task_id}/steps/{step_id}",
+    response_model=Step,
+    tags=["agent", "tasks", "steps"],
+)
+async def get_agent_task_step(task_id: str, step_id: str = ...) -> Step:
+    """
+    Get details about a specified task step.
+    """
+    step = next(
+        filter(
+            lambda t: t.task_id == task_id and t.step_id == step_id,
+            steps,
+        ),
+        None,
+    )
+    if not step:
+        raise Exception(f"Step with task id {task_id} and step id {step_id} not found")
+    return step
+
+
+class Agent:
+    @staticmethod
+    def handle_task(handler: Callable[[TaskInput | None], Awaitable[StepHandler]]):
+        global task_handler
+        task_handler = handler
+
+        return Agent
+
+    @staticmethod
+    def start(port: int = 8000):
+        config = Config()
+        config.bind = [f"localhost:{port}"]  # As an example configuration setting
+        asyncio.run(serve(app, config))
