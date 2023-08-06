9b0f8ed7caa4797db0299345ad76850a87f824a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:34 +0000

    Add package instadl/0.1/instadl-0.1.tar.gz

---
+++ packages/instadl/instadl-0.1.tar.gz/instadl-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='instadl',
+    version='0.1',
+    description='A library to download Instagram reels using instaloader',
+    packages=find_packages(),
+    install_requires=['instaloader'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
