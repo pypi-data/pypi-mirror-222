27e7be42a25bf7a653e5799fefe218f4a390929d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:27 +0000

    Add package ipyflow-core/0.0.178/ipyflow-core-0.0.178.tar.gz

---
+++ packages/ipyflow-core/ipyflow-core-0.0.178.tar.gz/ipyflow-core-0.0.178/ipyflow/utils/misc_utils.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from threading import Timer
+from typing import Callable
+
+
+class KeyDict(dict):
+    def __missing__(self, key):
+        return key
+
+
+def cleanup_discard(d, key, val):
+    s = d.get(key, set())
+    s.discard(val)
+    if len(s) == 0:
+        d.pop(key, None)
+
+
+def cleanup_pop(d, key, val):
+    d2 = d.get(key, {})
+    d2.pop(val, None)
+    if len(d2) == 0:
+        d.pop(key, None)
+
+
+def debounce(wait: float) -> Callable[[Callable[..., None]], Callable[..., None]]:
+    """Decorator that will postpone a functions
+    execution until after wait seconds
+    have elapsed since the last time it was invoked."""
+
+    def decorator(fn: Callable[..., None]) -> Callable[..., None]:
+        def debounced(*args, **kwargs) -> None:
+            def call_it():
+                fn(*args, **kwargs)
+
+            try:
+                debounced.t.cancel()  # type: ignore
+            except AttributeError:
+                pass
+            debounced.t = Timer(wait, call_it)  # type: ignore
+            debounced.t.start()  # type: ignore
+
+        return debounced
+
+    return decorator
+
+
+def yield_in_loop(*gens):
+    for gen in gens:
+        with gen:
+            yield
