9a073e0756a582711baf43535e9ec5659fe52f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:49 +0000

    Add package yvesCMD/0.0.1.1/yvesCMD-0.0.1.1.tar.gz

---
+++ packages/yvesCMD/yvesCMD-0.0.1.1.tar.gz/yvesCMD-0.0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yvesCMD',
+    version='0.0.1.1',
+    packages=["YvesCMD"],
+    install_requires=[],  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
