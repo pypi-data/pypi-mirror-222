d48e226b61b5546e20645be12c7b671eef066ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:29 +0000

    Add package midify/0.3.1/midify-0.3.1.tar.gz

---
+++ packages/midify/midify-0.3.1.tar.gz/midify-0.3.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='midify',
+    version='0.3.1',
+    packages=['midify'],
+    description='Command line tool for converting Audio to MIDI.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Daniel Santana',
+    author_email='danielsantanarocha@gmail.com',
+    url='https://github.com/DanielSanRocha/midify',
+    scripts=['midify/midify'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ],
+    install_requires=['click>=8.0.0', 'pretty-midi>=0.2.10', 'scipy>=1.0.0', 'numpy>=1.24.0', 'librosa>=0.10.0']
+)
