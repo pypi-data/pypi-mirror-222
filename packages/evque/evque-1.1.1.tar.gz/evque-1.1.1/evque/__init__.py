c32420421b825863a87744ff0d5122c55e99f906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:32 +0000

    Add package evque/1.1.1/evque-1.1.1.tar.gz

---
+++ packages/evque/evque-1.1.1.tar.gz/evque-1.1.1/evque/__init__.py
@@ -0,0 +1,12 @@
+from .evque import EvQueue
+
+# Create a singleton instance of EventQueue
+event_queue = EvQueue()
+
+# Expose the functions directly from the singleton instance
+subscribe = event_queue.subscribe
+publish = event_queue.publish
+publish_relative = event_queue.publish_relative
+run_until = event_queue.run_until
+empty = event_queue.empty
+
