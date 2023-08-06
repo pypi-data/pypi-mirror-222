f6a21d91f64bfb930e197f7b3bcc990dee2833c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:04 +0000

    Add package quiltcore/0.3.1/quiltcore-0.3.1.tar.gz

---
+++ packages/quiltcore/quiltcore-0.3.1.tar.gz/quiltcore-0.3.1/quiltcore/namespace.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from .resource_path import ResourcePath
+
+
+class Namespace(ResourcePath):
+    """
+    Namespace of Manifests by Hash
+    list/get returns a specific Manifest
+    """
+    SEP = "/"
+
+    def __init__(self, path: Path, **kwargs):
+        super().__init__(path, **kwargs)
+        self.manifests = kwargs[self.KEY_MAN]
+        self.name = self.pkg_name()
+
+    def hash(self, tag: str = ResourcePath.TAG_DEFAULT) -> str:
+        hash_file = self.path / tag
+        return hash_file.read_text()
+
+    def _child_path(self, key: str) -> Path:
+        """Return the path for a child resource."""
+        # TODO: match on partial hashes
+        hash = self.hash(key)
+        return self.manifests / hash
+
+    def pkg_name(self) -> str:
+        return self.SEP.join(self.path.parts[-2:])
