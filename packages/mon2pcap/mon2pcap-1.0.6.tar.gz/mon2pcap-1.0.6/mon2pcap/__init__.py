d0bb6a7cce3c336e07ad490426f3bb5714a83cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:30 +0000

    Add package mon2pcap/1.0.6/mon2pcap-1.0.6.tar.gz

---
+++ packages/mon2pcap/mon2pcap-1.0.6.tar.gz/mon2pcap-1.0.6/mon2pcap/__init__.py
@@ -0,0 +1,10 @@
+"""
+Library to convert StarOS "monitor subscriber" or "monitor protocol" ASCII dump to PCAP
+"""
+
+import logging
+
+from .mon2pcap import Mon2Pcap
+__all__ = ["Mon2Pcap"]
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
