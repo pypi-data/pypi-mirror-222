9051bddd4242d212c4088953d5182e1cff7a935c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:29 +0000

    Add package pytest-skip-markers/1.4.1/pytest-skip-markers-1.4.1.tar.gz

---
+++ packages/pytest-skip-markers/pytest-skip-markers-1.4.1.tar.gz/pytest-skip-markers-1.4.1/tests/functional/test_skip_on_env.py
@@ -0,0 +1,52 @@
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""
+Test the ``@pytest.mark.skip_on_env`` marker.
+"""
+import os
+import sys
+from unittest import mock
+
+import pytest
+
+pytestmark = [
+    pytest.mark.skipif(
+        sys.platform.startswith("win")
+        and sys.version_info >= (3, 8)
+        and sys.version_info < (3, 10),
+        reason="PyTest's capture and pytester.runpytest_inprocess looks broken on Windows and Py(>3.8,<3.10)",
+    ),
+]
+
+
+def test_skipped(pytester):
+    pytester.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.skip_on_env("FOOBAR")
+        def test_one():
+            assert True
+        """
+    )
+    with mock.patch.dict(os.environ, FOOBAR="1"):
+        res = pytester.runpytest_inprocess()
+        res.assert_outcomes(skipped=1)
+    res.stdout.no_fnmatch_line("*PytestUnknownMarkWarning*")
+
+
+def test_not_skipped(pytester):
+    pytester.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.skip_on_env("FOOBAR", eq="2")
+        def test_one():
+            assert True
+        """
+    )
+    with mock.patch.dict(os.environ, FOOBAR="1"):
+        res = pytester.runpytest_inprocess()
+        res.assert_outcomes(passed=1)
+    res.stdout.no_fnmatch_line("*PytestUnknownMarkWarning*")
