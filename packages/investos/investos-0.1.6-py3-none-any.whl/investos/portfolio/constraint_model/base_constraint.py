11a1bf7a628a34dbee14daeb81ecb8c1cbcfda9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:01 +0000

    Add package investos/0.1.6/investos-0.1.6-py3-none-any.whl

---
+++ packages/investos/investos-0.1.6-py3-none-any.whl/investos/portfolio/constraint_model/base_constraint.py
@@ -0,0 +1,13 @@
+# http://web.cvxr.com/cvx/doc/basics.html#constraints
+class BaseConstraint:
+    """
+    Base class for constraint objects used in convex portfolio optimization strategies.
+
+    Subclass `BaseConstraint`, and create your own `weight_expr` method to create custom constraints.
+    """
+
+    def __init__(self, **kwargs):
+        self.optimizer = None  # Set during Optimizer initialization
+
+    def weight_expr(self, t, w_plus, z, v):
+        raise NotImplementedError
---
+++ packages/investos/investos-0.1.6-py3-none-any.whl/investos/portfolio/strategy/base_strategy.py
@@ -0,0 +1,37 @@
+import datetime as dt
+
+import pandas as pd
+
+
+class BaseStrategy:
+    """Base class for an optimization strategy.
+
+    Must implement :py:meth:`~investos.portfolio.strategy.base_strategy.BaseStrategy.generate_trade_list` as per below.
+
+    Attributes
+    ----------
+    costs : list
+        Cost models evaluated during optimization strategy. Defaults to empty list. See :py:class:`~investos.portfolio.cost_model.base_cost.BaseCost` for cost model base class.
+    constraints : list
+        Constraints applied for optimization strategy. Defaults to empty list. See [TBU] for optimization model base class.
+    """
+
+    def __init__(self):
+        self.costs = []
+        self.constraints = []
+        self.risk_model = None
+
+    def _zerotrade(self, holdings):
+        return pd.Series(index=holdings.index, data=0.0)
+
+    def generate_trade_list(self, holdings: pd.Series, t: dt.datetime) -> pd.Series:
+        """Calculates and returns trade list (in units of currency passed in), given (added) optimization logic.
+
+        Parameters
+        ----------
+        holdings : pandas.Series
+            Holdings at beginning of period `t`.
+        t : datetime.datetime
+            The datetime for associated holdings `holdings`.
+        """
+        raise NotImplementedError
