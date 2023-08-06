831622c7811733a4f6ce3abc9f7d0a73f6bce2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:12 +0000

    Add package sciencehelper/1.0.0/sciencehelper-1.0.0.tar.gz

---
+++ packages/sciencehelper/sciencehelper-1.0.0.tar.gz/sciencehelper-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='sciencehelper',
+version='1.0.0',description='test',
+author='Sifer',
+author_email='sifed@qq.com',
+url='https://sife-shuo.github.io/',
+py_modules = ['sciencehelper'],
+)
