df6d22b87826f963e383dd1799758a4e56f449bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:11 +0000

    Add package lions-410e-midyear-2023-common/1.1.0/lions_410e_midyear_2023_common-1.1.0.tar.gz

---
+++ packages/lions-410e-midyear-2023-common/lions_410e_midyear_2023_common-1.1.0.tar.gz/lions_410e_midyear_2023_common-1.1.0/lions_410e_midyear_2023_common/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import Any, ClassVar, List, Optional
+
+try:
+    import constants
+except ImportError:
+    from . import constants
+
+from pydantic import BaseModel
+
+
+class Registration(BaseModel):
+    reg_num: Optional[int]
+    first_names: str
+    last_name: str
+    name_badge: str
+    cell: str
+    email: str
+    dietary: Optional[str]
+    disability: Optional[str]
+    lion: bool
+    club: str
+    timestamp: datetime
+    reg_num_string: Optional[str]
+    full_name: Optional[str]
+    auto_name_badge: Optional[bool]
+    cost: Optional[int]
+    deleted: bool = False
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        if self.reg_num:
+            self.reg_num_string = f"MY{self.reg_num:03}"
+        self.full_name = f"{self.first_names} {self.last_name}"
+        if not self.name_badge:
+            self.name_badge = self.full_name
+            self.auto_name_badge = True
+        self.cost = (
+            constants.COST_REG_LION if self.lion else constants.COST_REG_NON_LION
+        )
