a5d7908ce031f9eb0077a4346b07045191b05ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:20 +0000

    Add package SigProfilerAssignment/0.0.31/SigProfilerAssignment-0.0.31.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.31.tar.gz/SigProfilerAssignment-0.0.31/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.31'
+version = '0.0.31'
+Update = 'v0.0.31: Resolve issue with export probability per mutation for exome data'
+
+    
+    
