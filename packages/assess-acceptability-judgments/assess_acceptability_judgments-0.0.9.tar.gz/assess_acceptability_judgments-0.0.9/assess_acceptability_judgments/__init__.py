0a320420e855e91f4a8fc1c5739c4c57ce4f09df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:25 +0000

    Add package assess-acceptability-judgments/0.0.9/assess_acceptability_judgments-0.0.9.tar.gz

---
+++ packages/assess-acceptability-judgments/assess_acceptability_judgments-0.0.9.tar.gz/assess_acceptability_judgments-0.0.9/assess_acceptability_judgments/__init__.py
@@ -0,0 +1,8 @@
+from .constituency_parser import *
+from .dependency_parser import *
+from .nce import *
+from .ppl import *
+from .slor import *
+from .version import __version__
+
+CACHE_PATH = os.path.join(os.path.expanduser("~"), ".cache", "aaj")
