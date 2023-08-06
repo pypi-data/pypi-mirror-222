9c42ebb7f33dfceb8e365c4a43d24011419ee004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:09 +0000

    Add package labelu/0.6.0/labelu-0.6.0.tar.gz

---
+++ packages/labelu/labelu-0.6.0.tar.gz/labelu-0.6.0/labelu/internal/statics/frontend_version.js
@@ -0,0 +1,10 @@
+(function () {
+  window.__frontend = {
+    version: '2.1.0',
+    deps: {
+      '@label-u/annotation': '2.1.0',
+      '@label-u/components': '2.1.0',
+      '@label-u/formatter': '1.0.1',
+    },
+  };
+})();
