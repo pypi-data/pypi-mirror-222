5eaa32d7b07af8776633792348cf09d177654b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:42 +0000

    Add package sqlmesh/0.23.1.dev23/sqlmesh-0.23.1.dev23.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.23.1.dev23.tar.gz/sqlmesh-0.23.1.dev23/tests/core/test_seed.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import pytest
+from sqlglot import exp
+
+from sqlmesh.core.model.seed import Seed
+
+
+def test_read():
+    content = """key,value,ds
+1,one,2022-01-01
+2,two,2022-01-02
+3,three,2022-01-03
+"""
+    seed = Seed(content=content)
+
+    assert seed.columns_to_types == {
+        "key": exp.DataType.build("bigint"),
+        "value": exp.DataType.build("text"),
+        "ds": exp.DataType.build("text"),
+    }
+
+    expected_df = pd.DataFrame(
+        data={
+            "key": [1, 2, 3],
+            "value": ["one", "two", "three"],
+            "ds": ["2022-01-01", "2022-01-02", "2022-01-03"],
+        }
+    )
+
+    dfs = seed.read(batch_size=2)
+    pd.testing.assert_frame_equal(next(dfs), expected_df.iloc[:2, :])
+    pd.testing.assert_frame_equal(next(dfs), expected_df.iloc[2:, :])
+
+    with pytest.raises(StopIteration):
+        next(dfs)
+
+
+def test_column_hashes():
+    content = """key,value,ds
+1,one,2022-01-01
+2,two,2022-01-02
+3,three,2022-01-03
+"""
+    seed = Seed(content=content)
+    assert seed.column_hashes == {
+        "key": "122302783",
+        "value": "1969959181",
+        "ds": "725407375",
+    }
+
+    content_column_changed = """key,value,ds
+1,one,2022-01-01
+2,two,2022-01-05
+3,three,2022-01-03
+
+"""
+    seed_column_changed = Seed(content=content_column_changed)
+    assert seed_column_changed.column_hashes == {
+        **seed.column_hashes,
+        "ds": "3396890652",
+    }
