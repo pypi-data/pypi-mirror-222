1afbc082b071a02dc48079fdb326819e67e345f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:24 +0000

    Add package oceanum/0.8.1/oceanum-0.8.1.tar.gz

---
+++ packages/oceanum/oceanum-0.8.1.tar.gz/oceanum-0.8.1/oceanum/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for oceanum."""
+
+__author__ = """Oceanum Developers"""
+__email__ = "developers@oceanum.science"
+__version__ = "0.8.1"
+
+
+# Suppress tracebacks in an ipython environment
+try:
+    import sys
+
+    ipython = get_ipython()
+
+    def exception_handler(exception_type, exception, traceback):
+        print("%s: %s" % (exception_type.__name__, exception), file=sys.stderr)
+
+    ipython._showtraceback = exception_handler
+except:
+    pass
