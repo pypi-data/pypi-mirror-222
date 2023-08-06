f161191224f02db4d896410fcf22c5e9183da006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:07 +0000

    Add package dbxslackmessage/0.1.3/dbxslackmessage-0.1.3.tar.gz

---
+++ packages/dbxslackmessage/dbxslackmessage-0.1.3.tar.gz/dbxslackmessage-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbxslackmessage',
+    version='0.1.3',
+    author='Vaishali',
+    author_email='vkhairnar@ripple.com',
+    description='Package to send slack messages via databricks notebooks',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
