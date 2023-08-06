2f427a16380d90d75e2805831e9e0a3abd1b7d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:55 +0000

    Add package starlette_logger/0.0.2/starlette_logger-0.0.2.tar.gz

---
+++ packages/starlette_logger/starlette_logger-0.0.2.tar.gz/starlette_logger-0.0.2/starlette_logger/__init__.py
@@ -0,0 +1,10 @@
+"""
+starlette_logger provides two http scoped middlewares to a starlette (or FastAPI) based application that concern
+request handling
+"""
+
+from .middleware import RequestIdMiddleware, RequestLoggerMiddleware
+
+
+__version__ = "0.0.2"
+__all__ = ["RequestIdMiddleware", "RequestLoggerMiddleware"]
