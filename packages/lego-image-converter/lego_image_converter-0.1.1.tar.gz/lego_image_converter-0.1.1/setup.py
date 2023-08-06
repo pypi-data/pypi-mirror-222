f3489398b44ffaf4db6414fdb36339923e6c5092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:25 +0000

    Add package lego-image-converter/0.1.1/lego_image_converter-0.1.1.tar.gz

---
+++ packages/lego-image-converter/lego_image_converter-0.1.1.tar.gz/lego_image_converter-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='lego_image_converter',
+    packages=['lego_image_converter'],
+    version='0.1.1',
+    license='MIT',
+    description='First minor update after formal release',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Zequn Li',
+    author_email='zequn1992@gmail.com',
+    url='https://github.com/zl3311/lego_image_converter',
+    install_requires=['requests', 'numpy', 'matplotlib', 'pillow', 'basic_colormath'],
+    keywords=["pypi", "lego_image_converter", "lego", "image", "8bit", "art"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+
+    download_url="https://github.com/zl3311/lego_image_converter/archive/refs/tags/0.1.1.tar.gz",
+)
