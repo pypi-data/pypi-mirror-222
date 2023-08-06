be38e4abca4854ae63c93a92a42fe9fcad3cb0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:21 +0000

    Add package body-extractor-py3/0.0.2/body-extractor-py3-0.0.2.tar.gz

---
+++ packages/body-extractor-py3/body-extractor-py3-0.0.2.tar.gz/body-extractor-py3-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='body-extractor-py3',
+    version='0.0.2',
+    packages=['body_extractor'],
+    url='https://github.com/supercoderhawk/html-body-extractor',
+    license='MIT License',
+    author='supercoderhawk',
+    author_email='supercoderhawk@gmail.com',
+    description='HTML body content extractor',
+    install_requires=[
+        "beautifulsoup4 >= 4.5.1",
+        "lxml >= 3.6.4",
+        "requests >= 2.11.1"
+    ]
+)
