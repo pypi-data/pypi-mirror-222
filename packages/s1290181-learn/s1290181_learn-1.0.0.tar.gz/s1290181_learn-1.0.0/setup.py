612787e67ba4e3827a6e04ec75e1ee376986be86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:29 +0000

    Add package s1290181-learn/1.0.0/s1290181_learn-1.0.0.tar.gz

---
+++ packages/s1290181-learn/s1290181_learn-1.0.0.tar.gz/s1290181_learn-1.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="s1290181_learn",
+    version="1.0.0",
+    author="TakanoRiku",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['pandas', 'plotly','pami'],
+)
