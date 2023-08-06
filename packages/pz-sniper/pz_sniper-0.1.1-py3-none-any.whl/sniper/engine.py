1d972d2c7855a514818245b5b6cb17e4d6a6a6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:13 +0000

    Add package pz-sniper/0.1.1/pz_sniper-0.1.1-py3-none-any.whl

---
+++ packages/pz-sniper/pz_sniper-0.1.1-py3-none-any.whl/sniper/engine.py
@@ -0,0 +1,31 @@
+import sniper.snapshot as snapshot
+import sniper.ami as ami
+
+# @todo: maybe this shouild return something rather than printing things
+
+def process(rows, args):
+    dry_run = not args.delete
+    for row in rows:
+        print("★","\t",row['type'],row['id'],row['region'])
+        match row['type']:
+            case "Snapshot":
+                try:
+                    info = snapshot.describe(row['id'], row['region'])
+                    if snapshot.has_deleteme_tag(info):
+                        snapshot.delete(row['id'], dry_run, row['region'])
+                    else:
+                        print(row['type'],row['id'],"did not have delete tag")
+                except Exception as e:
+                    print(e, "skipping")
+            case "Image":
+                try:
+                    info = ami.describe(row['id'], row['region'])
+                    if ami.has_deleteme_tag(info):
+                        ami.deregister(row['id'], dry_run, row['region'])
+                    else:
+                        print(row['type'],row['id'],"did not have delete tag")
+                except Exception as e:
+                    print(e, "skipping")
+            case _:
+                print("method not implemented for type",row['type'])
+        print()
