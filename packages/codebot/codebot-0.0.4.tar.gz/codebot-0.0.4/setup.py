0a9a66da0e8f618d2a7def48cbe11b07514938d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:36 +0000

    Add package codebot/0.0.4/codebot-0.0.4.tar.gz

---
+++ packages/codebot/codebot-0.0.4.tar.gz/codebot-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codebot",
+    version="0.0.4",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "codebot = src:main",
+        ],
+    },
+    install_requires=[
+        "chainlit==0.5.2",
+        "loguru==0.5.3",
+        "tiktoken==0.4.0",
+        "prompt_toolkit==3.0.39",
+        "jupyter",
+        "mysql-connector-python",
+        "pillow",
+    ],
+    include_package_data=True,  # Add this line
+)
