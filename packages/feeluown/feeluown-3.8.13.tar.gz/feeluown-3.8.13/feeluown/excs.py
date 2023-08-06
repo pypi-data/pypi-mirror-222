1f156f4a0780cd51d359539cc3c324ddef20b1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:04 +0000

    Add package feeluown/3.8.13/feeluown-3.8.13.tar.gz

---
+++ packages/feeluown/feeluown-3.8.13.tar.gz/feeluown-3.8.13/feeluown/excs.py
@@ -0,0 +1,94 @@
+"""
+HELP: I do not know how to design exception classes,
+as a result, these interfaces can be changed frequently.
+"""
+
+from requests.exceptions import RequestException
+
+
+class FuoException(Exception):
+    pass
+
+
+class LibraryException(FuoException):
+    pass
+
+
+class ProviderIOError(LibraryException, RequestException):
+    """Read/write data from/to provider failed
+
+    currently, all providers use requests to send http request,
+    and many Requestexception are not catched, so ProviderIOError
+    inherit RequestException.
+    """
+
+    def __init__(self, message='', provider=None):
+        super().__init__(message)
+
+        self.message = message
+        self.provider = provider
+
+    def __str__(self):
+        if self.provider is None:
+            return self.message
+        return '[{}] {}'.format(self.provider, self.message)
+
+
+class CreateReaderFailed(ProviderIOError):
+    """(DEPRECATED) use ProviderIOError instead"""
+
+
+class ReaderException(ProviderIOError):
+    """(DEPRECATED) use ProviderIOError instead"""
+
+
+class ReadFailed(ProviderIOError):
+    """(DEPRECATED) use ProviderIOError instead"""
+
+
+class ResourceNotFound(LibraryException):
+    pass
+
+
+class ProviderAlreadyRegistered(LibraryException):
+    pass
+
+
+class ProviderNotFound(ResourceNotFound):
+    pass
+
+
+class ModelNotFound(ResourceNotFound):
+    """Model is not found
+
+    For example, a model identifier is invalid.
+
+    .. versionadded:: 3.7.7
+    """
+
+
+class NotSupported(LibraryException):
+    """Provider does not support the operation
+    """
+
+    def __init__(self, *args, provider=None, **kwargs):
+        self.provider = provider
+
+    @classmethod
+    def create_by_p_p(cls, provider, protocol_cls):
+        if isinstance(provider, str):
+            pid = provider
+        else:
+            if hasattr(provider, 'meta'):
+                pid = provider.meta.identifier
+            else:
+                pid = provider.identifier
+        return cls(f'provider:{pid} does not support {protocol_cls.__name__}')
+
+
+class MediaNotFound(ResourceNotFound):
+    pass
+
+
+class NoUserLoggedIn(LibraryException):
+    """(DEPRECATED) return None when there is no user logged in"""
