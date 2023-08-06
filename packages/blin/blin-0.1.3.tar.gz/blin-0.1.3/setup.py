d53192a3a8b89ebfd822a5e84d74379c33b0cac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:21 +0000

    Add package blin/0.1.3/blin-0.1.3.tar.gz

---
+++ packages/blin/blin-0.1.3.tar.gz/blin-0.1.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from pathlib import Path
+
+this_directory   = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+  name="blin",
+  version="0.1.3",
+  author="lemeni.com",
+  description="Python client library for Blin API (natural language to API service)",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages=["blin"]
+  )
