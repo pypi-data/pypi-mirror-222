20edf29ce8d1a7e24594b207505b4d99b205e4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:01 +0000

    Add package lego-image-converter/0.0.3/lego_image_converter-0.0.3.tar.gz

---
+++ packages/lego-image-converter/lego_image_converter-0.0.3.tar.gz/lego_image_converter-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='lego_image_converter',  # should match the package folder
+    packages=['lego_image_converter'],  # should match the package folder
+    version='0.0.3',  # important for updates
+    license='MIT',  # should match your chosen license
+    description='Testing installation of Package 3',
+    long_description=long_description,  # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    author='Zequn Li',
+    author_email='zequn1992@gmail.com',
+    url='https://github.com/mike-huls/toolbox_public',
+    install_requires=['requests', 'numpy', 'matplotlib', 'pillow', 'basic_colormath'],  # list all packages that your package uses
+    keywords=["pypi", "lego_image_converter", "Lego", "image", "8bit", "art"],  # descriptive meta-data
+    classifiers=[  # https://pypi.org/classifiers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+
+    download_url="https://github.com/zl3311/lego_image_converter/archive/refs/tags/0.0.3.tar.gz",
+)
