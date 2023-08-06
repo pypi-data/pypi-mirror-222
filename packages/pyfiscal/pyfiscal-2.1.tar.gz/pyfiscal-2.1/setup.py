3bf61503a4e7e797385210f1165311b5672cc6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:45 +0000

    Add package pyfiscal/2.1/pyfiscal-2.1.tar.gz

---
+++ packages/pyfiscal/pyfiscal-2.1.tar.gz/pyfiscal-2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+	name = 'pyfiscal',
+	packages = ['pyfiscal'],
+	version = '2.1',
+	description = 'Pyfiscal 2.7-3.8 calculation of tax data.',
+	author='Tomás Gonzalez',
+	author_email = 'thom.sgonzalez@gmail.com',
+	url = 'https://github.com/thomgonzalez/pyfiscal',
+	keywords = ['RFC', 'CURP', 'NSS', 'fiscal', 'tax'],
+	classifiers = [
+		'Programming Language :: Python',
+   		'Programming Language :: Python :: 2.7',  
+        'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.5',
+  		'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    	'Programming Language :: Python :: 3.8',
+	],
+)
