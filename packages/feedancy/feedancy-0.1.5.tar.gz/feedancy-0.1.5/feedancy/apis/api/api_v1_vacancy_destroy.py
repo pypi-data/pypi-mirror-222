4b18cb16cb85980cffa6eae6db5ce2b649d2f163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:26 +0000

    Add package feedancy/0.1.5/feedancy-0.1.5.tar.gz

---
+++ packages/feedancy/feedancy-0.1.5.tar.gz/feedancy-0.1.5/feedancy/apis/api/api_v1_vacancy_destroy.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+import datetime
+import pydantic
+import typing
+
+from pydantic import BaseModel
+
+from feedancy.lib.base import BaseApi
+from feedancy.lib.request import ApiRequest
+from feedancy.lib import json
+class Salary(BaseModel):
+    currency: str 
+    id: int 
+    max_value: typing.Optional[typing.Union[int, None]]  = None
+    min_value: typing.Optional[typing.Union[int, None]]  = None
+
+class Vacancy(BaseModel):
+    company: str 
+    contract_type: typing.Optional[typing.Union[typing.Union[str, str, str], None]]  = None
+    employment_format: typing.Optional[typing.Union[typing.Union[str, str, str], None]]  = None
+    experience: typing.Optional[typing.Union[typing.Union[str, str, str], None]]  = None
+    external_id: str 
+    has_insurance: typing.Optional[typing.Union[bool, None]]  = None
+    has_portfolio: typing.Optional[typing.Union[bool, None]]  = None
+    has_test_task: typing.Optional[typing.Union[bool, None]]  = None
+    id: int 
+    is_relocation_required: typing.Optional[typing.Union[bool, None]]  = None
+    link: str 
+    name: str 
+    publicated_at: typing.Optional[typing.Union[datetime.datetime, None]]  = None
+    raw_description: typing.Optional[typing.Union[str, None]]  = None
+    requirement: typing.Optional[typing.Union[str, None]]  = None
+    responsibility: typing.Optional[typing.Union[str, None]]  = None
+    salary: Salary 
+    source: str 
+    test_task_link: typing.Optional[typing.Union[str, None]]  = None
+    work_format: typing.Optional[typing.Union[typing.Union[str, str, str], None]]  = None
+
+def make_request(self: BaseApi,
+
+
+    id: str,
+
+) -> Vacancy:
+    
+
+    
+    body = None
+    
+
+    m = ApiRequest(
+        method="DELETE",
+        path="/api/v1/vacancy/{id}/".format(
+            
+                id=id,
+            
+        ),
+        content_type=None,
+        body=body,
+        headers=self._only_provided({
+        }),
+        query_params=self._only_provided({
+        }),
+        cookies=self._only_provided({
+        }),
+    )
+    return self.make_request({
+    
+        "200": {
+            
+                "application/json": Vacancy,
+            
+        },
+    
+    }, m)
