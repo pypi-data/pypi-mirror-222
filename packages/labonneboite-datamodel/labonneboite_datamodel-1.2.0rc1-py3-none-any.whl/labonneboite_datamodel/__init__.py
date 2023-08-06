52745a2d20a88f9f84a503e9ba0e6d4ca02b9a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:49 +0000

    Add package labonneboite-datamodel/1.2.0rc1/labonneboite_datamodel-1.2.0rc1-py3-none-any.whl

---
+++ packages/labonneboite-datamodel/labonneboite_datamodel-1.2.0rc1-py3-none-any.whl/labonneboite_datamodel/__init__.py
@@ -0,0 +1,16 @@
+# needed for alembic migrations
+from .job import Job, Naf
+from .rome import Rome, RomeNaf
+from .office import Office, OfficeGps, OfficeScore
+from .base import BaseMixin
+
+__all__ = [
+    "Job",
+    "Naf",
+    "Office",
+    "OfficeGps",
+    "OfficeScore",
+    "BaseMixin",
+    "Rome",
+    "RomeNaf"
+]
