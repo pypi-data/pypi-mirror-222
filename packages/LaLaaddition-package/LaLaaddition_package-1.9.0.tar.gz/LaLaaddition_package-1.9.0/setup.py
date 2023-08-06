aa37329a27b3d8781cee355d4e1c7a6d96fe06c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:51 +0000

    Add package LaLaaddition-package/1.9.0/LaLaaddition_package-1.9.0.tar.gz

---
+++ packages/LaLaaddition-package/LaLaaddition_package-1.9.0.tar.gz/LaLaaddition_package-1.9.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='LaLaaddition_package',
+    version='1.9.0',
+    description='A simple package for performing addition',
+    author='Nirmit Sakre',
+    packages=['LaLaaddition_package'],
+    install_requires=[],
+)
