5009dbf79c191fb3da5b769a4252fffe1742743f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:46 +0000

    Add package python-config-server-client/1.0.0/python_config_server_client-1.0.0.tar.gz

---
+++ packages/python-config-server-client/python_config_server_client-1.0.0.tar.gz/python_config_server_client-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+
+    name = 'python_config_server_client', 
+
+    version = '1.0.0',
+    
+
+    description = 'A config_server_client package for python framework',
+ 
+    author = 'Jayant',
+    author_email = 'jayantreddybodkurwar@gmail.com',
+    install_requires = [
+        'requests',
+    ],
+
+)
