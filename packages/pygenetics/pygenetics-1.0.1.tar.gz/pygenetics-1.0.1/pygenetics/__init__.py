bb7a6c1d218b30ab58ab37b0ba0600a66722aab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:32 +0000

    Add package pygenetics/1.0.1/pygenetics-1.0.1.tar.gz

---
+++ packages/pygenetics/pygenetics-1.0.1.tar.gz/pygenetics-1.0.1/pygenetics/__init__.py
@@ -0,0 +1,7 @@
+from pygenetics.population import Population
+from pygenetics.member import Member
+from pygenetics.parameter import Parameter
+import pygenetics.utils
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution('pygenetics').version
