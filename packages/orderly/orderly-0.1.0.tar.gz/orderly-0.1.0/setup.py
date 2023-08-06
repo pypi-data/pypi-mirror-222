2cf0dc4ee6801c83b7e084f07951b92ba8da0881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:45 +0000

    Add package orderly/0.1.0/orderly-0.1.0.tar.gz

---
+++ packages/orderly/orderly-0.1.0.tar.gz/orderly-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="orderly",
+    version="0.1.0",
+    description="A wrapper for downloading ORD-schema data, extracting and cleaning the data",
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author=[
+        "Daniel S. Wigh <dsw46@cam.ac.uk>",
+        "Joe Arrowsmith <joearrowsmith0@gmail.com>",
+        "Alexander Pomberger <ap2153@cam.ac.uk>",
+        "Kobi C. Felton <kcmf2@cam.ac.uk>",
+        "Alexei A. Lapkin <aal35@cam.ac.uk>",
+    ],
+    packages=["orderly"],
+)
