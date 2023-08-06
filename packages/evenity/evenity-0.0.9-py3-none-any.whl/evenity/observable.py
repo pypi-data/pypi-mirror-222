7ae8f34e38fb64a40d593abf135a8fd582870996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:27 +0000

    Add package evenity/0.0.9/evenity-0.0.9-py3-none-any.whl

---
+++ packages/evenity/evenity-0.0.9-py3-none-any.whl/evenity/observable.py
@@ -0,0 +1,20 @@
+"""Ovservable class"""
+
+class Observable:
+    """Observable class"""
+
+    def __init__(self):
+        self._observers = []
+
+    def register_observer(self, observer):
+        """Register an observer"""
+        self._observers.append(observer)
+
+    def unregister_observer(self, observer):
+        """Unregister an observer"""
+        self._observers.remove(observer)
+
+    def notify_observers(self, listener, event):
+        """Notify all observers"""
+        for obs in self._observers:
+            obs.notify(listener, event)
