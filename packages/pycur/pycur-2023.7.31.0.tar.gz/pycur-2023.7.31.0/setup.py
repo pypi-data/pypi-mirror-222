1de375dd651231bae5aa9aed1a62b0c3abdfe9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:04 +0000

    Add package pycur/2023.7.31.0/pycur-2023.7.31.0.tar.gz

---
+++ packages/pycur/pycur-2023.7.31.0.tar.gz/pycur-2023.7.31.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+import pycur
+
+setuptools.setup(
+	name="pycur",
+	version=pycur.__version__,
+	author="RixTheTyrunt",
+	author_email="rixthetyrunt@gmail.com",
+	description="Modifying cursors is now easier than ever!",
+	packages=["pycur"],
+	url="https://pycur.glitch.me/",
+	python_requires=">=3",
+	long_description=open("../README.md").read(),
+	long_description_content_type="text/markdown"
+)
