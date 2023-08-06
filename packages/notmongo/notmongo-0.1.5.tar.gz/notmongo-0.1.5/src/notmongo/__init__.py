3308072a722eab805430cde51ccc8b54d4347a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:03 +0000

    Add package notmongo/0.1.5/notmongo-0.1.5.tar.gz

---
+++ packages/notmongo/notmongo-0.1.5.tar.gz/notmongo-0.1.5/src/notmongo/__init__.py
@@ -0,0 +1,7 @@
+from .async_collection import *
+from .async_database import *
+from .persistence import *
+from .sync_collection import *
+from .sync_database import *
+from .typedefs import *
+from .meta import __version__
