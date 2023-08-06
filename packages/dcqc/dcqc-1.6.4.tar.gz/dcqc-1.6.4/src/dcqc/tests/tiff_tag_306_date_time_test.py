f2da662e4d6eb103105f8bc6c0f7e81719472352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:06 +0000

    Add package dcqc/1.6.4/dcqc-1.6.4.tar.gz

---
+++ packages/dcqc/dcqc-1.6.4.tar.gz/dcqc-1.6.4/src/dcqc/tests/tiff_tag_306_date_time_test.py
@@ -0,0 +1,27 @@
+from dcqc.target import SingleTarget
+from dcqc.tests.base_test import ExternalBaseTest, Process
+
+
+class TiffTag306DateTimeTest(ExternalBaseTest):
+    tier = 4
+    pass_code = "1"
+    target: SingleTarget
+
+    def generate_process(self) -> Process:
+        path = self.target.file.stage()
+
+        command_args = [
+            "tifftools",
+            "dump",
+            f"'{path.name}'",
+            "|",
+            "grep",  # pipe the output
+            "-a",  # treat input as text
+            "-q",  # suppress output
+            "'DateTime 306 (0x132) ASCII'",  # match the DateTime 306 tag
+        ]
+        process = Process(
+            container="ghcr.io/sage-bionetworks-workflows/tifftools:latest",
+            command_args=command_args,
+        )
+        return process
