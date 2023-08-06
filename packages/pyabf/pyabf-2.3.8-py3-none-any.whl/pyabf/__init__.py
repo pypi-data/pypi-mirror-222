1757221eaf9b4de1e0f6481c44445042c12f8a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:49 +0000

    Add package pyabf/2.3.8/pyabf-2.3.8-py3-none-any.whl

---
+++ packages/pyabf/pyabf-2.3.8-py3-none-any.whl/pyabf/__init__.py
@@ -0,0 +1,35 @@
+"""
+pyABF - A Python interface to files in the Axon Binary Format (ABF)
+    by Scott Harden
+
+Documentation and code examples, and more can be found at:
+    https://github.com/swharden/pyABF
+"""
+__version__ = '2.3.8'
+
+import sys
+import os
+
+from pyabf.abf import ABF
+from pyabf.atf import ATF
+
+def info():
+    """display information about the pyabf package."""
+    import platform
+    import numpy
+    print()
+    print("### pyABF Information ###")
+    print("Python", sys.version)
+    print("System:", platform.system(), platform.release())
+    print("numpy version:", numpy.__version__)
+    print("pyabf version:", __version__)
+    print("pyabf path:" , os.path.abspath(os.path.dirname(__file__)))
+    print()
+    
+def showInfo():
+    info()
+
+def help():
+    """launch the pyABF project page in a browser."""
+    import webbrowser
+    webbrowser.open("http://github.com/swharden/pyABF")
