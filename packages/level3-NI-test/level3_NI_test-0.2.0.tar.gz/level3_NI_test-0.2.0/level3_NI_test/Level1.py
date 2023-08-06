94a3dbdca8748dd726895047c32381224ab6a1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:49 +0000

    Add package level3-NI-test/0.2.0/level3_NI_test-0.2.0.tar.gz

---
+++ packages/level3-NI-test/level3_NI_test-0.2.0.tar.gz/level3_NI_test-0.2.0/level3_NI_test/Level1.py
@@ -0,0 +1,9 @@
+with open("./sample001.csv", 'r') as f:
+    lines = f.read().split('\n')
+    for l in lines[1:]:
+        data = l.split(',')
+        line = ''
+        for f in data[1:]:
+            line += f'{f},'
+        line = line[:-1]
+        print(line)
