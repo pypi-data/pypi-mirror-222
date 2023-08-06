7d0fa851e3debf945b20ec4a6aaa1a7d8fef69db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:41 +0000

    Add package trulens-eval/0.8.0/trulens_eval-0.8.0-py3-none-any.whl

---
+++ packages/trulens-eval/trulens_eval-0.8.0-py3-none-any.whl/trulens_eval/app.py
@@ -0,0 +1,423 @@
+"""
+Generalized root type for various libraries like llama_index and langchain .
+"""
+
+from abc import ABC
+from abc import abstractmethod
+import logging
+from pprint import PrettyPrinter
+from typing import Any, Dict, Iterable, Optional, Sequence, Set, Tuple
+
+import pydantic
+from pydantic import Field
+
+from trulens_eval.db import DB
+from trulens_eval.feedback import Feedback
+from trulens_eval.instruments import Instrument
+from trulens_eval.schema import AppDefinition
+from trulens_eval.schema import Cost
+from trulens_eval.schema import FeedbackMode
+from trulens_eval.schema import FeedbackResult
+from trulens_eval.schema import Perf
+from trulens_eval.schema import Record
+from trulens_eval.schema import Select
+from trulens_eval.tru import Tru
+from trulens_eval.util import all_objects
+from trulens_eval.util import Class
+from trulens_eval.util import CLASS_INFO
+from trulens_eval.util import GetItemOrAttribute
+from trulens_eval.util import JSON
+from trulens_eval.util import JSON_BASES
+from trulens_eval.util import JSON_BASES_T
+from trulens_eval.util import json_str_of_obj
+from trulens_eval.util import jsonify
+from trulens_eval.util import JSONPath
+from trulens_eval.util import SerialModel
+from trulens_eval.util import TP
+
+logger = logging.getLogger(__name__)
+
+pp = PrettyPrinter()
+
+# App component.
+COMPONENT = Any
+
+# Component category.
+# TODO: Enum
+COMPONENT_CATEGORY = str
+
+
+class ComponentView(ABC):
+    """
+    Views of common app component types for sorting them and displaying them in
+    some unified manner in the UI. Operates on components serialized into json
+    dicts representing various components, not the components themselves.
+    """
+
+    def __init__(self, json: JSON):
+        self.json = json
+        self.cls = Class.of_json(json)
+
+    @staticmethod
+    def of_json(json: JSON) -> 'ComponentView':
+        """
+        Sort the given json into the appropriate component view type.
+        """
+
+        cls = Class.of_json(json)
+
+        if LangChainComponent.class_is(cls):
+            return LangChainComponent.of_json(json)
+        elif LlamaIndexComponent.class_is(cls):
+            return LlamaIndexComponent.of_json(json)
+        elif TrulensComponent.class_is(cls):
+            return TrulensComponent.of_json(json)
+        else:
+            raise TypeError(f"Unhandled component type with class {cls}")
+
+    @staticmethod
+    @abstractmethod
+    def class_is(cls: Class) -> bool:
+        """
+        Determine whether the given class representation `cls` is of the type to
+        be viewed as this component type.
+        """
+        pass
+
+    def unsorted_parameters(self, skip: Set[str]) -> Dict[str, JSON_BASES_T]:
+        """
+        All basic parameters not organized by other accessors.
+        """
+
+        ret = {}
+
+        for k, v in self.json.items():
+            if k not in skip and isinstance(v, JSON_BASES):
+                ret[k] = v
+
+        return ret
+
+    @staticmethod
+    def innermost_base(
+        bases: Sequence[Class],
+        among_modules=set(["langchain", "llama_index", "trulens_eval"])
+    ) -> str:
+        """
+        Given a sequence of classes, return the first one which comes from one
+        of the `among_modules`. You can use this to determine where ultimately
+        the encoded class comes from in terms of langchain, llama_index, or
+        trulens_eval even in cases they extend each other's classes. Returns
+        None if no module from `among_modules` is named in `bases`.
+        """
+
+        for base in bases:
+            if "." in base.module.module_name:
+                root_module = base.module.module_name.split(".")[0]
+            else:
+                root_module = base.module.module_name
+
+            if root_module in among_modules:
+                return root_module
+
+        return None
+
+
+class LangChainComponent(ComponentView):
+
+    @staticmethod
+    def class_is(cls: Class) -> bool:
+        if ComponentView.innermost_base(cls.bases) == "langchain":
+            return True
+
+        return False
+
+    @staticmethod
+    def of_json(json: JSON) -> 'LangChainComponent':
+        from trulens_eval.utils.langchain import component_of_json
+        return component_of_json(json)
+
+
+class LlamaIndexComponent(ComponentView):
+
+    @staticmethod
+    def class_is(cls: Class) -> bool:
+        if ComponentView.innermost_base(cls.bases) == "llama_index":
+            return True
+
+        return False
+
+    @staticmethod
+    def of_json(json: JSON) -> 'LlamaIndexComponent':
+        from trulens_eval.utils.llama import component_of_json
+        return component_of_json(json)
+
+
+class TrulensComponent(ComponentView):
+    """
+    Components provided in trulens.
+    """
+
+    def class_is(cls: Class) -> bool:
+        if ComponentView.innermost_base(cls.bases) == "trulens_eval":
+            return True
+
+        #if any(base.module.module_name.startswith("trulens.") for base in cls.bases):
+        #    return True
+
+        return False
+
+    @staticmethod
+    def of_json(json: JSON) -> 'TrulensComponent':
+        from trulens_eval.utils.trulens import component_of_json
+        return component_of_json(json)
+
+
+class Prompt(ComponentView):
+    # langchain.prompts.base.BasePromptTemplate
+    # llama_index.prompts.base.Prompt
+
+    @property
+    @abstractmethod
+    def template(self) -> str:
+        pass
+
+
+class LLM(ComponentView):
+    # langchain.llms.base.BaseLLM
+    # llama_index ???
+
+    @property
+    @abstractmethod
+    def model_name(self) -> str:
+        pass
+
+
+class Memory(ComponentView):
+    # langchain.schema.BaseMemory
+    # llama_index ???
+    pass
+
+
+class Other(ComponentView):
+    # Any component that does not fit into the other named categories.
+
+    pass
+
+
+def instrumented_component_views(
+    obj: object
+) -> Iterable[Tuple[JSONPath, ComponentView]]:
+    """
+    Iterate over contents of `obj` that are annotated with the CLASS_INFO
+    attribute/key. Returns triples with the accessor/selector, the Class object
+    instantiated from CLASS_INFO, and the annotated object itself.
+    """
+
+    for q, o in all_objects(obj):
+        if isinstance(o, pydantic.BaseModel) and CLASS_INFO in o.__fields__:
+            yield q, ComponentView.of_json(json=o)
+
+        if isinstance(o, Dict) and CLASS_INFO in o:
+            yield q, ComponentView.of_json(json=o)
+
+
+class App(AppDefinition, SerialModel):
+    """
+    Generalization of a wrapped model.
+    """
+
+    # Non-serialized fields here while the serialized ones are defined in
+    # `schema.py:App`.
+
+    # Feedback functions to evaluate on each record.
+    feedbacks: Sequence[Feedback] = Field(exclude=True)
+
+    # Database interfaces for models/records/feedbacks.
+    # NOTE: Maybe move to schema.App .
+    tru: Optional[Tru] = Field(exclude=True)
+
+    # Database interfaces for models/records/feedbacks.
+    # NOTE: Maybe mobe to schema.App .
+    db: Optional[DB] = Field(exclude=True)
+
+    # The wrapped app.
+    app: Any = Field(exclude=True)
+
+    # Instrumentation class.
+    instrument: Instrument = Field(exclude=True)
+
+    def __init__(
+        self,
+        tru: Optional[Tru] = None,
+        feedbacks: Optional[Sequence[Feedback]] = None,
+        **kwargs
+    ):
+
+        feedbacks = feedbacks or []
+
+        # for us:
+        kwargs['tru'] = tru
+        kwargs['feedbacks'] = feedbacks
+
+        super().__init__(**kwargs)
+
+        if tru is None:
+            if self.feedback_mode != FeedbackMode.NONE:
+                logger.debug("Creating default tru.")
+                tru = Tru()
+        else:
+            if self.feedback_mode == FeedbackMode.NONE:
+                logger.warn(
+                    "`tru` is specified but `feedback_mode` is FeedbackMode.NONE. "
+                    "No feedback evaluation and logging will occur."
+                )
+
+        self.tru = tru
+        if self.tru is not None:
+            self.db = tru.db
+
+            if self.feedback_mode != FeedbackMode.NONE:
+                logger.debug(
+                    "Inserting app and feedback function definitions to db."
+                )
+                self.db.insert_app(app=self)
+                for f in self.feedbacks:
+                    self.db.insert_feedback_definition(f)
+
+        else:
+            if len(feedbacks) > 0:
+                raise ValueError(
+                    "Feedback logging requires `tru` to be specified."
+                )
+
+        if self.feedback_mode == FeedbackMode.DEFERRED:
+            for f in feedbacks:
+                # Try to load each of the feedback implementations. Deferred
+                # mode will do this but we want to fail earlier at app
+                # constructor here.
+                f.implementation.load()
+
+        self.instrument.instrument_object(
+            obj=self.app, query=Select.Query().app
+        )
+
+    def json(self, *args, **kwargs):
+        # Need custom jsonification here because it is likely the model
+        # structure contains loops.
+
+        return json_str_of_obj(self.dict(), *args, **kwargs)
+
+    def dict(self):
+        # Same problem as in json.
+        return jsonify(self, instrument=self.instrument)
+
+    def _post_record(
+        self, ret_record_args, error, cost, start_time, end_time, record
+    ):
+        """
+        Final steps of record construction common among model types.
+        """
+
+        ret_record_args['main_error'] = str(error)
+        ret_record_args['calls'] = record
+        ret_record_args['cost'] = cost
+        ret_record_args['perf'] = Perf(start_time=start_time, end_time=end_time)
+        ret_record_args['app_id'] = self.app_id
+        ret_record_args['tags'] = self.tags
+
+        ret_record = Record(**ret_record_args)
+
+        if error is not None:
+            if self.feedback_mode == FeedbackMode.WITH_APP:
+                self._handle_error(record=ret_record, error=error)
+
+            elif self.feedback_mode in [FeedbackMode.DEFERRED,
+                                        FeedbackMode.WITH_APP_THREAD]:
+                TP().runlater(
+                    self._handle_error, record=ret_record, error=error
+                )
+
+            raise error
+
+        if self.feedback_mode == FeedbackMode.WITH_APP:
+            self._handle_record(record=ret_record)
+
+        elif self.feedback_mode in [FeedbackMode.DEFERRED,
+                                    FeedbackMode.WITH_APP_THREAD]:
+            TP().runlater(self._handle_record, record=ret_record)
+
+        return ret_record
+
+    def _handle_record(self, record: Record):
+        """
+        Write out record-related info to database if set.
+        """
+
+        if self.tru is None or self.feedback_mode is None:
+            return
+
+        record_id = self.tru.add_record(record=record)
+
+        if len(self.feedbacks) == 0:
+            return
+
+        # Add empty (to run) feedback to db.
+        if self.feedback_mode == FeedbackMode.DEFERRED:
+            for f in self.feedbacks:
+                self.db.insert_feedback(
+                    FeedbackResult(
+                        name=f.name,
+                        record_id=record_id,
+                        feedback_definition_id=f.feedback_definition_id
+                    )
+                )
+
+        elif self.feedback_mode in [FeedbackMode.WITH_APP,
+                                    FeedbackMode.WITH_APP_THREAD]:
+
+            results = self.tru.run_feedback_functions(
+                record=record, feedback_functions=self.feedbacks, app=self
+            )
+
+            for result in results:
+                self.tru.add_feedback(result)
+
+    def _handle_error(self, record: Record, error: Exception):
+        if self.db is None:
+            return
+
+    def instrumented(self,) -> Iterable[Tuple[JSONPath, ComponentView]]:
+        """
+        Enumerate instrumented components and their categories.
+        """
+
+        for q, c in instrumented_component_views(self.dict()):
+            # Add the chain indicator so the resulting paths can be specified
+            # for feedback selectors.
+            q = JSONPath(
+                path=(GetItemOrAttribute(item_or_attribute="__app__"),) + q.path
+            )
+            yield q, c
+
+    def print_instrumented(self) -> None:
+        """
+        Print instrumented components and their categories.
+        """
+
+        print(
+            "\n".join(
+                f"{t[1].__class__.__name__} of {t[1].__class__.__module__} component: "
+                f"{str(t[0])}" for t in self.instrumented()
+            )
+        )
+
+
+class TruApp(App):
+
+    def __init__(self, *args, **kwargs):
+        # Since 0.2.0
+        logger.warning(
+            "Class TruApp is deprecated, "
+            "use trulens_eval.app.App instead."
+        )
+        super().__init__(*args, **kwargs)
---
+++ packages/trulens-eval/trulens_eval-0.8.0-py3-none-any.whl/trulens_eval/tru_chain.py
@@ -0,0 +1,336 @@
+"""
+# Langchain instrumentation and monitoring.
+"""
+
+from datetime import datetime
+import logging
+from pprint import PrettyPrinter
+import traceback
+from typing import Any, ClassVar, Dict, List, Sequence, Tuple, Union
+
+# import nest_asyncio # NOTE(piotrm): disabling for now, need more investigation
+from pydantic import Field
+
+from trulens_eval.app import App
+from trulens_eval.instruments import Instrument
+from trulens_eval.provider_apis import Endpoint
+from trulens_eval.schema import Cost
+from trulens_eval.schema import Record
+from trulens_eval.schema import RecordAppCall
+from trulens_eval.util import Class
+from trulens_eval.util import FunctionOrMethod
+from trulens_eval.util import jsonify
+from trulens_eval.util import noserio
+from trulens_eval.util import OptionalImports
+from trulens_eval.util import REQUIREMENT_LANGCHAIN
+
+logger = logging.getLogger(__name__)
+
+pp = PrettyPrinter()
+
+with OptionalImports(message=REQUIREMENT_LANGCHAIN):
+    import langchain
+    from langchain.chains.base import Chain
+
+
+class LangChainInstrument(Instrument):
+
+    class Default:
+        MODULES = {"langchain."}
+
+        # Thunk because langchain is optional.
+        CLASSES = lambda: {
+            langchain.chains.base.Chain,
+            langchain.vectorstores.base.BaseRetriever,
+            langchain.schema.BaseRetriever,
+            langchain.llms.base.BaseLLM,
+            langchain.prompts.base.BasePromptTemplate,
+            langchain.schema.BaseMemory,
+            langchain.schema.BaseChatMessageHistory
+        }
+
+        # Instrument only methods with these names and of these classes.
+        METHODS = {
+            "_call": lambda o: isinstance(o, langchain.chains.base.Chain),
+            "__call__": lambda o: isinstance(o, langchain.chains.base.Chain),
+            "_acall": lambda o: isinstance(o, langchain.chains.base.Chain),
+            "acall": lambda o: isinstance(o, langchain.chains.base.Chain),
+            "_get_relevant_documents":
+                lambda o: True,  # VectorStoreRetriever, langchain >= 0.230
+        }
+
+    def __init__(self):
+        super().__init__(
+            root_methods=set([TruChain.call_with_record, TruChain.acall_with_record]),
+            modules=LangChainInstrument.Default.MODULES,
+            classes=LangChainInstrument.Default.CLASSES(),
+            methods=LangChainInstrument.Default.METHODS
+        )
+
+    def _instrument_dict(self, cls, obj: Any, with_class_info: bool = False):
+        """
+        Replacement for langchain's dict method to one that does not fail under
+        non-serialization situations.
+        """
+
+        return jsonify
+
+    def _instrument_type_method(self, obj, prop):
+        """
+        Instrument the Langchain class's method _*_type which is presently used
+        to control chain saving. Override the exception behaviour. Note that
+        _chain_type is defined as a property in langchain.
+        """
+
+        # Properties doesn't let us new define new attributes like "_instrument"
+        # so we put it on fget instead.
+        if hasattr(prop.fget, Instrument.INSTRUMENT):
+            prop = getattr(prop.fget, Instrument.INSTRUMENT)
+
+        def safe_type(s) -> Union[str, Dict]:
+            # self should be chain
+            try:
+                ret = prop.fget(s)
+                return ret
+
+            except NotImplementedError as e:
+
+                return noserio(obj, error=f"{e.__class__.__name__}='{str(e)}'")
+
+        safe_type._instrumented = prop
+        new_prop = property(fget=safe_type)
+
+        return new_prop
+
+
+class TruChain(App):
+    """
+    Wrap a langchain Chain to capture its configuration and evaluation steps. 
+    """
+
+    app: Chain
+
+    # TODO: what if _acall is being used instead?
+    root_callable: ClassVar[FunctionOrMethod] = Field(
+        default_factory=lambda: FunctionOrMethod.of_callable(TruChain._call),
+        const=True
+    )
+
+    # Normally pydantic does not like positional args but chain here is
+    # important enough to make an exception.
+    def __init__(self, app: Chain, **kwargs):
+        """
+        Wrap a langchain chain for monitoring.
+
+        Arguments:
+        - app: Chain -- the chain to wrap.
+        - More args in App
+        - More args in AppDefinition
+        - More args in WithClassInfo
+        """
+
+        super().update_forward_refs()
+
+        # TruChain specific:
+        kwargs['app'] = app
+        kwargs['root_class'] = Class.of_object(app)
+        kwargs['instrument'] = LangChainInstrument()
+
+        super().__init__(**kwargs)
+
+    # Chain requirement
+    @property
+    def _chain_type(self):
+        return "TruChain"
+
+    # Chain requirement
+    @property
+    def input_keys(self) -> List[str]:
+        return self.app.input_keys
+
+    # Chain requirement
+    @property
+    def output_keys(self) -> List[str]:
+        return self.app.output_keys
+
+    def __getattr__(self, __name: str) -> Any:
+        # A message for cases where a user calls something that the wrapped
+        # chain has but we do not wrap yet.
+
+        if hasattr(self.app, __name):
+            return RuntimeError(
+                f"TruChain has no attribute {__name} but the wrapped app ({type(self.app)}) does. ",
+                f"If you are calling a {type(self.app)} method, retrieve it from that app instead of from `TruChain`. "
+                f"TruChain presently only wraps Chain.__call__, Chain._call, and Chain._acall ."
+            )
+        else:
+            raise RuntimeError(f"TruChain has no attribute named {__name}.")
+
+    """
+    # NOTE: Disabling this method for now as it may have compatibility issues
+    with various packages. Need some way to reduce code duplication between the
+    async and sync versions of various methods.
+
+    def _eval_sync_root_method(self, func, inputs, **kwargs) -> Any:
+        async def func_async(inputs, **kwargs):
+            return func(inputs, **kwargs)
+       
+        try:
+            # Required for reusing async methods inside sync methods if running
+            # inside some outer async loop. Note that jupyter notebook cells are
+            # run within such a loop.
+            
+            nest_asyncio.apply() evl = asyncio.get_event_loop()
+            
+            # Will fail if not inside an async loop, in that case, we are free #
+            to create one below.
+
+        except:
+            evl = asyncio.new_event_loop()
+
+        # requires nested asyncio
+        return evl.run_until_complete(self._eval_async_root_method(func_async, inputs, **kwargs))
+    """
+
+    # NOTE: Input signature compatible with langchain.chains.base.Chain.acall
+    async def acall_with_record(self, inputs: Union[Dict[str, Any], Any], **kwargs) -> Tuple[Any, Record]:
+        """
+        Run the chain and also return a record metadata object.
+
+        Returns:
+            Any: chain output
+            dict: record metadata
+        """
+
+        # Wrapped calls will look this up by traversing the call stack. This
+        # should work with threads.
+        record: Sequence[RecordAppCall] = []
+
+        ret = None
+        error = None
+
+        cost: Cost = Cost()
+
+        start_time = None
+        end_time = None
+
+        # langchain.__call__ specific:
+        inputs = self.app.prep_inputs(inputs)
+
+        try:
+            start_time = datetime.now()
+            ret, cost = await Endpoint.atrack_all_costs_tally(
+                lambda: self.app.acall(inputs=inputs, **kwargs)
+            )
+            end_time = datetime.now()
+
+        except BaseException as e:
+            end_time = datetime.now()
+            error = e
+            logger.error(f"App raised an exception: {e}")
+            logger.error(traceback.format_exc())
+            
+        assert len(record) > 0, "No information recorded in call."
+
+        ret_record_args = dict()
+
+        # Figure out the content of the "inputs" arg that __call__ constructs
+        # for _call so we can lookup main input and output.
+        input_key = self.input_keys[0]
+        output_key = self.output_keys[0]
+
+        ret_record_args['main_input'] = jsonify(inputs[input_key])
+
+        if ret is not None:
+            ret_record_args['main_output'] = jsonify(ret[output_key])
+
+        if error is not None:
+            ret_record_args['main_error'] = jsonify(error)
+
+        ret_record = self._post_record(
+            ret_record_args, error, cost, start_time, end_time, record
+        )
+
+        return ret, ret_record
+
+    # NOTE: Input signature compatible with langchain.chains.base.Chain.__call__
+    def call_with_record(self, inputs: Union[Dict[str, Any], Any], **kwargs) -> Tuple[Any, Record]:
+        """
+        Run the chain and also return a record metadata object.
+
+        Returns:
+            Any: chain output
+            dict: record metadata
+        """
+
+        # Wrapped calls will look this up by traversing the call stack. This
+        # should work with threads.
+        record: Sequence[RecordAppCall] = []
+
+        ret = None
+        error = None
+
+        cost: Cost = Cost()
+
+        start_time = None
+        end_time = None
+
+        # langchain.__call__ specific:
+        inputs = self.app.prep_inputs(inputs)
+
+        try:
+            start_time = datetime.now()
+            ret, cost = Endpoint.track_all_costs_tally(
+                lambda: self.app.__call__(inputs=inputs, **kwargs)
+            )
+            end_time = datetime.now()
+
+        except BaseException as e:
+            end_time = datetime.now()
+            error = e
+            logger.error(f"App raised an exception: {e}")
+            logger.error(traceback.format_exc())
+
+        assert len(record) > 0, "No information recorded in call."
+
+        ret_record_args = dict()
+
+        # Figure out the content of the "inputs" arg that __call__ constructs
+        # for _call so we can lookup main input and output.
+        input_key = self.input_keys[0]
+        output_key = self.output_keys[0]
+
+        ret_record_args['main_input'] = jsonify(inputs[input_key])
+
+        if ret is not None:
+            ret_record_args['main_output'] = jsonify(ret[output_key])
+
+        if error is not None:
+            ret_record_args['main_error'] = jsonify(error)
+
+        ret_record = self._post_record(
+            ret_record_args, error, cost, start_time, end_time, record
+        )
+
+        return ret, ret_record
+
+    
+    def __call__(self, *args, **kwargs) -> Dict[str, Any]:
+        """
+        Wrapped call to self.app._call with instrumentation. If you need to
+        get the record, use `call_with_record` instead. 
+        """
+
+        return self._call(*args, **kwargs)
+    
+    # langchain.chains.base.py:Chain requirement:
+    def _call(self, *args, **kwargs) -> Any:
+        ret, _ = self.call_with_record(*args, **kwargs)
+
+        return ret
+
+    # optional langchain.chains.base.py:Chain requirement:
+    async def _acall(self, *args, **kwargs) -> Any:
+        ret, _ = await self.acall_with_record(*args, **kwargs)
+
+        return ret
