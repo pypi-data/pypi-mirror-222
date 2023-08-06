ca58c717b7a7a69a081ac7f2b5958077442418d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:24 +0000

    Add package mypy-boto3-personalize-events/1.28.15.post1/mypy_boto3_personalize_events-1.28.15.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-personalize-events/mypy_boto3_personalize_events-1.28.15.post1-py3-none-any.whl/mypy_boto3_personalize_events/type_defs.pyi
@@ -0,0 +1,153 @@
+"""
+Type annotations for personalize-events service type definitions.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_personalize_events/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_personalize_events.type_defs import ResponseMetadataTypeDef
+
+    data: ResponseMetadataTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Dict, Sequence, Union
+
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "ResponseMetadataTypeDef",
+    "MetricAttributionTypeDef",
+    "ItemTypeDef",
+    "UserTypeDef",
+    "EmptyResponseMetadataTypeDef",
+    "EventTypeDef",
+    "PutItemsRequestRequestTypeDef",
+    "PutUsersRequestRequestTypeDef",
+    "PutEventsRequestRequestTypeDef",
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+MetricAttributionTypeDef = TypedDict(
+    "MetricAttributionTypeDef",
+    {
+        "eventAttributionSource": str,
+    },
+)
+
+_RequiredItemTypeDef = TypedDict(
+    "_RequiredItemTypeDef",
+    {
+        "itemId": str,
+    },
+)
+_OptionalItemTypeDef = TypedDict(
+    "_OptionalItemTypeDef",
+    {
+        "properties": str,
+    },
+    total=False,
+)
+
+class ItemTypeDef(_RequiredItemTypeDef, _OptionalItemTypeDef):
+    pass
+
+_RequiredUserTypeDef = TypedDict(
+    "_RequiredUserTypeDef",
+    {
+        "userId": str,
+    },
+)
+_OptionalUserTypeDef = TypedDict(
+    "_OptionalUserTypeDef",
+    {
+        "properties": str,
+    },
+    total=False,
+)
+
+class UserTypeDef(_RequiredUserTypeDef, _OptionalUserTypeDef):
+    pass
+
+EmptyResponseMetadataTypeDef = TypedDict(
+    "EmptyResponseMetadataTypeDef",
+    {
+        "ResponseMetadata": ResponseMetadataTypeDef,
+    },
+)
+
+_RequiredEventTypeDef = TypedDict(
+    "_RequiredEventTypeDef",
+    {
+        "eventType": str,
+        "sentAt": Union[datetime, str],
+    },
+)
+_OptionalEventTypeDef = TypedDict(
+    "_OptionalEventTypeDef",
+    {
+        "eventId": str,
+        "eventValue": float,
+        "itemId": str,
+        "properties": str,
+        "recommendationId": str,
+        "impression": Sequence[str],
+        "metricAttribution": MetricAttributionTypeDef,
+    },
+    total=False,
+)
+
+class EventTypeDef(_RequiredEventTypeDef, _OptionalEventTypeDef):
+    pass
+
+PutItemsRequestRequestTypeDef = TypedDict(
+    "PutItemsRequestRequestTypeDef",
+    {
+        "datasetArn": str,
+        "items": Sequence[ItemTypeDef],
+    },
+)
+
+PutUsersRequestRequestTypeDef = TypedDict(
+    "PutUsersRequestRequestTypeDef",
+    {
+        "datasetArn": str,
+        "users": Sequence[UserTypeDef],
+    },
+)
+
+_RequiredPutEventsRequestRequestTypeDef = TypedDict(
+    "_RequiredPutEventsRequestRequestTypeDef",
+    {
+        "trackingId": str,
+        "sessionId": str,
+        "eventList": Sequence[EventTypeDef],
+    },
+)
+_OptionalPutEventsRequestRequestTypeDef = TypedDict(
+    "_OptionalPutEventsRequestRequestTypeDef",
+    {
+        "userId": str,
+    },
+    total=False,
+)
+
+class PutEventsRequestRequestTypeDef(
+    _RequiredPutEventsRequestRequestTypeDef, _OptionalPutEventsRequestRequestTypeDef
+):
+    pass
