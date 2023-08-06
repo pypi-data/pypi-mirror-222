5ff7ed237a12aae2b9456c7d0a35de2a34967475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:33 +0000

    Add package fastmega/0.0.1/fastmega-0.0.1.tar.gz

---
+++ packages/fastmega/fastmega-0.0.1.tar.gz/fastmega-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools,re,sys
+
+setuptools.setup(
+    name = 'fastmega',         
+    packages = ['fastmega'],
+    version = '0.0.1',     
+    license='MIT', 
+    description = 'A fast way to use the MEGA cloud API', 
+    author = 'Rémi NGUYEN',                   
+    author_email = 'taindp98@gmail.com',      
+    url = 'https://github.com/taindp98/fastmega',   
+    download_url = 'https://github.com/taindp98/fastmega/archive/refs/tags/v.0.0.1.tar.gz',   
+    keywords = ['mega', 'storage'],  
+    install_requires=[
+        'pip', 'packaging',
+        'mega'
+    ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    ],
+)
