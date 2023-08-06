d8f62efeb515372862a6f7fbdab754e1387096c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:06 +0000

    Add package circleapi/2023.7.15/circleapi-2023.7.15.tar.gz

---
+++ packages/circleapi/circleapi-2023.7.15.tar.gz/circleapi-2023.7.15/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="circleapi",
+    author="miinorii",
+    version="2023.7.15",
+    packages=["circleapi"],
+    install_requires=[
+        "httpx[http2]>=0.24.1",
+        "pydantic>=2.0",
+        "python-dotenv>=1.0.0"
+    ],
+    python_requires=">=3.10"
+)
