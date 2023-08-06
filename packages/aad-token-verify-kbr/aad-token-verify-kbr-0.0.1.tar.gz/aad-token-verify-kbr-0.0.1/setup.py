3401b9cbdde50c024970ab9933e2e406f3a772cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:34 +0000

    Add package aad-token-verify-kbr/0.0.1/aad-token-verify-kbr-0.0.1.tar.gz

---
+++ packages/aad-token-verify-kbr/aad-token-verify-kbr-0.0.1.tar.gz/aad-token-verify-kbr-0.0.1/setup.py
@@ -0,0 +1,40 @@
+import io
+import os
+
+from setuptools import setup
+
+dir = os.path.dirname(__file__)
+
+with io.open(os.path.join(dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='aad-token-verify-kbr',
+    version='0.0.1',
+    description='Forked: A python utility library to verify an Azure Active Directory OAuth token',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/GeneralMills/azure-ad-token-verify',
+    author=['Daniel Thompson'],
+    author_email='daniel.thompson2@genmills.com ',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'requests>=2.25.1,<3',
+        'PyJWT>=2.1.0,<3',
+        'cryptography>=41.0.1,<42',
+        'cachetools>=5.3.1,<6'
+    ],
+    keywords='azure ad token oauth verify jwt',
+    packages=['aad_token_verify_kbr'],
+)
