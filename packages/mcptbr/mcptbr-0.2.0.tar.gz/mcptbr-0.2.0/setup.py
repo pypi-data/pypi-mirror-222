0da9d0d116be98081968c5b8a9c1c33626372d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:14 +0000

    Add package mcptbr/0.2.0/mcptbr-0.2.0.tar.gz

---
+++ packages/mcptbr/mcptbr-0.2.0.tar.gz/mcptbr-0.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mcptbr',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        "mcpi", # Dependências necessárias para sua biblioteca (se houver).
+    ],
+    author='Lucas Pereira',
+    author_email='lucasthe2@gmail.com',
+    description='Biblioteca para manipular o Minecraft através do python.',
+    #long_description=open('README.md'),
+    long_description_content_type='text/markdown',
+    #url='https://github.com/seu_usuario/minha_biblioteca',
+    license='MIT',  # Substitua pela licença adequada.
+)
