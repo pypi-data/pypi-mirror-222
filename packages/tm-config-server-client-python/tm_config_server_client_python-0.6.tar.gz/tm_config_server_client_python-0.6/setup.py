c046b1db83a70ba47cee6694d82a22d32035cab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:49 +0000

    Add package tm-config-server-client-python/0.6/tm_config_server_client_python-0.6.tar.gz

---
+++ packages/tm-config-server-client-python/tm_config_server_client_python-0.6.tar.gz/tm_config_server_client_python-0.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tm_config_server_client_python',
+      version='0.6',
+      description='Gaussian and Binomial distributions',
+      packages=['tm_config_server_client_python'],
+      author_email='jayantreddybodkurwar@gmail.com',
+      zip_safe=False)
