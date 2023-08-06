6842b1333f922292d6188a509b3790eaa7ab5c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:12 +0000

    Add package Adeepspeed/0.9.2/Adeepspeed-0.9.2.tar.gz

---
+++ packages/Adeepspeed/Adeepspeed-0.9.2.tar.gz/Adeepspeed-0.9.2/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.9.2'
+git_hash='e0e8085'
+git_branch='review-aiacc-offloading'
+installed_ops={'async_io': False, 'cpu_adagrad': False, 'cpu_adam': False, 'fused_adam': False, 'fused_lamb': False, 'quantizer': False, 'random_ltd': False, 'sparse_attn': False, 'spatial_inference': False, 'transformer': False, 'stochastic_transformer': False, 'transformer_inference': False, 'utils': False}
+compatible_ops={'async_io': False, 'cpu_adagrad': True, 'cpu_adam': True, 'fused_adam': True, 'fused_lamb': True, 'quantizer': True, 'random_ltd': True, 'sparse_attn': False, 'spatial_inference': True, 'transformer': True, 'stochastic_transformer': True, 'transformer_inference': True, 'utils': True}
+torch_info={'version': '1.12', 'bf16_support': False, 'cuda_version': '11.3', 'nccl_version': '2.10', 'hip_version': '0.0'}
