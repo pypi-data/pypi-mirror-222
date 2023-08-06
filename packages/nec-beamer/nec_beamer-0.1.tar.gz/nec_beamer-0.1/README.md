5a7e91c8156adddd0aff129537819022be6ca6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package nec-beamer/0.1/nec_beamer-0.1.tar.gz

---
+++ packages/nec-beamer/nec_beamer-0.1.tar.gz/nec_beamer-0.1/README.md
@@ -0,0 +1,7 @@
+# nec_beamer_control
+
+Command Line Interface for a NEC Beamer Web UI.
+This is implemented with the NEC Beamer NP2000
+
+Web Interface has a Copyright (C) NEC Viewtechnology, Ltd. 2002-2006
+
---
+++ packages/nec-beamer/nec_beamer-0.1.tar.gz/nec_beamer-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nec_beamer',
+      version='0.1',
+      description='NEC Beamer Web Interface Wrapper',
+      url='http://github.com/heinrich-foto/nec_beamer',
+      author='Heinrich-Foto',
+      author_email='nec_beamer@heinrich-foto.de',
+      license='MIT',
+      packages=['nec_beamer_heinrich_foto'],
+      zip_safe=False)
