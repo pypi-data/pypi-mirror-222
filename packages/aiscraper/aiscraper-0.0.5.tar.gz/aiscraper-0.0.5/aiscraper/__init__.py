9832856782df3a956be020fda623b5ba1f2b223f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:32 +0000

    Add package aiscraper/0.0.5/aiscraper-0.0.5.tar.gz

---
+++ packages/aiscraper/aiscraper-0.0.5.tar.gz/aiscraper-0.0.5/aiscraper/__init__.py
@@ -0,0 +1,4 @@
+from .vanila_crawler import WebCrawler
+
+def sup():
+    print("webcrawler")
---
+++ packages/aiscraper/aiscraper-0.0.5.tar.gz/aiscraper-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiscraper',
+    version='0.0.5',
+    description='Scrpers ,crawls and cleans the data it finds',
+    author='Harsh Avinash',
+    author_email='harsh.avinash.official@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+)
+
+
+
