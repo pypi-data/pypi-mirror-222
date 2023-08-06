7eee59276721064ad4e604697eecc5b548f1461f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:28 +0000

    Add package FastaTransformer/0.0.14/FastaTransformer-0.0.14-py3-none-any.whl

---
+++ packages/FastaTransformer/FastaTransformer-0.0.14-py3-none-any.whl/FastaTransformer/__init__.py
@@ -0,0 +1,6 @@
+from FastaTransformer.FastaTransformer import *
+
+#First, open only the FastaTransformer Folder
+#python -m build
+#python -m twine upload dist/*
+#Finally, upload this folder onto github
