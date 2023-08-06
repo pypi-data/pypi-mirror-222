4698209ad985faf25899fbbfda1dbb9b0786a144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:07 +0000

    Add package micropython-europi-contrib/0.10.0/micropython-europi-contrib-0.10.0.tar.gz

---
+++ packages/micropython-europi-contrib/micropython-europi-contrib-0.10.0.tar.gz/micropython-europi-contrib-0.10.0/contrib/menu.py
@@ -0,0 +1,50 @@
+"""See menu.md for details."""
+# Reset the module state and display bootsplash screen.
+from europi import bootsplash, usb_connected
+
+#  This is a fix for a USB connection issue documented in GitHub issue #179, and its removal condition is set out in GitHub issue #184
+if usb_connected.value() == 0:
+    from time import sleep
+
+    sleep(0.5)
+
+bootsplash()
+
+
+from bootloader import BootloaderMenu
+
+# Scripts that are included in the menu
+EUROPI_SCRIPTS = [
+    "contrib.bernoulli_gates.BernoulliGates",
+    "contrib.coin_toss.CoinToss",
+    "contrib.consequencer.Consequencer",
+    "contrib.cvecorder.CVecorder",
+    "contrib.diagnostic.Diagnostic",
+    "contrib.envelope_generator.EnvelopeGenerator",
+    "contrib.euclid.EuclideanRhythms",
+    "contrib.hamlet.Hamlet",
+    "contrib.harmonic_lfos.HarmonicLFOs",
+    "contrib.hello_world.HelloWorld",
+    "contrib.knob_playground.KnobPlayground",
+    "contrib.kompari.Kompari",
+    "contrib.logic.Logic",
+    "contrib.master_clock.MasterClock",
+    "contrib.noddy_holder.NoddyHolder",
+    "contrib.pams.PamsWorkout",
+    "contrib.piconacci.Piconacci",
+    "contrib.polyrhythmic_sequencer.PolyrhythmSeq",
+    "contrib.poly_square.PolySquare",
+    "contrib.probapoly.Probapoly",
+    "contrib.quantizer.QuantizerScript",
+    "contrib.radio_scanner.RadioScanner",
+    "contrib.scope.Scope",
+    "contrib.sequential_switch.SequentialSwitch",
+    "contrib.smooth_random_voltages.SmoothRandomVoltages",
+    "contrib.strange_attractor.StrangeAttractor",
+    "contrib.turing_machine.EuroPiTuringMachine",
+    "calibrate.Calibrate",
+]
+
+
+if __name__ == "__main__":
+    BootloaderMenu(EUROPI_SCRIPTS).main()
