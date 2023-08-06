302154c032e037a3419d7c29fe27b40ff9a24546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:40 +0000

    Add package odoo14-addon-ssi-account-statement-import/14.0.1.0.2.dev1/odoo14_addon_ssi_account_statement_import-14.0.1.0.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-account-statement-import/odoo14_addon_ssi_account_statement_import-14.0.1.0.2.dev1-py3-none-any.whl/odoo/addons/ssi_account_statement_import/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Add Wizard to Import Statement",
+    "version": "14.0.1.0.1",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "ssi_financial_accounting",
+        "account_statement_import",
+    ],
+    "data": [
+        "views/account_bank_statement_views.xml",
+    ],
+    "demo": [],
+}
