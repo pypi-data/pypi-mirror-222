ca63d71cdd2ee4a1b22a8d540c2e2c9e102fa28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:22 +0000

    Add package simpleworkspace/1.1.229/simpleworkspace-1.1.229.tar.gz

---
+++ packages/simpleworkspace/simpleworkspace-1.1.229.tar.gz/simpleworkspace-1.1.229/src/simpleworkspace/loader.py
@@ -0,0 +1,7 @@
+from .io import loader as io
+from .types import loader as types
+from .utility import loader as utility
+
+from . import logproviders
+from . import settingsproviders
+from .app import App
