2723722c726563c039e94942cca1c86c448a1bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:38 +0000

    Add package pythonGPT/0.2/pythonGPT-0.2.tar.gz

---
+++ packages/pythonGPT/pythonGPT-0.2.tar.gz/pythonGPT-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pythonGPT",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        "IPython",
+        "requests"
+    ]
+)
