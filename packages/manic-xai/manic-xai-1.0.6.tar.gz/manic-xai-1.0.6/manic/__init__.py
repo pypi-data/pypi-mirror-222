b59440db73e372d113572d7bda8250856d0e59b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:42 +0000

    Add package manic-xai/1.0.6/manic-xai-1.0.6.tar.gz

---
+++ packages/manic-xai/manic-xai-1.0.6.tar.gz/manic-xai-1.0.6/manic/__init__.py
@@ -0,0 +1,12 @@
+from .Baseline import Baseline
+from .Crossover import Crossover
+from .Disagreement import Disagreement
+from .Evaluation import Evaluation
+from .GeneticAlgorithm import GeneticAlgorithm
+from .Mutation import Mutation
+from .Replacement import Replacement
+from .Selection import Selection
+
+__version__ = '0.1.0'
+__author__ = 'Craig Pirie'
+__all__ = ['Baseline', 'Crossover', 'Disagreement', 'Evaluation', 'GeneticAlgorithm', 'Manic', 'Mutation', 'Replacement', 'Selection']
