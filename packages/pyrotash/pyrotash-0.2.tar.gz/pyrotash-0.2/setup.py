31b27746470d6955bfbfee9cc2e8535b5ffb16e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:21 +0000

    Add package pyrotash/0.2/pyrotash-0.2.tar.gz

---
+++ packages/pyrotash/pyrotash-0.2.tar.gz/pyrotash-0.2/setup.py
@@ -0,0 +1,18 @@
+# tpyconfig/setup.py
+
+from setuptools import setup
+
+setup(
+    name="pyrotash",
+    version="0.2",
+    packages=["tpyconfig"],
+    install_requires=[],
+    author="Tashri",
+    author_email="Tashrithakur2342@gmail.com",
+    description="A simple package for managing configuration variables.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
