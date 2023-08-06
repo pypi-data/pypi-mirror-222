da5212671c2bca3e944eff036badb132a7a9d332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:58 +0000

    Add package stable-trunc-gaussian/1.1.0/stable_trunc_gaussian-1.1.0-py3-none-any.whl

---
+++ packages/stable-trunc-gaussian/stable_trunc_gaussian-1.1.0-py3-none-any.whl/stable_trunc_gaussian/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+
+__version__ = "1.1.0"
+
+from .parallel_trunc_gaussian import ParallelTruncatedGaussian as TruncatedGaussian
+from .sequential_trunc_gaussian import SequentialTruncatedGaussian as SeqTruncatedGaussian
