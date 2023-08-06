d8c7ea626b508dc85ff3d17b428739a836239c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:24 +0000

    Add package setuptools-ocrd/0.0.17/setuptools_ocrd-0.0.17.tar.gz

---
+++ packages/setuptools-ocrd/setuptools_ocrd-0.0.17.tar.gz/setuptools_ocrd-0.0.17/setuptools_ocrd/_integration/setuptools.py
@@ -0,0 +1,26 @@
+import logging
+import warnings
+
+import setuptools
+
+from setuptools_ocrd import _get_version
+
+log = logging.getLogger(__name__)
+
+
+def _warn_on_old_setuptools(_version=setuptools.__version__):
+    if int(_version.split(".")[0]) < 61:
+        warnings.warn(RuntimeWarning(f"setuptools=={_version} is old!"))
+
+
+_warn_on_old_setuptools()
+
+
+def get_ocrd_tool_version(dist):
+    if dist.metadata.version is not None:
+        return
+
+    if dist.metadata.name == "setuptools_ocrd":
+        return
+
+    dist.metadata.version = _get_version()
