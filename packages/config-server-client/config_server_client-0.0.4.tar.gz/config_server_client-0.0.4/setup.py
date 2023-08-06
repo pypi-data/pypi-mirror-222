8f92ef53a45b4634285d4977f7217f38b471764e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:23 +0000

    Add package config-server-client/0.0.4/config_server_client-0.0.4.tar.gz

---
+++ packages/config-server-client/config_server_client-0.0.4.tar.gz/config_server_client-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+
+    name = 'config_server_client', 
+
+    version = '0.0.4',
+    
+
+    description = 'A test packahge',
+ 
+    author = 'Jayant',
+    author_email = 'jayantreddybodkurwar@gmail.com',
+    install_requires = [
+    ],
+
+)
