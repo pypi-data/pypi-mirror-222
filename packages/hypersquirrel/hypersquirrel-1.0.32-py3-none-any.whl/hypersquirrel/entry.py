aa5bfea9a770f5e302cf59f7c234dd02e6cb29b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:46 +0000

    Add package hypersquirrel/1.0.32/hypersquirrel-1.0.32-py3-none-any.whl

---
+++ packages/hypersquirrel/hypersquirrel-1.0.32-py3-none-any.whl/hypersquirrel/entry.py
@@ -0,0 +1,35 @@
+import re
+from typing import Iterator, List
+
+from hypersquirrel.core import Watchlist
+from hypersquirrel.literalinterpreterfactory import get_interpreter
+from hypersquirrel.scraperfactory import get_scraper
+
+PAGED_PATTERN = re.compile("<\d+-\d+>")
+
+
+def create_watchlist(url: str, max_items: int):
+    match = PAGED_PATTERN.findall(url)
+    if len(match) == 1:
+        pages = match[0].replace("<", "").replace(">", "").split("-")
+        return Watchlist(
+            url=url.replace(match[0], "${page}"),
+            max_items=max_items,
+            page_min=int(pages[0]),
+            page_max=int(pages[1])
+        )
+    return Watchlist(
+        url=url,
+        max_items=max_items
+    )
+
+
+def scrape(w: Watchlist) -> Iterator[dict]:
+    return w.scrape(get_scraper(w))
+
+
+def scrape_literal_urls(urls: List[str]) -> dict:
+    for url in set(urls):
+        interpreter = get_interpreter(url)
+        if interpreter:
+            yield from interpreter(url)
