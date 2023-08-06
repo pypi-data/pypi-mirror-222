81a46ac00a59776ba8e775d6ec41773528eb553f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:35 +0000

    Add package moosez/2.2.11/moosez-2.2.11-py3-none-any.whl

---
+++ packages/moosez/moosez-2.2.11-py3-none-any.whl/moosez/constants.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Author: Lalith Kumar Shiyam Sundar
+# Institution: Medical University of Vienna
+# Research Group: Quantitative Imaging and Medical Physics (QIMP) Team
+# Date: 13.02.2023
+# Version: 2.0.0
+#
+# Description:
+# This module contains the constants that are used in the moosez.
+#
+# Usage:
+# The variables in this module can be imported and used in other modules within the moosez.
+#
+# ----------------------------------------------------------------------------------------------------------------------
+
+import os
+
+from moosez import file_utilities
+
+project_root = file_utilities.get_virtual_env_root()
+
+NNUNET_RESULTS_FOLDER = os.path.join(project_root, 'models', 'nnunet_trained_models')
+MOOSEZ_MODEL_FOLDER = os.path.join(NNUNET_RESULTS_FOLDER, 'nnUNet', '3d_fullres')
+ALLOWED_MODALITIES = ['CT', 'PT', 'MR']
+TEMP_FOLDER = 'temp'
+
+# COLOR CODES
+ANSI_ORANGE = '\033[38;5;208m'
+ANSI_GREEN = '\033[38;5;40m'
+ANSI_VIOLET = '\033[38;5;141m'
+ANSI_RESET = '\033[0m'
+
+# SUPPORTED TRACERS (limited patch)
+
+TRACER_FDG = 'FDG'
+
+# FOLDER NAMES
+
+SEGMENTATIONS_FOLDER = 'segmentations'
+STATS_FOLDER = 'stats'
+
+# PREPROCESSING PARAMETERS
+
+MATRIX_THRESHOLD = 200 * 200 * 600
+Z_AXIS_THRESHOLD = 200
+MARGIN_PADDING = 20
+INTERPOLATION = 'bspline'
+CHUNK_THRESHOLD = 200
+
+# POSTPROCESSING PARAMETERS
+
+TUMOR_LABEL = 12
+
+# FILE NAMES
+
+RESAMPLED_IMAGE_FILE_NAME = 'resampled_image_0000.nii.gz'
+RESAMPLED_MASK_FILE_NAME = 'resampled_mask.nii.gz'
+CHUNK_FILENAMES = ["chunk01_0000.nii.gz", "chunk02_0000.nii.gz", "chunk03_0000.nii.gz"]
+CHUNK_PREFIX = 'chunk'
+
+# ORGAN INDICES
+
+ORGAN_INDICES = {
+    "clin_ct_organs": {
+        1: "spleen",
+        2: "kidney_right",
+        3: "kidney_left",
+        4: "gallbladder",
+        5: "liver",
+        6: "stomach",
+        7: "aorta",
+        8: "inferior_vena_cava",
+        9: "portal_vein_and_splenic_vein",
+        10: "pancreas",
+        11: "adrenal_gland_right",
+        12: "adrenal_gland_left",
+        13: "lung_upper_lobe_left",
+        14: "lung_lower_lobe_left",
+        15: "lung_upper_lobe_right",
+        16: "lung_middle_lobe_right",
+        17: "lung_lower_lobe_right"
+    },
+    "clin_ct_lungs": {
+        1: "lung_upper_lobe_left",
+        2: "lung_lower_lobe_left",
+        3: "lung_upper_lobe_right",
+        4: "lung_middle_lobe_right",
+        5: "lung_lower_lobe_right"
+    },
+    "clin_ct_body": {
+        1: "Extremities",
+        2: "Torso"
+    },
+    "clin_pt_fdg_tumor": {
+        1: "tumor"
+    }
+    # More index-to-name dictionaries for other models...
+}
+
+
