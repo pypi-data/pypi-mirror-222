110451f31d3972c8d700c3f1f6de5ec236da33e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:20 +0000

    Add package windmill-api/1.138.1/windmill_api-1.138.1.tar.gz

---
+++ packages/windmill-api/windmill_api-1.138.1.tar.gz/windmill_api-1.138.1/windmill_api/models/delete_completed_job_response_200_language.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class DeleteCompletedJobResponse200Language(str, Enum):
+    PYTHON3 = "python3"
+    DENO = "deno"
+    GO = "go"
+    BASH = "bash"
+    POSTGRESQL = "postgresql"
+    MYSQL = "mysql"
+    BIGQUERY = "bigquery"
+    GRAPHQL = "graphql"
+    NATIVETS = "nativets"
+    BUN = "bun"
+
+    def __str__(self) -> str:
+        return str(self.value)
