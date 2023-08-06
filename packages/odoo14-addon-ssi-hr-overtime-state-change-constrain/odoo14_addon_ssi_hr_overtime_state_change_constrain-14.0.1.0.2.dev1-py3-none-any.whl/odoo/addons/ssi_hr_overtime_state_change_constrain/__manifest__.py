9606d2ab1ef597896d706256de56ae6fe0550632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:41 +0000

    Add package odoo14-addon-ssi-hr-overtime-state-change-constrain/14.0.1.0.2.dev1/odoo14_addon_ssi_hr_overtime_state_change_constrain-14.0.1.0.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-hr-overtime-state-change-constrain/odoo14_addon_ssi_hr_overtime_state_change_constrain-14.0.1.0.2.dev1-py3-none-any.whl/odoo/addons/ssi_hr_overtime_state_change_constrain/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Overtime State Change Constrain",
+    "version": "14.0.1.0.1",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_hr_overtime",
+        "ssi_state_change_constrain_mixin",
+    ],
+    "data": [],
+}
