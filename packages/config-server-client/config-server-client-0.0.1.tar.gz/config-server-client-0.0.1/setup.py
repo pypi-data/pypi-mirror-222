5b78fd9b42aae30b713ca9f807be365be2173ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:52 +0000

    Add package config-server-client/0.0.1/config-server-client-0.0.1.tar.gz

---
+++ packages/config-server-client/config-server-client-0.0.1.tar.gz/config-server-client-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+
+    name = 'config-server-client', 
+
+    version = '0.0.1',
+    
+
+    description = 'A test packahge',
+ 
+    author = 'Jayant',
+    author_email = 'jayantreddybodkurwar@gmail.com',
+    install_requires = [
+        'pandas ~= 1.2.4'
+    ],
+
+)
