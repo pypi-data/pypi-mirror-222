ea8cf4d7971f48dbc9574444ae7dbe6be33e5053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:32 +0000

    Add package Pygeun/0.0.1/Pygeun-0.0.1.tar.gz

---
+++ packages/Pygeun/Pygeun-0.0.1.tar.gz/Pygeun-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='Pygeun',
+    version='0.0.1',
+    description='Pygeun is for convenience',
+    author='Daegeun02',
+    author_email='redhawkdg02@gmail.com',
+    url='https://github.com/Daegeun02/Pygeun.git',
+    install_requires=[],
+    packages=find_packages( exclude=[] ),
+    keywords=['Pygeun', 'by daegeun', 'for convenience'],
+    python_requires='>=3.7',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
