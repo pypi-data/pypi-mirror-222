555f79afefd81677f24202c2e0671a70562cfec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:05 +0000

    Add package django-tailwind-cli/2.0.5/django_tailwind_cli-2.0.5-py3-none-any.whl

---
+++ packages/django-tailwind-cli/django_tailwind_cli-2.0.5-py3-none-any.whl/django_tailwind_cli/templatetags/tailwind_cli.py
@@ -0,0 +1,16 @@
+"""Tailwind template tags."""
+
+
+from django import template
+from django.conf import settings
+
+from django_tailwind_cli.utils import Config
+
+register = template.Library()
+
+
+@register.inclusion_tag("tailwind_cli/tailwind_css.html")
+def tailwind_css():
+    """Template tag to include the css files into the html templates."""
+    config = Config()
+    return {"debug": settings.DEBUG, "tailwind_dist_css": config.dist_css}
