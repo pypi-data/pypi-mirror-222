4e40ec505ba22a20c5b20f9e38319c23f5644c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:02 +0000

    Add package traceloop-sdk/0.0.2/traceloop_sdk-0.0.2.tar.gz

---
+++ packages/traceloop-sdk/traceloop_sdk-0.0.2.tar.gz/traceloop_sdk-0.0.2/traceloop/tracing/tracer.py
@@ -0,0 +1,38 @@
+import os
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+TRACER_NAME = "traceloop.tracer"
+TRACELOOP_API_ENDPOINT = "https://api.traceloop.dev/v1/traces"
+
+
+class Tracer:
+    __instance = None
+
+    @staticmethod
+    def init():
+        api_key = os.getenv("TRACELOOP_API_KEY")
+        api_endpoint = os.getenv("TRACELOOP_API_ENDPOINT") or TRACELOOP_API_ENDPOINT
+        print(f"Initializing Tracer with API endpoint: {api_endpoint}")
+        provider = TracerProvider()
+        exporter = OTLPSpanExporter(
+            endpoint=api_endpoint,
+            headers={
+                "Authorization": f"Bearer {api_key}",
+            }
+        )
+        processor = BatchSpanProcessor(exporter)
+        provider.add_span_processor(processor)
+        trace.set_tracer_provider(provider)
+        Tracer.__instance = trace.get_tracer(TRACER_NAME)
+        OpenAIInstrumentor().instrument()
+
+    @staticmethod
+    def instance():
+        if Tracer.__instance is None:
+            raise Exception("Tracer is not initialized")
+        return Tracer.__instance
