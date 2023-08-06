6f56eb6e1211267a25013ef726d858193bdd80cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:17 +0000

    Add package yambs/2.2.1/yambs-2.2.1-py3-none-any.whl

---
+++ packages/yambs/yambs-2.2.1-py3-none-any.whl/yambs/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=2d51f6a1d40475c6189c064ddde32980
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Yet another meta build-system."
+PKG_NAME = "yambs"
+VERSION = "2.2.1"
