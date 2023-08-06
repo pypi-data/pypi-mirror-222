f7bf246d747106cdc8d950d34f3ef1abec1a9775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:20 +0000

    Add package myoconverter/0.0.1/myoconverter-0.0.1.tar.gz

---
+++ packages/myoconverter/myoconverter-0.0.1.tar.gz/myoconverter-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="myoconverter",
+    version="0.0.1",
+    author="Huawei Wang, Aleksi Ikkala",
+    author_email="",
+    description="Tool for converting OpenSim musculoskeletal (MSK) models to the MuJoCo model format with optimized muscle kinematics and kinetics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MyoHub/myo-converter",
+    packages=setuptools.find_packages(),
+    install_requires=[
+      'python>=3.8',
+      'mujoco-python==2.3.3',
+      'loguru==0.5.3',
+      'lxml==4.9.1',
+      'numpy==1.21.5',
+      'scipy==1.10.0',
+      'scikit-learn==1.2.0',
+      'opensim==4.4',
+      'matplotlib==3.6.2',
+      'trimesh==3.22.2',
+      'pyvista==0.39.1',
+      'networkx==2.8.4',
+      'fpdf2==2.7.4',
+      'seaborn==0.12.2'
+    ]
+)
