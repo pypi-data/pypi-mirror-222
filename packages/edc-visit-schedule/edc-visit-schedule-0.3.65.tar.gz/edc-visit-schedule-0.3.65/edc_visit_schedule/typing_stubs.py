453ae049f74fdb4319819b7fcd41296946010e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:07 +0000

    Add package edc-visit-schedule/0.3.65/edc-visit-schedule-0.3.65.tar.gz

---
+++ packages/edc-visit-schedule/edc-visit-schedule-0.3.65.tar.gz/edc-visit-schedule-0.3.65/edc_visit_schedule/typing_stubs.py
@@ -0,0 +1,15 @@
+from edc_protocol import Protocol
+
+
+class VisitScheduleFieldsProtocol(Protocol):
+    def visit_code(self) -> str:
+        ...
+
+    def visit_code_sequence(self) -> int:
+        ...
+
+    def visit_schedule_name(self) -> str:
+        ...
+
+    def schedule_name(self) -> str:
+        ...
