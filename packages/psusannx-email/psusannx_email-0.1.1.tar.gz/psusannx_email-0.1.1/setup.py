32b80b58c35286f9039248231d41417f494fdff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:15 +0000

    Add package psusannx-email/0.1.1/psusannx_email-0.1.1.tar.gz

---
+++ packages/psusannx-email/psusannx_email-0.1.1.tar.gz/psusannx_email-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Get the current directory of the setup.py file (as this is where the README.md will be too)
+current_dir = Path(__file__).parent
+long_description = (current_dir / "README.md").read_text()
+
+# Set up the package metadata
+setup(
+    name="psusannx_email",
+    author="Jamie O'Brien",
+    description="A package with a function for sending emails easily using sendgrid.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.1",
+    packages=find_packages(include=["psusannx_email", "psusannx_email.*"]),
+    install_requires=["sendgrid>=6.9.7"]
+)
