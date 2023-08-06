6ba4984bae19d5d91d8ff359ab2727fca2451abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:40 +0000

    Add package MY-ISAT/1.0/MY_ISAT-1.0.tar.gz

---
+++ packages/MY-ISAT/MY_ISAT-1.0.tar.gz/MY_ISAT-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MY_ISAT',
+    version='1.0',
+    author='BeanBUN',
+    author_email='1224273829@qq.com',
+    packages=find_packages()
+)
