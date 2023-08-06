317a98ca9b54bf8279bbe544aaa9648f9453c43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:40 +0000

    Add package arcticdb/1.6.1rc1/arcticdb-1.6.1rc1-cp311-cp311-win_amd64.whl

---
+++ packages/arcticdb/arcticdb-1.6.1rc1-cp311-cp311-win_amd64.whl/arcticdb/__init__.py
@@ -0,0 +1,15 @@
+import arcticdb_ext as _ext
+import os as _os
+import sys as _sys
+
+from arcticdb.arctic import Arctic
+from arcticdb.options import LibraryOptions
+from arcticdb.version_store.processing import QueryBuilder
+import arcticdb.version_store.library as library
+from arcticdb.options import LibraryOptions
+from arcticdb.tools import set_config_from_env_vars
+from arcticdb_ext.version_store import DataError
+
+set_config_from_env_vars(_os.environ)
+
+__version__ = "1.6.1-rc1"
