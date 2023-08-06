91ca8d5e8b8286b2e432e2dcab33854682eaa28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:54 +0000

    Add package Local-Xampp-Apache/0.0.1/Local_Xampp_Apache-0.0.1.tar.gz

---
+++ packages/Local-Xampp-Apache/Local_Xampp_Apache-0.0.1.tar.gz/Local_Xampp_Apache-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+
+
+setup(
+    name="Local_Xampp_Apache",
+    version="0.0.1",
+    author="Sridhar",
+    author_email="sridhardscv@gmail.com",
+    description="Access the Windows XamPP Apache Server via Command lines",
+    # long_description="A longer description or README for your package",
+    long_description_content_type="text/markdown",
+    url="https://git.selfmade.ninja/SRIDHARDSCV/automate_xampp_windows",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=required_packages,
+    entry_points={
+        "console_scripts": [
+            "My_Apache = src.main:main"
+        ]
+    }
+)
