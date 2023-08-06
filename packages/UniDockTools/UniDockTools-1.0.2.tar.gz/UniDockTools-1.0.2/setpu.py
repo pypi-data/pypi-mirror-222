c60bab184bde298e3b43c00cac69ced3c808ee60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:14 +0000

    Add package UniDockTools/1.0.2/UniDockTools-1.0.2.tar.gz

---
+++ packages/UniDockTools/UniDockTools-1.0.2.tar.gz/UniDockTools-1.0.2/setpu.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+install_requires = ['rdkit', 'networkx']
+
+setup(
+    name='UniDockTools',
+    version='1.0.2',
+    author='DP BayMax',
+    description="UniDock, a GPU-accelerated molecular docking program developed by DP Technology",
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    #scripts=['UniDock/bin/unidock'],
+    package_data={
+        "UniDock": ["data"],
+    },
+    entry_points={
+        'console_scripts': [
+            'Unidock = UniDock.unidock:main',
+        ],
+    },
+)
