0ed6bf12b44ffabc0f42ce6ce05fe47921b3f846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:11 +0000

    Add package simple-calculate-zmw/1.0.0/simple_calculate_zmw-1.0.0.tar.gz

---
+++ packages/simple-calculate-zmw/simple_calculate_zmw-1.0.0.tar.gz/simple_calculate_zmw-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simple_calculate_zmw',
+    version='1.0.0',
+    author='zhangmingwei',
+    author_email='zhangmingwei@megvii.com.com',
+    description='A simple Python package',
+    packages=['simple_calculate'],
+    install_requires=[
+    ],
+)
