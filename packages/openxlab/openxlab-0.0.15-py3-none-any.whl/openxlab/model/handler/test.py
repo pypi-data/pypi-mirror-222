8a8c1a02920e269026c8f5fb1e89377fb1f222e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:36 +0000

    Add package openxlab/0.0.15/openxlab-0.0.15-py3-none-any.whl

---
+++ packages/openxlab/openxlab-0.0.15-py3-none-any.whl/openxlab/model/handler/test.py
@@ -0,0 +1,10 @@
+# from openxlab.model.handler.model_inference import inference
+# import openxlab
+# # inference("mmocr/SVTR", ['./demo_text_ocr.jpg'])
+# openxlab.login(ak="zbp7y9yer7ywrzv90jwe", sk="v4owjbd1e9qedvnybaoap8oegpr68akyzwxbmrgo", re_login=True)
+# inference("demooooooo/CLIP", ['./demo_text_ocr.jpg'])
+
+import openxlab
+
+openxlab.login(ak='vegl9yn4xwd0nqz5rrwm', sk='x8an3z7ad9o0vw4don3g1jebvy6vggbeok2pekrq', re_login=True)
+# download_metafile_template()
