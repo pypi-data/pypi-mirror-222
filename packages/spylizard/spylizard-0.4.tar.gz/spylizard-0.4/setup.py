0f5767c02de720c71048473b5bfc7fe1557655aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:56 +0000

    Add package spylizard/0.4/spylizard-0.4.tar.gz

---
+++ packages/spylizard/spylizard-0.4.tar.gz/spylizard-0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='spylizard',
+    version='0.4',    
+    description='A python wrapper for spylizard',
+    url='https://github.com/halbux/sparselizard-users',
+    author='Hamid Badi',
+    author_email='badi.hamid@gmail.com',
+    license='GNU GPLv2+',
+    packages=['spylizard'],
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
