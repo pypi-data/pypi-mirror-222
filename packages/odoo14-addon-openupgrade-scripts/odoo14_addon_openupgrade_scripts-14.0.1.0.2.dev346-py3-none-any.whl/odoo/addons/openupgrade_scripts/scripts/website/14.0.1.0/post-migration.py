a5bb87dcd0a190084cea5c32fa25c9d2d1ed5e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:49 +0000

    Add package odoo14-addon-openupgrade-scripts/14.0.1.0.2.dev346/odoo14_addon_openupgrade_scripts-14.0.1.0.2.dev346-py3-none-any.whl

---
+++ packages/odoo14-addon-openupgrade-scripts/odoo14_addon_openupgrade_scripts-14.0.1.0.2.dev346-py3-none-any.whl/odoo/addons/openupgrade_scripts/scripts/website/14.0.1.0/post-migration.py
@@ -0,0 +1,46 @@
+# Copyright 2021 ForgeFlow S.L.  <https://www.forgeflow.com>
+# Copyright 2023 Tecnativa - Pilar Vargas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+from openupgradelib.openupgrade_140 import convert_html_string_13to14
+
+
+def website_cookie_notice_post_migration(env):
+    cookie_message = env.ref("website.cookie_message", raise_if_not_found=False)
+    if cookie_message:
+        websites = env["website"].search([])
+        for website in websites:
+            website.write({"cookies_bar": True})
+        env.cr.execute(
+            """WITH keys AS (
+                SELECT key
+                FROM ir_ui_view iuv
+                JOIN ir_model_data imd ON (imd.model = 'ir.ui.view'
+                    AND imd.res_id = iuv.id AND module = 'website')
+                WHERE imd.name IN ('cookie_message', 'cookie_message_container')
+            )
+            SELECT iuv.id
+            FROM ir_ui_view iuv
+            JOIN keys ON iuv.key = keys.key""",
+        )
+        view_ids = [x[0] for x in env.cr.fetchall()]
+        if view_ids:
+            env["ir.ui.view"].browse(view_ids).unlink()
+
+
+def convert_html_string(env):
+    website_views = env["ir.ui.view"].search(
+        [("type", "=", "qweb"), ("website_id", "!=", False)]
+    )
+    for view in website_views:
+        new_arch_db = convert_html_string_13to14(view.arch_db)
+        if new_arch_db != view.arch_db:
+            view.arch_db = new_arch_db
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "website", "14.0.1.0/noupdate_changes.xml")
+    openupgrade.delete_records_safely_by_xml_id(env, ["website.aboutus_page"])
+    website_cookie_notice_post_migration(env)
+    convert_html_string(env)
