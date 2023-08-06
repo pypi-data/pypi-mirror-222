0a733ab9dd064f2aea52f94554c91a6d679afd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:52 +0000

    Add package lsstream/1.2.1/lsstream-1.2.1-py3-none-any.whl

---
+++ packages/lsstream/lsstream-1.2.1-py3-none-any.whl/lsstream/defaults.py
@@ -0,0 +1,5 @@
+# Set up your default stuff in here, like the embed_template, test page, etc
+
+EMBED_TEMPLATE = '<a href="{LINK}">Click for {TITLE}</a>'
+
+TEST_PAGE = 'https://canvas.asu.edu/courses/26086/pages/test-page'
---
+++ packages/lsstream/lsstream-1.2.1-py3-none-any.whl/lsstream/file_generation.py
@@ -0,0 +1,45 @@
+import os
+import pyperclip
+from .defaults import EMBED_TEMPLATE    
+from .style import color
+from .prompts import prompt_directory, prompt_details
+
+# Creates the files and returns its contents
+def create_file(movie_title, media_link, output_directory):
+    output_file_path = os.path.join(output_directory, f'{movie_title}.txt')
+    file_content = EMBED_TEMPLATE.format(LINK=media_link, TITLE=movie_title)
+
+    with open(output_file_path, 'w') as f:
+        f.write(file_content)
+    
+    return output_file_path, file_content 
+
+# Loop for file and content generation
+def create_content():
+    movie_titles = []
+    embed_contents = []
+    new_contents = []
+    new_files = []
+    count = 0
+
+    output_directory = prompt_directory()
+
+    while True:
+        movie_title, media_link = prompt_details()
+        movie_titles.append(f'"{movie_title}"')
+        new_file, new_content = create_file(movie_title, media_link, output_directory)  
+
+        embed_content = EMBED_TEMPLATE.format(LINK = media_link, TITLE = movie_title)
+        embed_contents.append(embed_content) 
+
+        new_files.append(new_file)
+        new_contents.append(new_content)  
+
+        continue_prompt = input(color('\nContinue? (Y/N): ', 'white'))
+        if continue_prompt.lower() != 'y':
+            print('\n', color(f"✔ Embed code(s) for {', '.join(movie_titles)} successfully pasted to clipboard!", 'green'))
+            break
+    
+    pyperclip.copy('\n'.join(embed_contents))
+    
+    return new_files, new_contents
