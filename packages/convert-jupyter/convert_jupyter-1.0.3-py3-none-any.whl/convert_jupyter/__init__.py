4f8fcd02b317853eec64b8d308c430cfa2a7ff20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:47 +0000

    Add package convert-jupyter/1.0.3/convert_jupyter-1.0.3-py3-none-any.whl

---
+++ packages/convert-jupyter/convert_jupyter-1.0.3-py3-none-any.whl/convert_jupyter/__init__.py
@@ -0,0 +1,9 @@
+from .clean import clean, clean_all
+from .code import jupyter2py, py2jupyter
+
+__all__ = [
+    "py2jupyter",
+    "jupyter2py",
+    "clean",
+    "clean_all",
+]
---
+++ packages/convert-jupyter/convert_jupyter-1.0.3-py3-none-any.whl/convert_jupyter/constans.py
@@ -0,0 +1,3 @@
+CELL_SEPARATOR = "# New cell\n"
+MARKDOWN_BEGIN = '"""md\n'
+MARKDOWN_END = '"""\n'
