12e83f8c216164e8194a0ee73cb4412d56456cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:26 +0000

    Add package utz/0.3.28/utz-0.3.28.tar.gz

---
+++ packages/utz/utz-0.3.28.tar.gz/utz-0.3.28/setup.py
@@ -0,0 +1,28 @@
+from utz.setup import setup
+
+setup(
+    name="utz",
+    version="0.3.28",
+    install_requires=open('requirements.txt', 'r').read(),
+    extras_require={
+        'pdf': [
+            'reportlab',
+            'pyPDF2',
+        ],
+        'setup': [
+            'lxml',
+            'mistune',
+        ],
+        'test': [
+            'pytest',
+            'pytest-mock',
+        ],
+    },
+    url="https://github.com/runsascoded/utz",
+    entry_points={
+        'console_scripts': [
+            'git-update-submodules=utz.git.git_update_submodules:main',
+            'git-meta-branch-update=utz.git.git_meta_branch_update:main',
+        ],
+    },
+)
