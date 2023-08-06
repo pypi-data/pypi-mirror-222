6dad91de7aa9a69fd5c486b9230a2e78dfb8ab61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:30 +0000

    Add package umair/0.0.2/umair-0.0.2-py3-none-any.whl

---
+++ packages/umair/umair-0.0.2-py3-none-any.whl/umair/details.py
@@ -0,0 +1,60 @@
+def firstname():
+    print("Umair")
+
+
+def middlename():
+    print("Hamid")
+
+
+def lastname():
+    print("Thakur")
+
+
+def name():
+    print(f"""{firstname()} + " " + {middlename()} + " " + {lastname()}""")
+
+
+def dob():
+    print("16/08/1999")
+
+
+def gender():
+    print("Male")
+
+
+def address():
+    print("Mumbai, Maharashtra, India")
+
+
+def linkedin():
+    print("https://www.linkedin.com/in/umairthakur/")
+
+
+def github():
+    print("https://github.com/UmairThakur")
+
+
+def twitter():
+    print("No Account Found")
+
+
+def instagram():
+    print("No Account Found")
+
+
+def facebook():
+    print("No Account Found")
+
+
+def youtube():
+    print("No Account Found")
+
+
+def snapchat():
+    print("No Account Found")
+
+
+def test():
+    print("test")
+    i = input("Say something: ")
+    print("You said: ", i)
