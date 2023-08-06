705346bf8dde2825fa39143f0b5ecb4e8eb47404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:47 +0000

    Add package nonebot-plugin-savepic/0.1.1/nonebot_plugin_savepic-0.1.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-savepic/nonebot_plugin_savepic-0.1.1-py3-none-any.whl/nonebot_plugin_savepic/config.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Extra
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    """Plugin Config Here"""
+    savepic_admin: list[str] = []
+    savepic_dir: str = "savepic"
