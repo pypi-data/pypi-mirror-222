80b3e0d1093773960bee731124e78e761a65071c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:27 +0000

    Add package iot-sdk-demo/0.0.3/iot-sdk-demo-0.0.3.tar.gz

---
+++ packages/iot-sdk-demo/iot-sdk-demo-0.0.3.tar.gz/iot-sdk-demo-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools #导入setuptools打包工具
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="iot-sdk-demo", # 用自己的名替换其中的YOUR_USERNAME_
+    version="0.0.3",    #包版本号，便于维护版本
+    author="HFC",    #作者，可以写自己的姓名
+    author_email="2898534520@qq.com",    #作者联系方式，可写自己的邮箱地址
+    description="A small example package",#包的简述
+    long_description=long_description,    #包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",    #自己项目地址，比如github的项目地址
+    packages=setuptools.find_packages(),  # Make sure to include 'where' argument
+    zip_safe=False,  # Disable zip-safe mode for easier debugging
+    include_package_data=True, 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',    #对python的最低版本要求
+    install_requires=[  #对应依赖信息
+        "bottle==0.12.25",
+        "netifaces==0.11.0",
+        "paho_mqtt==1.6.1",
+        "psutil==5.9.4",
+        "Requests==2.31.0",
+        "ujson==5.7.0"
+    ],
+)
