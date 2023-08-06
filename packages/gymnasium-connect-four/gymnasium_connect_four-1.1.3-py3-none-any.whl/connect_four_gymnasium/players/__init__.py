5e26746b1f27aeb5f9132866b11b7b970c890c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:36 +0000

    Add package gymnasium-connect-four/1.1.3/gymnasium_connect_four-1.1.3-py3-none-any.whl

---
+++ packages/gymnasium-connect-four/gymnasium_connect_four-1.1.3-py3-none-any.whl/connect_four_gymnasium/players/__init__.py
@@ -0,0 +1,13 @@
+from .BabyPlayer import BabyPlayer
+from .ChildPlayer import ChildPlayer
+from .ChildSmarterPlayer import ChildSmarterPlayer
+from .TeenagerPlayer import TeenagerPlayer
+from .TeenagerSmarterPlayer import TeenagerSmarterPlayer
+from .AdultPlayer import AdultPlayer
+from .AdultSmarterPlayer import AdultSmarterPlayer
+
+from .MinMaxPlayer import MinMaxPlayer
+
+from .ModelPlayer import ModelPlayer
+from .ConsolePlayer import ConsolePlayer
+from .SimulatePlayer import SimulatePlayer
