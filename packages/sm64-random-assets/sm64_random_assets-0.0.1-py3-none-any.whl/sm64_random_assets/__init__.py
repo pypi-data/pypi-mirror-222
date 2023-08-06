be8a6bc03ca86163aed2f9ef9c904c6fe6e72ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:33 +0000

    Add package sm64-random-assets/0.0.1/sm64_random_assets-0.0.1-py3-none-any.whl

---
+++ packages/sm64-random-assets/sm64_random_assets-0.0.1-py3-none-any.whl/sm64_random_assets/__init__.py
@@ -0,0 +1,11 @@
+"""
+Basic
+"""
+__version__ = '0.0.1'
+__author__ = 'Jon Crall'
+__author_email__ = 'erotemic@gmail.com'
+__url__ = 'None'
+
+__mkinit__ = """
+mkinit /home/joncrall/code/sm64-random-assets/sm64-random-assets/__init__.py
+"""
