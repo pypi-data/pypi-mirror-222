708627deb645aac602af45c66d346e10d6b0f263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:36 +0000

    Add package pdr-sapphire-wrapper/0.1.12/pdr-sapphire-wrapper-0.1.12.tar.gz

---
+++ packages/pdr-sapphire-wrapper/pdr-sapphire-wrapper-0.1.12.tar.gz/pdr-sapphire-wrapper-0.1.12/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+readme = ""
+
+setup(
+    author="oceanprotocol",
+    author_email="devops@oceanprotocol.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Sapphire transaction wrapper",
+    install_requires=[],
+    name="pdr-sapphire-wrapper",
+    license="Apache Software License 2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    packages=find_packages(
+        include=[
+            "sapphire_wrapper",
+        ]
+    ),
+    url="https://github.com/oceanprotocol/pdr-sapphire-wrapper",
+    version="0.1.12",
+    zip_safe=False,
+    data_files=[
+        (
+            "sapphire_wrapper_bin",
+            ["bin/sapphirewrapper.dylib", "bin/sapphirewrapper.so"],
+        )
+    ],
+)
