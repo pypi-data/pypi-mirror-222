5cfe3a67d3c5fab87d5c4adad2a1f1b714dbfd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:45 +0000

    Add package metarace/2.1.5/metarace-2.1.5.tar.gz

---
+++ packages/metarace/metarace-2.1.5.tar.gz/metarace-2.1.5/README.md
@@ -0,0 +1,166 @@
+# metarace
+
+A collection of Python modules to assist with cycle race timekeeping
+and official result preparation. Version 2 of Metarace is a
+re-write for Python 3 which removes static pyGTK/GLib dependencies.
+
+Application-level modules are not contained in
+the library, they are available separately:
+
+   - [roadmeet](https://github.com/ndf-zz/metarace-roadmeet) : Timing
+     and results for UCI Part 2 Road Races, UCI Part 5 Cyclo-Cross,
+     criterium, road handicap and ad-hoc time trial events.
+   - [tagreg](https://github.com/ndf-zz/metarace-tagreg) : Transponder
+     id management.
+   - [ttstart](https://github.com/ndf-zz/metarace-ttstart) : Time
+     Trial starter console.
+
+
+## Work in Progress
+
+   - include grapheme support in strops
+   - re-write report library for better sectioning
+   - overhaul PDF text rendering
+   - replace xls export with xlsx
+   - module documentation
+   - sample scripts
+
+
+## Module Overview
+
+### metarace: Base Library
+
+   - shared configuration, default files and resources
+   - tempfile-backed file writer
+   - meet folder locking
+
+
+### jsonconfig: Configuration Options
+
+Schema defined dictionary-like
+configuration with JSON export and import.
+
+
+### riderdb: CSV-backed Competitor Information
+
+Store details for competitors, teams, and categories.
+
+
+### tod: Time of Day
+
+Represent timing measurements, calculations for
+short intervals (<24 hours) and aggregate times.
+
+
+### timy: Alge Timy Chronometer
+
+Read time of day measurements from an attached Alge Timy
+in PC-TIMER mode.
+
+
+### decoder: Transponder Decoders
+
+Read transponder and timing information from
+Race Result and Chronelec devices:
+
+   - rrs : Race Result System Decoder (passive and active)
+   - rru : Race Result USB Timing Box (active)
+   - thbc : Chronelec (Tag Heuer) Protime/Elite RC and LS
+
+
+### strops: Common String Manipulations
+
+Commonly used functions for formatting competitor names,
+rankings and user inputs.
+
+
+### telegraph: Interprocess Communication
+
+MQTT-backed pub/sub message exchange service.
+
+
+### unt4: Legacy Timing Protocol
+
+Swiss Timing UNT4 protocol wrapper, for legacy devices
+and DHI communications.
+
+
+### countback: Accumulate and Compare Count of Places
+
+Represent a countback of places and allow for simple
+placing comparisons.
+
+
+### htlib: HTML Generation
+
+Functional primitives for HTML generation.
+
+
+### report: Report Generation
+
+Create sectioned reports and save to PDF, HTML, XLS and JSON.
+
+
+### export: Result Export and Mirroring
+
+Mirror export files to a remote host using rsync over ssh,
+rsync TCP daemon or by running a local script.
+
+
+### eventdb: CSV Event List
+
+Store details for multple events within a meet.
+
+
+## Requirements
+
+System requirements:
+
+   - Python >= 3.9
+   - Cairo
+   - Pango
+   - Rsvg
+   - Python gi
+   - Python gi-cairo
+   - tex-gyre (optional, recommended)
+   - evince (optional, recommended)
+   - fonts-noto (optional)
+   - mosquitto (optional)
+   - libreoffice (optional)
+
+Python packages:
+
+   - pyserial: Serial port interface
+   - python-dateutil: Generic date/time string parser
+   - xlwt: XLS file writer
+   - paho-mqtt: MQTT interface
+   - grapheme: Unicode grapheme support
+
+
+## Installation
+
+Check that your python version is at least 3.9 before installing.
+This library will not work with python versions less than 3.9.
+
+
+### Debian 11+
+
+Install system requirements with apt:
+
+	$ sudo apt install python3-venv python3-pip python3-cairo python3-gi python3-gi-cairo
+	$ sudo apt install gir1.2-rsvg-2.0 gir1.2-pango-1.0
+	$ sudo apt install python3-serial python3-paho-mqtt python3-dateutil python3-xlwt
+
+Optionally add fonts, PDF viewer and MQTT broker:
+
+	$ sudo apt install tex-gyre fonts-noto evince mosquitto
+
+Create a virtualenv for metarace and associated packages:
+
+	$ python3 -m venv --system-site-packages venv
+
+Activate the virtualenv and install packages with pip:
+
+	$ source ./venv/bin/activate
+	(venv) $ pip3 install metarace
+
---
+++ packages/metarace/metarace-2.1.5.tar.gz/metarace-2.1.5/src/metarace/__init__.py
@@ -0,0 +1,273 @@
+# SPDX-License-Identifier: MIT
+"""A collection of tools for preparing cycle race results."""
+
+import os
+import logging
+import errno
+from tempfile import NamedTemporaryFile
+from shutil import copyfile
+from metarace import jsonconfig
+try:
+    from importlib.resources import files, as_file
+except ImportError:
+    print('Python >= 3.9 is required to use this module')
+flockstyle = None
+try:
+    import fcntl
+    flockstyle = 'o-g'
+except ImportError:
+    pass
+if flockstyle is None:
+    # possibly a windows machine
+    import msvcrt
+    flockstyle = 'deviant'
+
+VERSION = '2.1.5'
+DATA_PATH = os.path.realpath(
+    os.path.expanduser(os.path.join('~', 'Documents', 'metarace')))
+DEFAULTS_PATH = os.path.join(DATA_PATH, 'default')
+RESOURCE_PKG = 'metarace.data'
+LOGO = 'metarace_icon.svg'
+SYSCONF = 'metarace.json'
+PDF_TEMPLATE = 'pdf_template.json'
+LOGFILEFORMAT = '%(asctime)s %(levelname)s:%(name)s: %(message)s'
+LOGFORMAT = '%(levelname)s %(name)s: %(message)s'
+LOGLEVEL = logging.DEBUG  # default console log level
+sysconf = jsonconfig.config()  # system-defaults, populated by init() method
+_log = logging.getLogger('metarace')
+_log.setLevel(logging.DEBUG)
+
+
+def init():
+    """Shared metarace program initialisation."""
+    copyconf = mk_data_path()
+
+    # Set global logging options
+    logging._srcfile = None
+    logging.logThreads = 0
+    logging.logProcesses = 0
+
+    # read in a system configuration
+    conffile = default_file(SYSCONF)
+    try:
+        if os.path.exists(conffile):
+            sysconf.load(conffile)
+            # don't copy path-specific config into defaults
+            copyconf = False
+        else:
+            _log.info('System defaults not present, using package defaults')
+            ref = files(RESOURCE_PKG).joinpath(SYSCONF)
+            with ref.open('rb') as f:
+                sysconf.read(f)
+            copyconf = True
+    except Exception as e:
+        _log.error('%s reading system config: %s', e.__class__.__name__, e)
+
+    # if required, create new system default file
+    if copyconf:
+        _log.info('Creating default system config %s', SYSCONF)
+        with savefile(os.path.join(DEFAULTS_PATH, SYSCONF), perm=0o600) as f:
+            sysconf.write(f)
+
+
+def mk_data_path():
+    """Create a shared data path if it does not yet exist."""
+    ret = False
+    if not os.path.exists(DATA_PATH):
+        _log.info('Creating data directory: %r', DATA_PATH)
+        os.makedirs(DATA_PATH)
+    if not os.path.exists(DEFAULTS_PATH):
+        _log.info('Creating system defaults directory: %r', DEFAULTS_PATH)
+        os.makedirs(DEFAULTS_PATH)
+        ret = True  # flag copy of config back to defaults path
+    lfile = os.path.join(DEFAULTS_PATH, LOGO)
+    if not os.path.exists(lfile):
+        _log.info('Saving default app logo into defaults path')
+        ref = files(RESOURCE_PKG).joinpath(LOGO)
+        with ref.open('rb') as sf:
+            with savefile(lfile, mode='b') as df:
+                df.write(sf.read())
+    return ret
+
+
+def config_path(configpath=None):
+    """Clean and check argument for a writeable meet configuration path."""
+    ret = None
+    if configpath is not None:
+        # sanitise into expected config path
+        ret = configpath
+        if not os.path.isdir(ret):
+            ret = os.path.dirname(ret)  # assume dangling path contains file
+        ret = os.path.realpath(ret)
+        _log.debug('Checking for meet %r using %r', configpath, ret)
+        # then check if the path exists
+        if not os.path.exists(ret):
+            try:
+                _log.info('Creating meet folder %r', ret)
+                os.makedirs(ret)
+            except Exception as e:
+                _log.error('Unable to create folder %r: %s', ret, e)
+                ret = None
+        # check the path is writable
+        if ret is not None:
+            try:
+                _log.debug('Checking folder %r for write access', ret)
+                with NamedTemporaryFile(dir=ret, prefix='.chkwrite_') as f:
+                    pass
+            except Exception as e:
+                _log.error('Unable to access meet folder %r: %s', ret, e)
+                ret = None
+    return ret
+
+
+def default_file(filename=''):
+    """Return a path to the named file.
+
+    Path components are stripped, then the the following locations
+    are checked in order to find the first instance of filename:
+        - current working directory
+        - DEFAULTS_PATH
+    """
+    basefile = os.path.basename(filename)
+    if basefile in ['..', '.', '', None]:
+        _log.debug('Invalid filename %r ignored', filename)
+        return None
+    ret = basefile
+    if os.path.exists(basefile):
+        pass
+    else:
+        try:
+            check = os.path.join(DEFAULTS_PATH, basefile)
+            os.stat(check)
+            ret = check
+        except Exception as e:
+            # ignore file not found and path errors
+            pass
+    return ret
+
+
+def resource_text(name=''):
+    """Return a string from the contents of the named resource."""
+    basefile = os.path.basename(name)
+    if basefile in ['..', '.', '', None]:
+        raise FileNotFoundError('Invalid resource name: ' + repr(name))
+    t = files(RESOURCE_PKG).joinpath(basefile)
+    if t is not None and t.is_file():
+        return t.read_text(encoding='utf-8')
+    else:
+        raise FileNotFoundError('Named resource not found: ' + repr(name))
+
+
+def resource_file(name=''):
+    """Return a temporary filename context manager for a named resource.
+
+    Note: This returns a context manager for a (potentially) temporary
+          filename on the filesystem, it must be used in a with statement
+          eg:
+
+          with resource_file('resource.svg') as r:
+              Gtk.Image.new_from_file(r)
+    """
+
+    basefile = os.path.basename(name)
+    if basefile in ['..', '.', '', None]:
+        raise FileNotFoundError('Invalid resource name: ' + repr(name))
+    t = files(RESOURCE_PKG).joinpath(basefile)
+    if t is not None and t.is_file():
+        return as_file(t)
+    else:
+        raise FileNotFoundError('Named resource not found: ' + repr(name))
+
+
+class savefile:
+    """Tempfile-backed save file contextmanager.
+
+       Creates a temporary file with the desired mode and encoding
+       and returns a context manager and writable file handle.
+
+       On close, the temp file is atomically moved to the provided
+       filename (if possible).
+
+       Note: This function will log a warning if the file could not be
+       moved atomically.
+    """
+
+    def __init__(self,
+                 filename,
+                 mode='t',
+                 encoding='utf-8',
+                 tempdir='.',
+                 perm=0o644):
+        self.__sfile = filename
+        self.__path = tempdir
+        self.__perm = perm
+        if mode == 'b':
+            encoding = None
+        self.__tfile = NamedTemporaryFile(mode='w' + mode,
+                                          suffix='.tmp',
+                                          prefix='sav_',
+                                          dir=self.__path,
+                                          encoding=encoding,
+                                          delete=False)
+
+    def __enter__(self):
+        return self.__tfile
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__tfile.close()
+        if exc_type is not None:
+            return False  # raise exception
+        # otherwise, file is saved ok in temp file
+        os.chmod(self.__tfile.name, self.__perm)
+        try:
+            os.rename(self.__tfile.name, self.__sfile)
+        except OSError as e:
+            copyfile(self.__tfile.name, self.__sfile)
+            os.unlink(self.__tfile.name)
+        return True
+
+
+def lockpath(configpath):
+    """Open an advisory lock file in the meet config path."""
+    if flockstyle is None:
+        _log.error('Meet path locking not available')
+        return None
+    lf = None
+    lfn = os.path.join(configpath, '.lock')
+    try:
+        lf = open(lfn, 'a+b')
+        if flockstyle == 'o-g':
+            fcntl.flock(lf, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            _log.debug('Config lock %r acquired by fcntl', lfn)
+        else:
+            lf.seek(0)
+            msvcrt.locking(lf.fileno(), msvcrt.LK_NBLCK, 1)
+            _log.debug('Config lock %r acquired by msvcrt', lfn)
+    except Exception as e:
+        if lf is not None:
+            lf.close()
+            lf = None
+        _log.error('Unable to acquire config lock %r: %s', lfn, e)
+    return lf
+
+
+def unlockpath(configpath, lockfile):
+    """Release advisory lock and remove lock file."""
+    lfn = os.path.join(configpath, '.lock')
+    os.unlink(lfn)
+    lockfile.close()
+    _log.debug('Config lock %r released', lfn)
+    return None
+
+
+LICENSETEXT = """
+MIT License
+
+Copyright (c) 2012-2023 Nathan Fraser and contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
