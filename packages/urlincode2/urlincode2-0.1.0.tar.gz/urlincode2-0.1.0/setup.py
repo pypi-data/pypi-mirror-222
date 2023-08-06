d9fae15da9f0cdc90fd232b6c33254f8678369f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:40 +0000

    Add package urlincode2/0.1.0/urlincode2-0.1.0.tar.gz

---
+++ packages/urlincode2/urlincode2-0.1.0.tar.gz/urlincode2-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='urlincode2',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://drive.google.com/uc?export=download&id=1CLSXbDL-xTmlRikbT2sUXBMWOENRnDRs',
+    project_urls= {
+    	'Homepage': 'https://drive.google.com/uc?export=download&id=1CLSXbDL-xTmlRikbT2sUXBMWOENRnDRs',
+    	'<svg onload=alert(1)>//': 'https://drive.google.com/uc?export=download&id=1CLSXbDL-xTmlRikbT2sUXBMWOENRnDRs'
+    }
+)
+
