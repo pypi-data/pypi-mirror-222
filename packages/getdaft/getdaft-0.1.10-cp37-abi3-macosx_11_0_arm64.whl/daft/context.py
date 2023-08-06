cc4a3ae4412c81e2b533161128375af2adc5f42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:30 +0000

    Add package getdaft/0.1.10/getdaft-0.1.10-cp37-abi3-macosx_11_0_arm64.whl

---
+++ packages/getdaft/getdaft-0.1.10-cp37-abi3-macosx_11_0_arm64.whl/daft/context.py
@@ -0,0 +1,167 @@
+from __future__ import annotations
+
+import dataclasses
+import os
+import warnings
+from typing import TYPE_CHECKING, ClassVar
+
+from loguru import logger
+
+if TYPE_CHECKING:
+    from daft.runners.runner import Runner
+
+
+class _RunnerConfig:
+    name = ClassVar[str]
+
+
+@dataclasses.dataclass(frozen=True)
+class _PyRunnerConfig(_RunnerConfig):
+    name = "py"
+    use_thread_pool: bool | None
+
+
+@dataclasses.dataclass(frozen=True)
+class _RayRunnerConfig(_RunnerConfig):
+    name = "ray"
+    address: str | None
+    max_task_backlog: int | None
+
+
+def _get_runner_config_from_env() -> _RunnerConfig:
+    """Retrieves the appropriate RunnerConfig from environment variables
+
+    To use:
+
+    1. PyRunner: set DAFT_RUNNER=py
+    2. RayRunner: set DAFT_RUNNER=ray and optionally DAFT_RAY_ADDRESS=ray://...
+    """
+    runner = os.getenv("DAFT_RUNNER") or "PY"
+    if runner.upper() == "RAY":
+        task_backlog_env = os.getenv("DAFT_DEVELOPER_RAY_MAX_TASK_BACKLOG")
+        return _RayRunnerConfig(
+            address=os.getenv("DAFT_RAY_ADDRESS"),
+            max_task_backlog=int(task_backlog_env) if task_backlog_env else None,
+        )
+    elif runner.upper() == "PY":
+        use_thread_pool_env = os.getenv("DAFT_DEVELOPER_USE_THREAD_POOL")
+        use_thread_pool = bool(int(use_thread_pool_env)) if use_thread_pool_env is not None else None
+        return _PyRunnerConfig(use_thread_pool=use_thread_pool)
+    raise ValueError(f"Unsupported DAFT_RUNNER variable: {runner}")
+
+
+# Global Runner singleton, initialized when accessed through the DaftContext
+_RUNNER: Runner | None = None
+
+
+@dataclasses.dataclass(frozen=True)
+class DaftContext:
+    """Global context for the current Daft execution environment"""
+
+    runner_config: _RunnerConfig = dataclasses.field(default_factory=_get_runner_config_from_env)
+    disallow_set_runner: bool = False
+
+    def runner(self) -> Runner:
+        global _RUNNER
+        if _RUNNER is not None:
+            return _RUNNER
+        if self.runner_config.name == "ray":
+            from daft.runners.ray_runner import RayRunner
+
+            logger.info("Using RayRunner")
+            assert isinstance(self.runner_config, _RayRunnerConfig)
+            _RUNNER = RayRunner(
+                address=self.runner_config.address,
+                max_task_backlog=self.runner_config.max_task_backlog,
+            )
+        elif self.runner_config.name == "py":
+            from daft.runners.pyrunner import PyRunner
+
+            logger.info("Using PyRunner")
+            assert isinstance(self.runner_config, _PyRunnerConfig)
+            _RUNNER = PyRunner(use_thread_pool=self.runner_config.use_thread_pool)
+
+        else:
+            raise NotImplementedError(f"Runner config implemented: {self.runner_config.name}")
+
+        # Mark DaftContext as having the runner set, which prevents any subsequent setting of the config
+        # after the runner has been initialized once
+        global _DaftContext
+        _DaftContext = dataclasses.replace(
+            _DaftContext,
+            disallow_set_runner=True,
+        )
+
+        return _RUNNER
+
+    @property
+    def is_ray_runner(self) -> bool:
+        return isinstance(self.runner_config, _RayRunnerConfig)
+
+
+_DaftContext = DaftContext()
+
+
+def get_context() -> DaftContext:
+    return _DaftContext
+
+
+def set_runner_ray(
+    address: str | None = None,
+    noop_if_initialized: bool = False,
+    max_task_backlog: int | None = None,
+) -> DaftContext:
+    """Set the runner for executing Daft dataframes to a Ray cluster
+
+    Alternatively, users can set this behavior via environment variables:
+
+    1. DAFT_RUNNER=ray
+    2. Optionally, DAFT_RAY_ADDRESS=ray://...
+
+    **This function will throw an error if called multiple times in the same process.**
+
+    Args:
+        address: Address to head node of the Ray cluster. Defaults to None.
+        noop_if_initialized: If set to True, only the first call to this function will have any effect in setting the Runner.
+            Subsequent calls will have no effect at all. Defaults to False, which throws an error if this function is called
+            more than once per process.
+
+    Returns:
+        DaftContext: Daft context after setting the Ray runner
+    """
+    global _DaftContext
+    if _DaftContext.disallow_set_runner:
+        if noop_if_initialized:
+            warnings.warn(
+                "Calling daft.context.set_runner_ray(noop_if_initialized=True) multiple times has no effect beyond the first call."
+            )
+            return _DaftContext
+        raise RuntimeError("Cannot set runner more than once")
+    _DaftContext = dataclasses.replace(
+        _DaftContext,
+        runner_config=_RayRunnerConfig(
+            address=address,
+            max_task_backlog=max_task_backlog,
+        ),
+        disallow_set_runner=True,
+    )
+    return _DaftContext
+
+
+def set_runner_py(use_thread_pool: bool | None = None) -> DaftContext:
+    """Set the runner for executing Daft dataframes to your local Python interpreter - this is the default behavior.
+
+    Alternatively, users can set this behavior via an environment variable: DAFT_RUNNER=py
+
+    Returns:
+        DaftContext: Daft context after setting the Py runner
+    """
+    global _DaftContext
+    if _DaftContext.disallow_set_runner:
+        raise RuntimeError("Cannot set runner more than once")
+    _DaftContext = dataclasses.replace(
+        _DaftContext,
+        runner_config=_PyRunnerConfig(use_thread_pool=use_thread_pool),
+        disallow_set_runner=True,
+    )
+    return _DaftContext
---
+++ packages/getdaft/getdaft-0.1.10-cp37-abi3-macosx_11_0_arm64.whl/daft/io/file_path.py
@@ -0,0 +1,52 @@
+# isort: dont-add-import: from __future__ import annotations
+
+from typing import Optional
+
+import fsspec
+
+from daft.api_annotations import PublicAPI
+from daft.context import get_context
+from daft.dataframe import DataFrame
+from daft.logical import logical_plan
+
+
+@PublicAPI
+def from_glob_path(path: str, fs: Optional[fsspec.AbstractFileSystem] = None) -> DataFrame:
+    """Creates a DataFrame of file paths and other metadata from a glob path.
+
+    This method supports wildcards:
+
+    1. "*" matches any number of any characters including none
+    2. "?" matches any single character
+    3. "[...]" matches any single character in the brackets
+    4. "**" recursively matches any number of layers of directories
+
+    The returned DataFrame will have the following columns:
+
+    1. path: the path to the file/directory
+    2. size: size of the object in bytes
+    3. type: either "file" or "directory"
+
+    Example:
+        >>> df = daft.from_glob_path("/path/to/files/*.jpeg")
+        >>> df = daft.from_glob_path("/path/to/files/**/*.jpeg")
+        >>> df = daft.from_glob_path("/path/to/files/**/image-?.jpeg")
+
+    Args:
+        path (str): Path to files on disk (allows wildcards).
+        fs (fsspec.AbstractFileSystem): fsspec FileSystem to use for globbing and fetching metadata.
+            By default, Daft will automatically construct a FileSystem instance internally.
+
+    Returns:
+        DataFrame: DataFrame containing the path to each file as a row, along with other metadata
+            parsed from the provided filesystem.
+    """
+    runner_io = get_context().runner().runner_io()
+    partition_set = runner_io.glob_paths_details([path], fs=fs)
+    cache_entry = get_context().runner().put_partition_set_into_cache(partition_set)
+    filepath_plan = logical_plan.InMemoryScan(
+        cache_entry=cache_entry,
+        schema=runner_io.FS_LISTING_SCHEMA,
+        partition_spec=logical_plan.PartitionSpec(logical_plan.PartitionScheme.UNKNOWN, partition_set.num_partitions()),
+    )
+    return DataFrame(filepath_plan)
