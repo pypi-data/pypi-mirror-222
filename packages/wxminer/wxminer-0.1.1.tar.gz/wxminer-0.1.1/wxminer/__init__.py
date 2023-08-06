e39e10fbfd0ef62d6dd60ce1662b6cc0d1aba8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:19 +0000

    Add package wxminer/0.1.1/wxminer-0.1.1.tar.gz

---
+++ packages/wxminer/wxminer-0.1.1.tar.gz/wxminer-0.1.1/wxminer/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/evn python
+# -*- coding: utf-8 -*-
+"""
+WX Miner: Deep into WeChat
+
+Copyright (c) 2021-2023 xleven
+"""
+
+from wxminer.wechat import WeChat
+
+
+__all__ = [
+    "WeChat",
+]
