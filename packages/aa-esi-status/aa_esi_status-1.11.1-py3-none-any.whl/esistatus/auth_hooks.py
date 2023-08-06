ee61550f1b126413dab1ac0bd864382ffdf891fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:33 +0000

    Add package aa-esi-status/1.11.1/aa_esi_status-1.11.1-py3-none-any.whl

---
+++ packages/aa-esi-status/aa_esi_status-1.11.1-py3-none-any.whl/esistatus/auth_hooks.py
@@ -0,0 +1,61 @@
+"""
+Hook into AA
+"""
+
+# Django
+from django.utils.translation import gettext_lazy as _
+
+# Alliance Auth
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+# AA ESI Status
+from esistatus import __title__, urls
+
+
+class AaEsiStatusMenuItem(MenuItemHook):  # pylint: disable=too-few-public-methods
+    """
+    This class ensures only authorized users will see the menu entry
+    """
+
+    def __init__(self):
+        # Setup menu entry for sidebar
+        MenuItemHook.__init__(
+            self,
+            _(__title__),
+            "fas fa-signal fa-fw",
+            "esistatus:index",
+            navactive=["esistatus:index"],
+        )
+
+    def render(self, request):
+        """
+        Check if the user has the permission to view this app
+        :param request:
+        :return:
+        """
+
+        if request.user.has_perm("esistatus.basic_access"):
+            return MenuItemHook.render(self, request)
+
+        return ""
+
+
+@hooks.register("menu_item_hook")
+def register_menu():
+    """
+    Register our menu item
+    :return:
+    """
+
+    return AaEsiStatusMenuItem()
+
+
+@hooks.register("url_hook")
+def register_urls():
+    """
+    Register our base url
+    :return:
+    """
+
+    return UrlHook(urls, "esistatus", r"^esi-status/")
