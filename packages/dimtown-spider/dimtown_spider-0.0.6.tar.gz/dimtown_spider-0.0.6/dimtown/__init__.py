38a114bd20ec407b23ec842a831995c80e12626b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:58 +0000

    Add package dimtown-spider/0.0.6/dimtown_spider-0.0.6.tar.gz

---
+++ packages/dimtown-spider/dimtown_spider-0.0.6.tar.gz/dimtown_spider-0.0.6/dimtown/__init__.py
@@ -0,0 +1,40 @@
+from .AnimeAvatar import AnimeAvatar
+from .ArtAlbum import ArtAlbum
+from .cosplay import CosPlay
+from .CoupleAvatar import CoupleAvatar
+from .FemaleAvatar import FemaleAvatar
+from .Figure import Figure
+from .hanfu import HanFu
+from .jk import JK
+from .lolita import Lolita
+from .MaleAvatar import MaleAvatar
+from .pcpic import PcPic
+from .phonepic import PhonePic
+from .SelectedIllustrations import SelectedIllustrations
+from .SelectedPixiv import SelectedPixiv
+from .search import Search
+
+__all__ = [
+    "AnimeAvatar",
+    "ArtAlbum",
+    "CosPlay",
+    "CoupleAvatar",
+    "FemaleAvatar",
+    "Figure",
+    "HanFu",
+    "JK",
+    "Lolita",
+    "MaleAvatar",
+    "PcPic",
+    "PhonePic",
+    "SelectedIllustrations",
+    "SelectedPixiv",
+    "Search",
+]
+__title__ = "dimtown"
+__description__ = "A Spider for https://dimtown.com"
+__url__ = "https://github.com/Cvandia/dimtown-spider"
+__version__ = "0.0.6"
+__author__ = "Cvandia"
+__author_email__ = "1141538825@qq.com"
+__license__ = "Apache License 2.0"
