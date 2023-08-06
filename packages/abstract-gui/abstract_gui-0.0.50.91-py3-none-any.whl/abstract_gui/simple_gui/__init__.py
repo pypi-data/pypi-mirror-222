a291dec9f398d95e822647ad20763458f3ed7e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:53 +0000

    Add package abstract-gui/0.0.50.91/abstract_gui-0.0.50.91-py3-none-any.whl

---
+++ packages/abstract-gui/abstract_gui-0.0.50.91-py3-none-any.whl/abstract_gui/simple_gui/__init__.py
@@ -0,0 +1,5 @@
+import PySimpleGUI as sg
+from abstract_utilities.global_utils import global_registry,change_glob,update_registry,get_global_from_registry
+import .global_bridge as global_bridge
+import .window_manager as window_manager
+from .gui_template import get_gui_fun
