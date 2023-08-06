b1b5c8fb100da149583ece3f921b70960ddad1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:16 +0000

    Add package sedi/0.6/sedi-0.6.tar.gz

---
+++ packages/sedi/sedi-0.6.tar.gz/sedi-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sedi',
+    version='0.6',
+    py_modules=['provision', 'sed'],
+    install_requires=[
+        # list your script's dependencies here
+        # for example, 'requests' if your script uses the requests library
+    ],
+    entry_points='''
+        [console_scripts]
+        sedi=provision:main
+    ''',
+)
