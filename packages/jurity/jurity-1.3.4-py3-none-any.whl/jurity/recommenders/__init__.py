86d7d33fc5dc674d6a6484d88154c5d0593dfb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:12 +0000

    Add package jurity/1.3.4/jurity-1.3.4-py3-none-any.whl

---
+++ packages/jurity/jurity-1.3.4-py3-none-any.whl/jurity/recommenders/__init__.py
@@ -0,0 +1,27 @@
+from typing import NamedTuple
+
+from .auc import AUC
+from .combined import CombinedMetrics
+from .ctr import CTR
+from .map import MAP
+from .ndcg import NDCG
+from .precision import Precision
+from .recall import Recall
+from .diversity import InterListDiversity, IntraListDiversity
+
+
+class BinaryRecoMetrics(NamedTuple):
+    AUC = AUC
+    CTR = CTR
+
+
+class RankingRecoMetrics(NamedTuple):
+    MAP = MAP
+    NDCG = NDCG
+    Precision = Precision
+    Recall = Recall
+
+
+class DiversityRecoMetrics(NamedTuple):
+    InterListDiversity = InterListDiversity
+    IntraListDiversity = IntraListDiversity
