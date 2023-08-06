87498cf56b5fa75bc13bc5a756a96f12d57472b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:21 +0000

    Add package pyrotash/0.2/pyrotash-0.2-py3-none-any.whl

---
+++ packages/pyrotash/pyrotash-0.2-py3-none-any.whl/tpyconfig/tpyconfig.py
@@ -0,0 +1,7 @@
+# pyrotash/tpyconfig/tpyconfig.py
+#Here i will store my important variables Created by Tashri2342
+import os
+
+Chatbt_config = "Tashribots"
+chatgp_config = "Tashrichatting"
+tgnm_config = "Tashri2342"
