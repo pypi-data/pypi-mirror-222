9a974a70a55ebdcdc7ebc45884be84128f2080ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:19 +0000

    Add package easywrite/1.4.0/easywrite-1.4.0.tar.gz

---
+++ packages/easywrite/easywrite-1.4.0.tar.gz/easywrite-1.4.0/easywrite.py
@@ -0,0 +1,24 @@
+# easywrite.py
+import sys
+from enum import Enum
+
+class Color(Enum):
+    NORMAL = '\033[0m'
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    ORANGE = '\033[91m'
+    PINK = '\033[95m'
+    BROWN = '\033[33m'
+    GRAY = '\033[37m'
+    LIME = '\033[92m'
+
+def easywrite(example_text, color=Color.NORMAL):
+    # Map color to the corresponding color code
+    color_code = color.value
+    print(f"{color_code}{example_text}{Color.NORMAL.value}")
