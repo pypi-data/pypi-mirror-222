dc6f9730f3f331497e231e5700936e60b0f0482f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:45 +0000

    Add package nanohelp/0.1.0/nanohelp-0.1.0-py3-none-any.whl

---
+++ packages/nanohelp/nanohelp-0.1.0-py3-none-any.whl/nanohelp/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from __future__ import annotations
+
+# include these files in the build
+from nanohelp.wallet import WalletManager
+
+# imported into setup.py
+__version__ = "0.1.0"
