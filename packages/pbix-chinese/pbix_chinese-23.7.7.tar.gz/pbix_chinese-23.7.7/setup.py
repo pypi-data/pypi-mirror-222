a37f9fce718fe11fb65657467b5a58f54f1146aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:47 +0000

    Add package pbix-chinese/23.7.7/pbix_chinese-23.7.7.tar.gz

---
+++ packages/pbix-chinese/pbix_chinese-23.7.7.tar.gz/pbix_chinese-23.7.7/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+import setuptools
+
+packages = ['pbix_chinese']
+setup(name='pbix_chinese',
+      version='23.7.7',
+      author='xg',
+      packages=packages,
+      package_dir={'requests': 'requests'}, )
