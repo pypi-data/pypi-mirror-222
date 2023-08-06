7f23fb5291c2644eefd96b8f13ede660a6730647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:34 +0000

    Add package yvesCMD/0.0.3.2/yvesCMD-0.0.3.2-py3-none-any.whl

---
+++ packages/yvesCMD/yvesCMD-0.0.3.2-py3-none-any.whl/YvesCMD/src/extensions/select.py
@@ -0,0 +1,37 @@
+__all__ = ["Select", "SelectMap"]
+
+class Select:
+    def __init__(self, option, description, action):
+        self.option = option
+        self.description = description
+        self.action = action
+
+    def execute(self):
+        self.action()
+
+
+class SelectMap:
+    def __init__(self, *selects):
+        self.selects = {select.option: select for select in selects}
+
+    def print_options(self):
+        for select in self.selects.values():
+            print(f"[ {select.option} ] {select.description}")
+
+    def process_selection(self, option):
+        if option in self.selects:
+            self.selects[option].execute()
+        else:
+            print("Invalid option. Please select a valid option.")
+
+    def handle_select(self):
+        self.print_options()
+        while True:
+            user_input = input("[ ? ] ")
+            if user_input == "exit":
+                break
+            try:
+                option = int(user_input)
+                self.process_selection(option)
+            except ValueError:
+                print("Invalid input. Please enter a number or 'exit' to quit.")
