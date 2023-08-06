119cac4222f88fd7ce0f203572f205e4eb1e41c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:12 +0000

    Add package spiralpy/1.0.1/spiralpy-1.0.1.tar.gz

---
+++ packages/spiralpy/spiralpy-1.0.1.tar.gz/spiralpy-1.0.1/examples/foo.sh
@@ -0,0 +1,203 @@
+#! /bin/bash
+
+echo "Run run-batchdft.py 16x1 60 APar APar True Double"
+python run-batchdft.py 16x1 60 APar APar True Double
+echo "Run run-batchdft.py 16x1 64 APar APar True Double"
+python run-batchdft.py 16x1 64 APar APar True Double
+echo "Run run-batchdft.py 16x1 80 APar APar True Double"
+python run-batchdft.py 16x1 80 APar APar True Double
+echo "Run run-batchdft.py 16x1 128 APar APar True Double"
+python run-batchdft.py 16x1 128 APar APar True Double
+echo "Run run-batchdft.py 16x1 192 APar APar True Double"
+python run-batchdft.py 16x1 192 APar APar True Double
+echo "Run run-batchdft.py 16x1 224 APar APar True Double"
+python run-batchdft.py 16x1 224 APar APar True Double
+echo "Run run-batchdft.py 16x1 256 APar APar True Double"
+python run-batchdft.py 16x1 256 APar APar True Double
+echo "Run run-batchdft.py 16x1 440 APar APar True Double"
+python run-batchdft.py 16x1 440 APar APar True Double
+echo "Run run-batchdft.py 16x1 512 APar APar True Double"
+python run-batchdft.py 16x1 512 APar APar True Double
+echo "Run run-batchdft.py 16x1 768 APar APar True Double"
+python run-batchdft.py 16x1 768 APar APar True Double
+echo "Run run-batchdft.py 16x1 1024 APar APar True Double"
+python run-batchdft.py 16x1 1024 APar APar True Double
+echo "Run run-batchdft.py 16x1 2048 APar APar True Double"
+python run-batchdft.py 16x1 2048 APar APar True Double
+
+echo "Run run-batchdft.py 16x1 60 AVec APar True Double"
+python run-batchdft.py 16x1 60 AVec APar True Double
+echo "Run run-batchdft.py 16x1 64 AVec APar True Double"
+python run-batchdft.py 16x1 64 AVec APar True Double
+echo "Run run-batchdft.py 16x1 80 AVec APar True Double"
+python run-batchdft.py 16x1 80 AVec APar True Double
+echo "Run run-batchdft.py 16x1 128 AVec APar True Double"
+python run-batchdft.py 16x1 128 AVec APar True Double
+echo "Run run-batchdft.py 16x1 192 AVec APar True Double"
+python run-batchdft.py 16x1 192 AVec APar True Double
+echo "Run run-batchdft.py 16x1 224 AVec APar True Double"
+python run-batchdft.py 16x1 224 AVec APar True Double
+echo "Run run-batchdft.py 16x1 256 AVec APar True Double"
+python run-batchdft.py 16x1 256 AVec APar True Double
+echo "Run run-batchdft.py 16x1 440 AVec APar True Double"
+python run-batchdft.py 16x1 440 AVec APar True Double
+##  echo "Run run-batchdft.py 16x1 512 AVec APar True Double"
+##  python run-batchdft.py 16x1 512 AVec APar True Double
+echo "Run run-batchdft.py 16x1 768 AVec APar True Double"
+python run-batchdft.py 16x1 768 AVec APar True Double
+echo "Run run-batchdft.py 16x1 1024 AVec APar True Double"
+python run-batchdft.py 16x1 1024 AVec APar True Double
+echo "Run run-batchdft.py 16x1 2048 AVec APar True Double"
+python run-batchdft.py 16x1 2048 AVec APar True Double
+
+echo "Run run-batchdft.py 16x1 60 APar AVec True Double"
+python run-batchdft.py 16x1 60 APar AVec True Double
+echo "Run run-batchdft.py 16x1 64 APar AVec True Double"
+python run-batchdft.py 16x1 64 APar AVec True Double
+echo "Run run-batchdft.py 16x1 80 APar AVec True Double"
+python run-batchdft.py 16x1 80 APar AVec True Double
+echo "Run run-batchdft.py 16x1 128 APar AVec True Double"
+python run-batchdft.py 16x1 128 APar AVec True Double
+echo "Run run-batchdft.py 16x1 192 APar AVec True Double"
+python run-batchdft.py 16x1 192 APar AVec True Double
+echo "Run run-batchdft.py 16x1 224 APar AVec True Double"
+python run-batchdft.py 16x1 224 APar AVec True Double
+echo "Run run-batchdft.py 16x1 256 APar AVec True Double"
+python run-batchdft.py 16x1 256 APar AVec True Double
+echo "Run run-batchdft.py 16x1 440 APar AVec True Double"
+python run-batchdft.py 16x1 440 APar AVec True Double
+##  echo "Run run-batchdft.py 16x1 512 APar AVec True Double"
+##  python run-batchdft.py 16x1 512 APar AVec True Double
+##  echo "Run run-batchdft.py 16x1 768 APar AVec True Double"
+##  python run-batchdft.py 16x1 768 APar AVec True Double
+##  echo "Run run-batchdft.py 16x1 1024 APar AVec True Double"
+##  python run-batchdft.py 16x1 1024 APar AVec True Double
+##  echo "Run run-batchdft.py 16x1 2048 APar AVec True Double"
+##  python run-batchdft.py 16x1 2048 APar AVec True Double
+
+echo "Run run-batchdft.py 16x1 60 AVec AVec True Double"
+python run-batchdft.py 16x1 60 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 64 AVec AVec True Double"
+python run-batchdft.py 16x1 64 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 80 AVec AVec True Double"
+python run-batchdft.py 16x1 80 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 128 AVec AVec True Double"
+python run-batchdft.py 16x1 128 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 192 AVec AVec True Double"
+python run-batchdft.py 16x1 192 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 224 AVec AVec True Double"
+python run-batchdft.py 16x1 224 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 256 AVec AVec True Double"
+python run-batchdft.py 16x1 256 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 440 AVec AVec True Double"
+python run-batchdft.py 16x1 440 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 512 AVec AVec True Double"
+python run-batchdft.py 16x1 512 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 768 AVec AVec True Double"
+python run-batchdft.py 16x1 768 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 1024 AVec AVec True Double"
+python run-batchdft.py 16x1 1024 AVec AVec True Double
+echo "Run run-batchdft.py 16x1 2048 AVec AVec True Double"
+python run-batchdft.py 16x1 2048 AVec AVec True Double
+
+echo "Run run-batchdft.py 16x1 60 APar APar False Double"
+python run-batchdft.py 16x1 60 APar APar False Double
+echo "Run run-batchdft.py 16x1 64 APar APar False Double"
+python run-batchdft.py 16x1 64 APar APar False Double
+echo "Run run-batchdft.py 16x1 80 APar APar False Double"
+python run-batchdft.py 16x1 80 APar APar False Double
+echo "Run run-batchdft.py 16x1 128 APar APar False Double"
+python run-batchdft.py 16x1 128 APar APar False Double
+echo "Run run-batchdft.py 16x1 192 APar APar False Double"
+python run-batchdft.py 16x1 192 APar APar False Double
+echo "Run run-batchdft.py 16x1 224 APar APar False Double"
+python run-batchdft.py 16x1 224 APar APar False Double
+echo "Run run-batchdft.py 16x1 256 APar APar False Double"
+python run-batchdft.py 16x1 256 APar APar False Double
+echo "Run run-batchdft.py 16x1 440 APar APar False Double"
+python run-batchdft.py 16x1 440 APar APar False Double
+echo "Run run-batchdft.py 16x1 512 APar APar False Double"
+python run-batchdft.py 16x1 512 APar APar False Double
+echo "Run run-batchdft.py 16x1 768 APar APar False Double"
+python run-batchdft.py 16x1 768 APar APar False Double
+echo "Run run-batchdft.py 16x1 1024 APar APar False Double"
+python run-batchdft.py 16x1 1024 APar APar False Double
+echo "Run run-batchdft.py 16x1 2048 APar APar False Double"
+python run-batchdft.py 16x1 2048 APar APar False Double
+
+echo "Run run-batchdft.py 16x1 60 AVec APar False Double"
+python run-batchdft.py 16x1 60 AVec APar False Double
+echo "Run run-batchdft.py 16x1 64 AVec APar False Double"
+python run-batchdft.py 16x1 64 AVec APar False Double
+echo "Run run-batchdft.py 16x1 80 AVec APar False Double"
+python run-batchdft.py 16x1 80 AVec APar False Double
+echo "Run run-batchdft.py 16x1 128 AVec APar False Double"
+python run-batchdft.py 16x1 128 AVec APar False Double
+echo "Run run-batchdft.py 16x1 192 AVec APar False Double"
+python run-batchdft.py 16x1 192 AVec APar False Double
+echo "Run run-batchdft.py 16x1 224 AVec APar False Double"
+python run-batchdft.py 16x1 224 AVec APar False Double
+echo "Run run-batchdft.py 16x1 256 AVec APar False Double"
+python run-batchdft.py 16x1 256 AVec APar False Double
+echo "Run run-batchdft.py 16x1 440 AVec APar False Double"
+python run-batchdft.py 16x1 440 AVec APar False Double
+##  echo "Run run-batchdft.py 16x1 512 AVec APar False Double"
+##  python run-batchdft.py 16x1 512 AVec APar False Double
+echo "Run run-batchdft.py 16x1 768 AVec APar False Double"
+python run-batchdft.py 16x1 768 AVec APar False Double
+echo "Run run-batchdft.py 16x1 1024 AVec APar False Double"
+python run-batchdft.py 16x1 1024 AVec APar False Double
+echo "Run run-batchdft.py 16x1 2048 AVec APar False Double"
+python run-batchdft.py 16x1 2048 AVec APar False Double
+
+echo "Run run-batchdft.py 16x1 60 APar AVec False Double"
+python run-batchdft.py 16x1 60 APar AVec False Double
+echo "Run run-batchdft.py 16x1 64 APar AVec False Double"
+python run-batchdft.py 16x1 64 APar AVec False Double
+echo "Run run-batchdft.py 16x1 80 APar AVec False Double"
+python run-batchdft.py 16x1 80 APar AVec False Double
+echo "Run run-batchdft.py 16x1 128 APar AVec False Double"
+python run-batchdft.py 16x1 128 APar AVec False Double
+echo "Run run-batchdft.py 16x1 192 APar AVec False Double"
+python run-batchdft.py 16x1 192 APar AVec False Double
+echo "Run run-batchdft.py 16x1 224 APar AVec False Double"
+python run-batchdft.py 16x1 224 APar AVec False Double
+echo "Run run-batchdft.py 16x1 256 APar AVec False Double"
+python run-batchdft.py 16x1 256 APar AVec False Double
+echo "Run run-batchdft.py 16x1 440 APar AVec False Double"
+python run-batchdft.py 16x1 440 APar AVec False Double
+##  echo "Run run-batchdft.py 16x1 512 APar AVec False Double"
+##  python run-batchdft.py 16x1 512 APar AVec False Double
+##  echo "Run run-batchdft.py 16x1 768 APar AVec False Double"
+##  python run-batchdft.py 16x1 768 APar AVec False Double
+##  echo "Run run-batchdft.py 16x1 1024 APar AVec False Double"
+##  python run-batchdft.py 16x1 1024 APar AVec False Double
+##  echo "Run run-batchdft.py 16x1 2048 APar AVec False Double"
+##  python run-batchdft.py 16x1 2048 APar AVec False Double
+
+echo "Run run-batchdft.py 16x1 60 AVec AVec False Double"
+python run-batchdft.py 16x1 60 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 64 AVec AVec False Double"
+python run-batchdft.py 16x1 64 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 80 AVec AVec False Double"
+python run-batchdft.py 16x1 80 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 128 AVec AVec False Double"
+python run-batchdft.py 16x1 128 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 192 AVec AVec False Double"
+python run-batchdft.py 16x1 192 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 224 AVec AVec False Double"
+python run-batchdft.py 16x1 224 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 256 AVec AVec False Double"
+python run-batchdft.py 16x1 256 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 440 AVec AVec False Double"
+python run-batchdft.py 16x1 440 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 512 AVec AVec False Double"
+python run-batchdft.py 16x1 512 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 768 AVec AVec False Double"
+python run-batchdft.py 16x1 768 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 1024 AVec AVec False Double"
+python run-batchdft.py 16x1 1024 AVec AVec False Double
+echo "Run run-batchdft.py 16x1 2048 AVec AVec False Double"
+python run-batchdft.py 16x1 2048 AVec AVec False Double
+
+exit 0
