34f65e4c859e6186d484f72f7188a9ee6b589c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:10 +0000

    Add package pydantic-ome-ngff/0.3.0/pydantic_ome_ngff-0.3.0-py3-none-any.whl

---
+++ packages/pydantic-ome-ngff/pydantic_ome_ngff-0.3.0-py3-none-any.whl/pydantic_ome_ngff/latest/imageLabel.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from typing import Optional
+from pydantic_ome_ngff.v04.imageLabel import ImageLabel as ImageLabelV04
+from pydantic_ome_ngff.latest.base import version
+
+
+class ImageLabel(ImageLabelV04):
+    """
+    image-label metadata.
+    See https://ngff.openmicroscopy.org/latest/#label-md
+    """
+
+    # we need to put the version here as a private class attribute because the version
+    # is not required by the spec...
+    _version = version
+    version: Optional[str] = version
---
+++ packages/pydantic-ome-ngff/pydantic_ome_ngff-0.3.0-py3-none-any.whl/pydantic_ome_ngff/v04/plate.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+from typing import List, Optional
+
+from pydantic import BaseModel, PositiveInt, NonNegativeInt
+from pydantic_ome_ngff.base import VersionedBase
+
+from pydantic_ome_ngff.v04.base import version
+
+
+class Acquisition(BaseModel):
+    id: NonNegativeInt
+    name: Optional[str]
+    maximumfieldcount: PositiveInt
+
+
+class Entry(BaseModel):
+    name: str
+
+
+class Well(BaseModel):
+    # must be {rowName}/{columnName}
+    path: str
+    rowIndex: NonNegativeInt
+    columnIndex: NonNegativeInt
+
+
+class Plate(VersionedBase):
+    """
+    Plate metadata
+    see https://ngff.openmicroscopy.org/0.4/#plate-md
+    """
+
+    # we need to put the version here as a private class attribute because the version
+    # is not required by the spec...
+    _version = version
+    version: Optional[str] = version
+    name: Optional[str]
+    acquisitions: List[Acquisition]
+    columns: List[Entry]
+    rows: List[Entry]
+    field_count: PositiveInt
+    wells: List[Well]
