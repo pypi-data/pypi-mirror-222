328aa0741efcd1cfa43f9bce99b1571a7c66ca99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package sqlx-batis/0.1.6/sqlx-batis-0.1.6.tar.gz

---
+++ packages/sqlx-batis/sqlx-batis-0.1.6.tar.gz/sqlx-batis-0.1.6/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+# INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path), 'r', encoding='UTF-8') as fp:
+        return fp.read()
+
+long_description = read("README.rst")
+
+setup(
+    name='sqlx-batis',
+    packages=['sqlbatis'],
+    description="sqlx-batis is a sql executor for Python like MyBatis.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'Jinja2>=2.7.0',
+        'sqlx-exec>=1.3.6',
+    ],
+    version='0.1.6',
+    url='https://gitee.com/summry/sqlx-batis',
+    author='summy',
+    author_email='xiazhongbiao@126.com',
+    keywords=['sql', 'MySQL', 'PostgreSQL', 'MyBatis', 'python'],
+    package_data={
+        # include json and txt files
+        '': ['*.rst', '*.dtd', '*.tpl'],
+    },
+    include_package_data=True,
+    python_requires='>=3.5',
+    zip_safe=False
+)
+
