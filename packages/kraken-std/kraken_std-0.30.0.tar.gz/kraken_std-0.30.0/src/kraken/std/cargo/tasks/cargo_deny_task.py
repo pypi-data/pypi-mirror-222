64d911b3a636c32513be59dcf9c81841dceedf79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:55 +0000

    Add package kraken-std/0.30.0/kraken_std-0.30.0.tar.gz

---
+++ packages/kraken-std/kraken_std-0.30.0.tar.gz/kraken_std-0.30.0/src/kraken/std/cargo/tasks/cargo_deny_task.py
@@ -0,0 +1,22 @@
+import subprocess
+from collections.abc import Sequence
+from pathlib import Path
+
+from kraken.core import Property, Task, TaskStatus
+
+
+class CargoDenyTask(Task):
+    description = "Executes cargo deny to verify dependencies."
+    checks: Property[Sequence[str]] = Property.default_factory(list)
+    config_file: Property[Path]
+
+    def execute(self) -> TaskStatus:
+        command = ["cargo", "deny", "check"]
+
+        if self.config_file.is_filled():
+            command.extend(["--config", str(self.config_file.get().absolute())])
+
+        command.extend(self.checks.get())
+
+        result = subprocess.run(command, cwd=self.project.directory)
+        return TaskStatus.from_exit_code(command, result.returncode)
