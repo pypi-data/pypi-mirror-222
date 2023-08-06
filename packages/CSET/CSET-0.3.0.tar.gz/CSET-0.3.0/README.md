21d8f6d7c0b68c1e48af6e4be73a181858b4dd46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:22 +0000

    Add package CSET/0.3.0/CSET-0.3.0.tar.gz

---
+++ packages/CSET/CSET-0.3.0.tar.gz/CSET-0.3.0/README.md
@@ -0,0 +1,36 @@
+# CSET
+
+CSET is a toolkit for evaluation and investigation of numerical models for
+weather and climate applications. It aims to replace the collection of bespoke
+scripts littering people’s home directories, reducing effort wasted on
+duplicating already existing code. This centralisation of diagnostics should
+also make evaluations more consistent and comparable. Development takes place in
+the CSET repository on GitHub.
+
+Please read [the documentation](https://metoffice.github.io/CSET) to learn more
+about CSET, and how to use it.
+
+## Contributing
+
+Contributions are readily welcomed! To get started with developing CSET visit
+the [Contributing](https://metoffice.github.io/CSET/contributing/) section of
+the documentation.
+
+In addition to reading the working practices, the key
+recommendation is early communication. Open an [issue on
+Github](https://github.com/MetOffice/CSET/issues) with your proposed change or
+addition in the design phase, and then others can provide guidance early.
+
+## Licence
+
+Copyright © 2022-2023 Met Office and contributors.
+
+Licensed under the [Apache License, Version 2.0](LICENCE) (the "License"); You
+may obtain a copy of the License at
+
+<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
---
+++ packages/CSET/CSET-0.3.0.tar.gz/CSET-0.3.0/tests/test_cli.py
@@ -0,0 +1,142 @@
+# Copyright 2022 Met Office and contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Tests for the command line interface. In many ways these are integration tests.
+"""
+
+from pathlib import Path
+from uuid import uuid4
+import os
+import subprocess
+import shutil
+import tempfile
+
+
+def test_command_line_help():
+    """Check that help commands work."""
+
+    subprocess.run(["cset", "--help"], check=True)
+    # test verbose options. This is really just to up the coverage number.
+    subprocess.run(["cset", "-v"], check=True)
+    subprocess.run(["cset", "-vv"], check=True)
+    subprocess.run(["cset", "--version"], check=True)
+    # Gain coverage of __main__.py
+    subprocess.run(["python3", "-m", "CSET", "-h"], check=True)
+
+
+def test_recipe_execution():
+    """Test running CSET recipe from the command line."""
+
+    subprocess.run(
+        [
+            "cset",
+            "bake",
+            os.devnull,
+            os.devnull,
+            "tests/test_data/noop_recipe.yaml",
+        ],
+        check=True,
+    )
+
+
+def test_environ_var_recipe():
+    """Test recipe coming from environment variable."""
+
+    os.environ[
+        "CSET_RECIPE"
+    ] = """
+        steps:
+          - operator: misc.noop
+        """
+    subprocess.run(
+        [
+            "cset",
+            "bake",
+            os.devnull,
+            os.devnull,
+        ],
+        check=True,
+    )
+
+
+def test_graph_creation():
+    """Generates a graph with the command line interface."""
+
+    # We can't easily test running without the output specified from the CLI, as
+    # the call to xdg-open breaks in CI, due to it not being a graphical
+    # environment.
+
+    # Run with output path specified
+    output_file = Path(tempfile.gettempdir(), f"{uuid4()}.svg")
+    subprocess.run(
+        ("cset", "graph", "-o", str(output_file), "tests/test_data/noop_recipe.yaml"),
+        check=True,
+    )
+    assert output_file.exists()
+    output_file.unlink()
+
+
+def test_graph_creation_env_var():
+    """
+    Generates a graph with the command line interface from an environment
+    variable.
+    """
+
+    os.environ[
+        "CSET_RECIPE"
+    ] = """
+        steps:
+          - operator: misc.noop
+        """
+    # Run with output path specified
+    output_file = Path(tempfile.gettempdir(), f"{uuid4()}.svg")
+    subprocess.run(("cset", "graph", "-o", str(output_file)), check=True)
+    assert output_file.exists()
+    output_file.unlink()
+
+
+def test_graph_details():
+    """Generate a graph with details with details."""
+
+    output_file = Path(tempfile.gettempdir(), f"{uuid4()}.svg")
+    subprocess.run(
+        (
+            "cset",
+            "graph",
+            "--detailed",
+            "-o",
+            str(output_file),
+            "tests/test_data/noop_recipe.yaml",
+        ),
+        check=True,
+    )
+    assert output_file.exists()
+    output_file.unlink()
+
+
+def test_cookbook_cwd():
+    """Unpacking the recipes into the current working directory."""
+
+    subprocess.run(["cset", "cookbook"], check=True)
+    assert Path.cwd().joinpath("recipes/extract_instant_air_temp.yaml").exists()
+    shutil.rmtree(Path.cwd().joinpath("recipes"))
+
+
+def test_cookbook_path():
+    """Unpacking the recipes into a specified directory."""
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        subprocess.run(["cset", "cookbook", tmpdir], check=True)
+        assert Path(tmpdir, "extract_instant_air_temp.yaml").exists()
