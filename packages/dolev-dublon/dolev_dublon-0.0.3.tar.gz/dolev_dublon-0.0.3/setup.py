83e522a632cac11c3b01db5de09b714c445657fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:04 +0000

    Add package dolev-dublon/0.0.3/dolev_dublon-0.0.3.tar.gz

---
+++ packages/dolev-dublon/dolev_dublon-0.0.3.tar.gz/dolev_dublon-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dolev_dublon",
+    version="0.0.3",
+    author="dolev dublon",
+    author_email="dolev146@gmail.com",
+    description="A package for dolev",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
