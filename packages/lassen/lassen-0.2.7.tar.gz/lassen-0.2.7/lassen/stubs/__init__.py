bf53e78298851c4665de03a18612f9c037493a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:39 +0000

    Add package lassen/0.2.7/lassen-0.2.7.tar.gz

---
+++ packages/lassen/lassen-0.2.7.tar.gz/lassen-0.2.7/lassen/stubs/__init__.py
@@ -0,0 +1,4 @@
+from .base import BaseStub  # noqa: 401
+from .definition import UniqueDefinition  # noqa: 401
+from .field import Field  # noqa: 401
+from .generators import SchemaGenerator, StoreGenerator  # noqa: 401
