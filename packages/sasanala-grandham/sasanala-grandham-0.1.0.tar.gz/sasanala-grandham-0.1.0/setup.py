4c52d899d6be4579b174a49c1278902a806467db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:08 +0000

    Add package sasanala-grandham/0.1.0/sasanala-grandham-0.1.0.tar.gz

---
+++ packages/sasanala-grandham/sasanala-grandham-0.1.0.tar.gz/sasanala-grandham-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sasanala-grandham',
+    version='0.1.0',
+    description='Sasanala Grandham one stop solution for every problem',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    url='https://github.com/yellaturibabu/sasanala-grandham',
+    author='Babu Yellaturi',
+    author_email='yellaturibabu@gmail.com',
+)
