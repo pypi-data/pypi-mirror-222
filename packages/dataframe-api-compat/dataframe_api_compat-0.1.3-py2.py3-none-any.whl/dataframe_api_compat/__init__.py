6ebbdf8c1ff8a8418ed054f5aac340ee96ae4398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:11 +0000

    Add package dataframe-api-compat/0.1.3/dataframe_api_compat-0.1.3-py2.py3-none-any.whl

---
+++ packages/dataframe-api-compat/dataframe_api_compat-0.1.3-py2.py3-none-any.whl/dataframe_api_compat/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from dataframe_api_compat import pandas_standard
+from dataframe_api_compat import polars_standard
+
+__all__ = ["pandas_standard", "polars_standard"]
