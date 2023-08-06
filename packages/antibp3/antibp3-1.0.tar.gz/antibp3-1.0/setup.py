df153856e866bd82c47d64e2c0cdc5d20febed40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:55 +0000

    Add package antibp3/1.0/antibp3-1.0.tar.gz

---
+++ packages/antibp3/antibp3-1.0.tar.gz/antibp3-1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from setuptools import  find_namespace_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='antibp3',
+    version='1.0',
+    description='A tool to predict anti-bacterial peptides',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files = ('LICENSE.txt',),
+    url='https://github.com/raghavagps/AntiBP3', 
+    packages=find_namespace_packages(where="src"),
+    package_dir={'':'src'},
+    package_data={'antibp3.blast_binaries':['**/*'], 
+    'antibp3.blast_db':['**/*'],
+    'antibp3.model':['*'],
+    'antibp3.motif':['*'],
+    'antibp3.perl_scripts':['*']},
+    entry_points={ 'console_scripts' : ['antibp3 = antibp3.python_scripts.antibp3:main']},
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy', 'pandas',  'argparse',  'warnings', 'os', 'sys', 'itertools', 'pickle', 're', 'glob', 'time', 'time', 'uuid', 'zipfile', 'subprocess' # Add any Python dependencies here
+    ]
+)
