73d32f5bb08225667a38ac9c696a4e7500658ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:56 +0000

    Add package aix360/0.3.0/aix360-0.3.0.tar.gz

---
+++ packages/aix360/aix360-0.3.0.tar.gz/aix360-0.3.0/aix360/datasets/__init__.py
@@ -0,0 +1,64 @@
+try:
+    from .mnist_dataset import MNISTDataset
+except:
+    pass
+
+try:
+    from .meps_dataset import MEPSDataset
+except:
+    pass
+
+try:
+    from .heloc_dataset import HELOCDataset
+except:
+    pass
+
+try:
+    from .celeba_dataset import CelebADataset
+except:
+    pass
+
+try:
+    from .cdc_dataset import CDCDataset
+except:
+    pass
+
+try:
+    from .cifar_dataset import CIFARDataset
+except:
+    pass
+
+try:
+    from .ted_dataset import TEDDataset
+except:
+    pass
+
+try:
+    from .fashion_mnist_dataset import FMnistDataset
+except:
+    pass
+
+try:
+    from .esnli_dataset import eSNLIDataset
+except:
+    pass
+
+try:
+    from .sunspots_dataset import SunspotDataset
+except:
+    pass
+
+try:
+    from .ford_dataset import FordDataset
+except:
+    pass
+
+try:
+    from .climate_dataset import ClimateDataset
+except:
+    pass
+
+try:
+    from .diabetes_dataset import DiabetesDataset
+except:
+    pass
