548b59f24450950bfca223cdc16b1b3ac63c9955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:35 +0000

    Add package opentelemetry-resource-detector-container/1.0.0/opentelemetry-resource-detector-container-1.0.0.tar.gz

---
+++ packages/opentelemetry-resource-detector-container/opentelemetry-resource-detector-container-1.0.0.tar.gz/opentelemetry-resource-detector-container-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='opentelemetry-resource-detector-container', #package name
+    version='1.0.0',
+    description='Python Package By Predator_97',
+    author='Predator_97',
+    license='MIT',
+    zip_safe=False
+)
