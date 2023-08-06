413654407aa5a9678b15a2dc254664949b646f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package easyencryption/0.1.8.2/easyencryption-0.1.8.2-py3-none-any.whl

---
+++ packages/easyencryption/easyencryption-0.1.8.2-py3-none-any.whl/easyencryption/fernet.py
@@ -0,0 +1,42 @@
+import os, cryptography
+
+async def genkey():
+  key = cryptography.fernet.Fernet.generate_key()
+  with open("symeasyencryption.key", "wb") as key_file:
+    key_file.write(key)
+  return key
+
+async def regenkey():
+  if os.path.exists("symeasyencryption.key"):
+    os.remove("symeasyencryption.key")
+  await genkey()
+  
+async def callkey():
+  try:
+    key = open("symeasyencryption.key", "rb").read()
+    if str(key) == "b''":
+      await genkey()
+      key = open("symeasyencryption.key", "rb").read()
+    return key
+  except:
+    await genkey()
+    key = open("symeasyencryption.key", "rb").read()
+    return key
+
+async def fernetencrypt(slogan:str):
+  key = await callkey()
+  slogan = slogan.encode()
+  a = cryptography.fernet.Fernet(key)
+  coded_slogan = a.encrypt(slogan)
+  return coded_slogan
+
+async def fernetdecrypt(coded_slogan:bytes):
+  key = await callkey()
+  b = cryptography.fernet.Fernet(key)
+  decoded_slogan = b.decrypt(coded_slogan)
+  decoded_slogan = str(decoded_slogan)
+  decoded_slogan = decoded_slogan[2:]
+  decoded_slogan = decoded_slogan[:-1]
+  return(decoded_slogan)
+
+  
