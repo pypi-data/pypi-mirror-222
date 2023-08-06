929ff1b37bbf71bb9c8fd9caf03bb97fb377031c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:16 +0000

    Add package codebot/0.0.8/codebot-0.0.8.tar.gz

---
+++ packages/codebot/codebot-0.0.8.tar.gz/codebot-0.0.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codebot",
+    version="0.0.8",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "codebot = src:main",
+            "terminalbot = src:main_terminal",  # 添加这一行
+        ],
+    },
+    install_requires=[
+        "chainlit==0.5.2",
+        "loguru==0.5.3",
+        "tiktoken==0.4.0",
+        "prompt_toolkit==3.0.39",
+        "jupyter",
+        "mysql-connector-python",
+        "pillow",
+        "pyngrok"
+    ],
+    include_package_data=True,  # Add this line
+)
