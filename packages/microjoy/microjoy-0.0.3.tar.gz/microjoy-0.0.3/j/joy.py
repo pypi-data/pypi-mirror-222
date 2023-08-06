139be2d0352947861acb86a1d1419125912362f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:25 +0000

    Add package microjoy/0.0.3/microjoy-0.0.3.tar.gz

---
+++ packages/microjoy/microjoy-0.0.3.tar.gz/microjoy-0.0.3/j/joy.py
@@ -0,0 +1,21 @@
+import machine
+
+
+class Joy:
+    def __init__(self, pin, resolucao=4095, escala=(0, 180), invert=False):
+        self.analog_pin = machine.ADC(machine.Pin(pin))
+        self.analog_pin.atten(machine.ADC.ATTN_11DB)
+        self.in_max = resolucao
+        self.out_min = escala[0]
+        self.out_max = escala[1]
+        self.invert = invert
+
+    def convert_linear_scale(self, value):
+        x = ((value - 0) * (self.out_max - self.out_min)) / ((self.in_max - 0) + self.out_min)
+        if self.invert:
+            return self.out_max - x
+        else:
+            return x
+
+    def read(self):
+        return self.convert_linear_scale(self.analog_pin.read())
