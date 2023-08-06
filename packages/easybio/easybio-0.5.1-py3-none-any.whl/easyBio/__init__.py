b94e2a3bbf94754c43d2d06c3a02ec201c1263f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:46 +0000

    Add package easybio/0.5.1/easybio-0.5.1-py3-none-any.whl

---
+++ packages/easybio/easybio-0.5.1-py3-none-any.whl/easyBio/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Author: Lei
+# Date: 2023-04-20
+# Description:
+
+from .Utils import defineSession, requestGet, toFastq
+from .Utils import splitSRAfun, cellrangerRun
+from .Utils import getbioproject, downLoadSRA
+from .Utils import Download
+from .Utils import del_files, getNowTime, get_num_threads
+from .Utils import runvelocityc, copyDirs, get_available_memory, downLoadBAM
+from .Utils import toFastq, easyCellranger
+
