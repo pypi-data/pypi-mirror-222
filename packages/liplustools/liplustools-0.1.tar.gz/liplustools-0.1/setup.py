e4cf61a9c418e2072c878c97e6a5ea6388aa7111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:30 +0000

    Add package liplustools/0.1/liplustools-0.1.tar.gz

---
+++ packages/liplustools/liplustools-0.1.tar.gz/liplustools-0.1/setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8 
+# @Author : lizhankang
+# @Subject : 
+# @Time : 2023 - 08 - 03
+from setuptools import setup
+
+setup(name='liplustools',
+      version='0.1',
+      description='The liplustools joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='liplus',
+      author_email='liplus@example.com',
+      license='MIT',
+      packages=['liplustools'],
+      zip_safe=False)
