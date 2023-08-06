0df00f94955da44611bbd94c380af7816c49b489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:42 +0000

    Add package aeoprsmodel/0.2.2/aeoprsmodel-0.2.2.tar.gz

---
+++ packages/aeoprsmodel/aeoprsmodel-0.2.2.tar.gz/aeoprsmodel-0.2.2/setup.py
@@ -0,0 +1,19 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aeoprsmodel",
+    version="0.2.2",                        
+    author="Gisaïa",                     
+    description="ARLAS Earth Observation Product Registration Service Model",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    
+    python_requires='>=3.11',     
+    py_modules=["aeoprs.core.models.model","aeoprs.core.models.mapper"],  
+    package_dir={'':'src'},
+    install_requires=[]
+)
