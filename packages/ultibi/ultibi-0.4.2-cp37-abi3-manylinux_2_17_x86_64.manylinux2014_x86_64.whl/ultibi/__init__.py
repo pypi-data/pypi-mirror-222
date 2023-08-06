c64cbad06a381f86b8479be1b6a20bc5fb97a004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:30 +0000

    Add package ultibi/0.4.2/ultibi-0.4.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ultibi/ultibi-0.4.2-cp37-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ultibi/__init__.py
@@ -0,0 +1,56 @@
+"""
+FRTB usecase specific library which levrages on ultima's base engine
+"""
+
+import polars  # reexport
+
+from .internals import (
+    AggRequest,
+    BaseMeasure,
+    CalcParam,
+    ComputeRequest,
+    CustomCalculator,
+    DataSet,
+    DataSource,
+    DependantMeasure,
+    EqFilter,
+    FRTBDataSet,
+    InFilter,
+    NeqFilter,
+    NoDataError,
+    NotInFilter,
+    OtherError,
+    StandardCalculator,
+    UltimaError,
+    aggregation_ops,
+)
+
+# import pyarrow - not needed
+
+
+__all__ = [
+    "AggRequest",
+    "ComputeRequest",
+    "FRTBDataSet",
+    "DataSet",
+    "DataSource",
+    "aggregation_ops",
+    "OtherError",
+    "NoDataError",
+    "polars",
+    "pyarrow",
+    "Filter",
+    "FilterType",
+    "EqFilter",
+    "NeqFilter",
+    "InFilter",
+    "NotInFilter",
+    "Measure",
+    "BaseMeasure",
+    "DependantMeasure",
+    "Calculator",
+    "CustomCalculator",
+    "StandardCalculator",
+    "CalcParam",
+    "UltimaError",
+]
