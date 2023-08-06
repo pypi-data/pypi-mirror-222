9bad41ca30b599d4faab503a2ad360a93899b660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:57 +0000

    Add package ok-distri/0.1/ok_distri-0.1.tar.gz

---
+++ packages/ok-distri/ok_distri-0.1.tar.gz/ok_distri-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ok_distri',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ok_distri'],
+      author='Princewill Okechukwu',
+      author_email='okechukwuprincewillw@gmail.com',
+      zip_safe=False)
