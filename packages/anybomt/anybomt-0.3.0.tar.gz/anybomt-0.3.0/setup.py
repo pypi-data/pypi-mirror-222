7c0297c1b403f96befa3e7cb42ec157a7635dfb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:22 +0000

    Add package anybomt/0.3.0/anybomt-0.3.0.tar.gz

---
+++ packages/anybomt/anybomt-0.3.0.tar.gz/anybomt-0.3.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+# Leitura do arquivo README.md
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+# Configuração do pacote
+setup(
+    name='anybomt',
+    version='0.3.0',
+    license='MIT',
+    author=['AnyBoMath', 'Bidjory'],
+    author_email='bidjorys@gmail.com',
+    description='Uma biblioteca para facilitar sua jornada em matemática',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+  # Substitua com o link do seu repositório no GitHub
+    packages=[''],  # Lista de pacotes que serão incluídos
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'scipy',
+        'numpy',
+        'sympy',
+        'mpmath',
+        'matplotlib',
+        
+        # outras dependências da sua biblioteca
+    ],
+    python_requires='>=3.6',
+    
+)
