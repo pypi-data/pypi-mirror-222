e9f3b580462b6c6d0a07993beca0244f6bf2afb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:22 +0000

    Add package cmdhelper/0.3.1/cmdhelper-0.3.1.tar.gz

---
+++ packages/cmdhelper/cmdhelper-0.3.1.tar.gz/cmdhelper-0.3.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def read(fname):
+    """Return contents of file with name fname."""
+    with open(fname, 'r') as f:
+        return f.read()
+
+setup(
+    name = 'cmdhelper',
+    version = '0.3.1',
+    description = 'Python utility for writing command line scripts with consistent look and feel.',
+    long_description = read('README.rst'),
+    url = 'https://github.com/juergberinger/cmdhelper',
+    license = 'MIT',
+    author = 'Juerg Beringer',
+    author_email = 'juerg.beringer@gmail.com',
+    py_modules = ['cmdhelper'],
+    include_package_data = True,
+    install_requires = ['future'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Shells',
+        'Topic :: Utilities'
+        ],
+    keywords = 'command line utility, scripts',
+)
