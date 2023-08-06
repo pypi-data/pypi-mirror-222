0a20b891dcceb3c6040e6132addcaafed31476de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:57 +0000

    Add package statscend/0.1.9/statscend-0.1.9-py3-none-any.whl

---
+++ packages/statscend/statscend-0.1.9-py3-none-any.whl/statscend/__init__.py
@@ -0,0 +1,16 @@
+from .datasets import data
+from .linear_regression import linear_regression
+from .bn_logistic_regression import bn_logistic_regression
+from .mn_logistic_regression import mn_logistic_regression
+from .ordinal_regression import ordinal_regression
+from .vif import vif
+
+
+__all__ = [
+    'data',
+    'linear_regression',
+    'bn_logistic_regression',
+    'mn_logistic_regression',
+    'ordinal_regression',
+    'vif'
+]
