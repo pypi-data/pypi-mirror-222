0457223701683cbe940522bdbb0ba4a61b9f128a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:13 +0000

    Add package scinumtools/1.8.0/scinumtools-1.8.0-py3-none-any.whl

---
+++ packages/scinumtools/scinumtools-1.8.0-py3-none-any.whl/scinumtools/phys/units/__init__.py
@@ -0,0 +1,5 @@
+from .QuantityClass import Quantity
+from .UnitClass import Unit, Constant
+from .DimensionsClass import Dimensions
+from .FractionClass import Fraction
+from .BaseUnitsClass import BaseUnits
