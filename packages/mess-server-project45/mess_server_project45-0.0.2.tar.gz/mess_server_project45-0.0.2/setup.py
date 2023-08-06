3b163ec411510144604644ec809f78d4e9437656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:46 +0000

    Add package mess-server-project45/0.0.2/mess_server_project45-0.0.2.tar.gz

---
+++ packages/mess-server-project45/mess_server_project45-0.0.2.tar.gz/mess_server_project45-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_server_project45",
+      version="0.0.2",
+      description="mess_server_proj45",
+      author="dilbazi",
+      author_email="dilbazi.mardaliyeva@bk.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy==1.4.49', 'pycryptodome', 'pycryptodomex']
+      )
