82161d160f692980a6ad7f1531718c4096a6839e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:43 +0000

    Add package nanohelp/0.1.5/nanohelp-0.1.5.tar.gz

---
+++ packages/nanohelp/nanohelp-0.1.5.tar.gz/nanohelp-0.1.5/nanohelp/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from __future__ import annotations
+
+# include these files in the build
+from nanohelp.wallet import WalletManager
+from nanohelp.secret import SecretManager
+
+# imported into setup.py
+__version__ = "0.1.5"
