9bf67bf22f7c2cf153e6691f6fff1a795518dd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:10 +0000

    Add package chainmaker/2.3.2/chainmaker-2.3.2-py3-none-any.whl

---
+++ packages/chainmaker/chainmaker-2.3.2-py3-none-any.whl/chainmaker/user.py
@@ -0,0 +1,8 @@
+import warnings
+
+from chainmaker.client_user import ClientUser
+
+
+class User(ClientUser):
+    pass
+    # warnings.warn('请使用chainmaker.client_user.ClientUser', DeprecationWarning)
