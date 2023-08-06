d78eb1ff8b83387e40167a007a7a5b4c961702e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:16 +0000

    Add package dst-bpizzani/0.1/dst-bpizzani-0.1.tar.gz

---
+++ packages/dst-bpizzani/dst-bpizzani-0.1.tar.gz/dst-bpizzani-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dst-bpizzani',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dst-bpizzani'],
+      zip_safe=False)
