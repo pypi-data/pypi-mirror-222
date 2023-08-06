5d15d6726322255943169e3e51fbc3768c1c62ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:49 +0000

    Add package sheCry/0.0.10/sheCry-0.0.10.tar.gz

---
+++ packages/sheCry/sheCry-0.0.10.tar.gz/sheCry-0.0.10/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "sheCry",
+
+    version = "0.0.10",
+    
+    author = "Tahsin Ahmed",
+
+    description = "SHE Cryptography is architectured by Tahsin Ahmed.",
+
+    long_description = open("README.md", encoding="utf-8").read(),
+    
+    keywords = ["cryptography", "encryption", "decryption", "SHE", "SHE cryptography", "Symmetric Hybrid Encryption", "Symmetric Hybrid Encryption cryptography", "sheCry"],
+    
+    url = "",
+    
+    install_requires = [""],
+    
+    packages = find_packages(),
+
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Information Technology",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Natural Language :: English"
+    ]
+)
