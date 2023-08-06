16e22352df47b6fe0920c77e880885125e13e1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:28 +0000

    Add package sedi/0.11/sedi-0.11-py3-none-any.whl

---
+++ packages/sedi/sedi-0.11-py3-none-any.whl/sed.py
@@ -0,0 +1,58 @@
+import re, shutil, tempfile, subprocess
+
+def sed_inplace(filename, pattern, repl):
+    '''
+    Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
+    `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
+    '''
+    # For efficiency, precompile the passed regular expression.
+    pattern_compiled = re.compile(pattern)
+
+    # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
+    # writing with updating). This is usually a good thing. In this case,
+    # however, binary writing imposes non-trivial encoding constraints trivially
+    # resolved by switching to text writing. Let's do that.
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        with open(filename) as src_file:
+            for line in src_file:
+                tmp_file.write(pattern_compiled.sub(repl, line))
+
+    # Overwrite the original file with the munged temporary file in a
+    # manner preserving file attributes (e.g., permissions).
+    shutil.copystat(filename, tmp_file.name)
+    shutil.move(tmp_file.name, filename)
+
+def sed_to_file(filename, new_filename, pattern, repl):
+    '''
+    Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
+    `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
+    '''
+    # For efficiency, precompile the passed regular expression.
+    pattern_compiled = re.compile(pattern)
+
+    # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
+    # writing with updating). This is usually a good thing. In this case,
+    # however, binary writing imposes non-trivial encoding constraints trivially
+    # resolved by switching to text writing. Let's do that.
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        with open(filename) as src_file:
+            for line in src_file:
+                tmp_file.write(pattern_compiled.sub(repl, line))
+
+    # Overwrite the original file with the munged temporary file in a
+    # manner preserving file attributes (e.g., permissions).
+    shutil.copystat(filename, tmp_file.name)
+    shutil.move(tmp_file.name, new_filename)
+
+def main():
+    print("hello world!!! v0.4 hooray!!!")
+    # Do it for Johnny.
+    # try: 
+    #     proc = subprocess.run(["mkdir -p tmp"], shell=True, check=True, capture_output=True)
+    #     sed_to_file('./etc/regfile.tmp', './tmp/regfile', 'AWS_ACCOUNT', '1234')
+    # except Exception as ex:
+    #     print("error: " + str(ex))
+
+
+if __name__ == "__main__":
+    main()
