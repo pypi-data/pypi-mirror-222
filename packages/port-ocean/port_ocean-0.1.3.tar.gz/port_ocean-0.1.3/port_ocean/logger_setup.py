0389aff40f5f11fe8c4d83057e36091d13748941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:03 +0000

    Add package port-ocean/0.1.3/port_ocean-0.1.3.tar.gz

---
+++ packages/port-ocean/port_ocean-0.1.3.tar.gz/port_ocean-0.1.3/port_ocean/logger_setup.py
@@ -0,0 +1,22 @@
+import sys
+
+from loguru import logger
+
+from port_ocean.config.settings import LogLevelType
+
+
+def setup_logger(level: LogLevelType) -> None:
+    logger_format = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+        "<level>{level: <8}</level> | "
+        "<level>{message}</level> | {extra}"
+    )
+
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        level=level.upper(),
+        format=logger_format,
+        enqueue=True,  # process logs in background
+        diagnose=False,  # hide variable values in log backtrace
+    )
