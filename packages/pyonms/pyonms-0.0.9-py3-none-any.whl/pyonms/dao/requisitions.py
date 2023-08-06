463d1cd099f1cb77beeac3e305c8e02e20d869ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:46 +0000

    Add package pyonms/0.0.9/pyonms-0.0.9-py3-none-any.whl

---
+++ packages/pyonms/pyonms-0.0.9-py3-none-any.whl/pyonms/dao/requisitions.py
@@ -0,0 +1,95 @@
+# dao.requisitions.py
+
+from typing import List, Union
+
+import requests
+
+import pyonms.models.requisition
+from pyonms.dao import Endpoint
+from pyonms.utils import normalize_dict
+
+
+class RequisitionsAPI(Endpoint):
+    def __init__(self, kwargs):
+        super().__init__(**kwargs)
+        self.url = self.base_v1 + "requisitions"
+
+    def get_requisition(
+        self, name: str
+    ) -> Union[pyonms.models.requisition.Requisition, None]:
+        record = self._get(
+            uri=f"{self.url}/{name}", endpoint="requisitions", headers=self.headers
+        )
+        if record is not None:
+            return self._process_requisition(record)
+        else:
+            return None
+
+    def get_requisitions(
+        self,
+        limit=100,
+        batch_size=100,
+    ) -> List[Union[pyonms.models.requisition.Requisition, None]]:
+        requisitions = []
+        params = {}
+        records = self._get_batch(
+            url=self.url,
+            endpoint="model-import",
+            limit=limit,
+            batch_size=batch_size,
+            params=params,
+        )
+        if records == [None]:
+            return [None]
+        for record in records:
+            requisitions.append(self._process_requisition(record))
+        return requisitions
+
+    def _process_requisition(self, data: dict) -> pyonms.models.requisition.Requisition:
+        # if data.get("foreign-source"):
+        #     data["foreign_source"] = data["foreign-source"]
+        #     del data["foreign-source"]
+        # if data.get("date-stamp"):
+        #     data["date_stamp"] = data["date-stamp"]
+        #     del data["date-stamp"]
+        # if data.get("last-import") or data["last-import"] is None:
+        #     data["last_import"] = data["last-import"]
+        #     del data["last-import"]
+        data = normalize_dict(data)
+        return pyonms.models.requisition.Requisition(**data)
+
+    def get_requisition_active_count(self) -> int:
+        count = self._get(uri=f"{self.url}/count", endpoint="raw")
+        return int(count)
+
+    def get_requisition_deployed_count(self) -> int:
+        count = self._get(uri=f"{self.url}/deployed/count", endpoint="raw")
+        return int(count)
+
+    def import_requisition(self, name: str, rescan: bool = False) -> bool:
+        response = self._put(
+            uri=f"{self.url}/{name}/import",
+            params={"rescanExisting": rescan},
+        )
+        if response.status_code in [202, 204]:
+            return True
+        else:
+            return False
+
+    def _put(
+        self,
+        uri: str,
+        data: dict = {},
+        params: dict = {},
+    ) -> requests.Response:
+        response = requests.put(
+            uri, auth=self.auth, headers=self.headers, data=data, params=params
+        )
+        return response
+
+    def update_requisition(self, requisition: pyonms.models.requisition.Requisition):
+        """Post an entire requisition to create or overwrite."""
+        response = self._post(
+            uri=self.url, headers=self.headers, json=requisition._to_dict()
+        )
+        return response
---
+++ packages/pyonms/pyonms-0.0.9-py3-none-any.whl/pyonms/models/event.py
@@ -0,0 +1,113 @@
+# models.event.py
+
+
+from dataclasses import dataclass, field
+from datetime import datetime
+from enum import Enum
+from typing import List, Union
+
+from pyonms.models.node import ServiceType
+from pyonms.utils import convert_time
+
+
+class Severity(Enum):
+    """Severity Levels"""
+
+    INDETERMINATE = 1
+    "Indeterminate"
+    CLEARED = 2
+    "Cleared"
+    NORMAL = 3
+    "Normal"
+    WARNING = 4
+    "Warning"
+    MINOR = 5
+    "Minor"
+    MAJOR = 6
+    "Major"
+    CRITICAL = 7
+    "Critical"
+
+
+@dataclass
+class EventParameter:
+    name: str
+    value: Union[int, str]
+    type: str = "string"
+
+    def _to_dict(self) -> dict:
+        payload = {"parmName": self.name, "value": self.value}
+        return payload
+
+    def __hash__(self):
+        return hash((self.name))
+
+
+@dataclass(repr=False)
+class Event:
+    uei: str
+    id: int = None
+    label: str = None
+    time: datetime = None
+    source: str = None
+    createTime: datetime = None
+    description: str = None
+    logMessage: str = None
+    severity: Severity = None
+    log: bool = None
+    display: bool = None
+    location: str = None
+    nodeId: int = None
+    nodeLabel: str = None
+    ipAddress: str = None
+    operatorInstructions: str = None
+    host: str = None
+    snmp: str = None
+    snmpHost: str = None
+    ifIndex: int = None
+    parameters: List[Union[EventParameter, None]] = field(default_factory=list)
+    serviceType: ServiceType = field(default_factory=dict)
+
+    def __post_init__(self):
+        if isinstance(self.time, int):
+            self.time = convert_time(self.time)
+        if isinstance(self.createTime, int):
+            self.createTime = convert_time(self.createTime)
+        if isinstance(self.severity, str):
+            self.severity = Severity[self.severity]
+        if self.display == "Y":
+            self.display = True
+        elif self.display == "N":
+            self.display = False
+        if self.log == "Y":
+            self.log = True
+        elif self.log == "N":
+            self.log = False
+        self.parameters = [EventParameter(**parameter) for parameter in self.parameters]
+        if self.serviceType and isinstance(self.serviceType, dict):
+            self.serviceType = ServiceType(**self.serviceType)
+
+    def __repr__(self) -> str:
+        return f"Event(id={self.id}, uei={self.uei})"
+
+    def _to_dict(self) -> dict:
+        payload = {}
+        for key, value in vars(self).items():
+            if key == "description":
+                key = "descr"
+            elif key == "operatorInstructions":
+                key = "operinstruct"
+            elif key == "ipAddress":
+                key = "interface"
+            elif key in ["logMessage", "display"]:
+                continue
+            if value:
+                payload[key.lower()] = value
+        if isinstance(payload.get("severity"), Severity):
+            payload["severity"] = self.severity.name
+        if self.parameters:
+            del payload["parameters"]
+            payload["parms"] = []
+            for parameter in self.parameters:
+                payload["parms"].append(parameter._to_dict())
+        return payload
