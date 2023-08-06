a12104fa002ebf92c8242075664fb7a30e5087e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package sotai/0.3.1/sotai-0.3.1-py3-none-any.whl

---
+++ packages/sotai/sotai-0.3.1-py3-none-any.whl/sotai/__init__.py
@@ -0,0 +1,12 @@
+"""SOTAI SDK."""
+
+# This version must always be one version ahead of the current release, so it
+# matches the current state of development, which will always be ahead of the
+# current release. Use Semantic Versioning.
+__version__ = "0.3.1"
+
+from . import layers
+from .enums import *
+from .pipeline import *
+from .trained_model import *
+from .types import *
