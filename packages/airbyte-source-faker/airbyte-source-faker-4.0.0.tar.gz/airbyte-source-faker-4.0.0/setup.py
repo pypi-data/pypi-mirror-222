7e7c6ae48bdb65f9de7fa002cbdba682d6cc0150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:51 +0000

    Add package airbyte-source-faker/4.0.0/airbyte-source-faker-4.0.0.tar.gz

---
+++ packages/airbyte-source-faker/airbyte-source-faker-4.0.0.tar.gz/airbyte-source-faker-4.0.0/setup.py
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = ["airbyte-cdk~=0.2", "mimesis==6.1.1"]
+
+TEST_REQUIREMENTS = [
+    "pytest~=6.2",
+    "connector-acceptance-test",
+]
+
+setup(
+    description="Source implementation for fake but realistic looking data.",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "schemas/*.json", "record_data/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
