974971daa94b8ecfc2986f462acc39d1e6d54772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:03 +0000

    Add package passwordless-client/0.1.4/passwordless-client-0.1.4.tar.gz

---
+++ packages/passwordless-client/passwordless-client-0.1.4.tar.gz/passwordless-client-0.1.4/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='passwordless-client',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Matthew Fiallos',
+    author_email='matthew.fiallos@randstadusa.com',
+    description='A client library for Passwordless.dev API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/passwordless-client',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
