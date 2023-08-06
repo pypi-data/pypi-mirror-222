2cab8a218f4337c9a98bbe506b2890f6c30c251e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:41 +0000

    Add package nec-beamer/0.1.3/nec_beamer-0.1.3.tar.gz

---
+++ packages/nec-beamer/nec_beamer-0.1.3.tar.gz/nec_beamer-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='nec_beamer',
+      version='0.1.3',
+      description='NEC Beamer Web Interface Wrapper',
+      url='https://github.com/heinrich-foto/nec_beamer',
+      author='Heinrich-Foto',
+      author_email='nec_beamer@heinrich-foto.de',
+      license='MIT',
+      packages=['nec_beamer'],
+      install_requires=[
+          'requests',
+          'click'
+      ],
+      zip_safe=False,
+      scripts=['bin/nec_beamer'],
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      )
