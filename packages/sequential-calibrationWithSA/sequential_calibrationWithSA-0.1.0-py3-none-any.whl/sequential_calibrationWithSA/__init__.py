13f97d972a6af6d30893a75f17febf1a494cc62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:47 +0000

    Add package sequential-calibrationWithSA/0.1.0/sequential_calibrationWithSA-0.1.0-py3-none-any.whl

---
+++ packages/sequential-calibrationWithSA/sequential_calibrationWithSA-0.1.0-py3-none-any.whl/sequential_calibrationWithSA/__init__.py
@@ -0,0 +1,1 @@
+from .seqC import *
---
+++ packages/sequential-calibrationWithSA/sequential_calibrationWithSA-0.1.0-py3-none-any.whl/sequential_calibrationWithSA/seqC.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Variance_based sensitivity analysis
+
+'''
+function
+1) VSA
+2) Calibration
+3) 
+'''
+
+
+# ## 1) Calibration
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+def VSA(msg):
+    print(msg + "asdefasdasd")
+    print(np.zeros(3))
+
+
+# ### 방법
+# 
+# - 시뮬레이션을 돌린다.
+# - GSA하는 법 소개 -> 다른 package 소개
+# - SEQ돌리기
+
+# In[ ]:
+
+
+
+
