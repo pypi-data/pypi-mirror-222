bbded9fe49e727e6667f208c631bca71724b3897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:57 +0000

    Add package mkdocs-auto-tag-plugin/0.1.3/mkdocs_auto_tag_plugin-0.1.3-py3-none-any.whl

---
+++ packages/mkdocs-auto-tag-plugin/mkdocs_auto_tag_plugin-0.1.3-py3-none-any.whl/mkdocs_auto_tag_plugin/__init__.py
@@ -0,0 +1,18 @@
+import logging
+
+# Set up a logger for my code to use
+LOGGER = logging.getLogger("mkdocs.plugins.autotags")
+
+def warning(message: str) -> None:
+    LOGGER.warning(f"[auto-tags] {message}")
+
+
+def debug(message: str, *args) -> None:
+    LOGGER.debug(f"[auto-tags] {message}", *args)
+
+# Import local files in the correct order
+# from .utils import replace_regex_matches
+# from .normal_badge import replace_normal_badges
+from .plugin import Plugin
+
+__all__ = ["Plugin"]
