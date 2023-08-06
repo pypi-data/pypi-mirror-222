c1d0fc6c3ae1cf9be3be8be507ea33148148724f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:47 +0000

    Add package model2queue/0.1.9/model2queue-0.1.9.tar.gz

---
+++ packages/model2queue/model2queue-0.1.9.tar.gz/model2queue-0.1.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="model2queue",
+    packages=find_packages(),
+    version="0.1.9",
+    description="A library to expose an api with a queue for batch predictions",
+    author="collective.ai",
+    author_email="team.collective.ai@gmail.com",
+    url="https://github.com/collectiveai-team/model2queue",
+    install_requires=["kombu", "fastapi", "uvicorn", "rich"],
+    package_data={"": ["*.yml", "*.yaml"]},
+    include_package_data=True,
+    classifiers=["Programming Language :: Python :: 3"],
+    extras_require={"dev": ["pytest==7.1.2", "tensorflow", "python-multipart"]},
+)
