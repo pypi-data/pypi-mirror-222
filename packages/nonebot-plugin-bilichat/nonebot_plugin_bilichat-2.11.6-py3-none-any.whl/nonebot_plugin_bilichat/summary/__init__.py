b8d7b6ebc18d83c305e6f96dfa130aa2a8f80230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:45 +0000

    Add package nonebot-plugin-bilichat/2.11.6/nonebot_plugin_bilichat-2.11.6-py3-none-any.whl

---
+++ packages/nonebot-plugin-bilichat/nonebot_plugin_bilichat-2.11.6-py3-none-any.whl/nonebot_plugin_bilichat/summary/__init__.py
@@ -0,0 +1,36 @@
+from nonebot.log import logger
+
+from ..config import plugin_config
+from ..model.cache import Cache
+
+if plugin_config.bilichat_newbing_cookie:
+    logger.info("Using newbing as summarization tool")
+    from .newbing_summarise import newbing_summarization
+
+if plugin_config.bilichat_openai_token:
+    logger.info("Using openai as summarization tool")
+    from .openai_summarise import openai_summarization
+
+
+async def summarization(cache: Cache, cid: str = "0"):
+    # summarization will be returned in the following priority
+    # openai cache -> newbing cache -> newbing new sum -> openai new sum
+    logger.info(f"Generation summary of Video(Column) {cache.id}")
+    if not cache.episodes[cid] or not cache.episodes[cid].content:
+        return "视频无有效字幕"
+
+    # try using openai cache
+    # this will not cause new summarization
+    if cache.episodes[cid].openai and plugin_config.bilichat_openai_token:
+        return await openai_summarization(cache, cid)
+
+    # try newbing
+    if plugin_config.bilichat_newbing_cookie:
+        summary, newbing_meaning = await newbing_summarization(cache, cid)
+        if newbing_meaning or not plugin_config.bilichat_openai_token:
+            return summary
+        logger.error("newbing summary failed, retry with openai")
+
+    # try openai cache
+    if plugin_config.bilichat_openai_token:
+        return await openai_summarization(cache, cid)
