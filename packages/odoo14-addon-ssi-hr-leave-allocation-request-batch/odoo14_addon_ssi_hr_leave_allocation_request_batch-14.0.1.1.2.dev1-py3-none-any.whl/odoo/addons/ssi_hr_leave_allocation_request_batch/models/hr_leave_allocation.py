dfc03a6e6ae46b14cb27e21a5abb4647419dc000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:42 +0000

    Add package odoo14-addon-ssi-hr-leave-allocation-request-batch/14.0.1.1.2.dev1/odoo14_addon_ssi_hr_leave_allocation_request_batch-14.0.1.1.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-hr-leave-allocation-request-batch/odoo14_addon_ssi_hr_leave_allocation_request_batch-14.0.1.1.2.dev1-py3-none-any.whl/odoo/addons/ssi_hr_leave_allocation_request_batch/models/hr_leave_allocation.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class HrLeaveAllocation(models.Model):
+    _inherit = "hr.leave_allocation"
+
+    batch_id = fields.Many2one(
+        comodel_name="hr.leave_allocation_request_batch", string="# Batch"
+    )
+
+    def _cron_recompute_wrong_value(self):
+        super(HrLeaveAllocation, self)._cron_recompute_wrong_value()
+        allocation_batch_ids = self.search([("date_extended", "=", False)])
+        for allocation_batch_id in allocation_batch_ids:
+            allocation_batch_id.write({"date_extended": allocation_batch_id.date_end})
