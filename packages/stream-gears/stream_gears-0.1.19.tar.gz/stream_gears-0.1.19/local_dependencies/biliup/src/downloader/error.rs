b06f15356e82caabdfc787ce916ec663bbdaa910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:06 +0000

    Add package stream-gears/0.1.19/stream_gears-0.1.19.tar.gz

---
+++ packages/stream-gears/stream_gears-0.1.19.tar.gz/stream_gears-0.1.19/local_dependencies/biliup/src/downloader/error.rs
@@ -0,0 +1,26 @@
+use nom::Needed;
+use std::io;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    Custom(String),
+
+    #[error(transparent)]
+    IOError(#[from] io::Error),
+
+    #[error(transparent)]
+    ReqwestError(#[from] reqwest::Error),
+
+    #[error(transparent)]
+    UrlParseError(#[from] url::ParseError),
+
+    #[error("Parsing {0} requires {1:?} bytes/chars.")]
+    NomIncomplete(String, Needed),
+
+    #[error(transparent)]
+    SerdeJson(#[from] serde_json::Error),
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
