21f6963301584de064e2842a468c805ab120c52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:34 +0000

    Add package zrb/0.0.81/zrb-0.0.81-py3-none-any.whl

---
+++ packages/zrb/zrb-0.0.81-py3-none-any.whl/zrb/builtin/devtool/sdkman/finalize.sh
@@ -0,0 +1,15 @@
+set -e
+source {{ os.path.expandvars(os.path.expanduser(input.config_file)) }}
+
+{% if input.install_java %}
+echo "Install Java"
+sdk install java
+{% endif %}
+
+{% if input.install_scala %}
+echo "Install Scala"
+sdk install scala
+{% endif %}
+
+echo "Please reload your terminal to continue (i.e., source {{ input.config_file }})"
+echo "Happy coding"
