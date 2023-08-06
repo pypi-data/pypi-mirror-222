b0aad4bd3a2ee40936c05414fb6f12d0a3c8b2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:41 +0000

    Add package playerguayyy/0.0.1/playerguayyy-0.0.1-py3-none-any.whl

---
+++ packages/playerguayyy/playerguayyy-0.0.1-py3-none-any.whl/playerguayyy/player.py
@@ -0,0 +1,19 @@
+"""
+Esta es el modulo que incluye la clase
+de reproductor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproductor de
+    musica
+
+    
+    """
+
+    def play(self, song):
+        """
+        reproduce la cancion que recibio en el constructor
+        """
+        print("reproduciendo una cancion ")
