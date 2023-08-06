e90b353535202264a42991ce591a9286be1d6680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:42 +0000

    Add package coar-notify-validator/0.0.3/coar_notify_validator-0.0.3.tar.gz

---
+++ packages/coar-notify-validator/coar_notify_validator-0.0.3.tar.gz/coar_notify_validator-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+import coar_notify_validator
+
+setup(
+    name="coar_notify_validator",
+    version=coar_notify_validator.__version__,
+    description="Utility for validating COAR Notify payloads.",
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=[
+        "coar_notify_validator",
+        "coar_notify_validator.shape_files",
+    ],
+    include_package_data=True,
+    install_requires=[
+        "kglab == 0.6.6",
+        "rdflib == 6.3.2",
+        "pyshacl == 0.23.0",
+    ],
+    license="MIT",
+    url="https://github.com/seanwiseman/coar-notify-validator.git",
+    maintainer="Sean Wiseman",
+    maintainer_email="seanwiseman2012@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ]
+)
