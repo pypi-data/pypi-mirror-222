062792e1a944c5b0962ebef162efb8c55d1850b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package mcptbr/0.2.4/mcptbr-0.2.4.tar.gz

---
+++ packages/mcptbr/mcptbr-0.2.4.tar.gz/mcptbr-0.2.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+readme_content = """
+# mcptbr
+
+
+mcptbr é uma pequena biblioteca que permite o usuário a manipular blocos do Minecraft com python.
+
+## Para quem serve?
+Material criado para os alunos do curso de programação em python no Minecraft.
+
+Observação: Versão em português de outras bibliotecas já existentes.
+
+
+
+## Instalação
+Vá ao CMD e digite 
+```
+pip install mcptbr.
+```
+
+
+## Exemplo de uso
+```
+from mcptbr.model import *
+
+
+print(pegarPosicaoJogador())
+```
+
+"""
+
+setup(
+    name='mcptbr',
+    version='0.2.4',
+    packages=find_packages(),
+    install_requires=[
+        "mcpi", # Dependências necessárias para sua biblioteca (se houver).
+    ],
+    author='Lucas Pereira',
+    author_email='lucasthe2@gmail.com',
+    description='Biblioteca para manipular o Minecraft através do python.',
+    long_description=readme_content,
+    #long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type='text/markdown',
+    #url='https://github.com/seu_usuario/minha_biblioteca',
+    license='MIT',  # Substitua pela licença adequada.
+)
