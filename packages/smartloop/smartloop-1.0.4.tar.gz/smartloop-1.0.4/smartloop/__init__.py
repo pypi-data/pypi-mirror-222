8e753068ec35d67f452521b93c0d3a71eff2c5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:32 +0000

    Add package smartloop/1.0.4/smartloop-1.0.4.tar.gz

---
+++ packages/smartloop/smartloop-1.0.4.tar.gz/smartloop-1.0.4/smartloop/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import tensorflow as tf
+import hashlib
+
+from .config import Config
+from .default_config import DefaultConfig
+from .file_config import FileConfig
+from .project import Project
+from .model_loader import ModelLoader
+
+LANG_MAPS = {
+    'en': 'english',
+    'es': 'spanish',
+    'de': 'german'
+}
+
+tf.random.set_seed(hash('smartloop'))
