c793a3138e0ccd1e209133cb8543b24bb70dab9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:07 +0000

    Add package nepattern/0.5.12/nepattern-0.5.12.tar.gz

---
+++ packages/nepattern/nepattern-0.5.12.tar.gz/nepattern-0.5.12/nepattern/__init__.py
@@ -0,0 +1,37 @@
+from tarina import Empty as Empty  # noqa
+from .base import (
+    DirectPattern,
+    MappingPattern,
+    RegexPattern,
+    SequencePattern,
+    SwitchPattern,
+    UnionPattern,
+)
+from .context import (
+    Patterns,
+    all_patterns,
+    create_local_patterns,
+    local_patterns,
+    switch_local_patterns,
+    reset_local_patterns
+)
+from .core import BasePattern, MatchMode, ValidateResult, set_unit
+from .exception import MatchFailed
+from .main import (
+    DATETIME,
+    EMAIL,
+    FLOAT,
+    HEX,
+    HEX_COLOR,
+    INTEGER,
+    IP,
+    NUMBER,
+    URL,
+    AnyOne,
+    AnyString,
+    Bind,
+    type_parser,
+)
+from .util import AllParam, TPattern, RawStr
+
+PatternModel = MatchMode
