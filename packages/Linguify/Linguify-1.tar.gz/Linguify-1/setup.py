959659b87b866302099f91f0ec0d5f29d0384d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:25 +0000

    Add package Linguify/1/Linguify-1.tar.gz

---
+++ packages/Linguify/Linguify-1.tar.gz/Linguify-1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Linguify',
+    version=1,
+    packages=find_packages(),
+    install_requires=[
+    "openai",
+    "python-docx",
+    "google-api-python-client",
+    "google-auth-httplib2",
+    "google-auth-oauthlib",
+    "click"
+    ],
+    entry_points={
+        'console_scripts': [
+            'linguify=linguify:start',
+        ],},
+    author='Abdulrahman Khafagy',
+    author_email='abdul_khafagy2004@hotmail.com',
+    description='Linguify is designed to automate voicemail transcription via email parsing.',
+)
