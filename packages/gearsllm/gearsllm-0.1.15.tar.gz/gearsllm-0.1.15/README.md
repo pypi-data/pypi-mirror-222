b86dbc9fb8a8eaf0fc707758198aa142bdb9d122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package gearsllm/0.1.15/gearsllm-0.1.15.tar.gz

---
+++ packages/gearsllm/gearsllm-0.1.15.tar.gz/gearsllm-0.1.15/README.md
@@ -0,0 +1,7 @@
+# Gears
+
+Gears is a lightweight framework for writing control flow with LLMs. It allows you to build complex chains of actions and conditions, and execute them in a single call. You have full control over the prompts; you just provide the text and control flow and Gears will handle the rest.
+
+TODO:
+
+- [ ] Write docs
---
+++ packages/gearsllm/gearsllm-0.1.15.tar.gz/gearsllm-0.1.15/gears/gear.py
@@ -0,0 +1,70 @@
+from typing import Any
+from pydantic import BaseModel
+
+from jinja2 import Template
+import inspect
+
+from gears.history import History
+from gears.llms.base import BaseLLM
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Gear:
+    def __init__(
+        self,
+        model: BaseLLM,
+    ):
+        self.model = model
+
+    async def run(
+        self,
+        context: BaseModel,
+        history: History,
+        **kwargs,
+    ):
+        # Construct the template with the pydantic model
+        prompt = Template(self.template(context)).render(context=context)
+
+        # Call the model
+        logger.info(f"Running model with prompt: {prompt}")
+        response = await self.model.run(prompt, history, **kwargs)
+
+        # Transform the data from the response
+        try:
+            response = self.transform(response, context)
+            # Verify that the structured data is a pydantic model
+            if not isinstance(response, BaseModel):
+                raise TypeError(
+                    "Transform must return a pydantic model instance"
+                )
+        except NotImplementedError:
+            pass
+
+        # Load which other gear to run, if any
+        try:
+            child = self.switch(response)
+            if isinstance(child, Gear):
+                return await child.run(response, history, **kwargs)
+            elif not child:
+                logger.info("No child gear to run. Returning response.")
+                return response
+            else:
+                raise TypeError(f"Switch must return a Gear instance or None.")
+
+        except NotImplementedError:
+            pass
+
+        # If there is no child, return the response
+        return response
+
+    def template(self, context: BaseModel) -> str:
+        raise NotImplementedError("Gear must implement prompt template")
+
+    def transform(self, response: dict, **kwargs) -> BaseModel:
+        raise NotImplementedError
+
+    def switch(self, response: BaseModel, **kwargs) -> "Gear":
+        raise NotImplementedError
