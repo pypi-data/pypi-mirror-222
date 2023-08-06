f8984a0bd192a9e9cb61eea7f9e24421bdb91190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:33 +0000

    Add package templaterepo/0.1.2/templaterepo-0.1.2.tar.gz

---
+++ packages/templaterepo/templaterepo-0.1.2.tar.gz/templaterepo-0.1.2/src/templaterepo/__init__.py
@@ -0,0 +1,22 @@
+"""
+ProjectName
+
+A short description of the project.
+
+A longer description of the project.
+
+Copyright disclaimer:
+
+A copyright disclaimer.
+"""
+
+__version_details__ = dict(
+    version="0.1.2",
+    version_tuple=(0, 1, 2),
+    author_date="2023.08.01",
+    build_date="2023.08.01",
+    branch="None",
+    distance="0",
+    head_commit_hash="fcb3d021bbae0c23cf47046b240e5665f3393875",
+)
+__version__ = __version_details__["version"]
