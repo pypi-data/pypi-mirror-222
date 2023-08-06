efa8820eb192a18a12be54af6307e4d7e8365b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package pyprland/1.4.0/pyprland-1.4.0-py3-none-any.whl

---
+++ packages/pyprland/pyprland-1.4.0-py3-none-any.whl/pyprland/plugins/toggle_dpms.py
@@ -0,0 +1,16 @@
+" Toggle monitors on or off "
+from typing import Any, cast
+from .interface import Plugin
+
+from ..ipc import hyprctlJSON, hyprctl
+
+
+class Extension(Plugin):  # pylint: disable=missing-class-docstring
+    async def run_toggle_dpms(self):
+        """toggles dpms on/off for every monitor"""
+        monitors = cast(list[dict[str, Any]], await hyprctlJSON("monitors"))
+        powered_off = any(m["dpmsStatus"] for m in monitors)
+        if not powered_off:
+            await hyprctl("dpms on")
+        else:
+            await hyprctl("dpms off")
