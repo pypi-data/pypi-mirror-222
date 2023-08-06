03e1b792cae8ce8db734f548643a3c8c17baa75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:29 +0000

    Add package knnor/0.1.3/knnor-0.1.3.tar.gz

---
+++ packages/knnor/knnor-0.1.3.tar.gz/knnor-0.1.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='knnor',
+      version='0.1.3',
+      description='Generic python library to perform augmentation of multi class data.',
+      long_description='A generic package to help data scientists balance their dataset by increasing the datapoints for an imbalanced class. Supports multi class'
+                  ,
+      url='',
+      author='Ashhadul Islam, Sameer Brahim Belhaouari, Atiq Ur Rahman, Halima Bensmail',
+      author_email='ashhadulislam@gmail.com, samir.brahim@gmail.com, atrehman2@hbku.edu.qa, hbensmail@hbku.edu.qa',
+      keywords='Binary Classification, Data Augmentation, Imabalnced Data, MultiClass Classification',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=[            
+            "numpy",
+            "scikit-learn"
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False
+)
