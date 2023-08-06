5a24a9d9707551fd0a02a8f130b39dadd87cf5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:33 +0000

    Add package kubernetes-square/1.5.0/kubernetes_square-1.5.0-py3-none-any.whl

---
+++ packages/kubernetes-square/kubernetes_square-1.5.0-py3-none-any.whl/square/cfgfile.py
@@ -0,0 +1,140 @@
+"""Define which manifest fields to exclude from diffs and patches.
+
+For instance, almost all manifests can have a "status" field or a
+`manifest.uid` that K8s manages itself. It does not make sense, or is even
+dangerous, for Square to compute diffs on such field or patch them.
+
+This module defines utility functions to define these filters.
+
+"""
+import copy
+import logging
+from pathlib import Path
+from types import SimpleNamespace
+from typing import Tuple
+
+import pydantic
+import yaml
+
+from square.dtypes import Config
+
+from .dtypes import FiltersKind
+
+# Convenience.
+logit = logging.getLogger("square")
+
+
+def valid(filters: FiltersKind) -> bool:
+    """Return `True` iff `filters` is valid."""
+    return _valid(filters)
+
+
+def _valid(filters) -> bool:
+    """Return `True` iff `filters` is valid."""
+    if not isinstance(filters, list):
+        logit.error(f"<{filters}> must be a list")
+        return False
+
+    # Iterate over all fields of all K8s resource type.
+    for el in filters:
+        # All entries must be either strings or dicts of lists.
+        if isinstance(el, dict):
+            if len(el) != 1:
+                logit.error(f"<{el}> must have exactly one key")
+                return False
+            value = list(el.values())[0]
+
+            # Recursively check the dictionary values.
+            if not _valid(value):
+                logit.error(f"<{value}> is invalid")
+                return False
+        elif isinstance(el, str):
+            if el == "":
+                logit.error("Strings must be non-empty")
+                return False
+        else:
+            logit.error(f"<{el}> must be a string or dict")
+            return False
+    return True
+
+
+def merge(src: list, dst: list) -> list:
+    """Merge `src` into `dst` and return a copy of `dst`."""
+    # Avoid side effects.
+    dst = copy.deepcopy(dst)
+
+    def find_dict(data: list, key: str) -> dict:
+        """Find and return the dictionary in `data` that has the `key`."""
+        tmp = [_ for _ in data if isinstance(_, dict) and set(_.keys()) == {key}]
+        assert len(tmp) == 1
+        return tmp[0]
+
+    def _update(src, dst):
+        # Add all string keys from `src` to `dst` if they are not yet in `dst`.
+        str_keys = [_ for _ in src if isinstance(_, str) and _ not in dst]
+        dst.extend(str_keys)
+
+        # Find the all dicts and their one and only key.
+        dict_src = {tuple(_.keys())[0] for _ in src if isinstance(_, dict)}
+        dict_dst = {tuple(_.keys())[0] for _ in dst if isinstance(_, dict)}
+
+        # Recursively merge the dictionaries.
+        for key in dict_src:
+            if key not in dict_dst:
+                # `dst` does not have the dict at all - just copy it.
+                dst.append(find_dict(src, key))
+            else:
+                # `dst` already has a dict for `key` -> recursively update it.
+                src_val = find_dict(src, key)[key]
+                dst_val = find_dict(dst, key)[key]
+                _update(src_val, dst_val)
+
+        # Sort the list alphabetically (if the entry is a dict then use its one
+        # and only key as the comparative element).
+        dst.sort(key=lambda _: _ if isinstance(_, str) else tuple(_.keys())[0])
+
+    _update(src, dst)
+    return dst
+
+
+def load(fname: Path) -> Tuple[Config, bool]:
+    """Parse the Square configuration file `fname` and return it as a `Config`."""
+    err_resp = Config(folder=Path(), kubeconfig=Path()), True
+
+    # Load the configuration file.
+    try:
+        raw = yaml.safe_load(fname.read_text())
+    except FileNotFoundError as e:
+        logit.error(f"Cannot load config file <{fname}>: {e.args[1]}")
+        return err_resp
+    except yaml.YAMLError as exc:
+        msg = f"Could not parse YAML file {fname}"
+
+        # Special case: parser supplied location information.
+        mark = getattr(exc, "problem_mark", SimpleNamespace(line=-1, column=-1))
+        line, col = (mark.line + 1, mark.column + 1)
+        msg = f"YAML format error in {fname}: Line {line} Column {col}"
+        logit.error(msg)
+        return err_resp
+
+    # Parse the configuration into `ConfigFile` structure.
+    try:
+        cfg = Config.model_validate(raw)
+
+        # Explicitly access the computed `_kinds_names` attribute because
+        # Pydantic will not compute it until it is accessed.
+        cfg.selectors._kinds_names
+        cfg.selectors._kinds_only
+    except (pydantic.ValidationError, TypeError) as e:
+        logit.error(f"Schema is invalid: {e}")
+        return err_resp
+
+    # Remove the "_common_" filter and merge it into all the other filters.
+    common = cfg.filters.pop("_common_", [])
+    cfg.filters = {k: merge(common, v) for k, v in cfg.filters.items()}
+    cfg.filters["_common_"] = common
+
+    # Ensure the path is an absolute path.
+    cfg.folder = fname.parent.absolute() / cfg.folder
+
+    return cfg, False
---
+++ packages/kubernetes-square/kubernetes_square-1.5.0-py3-none-any.whl/square/dtypes.py
@@ -0,0 +1,253 @@
+from pathlib import Path
+from typing import Any, Callable, Dict, List, NamedTuple, Optional, Set, Tuple
+
+from pydantic import BaseModel, Field
+
+# Square will first save/deploy the resources in this list in this order.
+# Afterwards it will move on to all those resources not in this list. The order
+# in which it does that is undefined.
+DEFAULT_PRIORITIES = (
+    # Custom Resources should come first.
+    "CustomResourceDefinition",
+
+    # Commone non-namespaced resources.
+    "ClusterRole", "ClusterRoleBinding",
+
+    # Namespaces must come before any namespaced resources,
+    "Namespace",
+
+    # Configuration and PVC before Deployments & friends use them.
+    "ConfigMap", "PersistentVolumeClaim", "Secret",
+
+    # RBAC.
+    "Role", "RoleBinding", "ServiceAccount",
+
+    # Define Services before creating Deployments & friends.
+    "PodDisruptionBudget", "Service",
+
+    # Everything that will spawn pods.
+    "CronJob", "DaemonSet", "Deployment", "StatefulSet",
+
+    # Other.
+    "HorizontalPodAutoscaler", "Ingress",
+)
+
+
+# -----------------------------------------------------------------------------
+#                                  Kubernetes
+# -----------------------------------------------------------------------------
+class K8sClientCert(NamedTuple):
+    crt: Path = Path()
+    key: Path = Path()
+
+
+class MetaManifest(NamedTuple):
+    """Minimum amount of information to uniquely identify a K8s resource.
+
+    The primary purpose of this tuple is to provide an immutable UUID that
+    we can use as keys in dictionaries and sets.
+
+    """
+    apiVersion: str
+    kind: str
+    namespace: Optional[str]
+
+    # Every resource must have a name except for Namespaces, which encode their
+    # name in the `namespace` field.
+    name: str | None
+
+
+class K8sResource(NamedTuple):
+    """Describe a specific K8s resource kind."""
+    apiVersion: str   # "batch/v1beta1" or "extensions/v1beta1".
+    kind: str         # "Deployment" (as specified in manifest)
+    name: str         # "deployment" (usually lower case version of above)
+    namespaced: bool  # Whether or not the resource is namespaced.
+    url: str          # API endpoint, eg "k8s-host.com/api/v1/pods".
+
+
+class K8sConfig(NamedTuple):
+    """Everything we need to know to connect and authenticate with Kubernetes."""
+    url: str = ""               # Kubernetes API
+    token: str = ""             # Optional access token (eg Minikube).
+
+    # Certificate authority credentials and self signed client certificate.
+    # Used to authenticate to eg GKE.
+    ca_cert: Path = Path()
+    client_cert: Optional[K8sClientCert] = None
+
+    # HttpX client to access the cluster.
+    client: Any = None
+
+    # Kubernetes version and name.
+    version: str = ""
+    name: str = ""
+
+    # Kubernetes API endpoints (see k8s.compile_api_endpoints).
+    apis: Dict[Tuple[str, str], K8sResource] = {}
+
+    # LUT to translate short names into their proper resource kind,
+    # for instance short = {"service":, "Service", "svc": "Service"}
+    short2kind: Dict[str, str] = {}
+
+    # The set of supported K8s resource kinds, eg {"Deployment", "Service"}.
+    # NOTE: these are the `manifest.kind` spellings. "Deployment" is a valid
+    # K8s resource kind, whereas "deployment" or "Deployments" are not.
+    kinds: Set[str] = set()
+
+
+# -----------------------------------------------------------------------------
+#                                Deployment Plan
+# -----------------------------------------------------------------------------
+class JsonPatch(NamedTuple):
+    """The URL for the patches as well as the patch payloads themselves."""
+    # Send the patch to https://1.2.3.4/api/v1/namespace/foo/services
+    url: str
+
+    # The list of JSON patches.
+    ops: List[Dict[str, str]]
+
+
+class DeltaCreate(NamedTuple):
+    meta: MetaManifest
+    url: str
+    manifest: Dict[str, Any]
+
+
+class DeltaDelete(NamedTuple):
+    meta: MetaManifest
+    url: str
+    manifest: Dict[str, Any]
+
+
+class DeltaPatch(NamedTuple):
+    meta: MetaManifest
+    diff: str
+    patch: JsonPatch
+
+
+class DeploymentPlan(NamedTuple):
+    """Describe Square plan.
+
+    Collects all resources manifests to add/delete as well as the JSON
+    patches that make up a full plan.
+
+    """
+    create: List[DeltaCreate] | Tuple[DeltaCreate, ...]
+    patch: List[DeltaPatch] | Tuple[DeltaPatch, ...]
+    delete: List[DeltaDelete] | Tuple[DeltaDelete, ...]
+
+
+class DeploymentPlanMeta(NamedTuple):
+    """Same as `DeploymentPlan` but contains `MetaManifests` only."""
+    create: List[MetaManifest] | Tuple[MetaManifest, ...]
+    patch: List[MetaManifest] | Tuple[MetaManifest, ...]
+    delete: List[MetaManifest] | Tuple[MetaManifest, ...]
+
+
+# -----------------------------------------------------------------------------
+#                             Square Configuration
+# -----------------------------------------------------------------------------
+class KindName(BaseModel):
+    """Holds a K8s resource Kind."""
+    kind: str = Field(min_length=1)  # Eg 'Service'
+    name: str                        # Eg. 'appname'
+
+
+class Selectors(BaseModel):
+    """Parameters to target specific groups of manifests."""
+    model_config = {"str_strip_whitespace": True}
+
+    kinds: Set[str] = set()
+    namespaces: List[str] = []
+    labels: List[str] = []
+
+    @property
+    def _kinds_names(self) -> List[KindName]:
+        """Deconstruct the Selector Kinds into Kind and Name.
+
+        These are the valid scenarios:
+          'Service'     -> ('Service', '')
+          'Service/'    -> ('Service', '')
+          'Service/foo' -> ('Service', 'foo')
+
+        """
+        # Unpack the 'Kind/Name' into a dedicated `KindName` model.
+        ans: List[KindName] = []
+        for src_kind in sorted(self.kinds):
+            # Ensure the `src_kind` has at most one '/'.
+            parts = src_kind.split("/")
+            if len(parts) == 1:
+                kind, name = parts[0], ""
+            elif len(parts) == 2:
+                kind, name = parts
+            else:
+                raise ValueError(f"Invalid kind {src_kind}")
+
+            # There must be no leading/trailing white space.
+            if (kind.strip() != kind) or (name.strip() != name):
+                raise ValueError(f"Invalid kind {src_kind}")
+
+            # Add the deconstructed Kind/Name selector.
+            ans.append(KindName(kind=kind, name=name))
+        return ans
+
+    @property
+    def _kinds_only(self) -> Set[str]:
+        """Compile the set of resource KINDS we are interested in.
+
+        NOTE: If the user selects `{"pod/app1", "deploy", "deploy/app1"}` then
+        we only want to retain `deploy` since `pod/app1` selects a unique Pod
+        whereas `deploy` selects all `Deployment` resources.
+
+        """
+        return {_.kind for _ in self._kinds_names if _.name == ""}
+
+
+class GroupBy(BaseModel):
+    """Define how to organise downloaded manifests on the files system."""
+    label: str = ""                  # "app"
+    order: List[str] = []            # ["ns", "label=app", kind"]
+
+
+"""Define the filters to exclude sections of manifests."""
+FiltersKind = List[str | dict]
+Filters = Dict[str, FiltersKind]
+
+
+class Config(BaseModel):
+    """Uniform interface into top level Square API."""
+    # Path to local manifests eg "./foo"
+    folder: Path
+
+    # Path to Kubernetes credentials.
+    kubeconfig: Path
+
+    # Kubernetes context (use `None` to use the default).
+    kubecontext: Optional[str] = None
+
+    # Only operate on resources that match the selectors.
+    selectors: Selectors = Selectors()
+
+    # Sort the manifest in this order, or alphabetically at the end if not in the list.
+    priorities: List[str] = list(DEFAULT_PRIORITIES)
+
+    # How to structure the folder directory when syncing manifests.
+    groupby: GroupBy = GroupBy()
+
+    # Define which fields to skip for which resource.
+    filters: Filters = {}
+
+    # Callable: will be invoked for every local/server manifest that requires
+    # patching before the actual patch will be computed.
+    patch_callback: Optional[Callable] = None
+
+    version: str = ""
+
+
+# -----------------------------------------------------------------------------
+#                                 Miscellaneous
+# -----------------------------------------------------------------------------
+LocalManifests = Dict[Path, Tuple[MetaManifest, dict]]
+LocalManifestLists = Dict[Path, List[Tuple[MetaManifest, dict]]]
+SquareManifests = Dict[MetaManifest, dict]
