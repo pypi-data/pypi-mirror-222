072110ba141c441252fdfa7037420d9d84cbe8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:20 +0000

    Add package ansys-motorcad-core/0.2.0/ansys_motorcad_core-0.2.0.tar.gz

---
+++ packages/ansys-motorcad-core/ansys_motorcad_core-0.2.0.tar.gz/ansys_motorcad_core-0.2.0/src/ansys/motorcad/core/geometry.py
@@ -0,0 +1,52 @@
+"""Function for ``Motor-CAD geometry`` not attached to Motor-CAD instance."""
+from cmath import polar, rect
+from math import degrees, radians
+
+
+def xy_to_rt(x, y):
+    """Convert Motor-CAD Cartesian coordinates to polar coordinates in degrees.
+
+    Parameters
+    ----------
+    x : float
+        X coordinate value.
+    y : float
+        Y coordinate value.
+
+    Returns
+    -------
+    radius : float
+        Radial coordinate value.
+    theta : float
+        Angular coordinate value.
+    """
+    rect_coordinates = complex(x, y)
+
+    radius, theta = polar(rect_coordinates)
+
+    return radius, degrees(theta)
+
+
+def rt_to_xy(radius, theta):
+    """Convert Motor-CAD polar coordinates to Cartesian coordinates in degrees.
+
+    Parameters
+    ----------
+    radius : float
+        Radial coordinate.
+    theta : float
+        Angular coordinate.
+
+    Returns
+    -------
+    x : float
+        X coordinate value.
+    y : float
+        Y coordinate value.
+    """
+    coordinates_complex = rect(radius, radians(theta))
+
+    x = coordinates_complex.real
+    y = coordinates_complex.imag
+
+    return x, y
