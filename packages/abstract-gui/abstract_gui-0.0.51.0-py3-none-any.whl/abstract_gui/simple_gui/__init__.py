df4deed93c98c5f64e66a0dc1d5353c47a8c6204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:13 +0000

    Add package abstract-gui/0.0.51.0/abstract_gui-0.0.51.0-py3-none-any.whl

---
+++ packages/abstract-gui/abstract_gui-0.0.51.0-py3-none-any.whl/abstract_gui/simple_gui/__init__.py
@@ -0,0 +1,5 @@
+import PySimpleGUI as sg
+from abstract_utilities.global_utils import global_registry, change_glob, update_registry, get_global_from_registry
+from abstract_gui.simple_gui import global_bridge
+from abstract_gui.simple_gui import window_manager
+from abstract_gui.simple_gui.gui_template import get_gui_fun
