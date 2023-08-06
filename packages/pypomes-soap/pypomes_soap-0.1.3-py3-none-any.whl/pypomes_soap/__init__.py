2da823a57310d98fedd1afc1fff6505955384ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:50 +0000

    Add package pypomes-soap/0.1.3/pypomes_soap-0.1.3-py3-none-any.whl

---
+++ packages/pypomes-soap/pypomes_soap-0.1.3-py3-none-any.whl/pypomes_soap/__init__.py
@@ -0,0 +1,11 @@
+from .soap_pomes import (
+    soap_post, soap_post_zeep, soap_get_attachment, soap_get_dict, soap_get_cids, soap_build_envelope
+)
+
+__all__ = [
+    # soap_pomes
+    soap_post, soap_post_zeep, soap_get_attachment, soap_get_dict, soap_get_cids, soap_build_envelope
+]
+
+__version__ = "0.1.3"
+__version_info__ = (0, 1, 3)
