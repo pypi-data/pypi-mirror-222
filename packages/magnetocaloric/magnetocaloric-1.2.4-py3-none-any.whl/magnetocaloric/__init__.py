b9e336b71a74c4b4580f2b9c20562391dcd8e169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:27 +0000

    Add package magnetocaloric/1.2.4/magnetocaloric-1.2.4-py3-none-any.whl

---
+++ packages/magnetocaloric/magnetocaloric-1.2.4-py3-none-any.whl/magnetocaloric/__init__.py
@@ -0,0 +1,31 @@
+from .arrott_plot import arrott_plot
+from .collect_from_database import collect_from_database
+from .Color_marker import Color_marker
+from .data_visualization01 import data_visualization01
+from .data_visualization02 import data_visualization02
+from .entropy_change01 import entropy_change01
+from .entropy_change02 import entropy_change02
+from .M_H_reshaping import M_H_reshaping
+from .modified_arrott_plot import modified_arrott_plot
+from .RCP_plot import RCP_plot
+from .store_to_database01 import store_to_database01
+from .store_to_database02 import store_to_database02
+from .T_FWHM_RCP import T_FWHM_RCP
+from .take_temp import take_temp
+
+__all__ = [
+    'arrott_plot',
+    'collect_from_database',
+    'Color_marker',
+    'data_visualization01',
+    'data_visualization01',
+    'entropy_change01',
+    'entropy_change02',
+    'M_H_reshaping',
+    'modified_arrott_plot',
+    'RCP_plot',
+    'store_to_database01',
+    'store_to_database02',
+    'T_FWHM_RCP',
+    'take_temp',
+]
