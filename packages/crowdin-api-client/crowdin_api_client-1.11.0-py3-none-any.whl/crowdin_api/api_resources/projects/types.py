694e9913cdfd254b3f341f051fa74019245ea52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:38 +0000

    Add package crowdin-api-client/1.11.0/crowdin_api_client-1.11.0-py3-none-any.whl

---
+++ packages/crowdin-api-client/crowdin_api_client-1.11.0-py3-none-any.whl/crowdin_api/api_resources/projects/types.py
@@ -0,0 +1,104 @@
+from typing import Any, Iterable, List, Union
+
+from crowdin_api.api_resources.enums import PatchOperation
+from crowdin_api.api_resources.projects.enums import (
+    ProjectPatchPath,
+    EscapeQuotes,
+    EscapeSpecialCharacters,
+    ProjectFilePatchPath,
+)
+from crowdin_api.typing import TypedDict
+
+
+class ProjectPatchRequest(TypedDict):
+    value: Any
+    op: PatchOperation
+    path: ProjectPatchPath
+
+
+class NotificationSettings(TypedDict):
+    translatorNewStrings: bool
+    managerNewStrings: bool
+    managerLanguageCompleted: bool
+
+
+class QACheckCategories(TypedDict):
+    EMPTY: bool
+    SIZE: bool
+    TAGS: bool
+    SPACES: bool
+    VARIABLES: bool
+    PUNCTUATION: bool
+    SYMBOLREGISTER: bool
+    SPECIALSYMBOLS: bool
+    WRONGTRANSLATION: bool
+    SPELLCHECK: bool
+    ICU: bool
+    TERMS: bool
+    DUPLICATE: bool
+
+
+class PropertyFileFormatSettings(TypedDict):
+    escapeQuotes: EscapeQuotes
+    escapeSpecialCharacters: EscapeSpecialCharacters
+    exportPattern: str
+
+
+class XmlFileFormatSettings(TypedDict):
+    translateContent: bool
+    translateAttributes: bool
+    translatableElements: Iterable[str]
+    contentSegmentation: bool
+    srxStorageId: int
+    exportPattern: str
+
+
+class SpecificFileFormatSettings(TypedDict):
+    """
+    Includes kind standard file format settings:
+        - WebXml file
+        - Html file
+        - Adoc file
+        - Md file
+        - FmMd file
+        - FmHtml file
+        - MadcapFisnp file
+        - Idml file
+        - Mif file
+        - Dita file
+    """
+    contentSegmentation: bool
+    srxStorageId: int
+    exportPattern: str
+
+
+class DocxFileFormatSettings(TypedDict):
+    cleanTagsAggressively: bool
+    translateHiddenText: bool
+    translateHyperlinkUrls: bool
+    translateHiddenRowsAndColumns: bool
+    importNotes: bool
+    importHiddenSlides: bool
+    contentSegmentation: bool
+    srxStorageId: int
+    exportPattern: str
+
+
+class MediaWikiFileFormatSettings(TypedDict):
+    srxStorageId: int
+    exportPattern: str
+
+
+class TxtFileFormatSettings(TypedDict):
+    srxStorageId: int
+    exportPattern: str
+
+
+class OtherFileFormatSettings(TypedDict):
+    exportPattern: str
+
+
+class ProjectFilePatchRequest(TypedDict):
+    value: Union[str, List[str]]
+    op: PatchOperation
+    path: ProjectFilePatchPath
