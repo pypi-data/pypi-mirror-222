29e8a888c3e245c395b0f629b0e8d88a6be722ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:36 +0000

    Add package reata/1.1.3/reata-1.1.3.tar.gz

---
+++ packages/reata/reata-1.1.3.tar.gz/reata-1.1.3/setup.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+src = Path(__file__).parent
+
+setup(
+    name="reata",
+    version="1.1.3",
+    description=(
+        "A simple MySQL DBAPI wrapper for simplifying "
+        "data processing pipelines."
+    ),
+    long_description=(src/"README.rst").read_text(),
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.22.3",
+        "pandas>=1.4.2",
+        "pdxtra>=1.0.0",
+        "mysql-connector-python>=8.0.31",
+    ],
+    python_requires=">=3.10.4",
+    license=(src/"LICENSE").read_text(),
+    include_package_data=True,
+)
