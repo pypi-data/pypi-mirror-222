8c4c5ba841fc2ff62096d1531083ca2458baa030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:27 +0000

    Add package nbformat/5.9.2/nbformat-5.9.2.tar.gz

---
+++ packages/nbformat/nbformat-5.9.2.tar.gz/nbformat-5.9.2/.github/scripts/parse_ref.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# Standard library imports
+import os
+
+# Constants
+HERE = os.path.abspath(os.path.dirname(__file__))
+REPO_ROOT = os.path.dirname(os.path.dirname(HERE))
+
+
+def parse_ref(current_ref):
+    """
+    Extract version string from github reference string and create environment
+    variable for use within the CI workflows.
+
+    Parameters
+    ----------
+    current_ref: str
+        The github reference string.
+    """
+    if not current_ref.startswith("refs/tags/"):
+        raise Exception(f"Invalid ref `{current_ref}`!")  # noqa
+
+    tag_name = current_ref.replace("refs/tags/", "")
+    print(tag_name)  # noqa
+
+
+if __name__ == "__main__":
+    current_ref = os.environ.get("GITHUB_REF")
+    parse_ref(current_ref)
