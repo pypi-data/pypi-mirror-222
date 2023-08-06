84b8fb7547dcc12138b6b5af5234c2834e963fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:34 +0000

    Add package poker-tracker/0.1/poker-tracker-0.1.tar.gz

---
+++ packages/poker-tracker/poker-tracker-0.1.tar.gz/poker-tracker-0.1/README.md
@@ -0,0 +1,3 @@
+This is a simple CLI poker tracker intended to track the results of multiple players over multiple sessions.
+
+Output includes net result, average profit per session, and a graph.
---
+++ packages/poker-tracker/poker-tracker-0.1.tar.gz/poker-tracker-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="poker-tracker",
+    version="0.1",
+    description="A package to track poker results",
+    author="Joewe Low",
+    # author_email="",
+    # url="https://github.com/joewelow/poker-tracker",
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
