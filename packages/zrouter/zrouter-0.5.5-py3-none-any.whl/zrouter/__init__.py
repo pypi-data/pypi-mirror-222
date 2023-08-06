49ecefb6d8ff939faf2555f50ff3c00f3c4edf09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:54 +0000

    Add package zrouter/0.5.5/zrouter-0.5.5-py3-none-any.whl

---
+++ packages/zrouter/zrouter-0.5.5-py3-none-any.whl/zrouter/__init__.py
@@ -0,0 +1,14 @@
+from zrouter.router import Router
+from flask import request 
+
+
+def get_user_agent():
+    return request.user_agent.string.lower()
+
+
+def get_ip():
+    nodes = request.headers.getlist("X-Forwarded-For")
+    return nodes[0] if nodes else request.remote_addr
+
+def get_path():
+    pass 
