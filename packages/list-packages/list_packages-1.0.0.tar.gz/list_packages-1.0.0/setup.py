ca7c7d9398750f17f6364025bd5438cb81649ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:47 +0000

    Add package list-packages/1.0.0/list_packages-1.0.0.tar.gz

---
+++ packages/list-packages/list_packages-1.0.0.tar.gz/list_packages-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='list_packages',
+    version='1.0.0',
+    long_description='A package to list installed Python packages',
+    author='Anand Maurya',
+    author_email='anandmaurya@hotmail.com',
+    platforms=['Any'],
+    packages=find_packages(),
+    url='https://github.com/maurya-anand/py-list-packages',
+    entry_points={
+        'console_scripts': ['list_packages=list_packages.main:command_line']
+    },
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+)
