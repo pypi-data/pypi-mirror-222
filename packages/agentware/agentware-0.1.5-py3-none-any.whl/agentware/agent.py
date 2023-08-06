2bded47ddc1c33a1858b74e57637cf864049da61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:26 +0000

    Add package agentware/0.1.5/agentware-0.1.5-py3-none-any.whl

---
+++ packages/agentware/agentware-0.1.5-py3-none-any.whl/agentware/agent.py
@@ -0,0 +1,134 @@
+from agentware.agent_logger import Logger
+from agentware.memory import Memory
+from agentware.base import BaseAgent
+from agentware.hub import register_agent
+import yaml
+import openai
+import re
+import json
+import os
+import datetime
+import copy
+
+import traceback
+from typing import List, Any, Dict
+import agentware
+
+
+logger = Logger()
+
+
+class Agent(BaseAgent):
+
+    @classmethod
+    def pull(cls, agent_id: int):
+        logger.info("Creating agent from connector")
+        agent = cls()
+        memory = Memory.pull(agent_id, agent)
+        agent.memory = memory
+        return agent
+
+    def __init__(self):
+        """ Creates a new agent instead of fetching from connector
+
+        Construct the agent with or without memory.
+
+        :param cfg: agent config used for initializing
+        :type cfg: object
+
+        :type arg2: The data type of the second argument
+        :ivar attribute1: A description of the first attribute
+        :ivar attribute2: A description of the second attribute
+        """
+        super().__init__()
+        self.id = None
+        self._memory = Memory(self)
+        self._update_mode = False
+        openai.api_key = agentware.openai_api_key
+
+    def update(self):
+        return self.UpdateContext(self)
+
+    class UpdateContext:
+        def __init__(self, obj):
+            self.obj = obj
+
+        def __enter__(self):
+            self.obj._update_mode = True
+            return self.obj
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            # update knowledge
+            self.obj._update_mode = False
+
+    def run(self, prompt):
+        output_valid = False
+        num_retries = 0
+        raw_output = ""
+        logger.debug(f"Adding prompt to memory: {prompt}")
+        self._memory.update_context(self._prompt_prefix, prompt)
+        self._memory.add_memory({
+            "role": "user",
+            "content": f"{self._prompt_prefix} {prompt} {self._format_instruction}."
+        })
+        memory_with_error = copy.deepcopy(self._memory)
+        logger.info(f"Copy of memory made: {memory_with_error}")
+        while True:
+            if num_retries > 0:
+                logger.debug(f"Retrying for the {num_retries} time")
+            try:
+                messages = memory_with_error.to_messages()
+                raw_output = self._run(messages)
+                try:
+                    output = ""
+                    if self._output_schema:
+                        output = self.parse_output(raw_output)
+                        if self._termination_observation in output:
+                            # Whatever sub structure in outupt is dumped to str
+                            output = json.dumps(
+                                output[self._termination_observation])
+                    else:
+                        output = raw_output
+                    logger.debug(f"Adding response to memory: {output}")
+                    self._memory.add_memory({
+                        "role": "assistant",
+                        "content": output
+                    })
+
+                    output_valid = True
+                    return output
+                except Exception as err:
+                    logger.warning(f"Error parsing with exception {err}")
+
+                    traceback.print_exc()
+                    if num_retries >= 1:
+                        # Delete the previous error information
+                        memory_with_error.delete_memory(-1)
+                        memory_with_error.delete_memory(-1)
+                    memory_with_error.add_memory({
+                        "role": "assistant",
+                        "content": raw_output
+                    })
+                    memory_with_error.add_memory({
+                        "role": "user",
+                        "content": f"Failed to parse output. Ignore all the format instructions you were given previously. Your output must be a json that strictly follow the schema while not including it {self._output_schema}"
+                    })
+            except Exception as e:
+                logger.warning(f"Error getting agent output with error {e}")
+            if output_valid or num_retries >= self.MAX_NUM_RETRIES:
+                break
+            num_retries += 1
+
+    def register(self, agent_id: str = ""):
+        if not agent_id:
+            agent_id = self._config["name"]
+        assert agent_id
+        register_agent(agent_id)
+        self.agent_id = agent_id
+        self._memory.agent_id = agent_id
+
+    def push(self):
+        # Check agent id valid
+        assert self.agent_id
+        logger.debug(f"Pushing agent with name {self.agent_id}")
+        self._memory.update_agent()
---
+++ packages/agentware/agentware-0.1.5-py3-none-any.whl/tests/agent_workflow_test.py
@@ -0,0 +1,79 @@
+import unittest
+
+from agentware import hub
+from agentware.base import OneshotAgent, Connector
+from agentware.agent import Agent
+from agentware.agent_logger import Logger
+from utils import DbClient, FakeCoreEngine, SummarizerCoreEngine, FactCoreEngine
+logger = Logger()
+
+TEST_CFG = {
+    "name": "test agent",
+            "conversation_setup": "You are DarthHololens, an AI assistant who lives in the universe of Star Wars series and knows everything about this world",
+            "prompt_prefix": "Constraint: answer in no more than 200 tokens"
+}
+
+
+class AgentWorkflowTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.db_client = DbClient()
+        cls.main_core_engine = FakeCoreEngine("software " * 200)
+        cls.summarizer_core_engine = FakeCoreEngine(
+            "{\"summary\": \"this is a summary\"}")
+
+    def setUp(self):
+        self.db_client.client.flushall()
+
+    def tearDown(self):
+        pass
+
+    def test_register_agent(self):
+        agent_id = "some agent idz"
+        hub.register_agent(agent_id)
+        agent_ids = hub.list_agents()
+        print('agents are', agent_ids)
+        assert agent_ids[0] == agent_id
+
+    def test_duplicate_register_fail(self):
+        agent_id = "some_agent_id"
+        hub.register_agent(agent_id)
+        self.assertRaises(ValueError, hub.register_agent, agent_id)
+
+    def test_fail_to_fetch_unexist(self):
+        self.assertRaises(ValueError, Agent.pull, "some/unexisted")
+
+    def test_register_and_push_and_fetch_and_run_agent(self):
+        agent = Agent()
+        agent_id = "some_agent_name"
+        agent.register(agent_id)
+        agent.set_config(TEST_CFG)
+        agent.push()
+        agents = hub.list_agents()
+        print("agents are", agents)
+        assert agents[0] == agent_id
+        updated_agent = Agent.pull(agent_id)
+        assert updated_agent.get_config() == TEST_CFG
+
+    def test_memory_compression(self):
+        agent = Agent.init(TEST_CFG)
+        agent.set_core_engine(self.main_core_engine)
+        agent._memory._helper_agents["summarizer"].set_core_engine(
+            SummarizerCoreEngine())
+        agent._memory._helper_agents["fact"].set_core_engine(
+            FactCoreEngine())
+        for i in range(10):
+            agent.run("What is your name?")
+
+    def test_agent_reflection_run_in_update_mode(self):
+        agent = Agent.init(TEST_CFG)
+        assert agent._update_mode == False
+        with agent.update():
+            assert agent._update_mode == True
+            agent.run("What is your name?")
+        assert agent._update_mode == False
+
+
+if __name__ == '__main__':
+    unittest.main()
