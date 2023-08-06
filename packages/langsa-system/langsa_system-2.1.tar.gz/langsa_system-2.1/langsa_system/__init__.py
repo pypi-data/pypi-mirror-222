cc2f1a2cf99a24a8e78292fb0db94fc0a88f50de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:17 +0000

    Add package langsa-system/2.1/langsa_system-2.1.tar.gz

---
+++ packages/langsa-system/langsa_system-2.1.tar.gz/langsa_system-2.1/langsa_system/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# @Time:  14:19
+# @Author: 浪飒
+# @File: __init__.py
+# @Software: PyCharm
---
+++ packages/langsa-system/langsa_system-2.1.tar.gz/langsa_system-2.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+# @Time:  10:08
+# @Author: 浪飒
+# @File: setup.py
+# @Software: PyCharm
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='langsa_system',
+    version='2.1',
+    author="浪飒",
+    url='https://github.com/langsasec/langsa-system',
+    author_email="langsa-hy@qq.com",
+    license="GPL",
+    description='langsa-system：浪飒（langsa）进制，代表自己的进制',
+    packages=find_packages()
+    )
+
+
+
+
