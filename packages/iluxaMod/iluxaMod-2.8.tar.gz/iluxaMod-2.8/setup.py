aa75107037688b8eb150cd0057d6063974bc442e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:20 +0000

    Add package iluxaMod/2.8/iluxaMod-2.8.tar.gz

---
+++ packages/iluxaMod/iluxaMod-2.8.tar.gz/iluxaMod-2.8/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = str(f.read())
+
+setup(
+    name='iluxaMod',
+    version='2.8',  # Версия вашего пакета
+    packages=['iluxaMod'],  # Автоматически найдет все пакеты в корневой директории
+    install_requires=[  # Зависимости вашего пакета
+        'geopy',
+        'psycopg2-binary',
+        'pyTelegramBotAPI',
+        'requests',
+        'pyfirmata',
+        'Pillow',
+        'pyzbar',
+        'qrcode',
+        'numpy',
+        'opencv-python',
+        'selenium'
+
+    ],
+    author='Illya Lazarev',
+    author_email='lazarevillya031@gmail.com',
+    description='Module for simplified work with libraries: TG, PostgreSQL, locations and more...',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://sbdt.pro',
+    classifiers=[  # Классификаторы, чтобы помочь пользователям понять ваш пакет
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+
+    ],
+
+)
